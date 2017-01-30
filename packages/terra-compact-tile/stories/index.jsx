599a89a0c14b32ac226f63d5e1028c33f4ccdf46
Kasper,Dave <Dave.Kasper@Cerner.com>
2017-01-30 16:14:21 -0600

    More index updates.

--- packages/terra-compact-tile/stories/index.jsx
+++ packages/terra-compact-tile/stories/index.jsx
@@ -9,6 +9,8 @@ import 'storybook-addon-i18n-tools';
 import { withKnobs, boolean, select } from '@kadira/storybook-addon-knobs';
 import { setOptions } from '@kadira/storybook-addon-options';
 
+const testElement = <img alt="Graphic" />;
+
 const display1 = <Display text="display 1" />;
 const display2 = <Display text="display 2" />;
 const display3 = <Display text="display 3" />;
@@ -19,10 +21,23 @@ const displays = [display1, display2, display3, display4, display5, display6];
 
 const longDisplay1 = <Display text="display1display1display1display1display1display1display1" />;
 const longDisplay2 = <Display text="display2display2display2display2display2display2display2" />;
+const longDisplays = [longDisplay1, longDisplay2];
 
-const testElement = <img alt="Graphic" />;
+const styleDisplay1 = <Display text="display 1" style="secondary" />;
+const styleDisplay2 = <Display text="display 2" style="strikeThrough" />;
+const styleDisplay3 = <Display text="display 3" style="attention" />;
+const styleDisplays = [styleDisplay1, styleDisplay2, styleDisplay3];
+
 const params = {
   layout: 'oneColumn',
+  theme: 'defaultTheme',
+  displays: displays,
+  leftAccessory: testElement,
+  accessoryAlignment: 'alignCenter',
+};
+
+const paramsDouble = {
+  layout: 'twoColumns',
   theme: 'defaultTheme',
   displays: displays,
   leftAccessory: testElement,
@@ -40,15 +55,21 @@ storiesOf('TerraCompactTile ', module)
   .add('With two columns left', () => (
     <CompactTile theme="leftEmphasisTheme" layout="twoColumns" displays={displays} />
   ))
-  .add('With params and graphics', () => (
+  .add('With 1 column and 1 graphic', () => (
     <CompactTile {...params} />
+  ))
+  .add('With 2 columns and 2 graphics', () => (
+    <CompactTile {...paramsDouble} />
   ))
   .add('With two rows', () => (
     <CompactTile layout="twoColumns" displays={[display1, display2, display3, display4]} />
   ))
   .add('With long text', () => (
-    <CompactTile layout="twoColumns" displays={[longDisplay1, longDisplay2]} />
+    <CompactTile layout="twoColumns" displays={longDisplays} />
   ))
   .add('With long text with truncation', () => (
-    <CompactTile layout="twoColumns" isTruncated={true} displays={[longDisplay1, longDisplay2]} />
+    <CompactTile layout="twoColumns" isTruncated={true} displays={longDisplays} />
+  ))
+  .add('With styles', () => (
+    <CompactTile displays={styleDisplays} />
   ))
