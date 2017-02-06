07ca4aec6c6c2bbc5b2247f7cce08d11663c2b4c
Kasper,Dave <Dave.Kasper@Cerner.com>
2017-02-06 15:58:56 -0600

    Update jsx file with basic stubs for list and list item.

--- packages/terra-list/src/List.jsx
+++ packages/terra-list/src/List.jsx
@@ -1,53 +1,56 @@
-// For stateless components, use a functional component
-// For stateful components, use a class
-
-// https://facebook.github.io/react/docs/components-and-props.html#functional-and-class-components
-
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import '../src/list.scss';
 
 class List extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isSelected: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
+
+  static processItemSelection(items, multiselect) {
+    return [];
   }
 
-  handleClick() {
-    this.setState({ isSelected: !this.state.isSelected });
+  static classesForListFromProps(divided, chevrons, numberOfColumns) {
+    return '';
   }
 
   render() {
-    const classes = classNames(['terra-List',
-      this.props.variant,
-      this.state.isSelected && 'u-selected',
+    const itemsWithSelection = List.processItemSelection(this.props.items,
+                                                         this.props.itemsSelectable,
+                                                         this.props.isMultiselect);
+    const listClasses = List.classesForListFromProps(this.props.isDivided,
+                                                     this.props.hasChevrons,
+                                                     this.props.numberOfColumns);
+    const listAttributes = Object.assign({}, this.props.attributes);
+    listAttributes.className = classNames([
+      listClasses,
+      listAttributes.className,
     ]);
 
-
-    if (!this.props.name) {
-      return null;
-    }
-    if (!this.props.variant) {
-      return null;
-    }
-    return (<button
-      className={classes}
-      onClick={this.handleClick}
-    >Terra, {this.props.name}</button>);
+    return (
+      <div {...listAttributes}>
+        {itemsWithSelection.map(item  => { item })}
+      </div>
+    );
   }
 }
 
 List.propTypes = {
-  name: PropTypes.string.isRequired,
-  variant: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(PropTypes.element),
+  itemsSelectable: PropTypes.bool,
+  isDivided: PropTypes.bool,
+  hasChevrons: PropTypes.bool,
+  isMultiselect: PropTypes.bool,
+  numberOfColumns: PropTypes.number,
+  attributes: PropTypes.oneOfType([PropTypes.object]),
 };
 
 List.defaultProps = {
-  name: 'default',
-  variant: 'terra-List--default',
+  items: [],
+  itemsSelectable: false,
+  isDivided: false,
+  hasChevrons: true,
+  isMultiselect: false,
+  numberOfColumns: 1,
+  attributes: {},
 };
 
 export default List;
