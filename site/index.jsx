35b7bf7ba3322ba7d5d64dca054a9c3adbcbae0e
Brett Jankord <Brett.Jankord@Cerner.com>
2017-02-20 14:57:17 -0600

    Add directionality toggle

--- site/index.jsx
+++ site/index.jsx
@@ -6,6 +6,10 @@ import ButtonExamples from '../packages/terra-button/examples/index';
 
 const App = () => (
   <div>
+    <bdo dir="ltr">
+      <button onClick={() => document.getElementsByTagName('html')[0].setAttribute( 'dir', 'ltr' )} >ltr</button>
+      <button onClick={() => document.getElementsByTagName('html')[0].setAttribute( 'dir', 'rtl' )} >rtl</button>
+    </bdo>
     <ButtonExamples />
   </div>
 );
