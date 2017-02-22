06a1c47269894d7929b379c5ffa89550b41d38ae
Brett Jankord <Brett.Jankord@Cerner.com>
2017-02-21 22:33:59 -0600

    Consolidate alignment example code

--- packages/terra-arrange/examples/index.jsx
+++ packages/terra-arrange/examples/index.jsx
@@ -7,20 +7,27 @@ import ReadMe from '../docs/README.md';
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/first, import/no-unresolved, import/extensions
 import ArrangeSrc from '!raw-loader!../src/Arrange.jsx';
 // Example Files
-import ArrangeAlignAll from './ArrangeAlignAll';
-import ArrangeAlignIndividual from './ArrangeAlignIndividual';
-import ArrangeFitBgColor from './ArrangeFitBgColor';
+import { icon, IconWrapper, image, simpleText } from './examplesetup';
+import ArrangeAlignment from './ArrangeAlignment';
 
 const ArrangeExamples = () => (
   <div>
     <Markdown id="readme" src={ReadMe} />
     <PropsTable id="props" src={ArrangeSrc} />
     <h2 id="align-all">Align All Containers</h2>
-    <ArrangeAlignAll />
+    <ArrangeAlignment alignment="all" fitStart={image} fill={simpleText} />
+    <br />
+    <ArrangeAlignment alignment="all" fill={simpleText} fitEnd={icon} />
+    <br />
+    <ArrangeAlignment alignment="all" fitStart={image} fill={simpleText} fitEnd={icon} />
     <h2 id="align-individual">Align Individual Containers</h2>
-    <ArrangeAlignIndividual />
+    <ArrangeAlignment alignment="individual" fitStart={image} fill={simpleText} />
+    <br />
+    <ArrangeAlignment alignment="individual" fill={simpleText} fitEnd={icon} />
+    <br />
+    <ArrangeAlignment alignment="individual" fitStart={image} fill={simpleText} fitEnd={icon} />
     <h2 id="fit-bg-color">Background Color in Fit</h2>
-    <ArrangeFitBgColor />
+    <ArrangeAlignment alignment="individual" fitStart={image} fill={simpleText} fitEnd={<IconWrapper>{icon}</IconWrapper>} />
   </div>
 );
 
