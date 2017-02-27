a7c7daee8ba1e088106af262e0acf8d6aad4607f
Matt Butler <mattwbutler@gmail.com>
2017-02-27 09:08:17 -0600

    verbose prepush

---
+++ dev/scripts/prepush/README.md
@@ -0,0 +1,12 @@
+#prepush
+
+The prepush script will automatically run when executing `git push ...` or it can manually be executed using the command below:
+
+```
+npm run prepush
+```
+
+The prepush script will:
+* Test all packages (includes linting)
+* Compile all packages
+* Verify that all packages have their compiled code commited.
---
+++ dev/scripts/prepush/index.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const shell = require('shelljs');
+
+// Test all packages
+shell.echo('Testing and linting all packages...');
+
+if (shell.exec('npm test').code !== 0) {
+  shell.echo('Error: test or lint failure.');
+  shell.exit(1);
+} else {
+  shell.echo('All testing and linting successful.');
+}
+
+// Compile all packages
+shell.echo('Compiling all packages...');
+
+if (shell.exec('npm run compile').code !== 0) {
+  shell.echo('Compile failed.');
+  shell.exit(1);
+} else {
+  shell.echo('Compile successful.');
+}
+
+// Diff lib directory in package
+shell.echo('Diffing lib directory in each package. Verifying all packages have been compiled...');
+
+if (shell.exec('git diff --exit-code packages/*/lib/*').code !== 0) {
+  shell.echo('Compiled assets have not been commited. Add the compiled assets to your commit before pushing.');
+  shell.exit(1);
+} else {
+  shell.echo('Verified that all packages have been compiled and commited.');
+}
+
+shell.exit(0);
