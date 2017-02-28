7d9f478534b24b39914a3bd3489cee33dc724de7
Ryan Manuel <rfmanuel@gmail.com>
2017-02-28 11:32:15 -0600

    Moving npm run test outside of the common scripts to avoid false positives.

--- scripts/release/release.js
+++ scripts/release/release.js
@@ -3,7 +3,7 @@ const shell = require('shelljs');
 
 const releaseType = process.argv[2] || 'patch';
 
-if (shell.exec(`npm test && npm run compile && npm version ${releaseType} -m "Released version %s" && npm publish && git push`).code !== 0) {
+if (shell.exec(`npm run compile && npm version ${releaseType} -m "Released version %s" && npm publish && git push`).code !== 0) {
   shell.exit(1);
 }
 
