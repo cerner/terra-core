6e65be97bac9777ab75b821004e03879345b714f
Ryan Manuel <rfmanuel@gmail.com>
2017-03-02 13:46:45 -0600

    Additional changes to support sauce labs so that we will not be launching the local selenium server if running remote tests. (#72)

--- packages/terra-button/tests/nightwatch.conf.js
+++ packages/terra-button/tests/nightwatch.conf.js
@@ -2,9 +2,8 @@
 
 const testSettings = require('terra-toolkit').testSettings;
 const resolve = require('path').resolve;
+const nightwatchConfiguration = require('../node_modules/terra-toolkit/lib/nightwatch.json');
 
-module.exports = ((settings) => {
-  const returnSettings = settings;
-  returnSettings.test_settings = testSettings(resolve('../../webpack.config'));
-  return returnSettings;
-})(require('../node_modules/terra-toolkit/lib/nightwatch.json'));
+module.exports = (settings => (
+  testSettings(resolve('../../webpack.config'), settings)
+))(nightwatchConfiguration);
