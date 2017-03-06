32a42553e4ef76b76ce12053c1b2bda5cf154467
Brett Jankord <brett.jankord@cerner.com>
2017-03-06 12:58:37 -0600

    Add lib file updates

--- packages/terra-status/lib/Status.js
+++ packages/terra-status/lib/Status.js
@@ -14,7 +14,7 @@ var _classnames = require('classnames');
 
 var _classnames2 = _interopRequireDefault(_classnames);
 
-require('../src/Status.scss');
+require('./Status.scss');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
