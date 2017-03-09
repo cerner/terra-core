72ad33e66280c9607ad5f5c8c4cb2071db0d4769
Siddharth Kalluru <sid141288@gmail.com>
2017-03-09 16:33:44 -0500

    Refactoring PropsTable and Markdown react components into their own p… (#100)
    * Refactoring PropsTable and Markdown react components into their own packages.
    
    * Implementing review comments.
    
    * Updates to slide-panel to use refactored PropsTable and Markdown.
    
    * Updating package.json for PropsTable and Markdown to fix linter issues.
    
    * Adding terra-toolkit to devDependency, for all the respective packages.
    
    * Fixing linter issue
    
    * Resolving import missed in last commit.
    
    * Updating the lib directory for missed import.

--- packages/terra-application/examples/Index.jsx
+++ packages/terra-application/examples/Index.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import Markdown from '../../../packages/terra-site/src/Markdown';
+import Markdown from 'terra-markdown';
 import ReadMe from '../docs/README.md';
 // Component Source
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/first, import/no-unresolved, import/extensions
