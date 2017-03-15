754f9f8de053aef2bf498d467d2de07af22b8cdb
Esser,Stephen <Stephen.Esser@Cerner.com>
2017-03-15 14:13:12 -0500

    Added examples responding to parent and window viewports

--- packages/terra-responsive-element/examples/Index.jsx
+++ packages/terra-responsive-element/examples/Index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import PropsTable from 'terra-props-table';
 import Markdown from 'terra-markdown';
 import ReadMe from '../docs/README.md';
+import ParentResponsive from './ParentResponsive';
+import WindowResponsive from './WindowResponsive';
 // Component Source
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/first, import/no-unresolved, import/extensions
 import ResponsiveElementSrc from '!raw-loader!../src/ResponsiveElement.jsx';
@@ -11,6 +13,10 @@ const ResponsiveElementExamples = () => (
   <div>
     <Markdown id="readme" src={ReadMe} />
     <PropsTable id="props" src={ResponsiveElementSrc} />
+    <h2>Responsive to Parent</h2>
+    <ParentResponsive />
+    <h2>Responsive to Window</h2>
+    <WindowResponsive />
   </div>
 );
 
