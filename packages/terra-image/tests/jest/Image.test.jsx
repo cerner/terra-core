ccbe31030cefcc2ef387ca728866428f0cf131d0
Brett Jankord <brett.jankord@cerner.com>
2017-03-14 19:06:00 -0500

    Add output from generator

---
+++ packages/terra-image/tests/jest/Image.test.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Image from '../../src/Image';
+
+it('should render a default component', () => {
+  expect(1).toEqual(1);
+});
---
+++ packages/terra-image/tests/nightwatch/ImageTests.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React from 'react';
+import { Link } from 'react-router';
+
+const ImageTests = () => (
+  <div>
+    <ul>
+      <li><Link to="/tests/image-tests/default">Default Image</Link></li>
+    </ul>
+  </div>
+);
+
+export default ImageTests;
