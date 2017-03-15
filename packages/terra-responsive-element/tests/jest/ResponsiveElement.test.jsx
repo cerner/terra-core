be13eb5bb66924110b2909f708a5cc2a2548a419
Esser,Stephen <Stephen.Esser@Cerner.com>
2017-03-15 16:23:47 -0500

    Add snapshot tests

--- packages/terra-responsive-element/tests/jest/ResponsiveElement.test.jsx
+++ packages/terra-responsive-element/tests/jest/ResponsiveElement.test.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import ResponsiveElement from '../../src/ResponsiveElement';
 
+// Snapshot Tests
 it('should render a default component', () => {
-  expect(1).toEqual(1);
+  const responsiveElement = shallow(<ResponsiveElement />);
+  expect(responsiveElement).toMatchSnapshot();
+});
+
+it('should merge classes passed in through props', () => {
+  const responsiveElement = shallow(<ResponsiveElement className="TestClass" />);
+  expect(responsiveElement).toMatchSnapshot();
+});
+
+// Prop Tests
+it('should have the class terra-ResponsiveElement', () => {
+  const responsiveElement = shallow(<ResponsiveElement />);
+  expect(responsiveElement.prop('className')).toContain('terra-ResponsiveElement');
+});
+
+// Custom Props
+it('should merge classes passed in through props', () => {
+  const responsiveElement = shallow(<ResponsiveElement className="TestClass" />);
+  expect(responsiveElement.prop('className')).toContain('terra-ResponsiveElement TestClass');
+});
+
+it('should pass custom props onto the root element', () => {
+  const responsiveElement = shallow(<ResponsiveElement data-terra-mock="MockData" />);
+  expect(responsiveElement.prop('data-terra-mock')).toContain('MockData');
 });
