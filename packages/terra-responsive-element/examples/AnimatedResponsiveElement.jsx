e7f89509f0ddff73650b6a97d04a284fb2d0ebdc
Stephen Esser <Stephen.Esser@gmail.com>
2017-03-16 16:42:02 -0500

    Use pure CSS for ResponsiveElement animation examples (#140)
    * Cleanup examples, use keyframes for animations
    
    * Move AnimatedResponsiveElement inline styles into SCSS

--- packages/terra-responsive-element/examples/AnimatedResponsiveElement.jsx
+++ packages/terra-responsive-element/examples/AnimatedResponsiveElement.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import ResponsiveElement from '../src/ResponsiveElement';
+import './AnimatedResponsiveElement.scss';
 
 const propTypes = {
   /**
@@ -12,45 +13,19 @@ const defaultProps = {
   responsiveTo: 'parent',
 };
 
-class AnimatedResponsiveElement extends React.Component {
-  constructor() {
-    super();
-    this.state = { width: '20%' };
-    this.interval = null;
-  }
-
-  componentDidMount() {
-    setTimeout(() => this.setState({ width: '100%' }), 1000);
-    this.interval = setInterval(() => { this.setState({ width: (this.state.width === '100%' ? '20%' : '100%') }); }, 5000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-    this.interval = null;
-  }
-
-  render() {
-    const tiny = <div>Tiny</div>;
-    const small = <div>Small</div>;
-    const medium = <div>Medium</div>;
-    const large = <div>Large</div>;
-    const huge = <div>Huge</div>;
-    const defaultElement = <div>Default</div>;
-    return (
-      <div style={{ width: this.state.width, border: '1px dashed grey', padding: '5px', transition: 'all 5.0s ease-in-out' }}>
-        <ResponsiveElement
-          responsiveTo={this.props.responsiveTo}
-          tiny={tiny}
-          small={small}
-          medium={medium}
-          large={large}
-          huge={huge}
-          defaultElement={defaultElement}
-        />
-      </div>
-    );
-  }
-}
+const AnimatedResponsiveElement = ({ responsiveTo }) => (
+  <div className="terra-AnimatedResponsiveElement">
+    <ResponsiveElement
+      responsiveTo={responsiveTo}
+      tiny={<div>Tiny</div>}
+      small={<div>Small</div>}
+      medium={<div>Medium</div>}
+      large={<div>Large</div>}
+      huge={<div>Huge</div>}
+      defaultElement={<div>Default</div>}
+    />
+  </div>
+);
 
 AnimatedResponsiveElement.propTypes = propTypes;
 AnimatedResponsiveElement.defaultProps = defaultProps;
