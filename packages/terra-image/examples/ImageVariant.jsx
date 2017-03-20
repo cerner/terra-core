78f8f00bbef4c7b335fd3f4ea142dd5a449efc9b
Anusha Ganapuram <anushareddy.ganapuram@cerner.com>
2017-03-20 10:56:48 -0400

    Fixing the build

--- packages/terra-image/examples/ImageVariant.jsx
+++ packages/terra-image/examples/ImageVariant.jsx
@@ -3,36 +3,20 @@ import Image from '../src/Image';
 
 const ImageVariant = () => (
   <div>
-    <p>
-    <h2>Default Image Style</h2>
+    <p>Default Image Style</p>
     <Image src="http://lorempixel.com/320/320/nature/" alt="example of defualt styles" />
-    <br />
-    <br />
-    <h2>Default Image without alt</h2>
+    <p>Default Image without alt</p>
     <Image src="http://lorempixel.com/320/320/nature/" variant="default" />
-    <br />
-    <br />
-    <h2>Circle Style</h2>
+    <p>Circle Style</p>
     <Image src="http://lorempixel.com/320/320/nature/" alt="example of circle styles" variant="circle" />
-    <br />
-    <br />
-    <h2>Rounded Style</h2>
+    <p>Rounded Style</p>
     <Image src="http://lorempixel.com/320/320/nature/" alt="example of rounded styles" variant="rounded" />
-    <br />
-    <br />
-    <h2>Thumbnail Style</h2>
+    <p>Thumbnail Style</p>
     <Image src="http://lorempixel.com/320/320/nature/" alt="example of thumbnail styles" variant="thumbnail" />
-    <br />
-    <br />
-    <h2>Image with height and width defined</h2>
+    <p>Image with height and width defined</p>
     <Image src="http://lorempixel.com/320/320/nature/" alt="example of thumbnail styles" variant="thumbnail" height={150} width={200} />
-    <br />
-    <br />
-    <h2>Image with fluid behavior</h2>
+    <p>Image with fluid behavior</p>
     <Image src="http://lorempixel.com/1920/480/nature/" alt="example of thumbnail styles" variant="thumbnail" isFluid />
-    <br />
-    <br />
-    </p>
   </div>
 );
 
