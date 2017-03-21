b1387dad3980d0bcc7a798f751731cb3009aa47c
Kasper,Dave <Dave.Kasper@Cerner.com>
2017-03-21 12:55:39 -0500

    updated example styling.

--- packages/terra-list/examples/ListDemo.jsx
+++ packages/terra-list/examples/ListDemo.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import List from '../lib/List';
 
-export default () => <List isDivided>
-  <List.Item content={<p>test</p>} key="123" />
-  <List.Item content={<p>test</p>} key="124" />
-  <List.Item content={<p>test</p>} key="125" />
-</List>;
+const list = () => (
+  <List isDivided>
+    <List.Item content={<p>test</p>} key="123" />
+    <List.Item content={<p>test</p>} key="124" />
+    <List.Item content={<p>test</p>} key="125" />
+  </List>);
+
+export default list;
--- packages/terra-list/examples/SingleSelectListDemo.jsx
+++ packages/terra-list/examples/SingleSelectListDemo.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import SingleSelectList from '../lib/SingleSelectList';
 
-export default () => <SingleSelectList hasChevrons isDivided>
-  <SingleSelectList.Item content={<p>test</p>} key="123" />
-  <SingleSelectList.Item content={<p>test</p>} key="124" />
-  <SingleSelectList.Item content={<p>test</p>} key="125" />
-</SingleSelectList>;
+const list = () => (
+  <SingleSelectList hasChevrons isDivided>
+    <SingleSelectList.Item content={<p>test</p>} key="123" />
+    <SingleSelectList.Item content={<p>test</p>} key="124" />
+    <SingleSelectList.Item content={<p>test</p>} key="125" />
+  </SingleSelectList>);
+
+export default list;
