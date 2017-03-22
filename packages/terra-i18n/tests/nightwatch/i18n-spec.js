b3fb61889a7fddc205c9639ac90c81041ccbe506
Tao Zhang <windse7en@gmail.com>
2017-03-22 11:41:56 -0500

    Update to use expect syntax with before

--- packages/terra-i18n/tests/nightwatch/i18n-spec.js
+++ packages/terra-i18n/tests/nightwatch/i18n-spec.js
@@ -12,7 +12,7 @@ const defaultTranslations = enTranslations;
 
 const screenshot = require('terra-toolkit').screenshot;
 
-const pauseInms = 1000;
+const waitInms = 1000;
 
 module.exports = {
   afterEach: (browser, done) => {
@@ -22,79 +22,80 @@ module.exports = {
   'Displays ajax error message in default': (browser) => {
     browser
       .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/i18n-tests/default`)
-      .waitForElementVisible('select', 1000)
-      .assert.containsText('div', defaultTranslations['Terra.ajax.error']);
+      .waitForElementVisible('select', waitInms);
+
+    browser.expect.element('div').text.to.contain(defaultTranslations['Terra.ajax.error']).before(waitInms);
   },
 
   'Displays ajax error message in de': (browser) => {
     browser
       .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/i18n-tests/default`)
-      .waitForElementVisible('select', 1000)
-      .click('select option[value="de"]')
-      .pause(pauseInms)
-      .assert.containsText('div', deTranslations['Terra.ajax.error']);
+      .waitForElementVisible('select', waitInms)
+      .click('select option[value="de"]');
+
+    browser.expect.element('div').text.to.contain(deTranslations['Terra.ajax.error']).before(waitInms);
   },
 
   'Displays ajax error message in en': (browser) => {
     browser
       .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/i18n-tests/default`)
-      .waitForElementVisible('select', 1000)
-      .click('select option[value="en"]')
-      .pause(pauseInms)
-      .assert.containsText('div', enTranslations['Terra.ajax.error']);
+      .waitForElementVisible('select', waitInms)
+      .click('select option[value="en"]');
+
+    browser.expect.element('div').text.to.contain(enTranslations['Terra.ajax.error']).before(waitInms);
   },
 
   'Displays ajax error message in en-GB': (browser) => {
     browser
       .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/i18n-tests/default`)
-      .waitForElementVisible('select', 1000)
-      .click('select option[value="en-GB"]')
-      .pause(pauseInms)
-      .assert.containsText('div', enGBTranslations['Terra.ajax.error']);
+      .waitForElementVisible('select', waitInms)
+      .click('select option[value="en-GB"]');
+
+    browser.expect.element('div').text.to.contain(enGBTranslations['Terra.ajax.error']).before(waitInms);
   },
 
   'Displays ajax error message in en-US': (browser) => {
     browser
       .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/i18n-tests/default`)
-      .waitForElementVisible('select', 1000)
-      .click('select option[value="en-US"]')
-      .pause(pauseInms)
-      .assert.containsText('div', enUSTranslations['Terra.ajax.error']);
+      .waitForElementVisible('select', waitInms)
+      .click('select option[value="en-US"]');
+
+    browser.expect.element('div').text.to.contain(enUSTranslations['Terra.ajax.error']).before(waitInms);
   },
 
   'Displays ajax error message in es': (browser) => {
     browser
       .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/i18n-tests/default`)
-      .waitForElementVisible('select', 1000)
-      .click('select option[value="es"]')
-      .pause(pauseInms)
-      .assert.containsText('div', esTranslations['Terra.ajax.error']);
+      .waitForElementVisible('select', waitInms)
+      .click('select option[value="es"]');
+
+    browser.expect.element('div').text.to.contain(esTranslations['Terra.ajax.error']).before(waitInms);
   },
 
   'Displays ajax error message in fi-FI': (browser) => {
     browser
       .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/i18n-tests/default`)
-      .waitForElementVisible('select', 1000)
-      .click('select option[value="fi-FI"]')
-      .pause(pauseInms)
-      .assert.containsText('div', fiFITranslations['Terra.ajax.error']);
+      .waitForElementVisible('select', waitInms)
+      .click('select option[value="fi-FI"]');
+
+    browser.expect.element('div').text.to.contain(fiFITranslations['Terra.ajax.error']).before(waitInms);
   },
 
   'Displays ajax error message in fr': (browser) => {
     browser
       .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/i18n-tests/default`)
-      .waitForElementVisible('select', 1000)
-      .click('select option[value="fr"]')
-      .pause(pauseInms)
-      .assert.containsText('div', frTranslations['Terra.ajax.error']);
+      .waitForElementVisible('select', waitInms)
+      .click('select option[value="fr"]');
+
+    browser.expect.element('div').text.to.contain(frTranslations['Terra.ajax.error']).before(waitInms);
   },
 
   'Displays ajax error message in pt': (browser) => {
     browser
       .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/i18n-tests/default`)
-      .waitForElementVisible('select', 1000)
-      .click('select option[value="pt"]')
-      .pause(pauseInms)
-      .assert.containsText('div', ptTranslations['Terra.ajax.error']);
+      .waitForElementVisible('select', waitInms)
+      .click('select option[value="pt"]');
+
+    browser.expect.element('div').text.to.contain(ptTranslations['Terra.ajax.error']).before(waitInms);
   },
 };
