3db16181e668b788aeac0c7bedd0c6d3409f340e
Matt Butler <mattwbutler@gmail.com>
2017-03-29 11:21:16 -0500

    update to new one-cerner-style-icons commit

--- packages/terra-icon/lib/icon/IconReload.js
+++ packages/terra-icon/lib/icon/IconReload.js
@@ -28,7 +28,7 @@ var SvgIcon = function SvgIcon(customProps) {
 };
 
 SvgIcon.displayName = "IconReload";
-SvgIcon.defaultProps = { "className": "", "viewBox": "0 0 48 48", "xmlns": "http://www.w3.org/2000/svg", "isBidi": true };
+SvgIcon.defaultProps = { "viewBox": "0 0 48 48", "xmlns": "http://www.w3.org/2000/svg" };
 
 exports.default = SvgIcon;
 /* eslint-enable */
