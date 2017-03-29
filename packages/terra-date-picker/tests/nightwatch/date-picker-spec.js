2454dd83b88e74c4b80fcd01e9217e926d536aeb
Cai,Ben <BCAI@CERNER.COM>
2017-03-29 17:25:36 -0500

    Spectre changes

--- packages/terra-date-picker/tests/nightwatch/date-picker-spec.js
+++ packages/terra-date-picker/tests/nightwatch/date-picker-spec.js
@@ -4,8 +4,12 @@
 const screenshot = require('terra-toolkit').screenshot;
 
 module.exports = {
+  before: (browser, done) => {
+    browser.resizeWindow(browser.globals.width, browser.globals.height, done);
+  },
+
   afterEach: (browser, done) => {
-    screenshot(browser, done);
+    screenshot(browser, 'terra-date-picker', done);
   },
 
   'Displays the DatePicker with defaulted props': (browser) => {
