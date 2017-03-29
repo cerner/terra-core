5faf70be8057b24ff55b06348bc8c64fadc6dff3
Kasper,Dave <Dave.Kasper@Cerner.com>
2017-03-29 16:57:27 -0500

    More work on night watch.

---
+++ packages/terra-content-container/tests/nightwatch/DefaultContentContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ContentContainer from 'terra-content-container';
+
+const header = <p>header</p>;
+const content = <p>content</p>;
+
+const container = () => (
+  <div style={{ height: '200px', overflow: 'auto' }}>
+    <ContentContainer header={header}>
+      {content}
+    </ContentContainer>
+  </div>);
+
+export default container;
