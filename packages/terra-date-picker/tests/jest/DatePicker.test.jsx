90079de09bfc8576ec5b15cd4e0ad48416f03a9e
Cai,Ben <BCAI@CERNER.COM>
2017-03-30 12:30:47 -0500

    Fix travis CI build due to having different utcOffSet.

--- packages/terra-date-picker/tests/jest/DatePicker.test.jsx
+++ packages/terra-date-picker/tests/jest/DatePicker.test.jsx
@@ -11,13 +11,13 @@ const isWeekday = (date) => {
 const format = 'MM-DD-YYYY';
 
 it('should render a default date input and date picker', () => {
-  const datePicker = <DatePicker utcOffset={moment().utcOffset()} />;
+  const datePicker = <DatePicker utcOffset={0} />;
   const wrapper = shallow(datePicker);
   expect(wrapper).toMatchSnapshot();
 });
 
 it('should render a date range', () => {
-  const dateRange = <DateRange utcOffset={moment().utcOffset()} />;
+  const dateRange = <DateRange utcOffset={0} />;
   const wrapper = shallow(dateRange);
   expect(wrapper).toMatchSnapshot();
 });
@@ -25,14 +25,14 @@ it('should render a date range', () => {
 it('should render a date picker with disabled dates', () => {
   const datePicker = (<DatePicker
     excludeDates={[moment.utc('04-01-2016', format), moment.utc('04-03-2016', format)]}
-    utcOffset={moment().utcOffset()}
+    utcOffset={0}
   />);
   const wrapper = shallow(datePicker);
   expect(wrapper).toMatchSnapshot();
 });
 
 it('should render a date picker with filtered dates', () => {
-  const datePicker = <DatePicker filterDate={isWeekday} utcOffset={moment().utcOffset()} />;
+  const datePicker = <DatePicker filterDate={isWeekday} utcOffset={0} />;
   const wrapper = shallow(datePicker);
   expect(wrapper).toMatchSnapshot();
 });
@@ -40,7 +40,7 @@ it('should render a date picker with filtered dates', () => {
 it('should render a date picker with included dates', () => {
   const datePicker = (<DatePicker
     includeDates={[moment.utc('04-01-2016', format), moment.utc('04-02-2016', format), moment.utc('04-03-2016', format)]}
-    utcOffset={moment().utcOffset()}
+    utcOffset={0}
   />);
   const wrapper = shallow(datePicker);
   expect(wrapper).toMatchSnapshot();
@@ -50,14 +50,14 @@ it('should render a date picker with min and max dates', () => {
   const datePicker = (<DatePicker
     minDate={moment.utc('04-01-2016', format)}
     maxDate={moment.utc('04-10-2016', format)}
-    utcOffset={moment().utcOffset()}
+    utcOffset={0}
   />);
   const wrapper = shallow(datePicker);
   expect(wrapper).toMatchSnapshot();
 });
 
 it('should render a date picker in portal', () => {
-  const datePicker = <DatePicker portal utcOffset={moment().utcOffset()} />;
+  const datePicker = <DatePicker portal utcOffset={0} />;
   const wrapper = shallow(datePicker);
   expect(wrapper).toMatchSnapshot();
 });
