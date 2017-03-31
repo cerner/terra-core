624913119a3359b97ede150b918535e522472faf
Stephen Esser <Stephen.Esser@gmail.com>
2017-03-31 07:42:33 -0500

    Update ResponsiveElement examples to be interactive (#186)
    * Update ResponsiveElement examples to be interactive
    
    * Replace h4 tags with h3
    
    * Replace note headers with simple paragraphs

--- packages/terra-site/src/examples/responsive-element/Index.jsx
+++ packages/terra-site/src/examples/responsive-element/Index.jsx
@@ -9,8 +9,7 @@ import { version } from 'terra-responsive-element/package.json';
 import ResponsiveElementSrc from '!raw-loader!terra-responsive-element/src/ResponsiveElement';
 
 // Example Files
-import Responsive from './Responsive';
-import AnimatedResponsiveElement from './AnimatedResponsiveElement';
+import ResponsiveExample from './ResponsiveExample';
 
 const ResponsiveElementExamples = () => (
   <div>
@@ -18,11 +17,36 @@ const ResponsiveElementExamples = () => (
     <Markdown id="readme" src={ReadMe} />
     <PropsTable id="props" src={ResponsiveElementSrc} />
     <h2>Responsive to Parent</h2>
-    <AnimatedResponsiveElement />
-    <Responsive />
+    <h3>All Breakpoints</h3>
+    <ResponsiveExample
+      defaultElement={<div>Default</div>}
+      tiny={<div>Tiny</div>}
+      small={<div>Small</div>}
+      medium={<div>Medium</div>}
+      large={<div>Large</div>}
+      huge={<div>Huge</div>}
+    />
+    <br />
+    <h3>Default and Medium Breakpoints</h3>
+    <ResponsiveExample
+      defaultElement={<div>Default</div>}
+      medium={<div>Medium</div>}
+    />
+    <br />
+    <h3>Only Medium</h3>
+    <p>Note: Does not render elements when smaller than medium.</p>
+    <ResponsiveExample medium={<div>Medium</div>} />
     <h2>Responsive to Window</h2>
-    <AnimatedResponsiveElement responsiveTo="window" />
-    <Responsive responsiveTo="window" />
+    <p>Note: Changing the container size has no effect when responsive to window. Resize the window to examine changes.</p>
+    <ResponsiveExample
+      responsiveTo="window"
+      defaultElement={<div>Default</div>}
+      tiny={<div>Tiny</div>}
+      small={<div>Small</div>}
+      medium={<div>Medium</div>}
+      large={<div>Large</div>}
+      huge={<div>Huge</div>}
+    />
   </div>
 );
 
