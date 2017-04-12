602c51f26084191fb54927d20558e4ea59789b49
Kasper,Dave <Dave.Kasper@Cerner.com>
2017-04-12 15:32:29 -0500

    Remove detail view from the core repo.

--- packages/terra-site/src/App.jsx
+++ packages/terra-site/src/App.jsx
@@ -23,7 +23,6 @@ const App = props => (
       <li><Link to="/site/button-group">Button Group</Link></li>
       <li><Link to="/site/content">Content</Link></li>
       <li><Link to="/site/datepicker">Date Picker</Link></li>
-      <li><Link to="/site/detail-view">Detail View</Link></li>
       <li><Link to="/site/grid">Grid</Link></li>
       <li><Link to="/site/i18n">I18n</Link></li>
       <li><Link to="/site/icon">Icon</Link></li>
