bb70ee70f6c4c14d59215bd7d86c24e63a0a1ec4
Matt Butler <mattwbutler@gmail.com>
2017-04-21 14:35:41 -0500

    mock icon

--- packages/terra-status/tests/nightwatch/StatusIcon.jsx
+++ packages/terra-status/tests/nightwatch/StatusIcon.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import IconDue from 'terra-icon/lib/icon/IconDue';
 import Status from '../../lib/Status';
 
-const icon = <IconDue height="60" width="60" />;
+const icon = (<svg className="terra-Icon" height="4em" width="4em" viewBox="0 0 48 48">
+  <path d="M24 0h-.1C10.7 0 0 10.8 0 24c0 13.3 10.7 24 24 24s24-10.7 24-24S37.3 0 24 0zm-4 36.4L6.7 23.1l3.6-3.6 9.7 9.9 17.7-17.9 3.6 3.6L20 36.4z" fill="#FFF" />
+</svg>);
 
 const StatusIcon = () => (
   <Status color="#ff0000">{icon}</Status>
