2a222a9635c4fdf0440fca0e1058a008b1ebfafc
Matt Butler <Matt-Butler@users.noreply.github.com>
2017-04-27 17:49:11 -0500

    Add nightwatch tests for progress bar (#265)

---
+++ packages/terra-progress-bar/tests/nightwatch/components/ProgressBarDefault.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProgressBar from '../../../lib/ProgressBar';
+
+const ProgressBarDefault = () => (
+  <ProgressBar value={15} />
+);
+
+export default ProgressBarDefault;
---
+++ packages/terra-progress-bar/tests/nightwatch/components/ProgressBarSize.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ProgressBar from '../../../lib/ProgressBar';
+
+const ProgressBarSize = () => (
+  <div>
+    <ProgressBar heightSize="tiny" value={15} />
+    <br />
+    <br />
+    <ProgressBar heightSize="small" value={30} />
+    <br />
+    <br />
+    <ProgressBar heightSize="medium" value={45} />
+    <br />
+    <br />
+    <ProgressBar heightSize="large" value={60} />
+    <br />
+    <br />
+    <ProgressBar heightSize="huge" value={75} />
+  </div>
+);
+
+export default ProgressBarSize;
