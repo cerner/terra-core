9fdeeeabebc6a939734d6d315e85d16198e456e5
Ryan Manuel <Ryan.Manuel@Cerner.com>
2017-04-27 13:39:16 -0500

    Changes to support the i18n webpack plugin

--- packages/terra-i18n/lib/i18nSupportedLanguages.js
+++ packages/terra-i18n/lib/i18nSupportedLanguages.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = ['de', 'en', 'en-US', 'en-GB', 'es', 'fi-FI', 'fr', 'pt'];
+module.exports = ['en', 'en-US', 'en-GB', 'es', 'de', 'fi-FI', 'fr', 'pt'];
--- packages/terra-i18n/src/i18nSupportedLanguages.js
+++ packages/terra-i18n/src/i18nSupportedLanguages.js
@@ -1,1 +1,1 @@
-module.exports = ['de', 'en', 'en-US', 'en-GB', 'es', 'fi-FI', 'fr', 'pt'];
+module.exports = ['en', 'en-US', 'en-GB', 'es', 'de', 'fi-FI', 'fr', 'pt'];
