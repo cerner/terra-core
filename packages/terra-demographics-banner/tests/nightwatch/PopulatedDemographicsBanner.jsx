af18a83d40263082c4bbda41c36f35a72275f4e4
Jake LaCombe <jake.lacombe@cerner.com>
2017-04-27 16:39:29 -0500

    Style Cleanup

--- packages/terra-demographics-banner/tests/nightwatch/PopulatedDemographicsBanner.jsx
+++ packages/terra-demographics-banner/tests/nightwatch/PopulatedDemographicsBanner.jsx
@@ -4,7 +4,7 @@ import DemographicsBanner from '../../lib/DemographicsBanner';
 
 export default () =>
   <DemographicsBanner
-    additionalDetails={<span className="risk-score">5%</span>}
+    additionalDetails={<p>St. Johns West Wing Room 253</p>}
     age="25 Years"
     additionalDetailsTruncated
     dateOfBirth="May 9, 1993"
