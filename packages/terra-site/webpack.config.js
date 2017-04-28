d3dce4de05bfd7fed76f189d22938195fce18b7d
Tao Zhang <windse7en@gmail.com>
2017-04-28 15:38:31 -0500

    Webpack2 Integration (#270)

--- packages/terra-site/webpack.config.js
+++ packages/terra-site/webpack.config.js
@@ -1,9 +1,10 @@
 // By default eslint assumes packages imported are supposed to be dependencies,
 // not devDependencies. Disabling this rule in webpack.conig.js
 /* eslint-disable import/no-extraneous-dependencies */
+const webpack = require('webpack');
 
 const path = require('path');
-const autoprefixer = require('autoprefixer');
+const Autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -11,32 +12,53 @@ module.exports = {
   entry: {
     'terra-core': path.resolve(path.join(__dirname, 'src', 'Index')),
   },
-  resolveLoader: {
-    root: path.resolve(path.join(__dirname, 'node_modules')),
-  },
   module: {
-    loaders: [
-      {
-        test: /\.(jsx|js)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
-      },
-      {
-        test: /\.(scss|css)$/,
-        loader: ExtractTextPlugin.extract('style', 'css!postcss!sass'),
-      },
-      {
-        test: /\.md$/,
-        loader: 'raw-loader',
-      },
+    loaders: [{
+      test: /\.(jsx|js)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    },
+    {
+      test: /\.json$/,
+      loader: 'json-loader',
+    },
+    {
+      test: /\.(scss|css)$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [{
+          loader: 'css-loader',
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            plugins() {
+              return [
+                Autoprefixer({
+                  browsers: [
+                    'ie >= 10',
+                    'last 2 versions',
+                    'last 2 android versions',
+                    'last 2 and_chr versions',
+                    'iOS >= 8',
+                  ],
+                }),
+              ];
+            },
+          },
+        }, {
+          loader: 'sass-loader',
+          options: {
+            data: `@import "${path.resolve(path.join(__dirname, 'node_modules/terra-legacy-theme/src/LegacyTheme.scss'))}"; $terra-bidi: true;`,
+          },
+        }],
+      }),
+
+    },
+    {
+      test: /\.md$/,
+      loader: 'raw-loader',
+    },
     ],
-  },
-  sassLoader: {
-    data: `@import "${path.resolve(path.join(__dirname, 'node_modules/terra-legacy-theme/src/LegacyTheme.scss'))}"; $terra-bidi: true;`,
   },
   plugins: [
     new ExtractTextPlugin('[name]-[hash].css'),
@@ -44,26 +66,33 @@ module.exports = {
       template: path.join(__dirname, 'src', 'index.html'),
       chunks: ['terra-core'],
     }),
-  ],
-  postcss: [
-    autoprefixer({
-      browsers: [
-        'ie >= 10',
-        'last 2 versions',
-        'last 2 android versions',
-        'last 2 and_chr versions',
-        'iOS >= 8',
-      ],
-    }),
+    new webpack.NamedChunksPlugin(),
   ],
   resolve: {
-    extensions: ['', '.js', '.jsx'],
-
-    // See https://github.com/facebook/react/issues/8026
+    extensions: ['.js', '.jsx'],
     alias: { react: path.resolve(__dirname, 'node_modules', 'react') },
   },
   output: {
     filename: '[name].js',
     path: path.join(__dirname, 'dist'),
+  },
+  devtool: 'cheap-source-map',
+  devServer: {
+    host: '0.0.0.0',
+    stats: {
+      assets: true,
+      children: false,
+      chunks: false,
+      hash: false,
+      modules: false,
+      publicPath: false,
+      timings: true,
+      version: true,
+      warnings: true,
+    },
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
   },
 };
--- packages/terra-toolkit/lib/server-launcher.js
+++ packages/terra-toolkit/lib/server-launcher.js
@@ -8,11 +8,28 @@ var _webpackDevServer = require('webpack-dev-server');
 
 var _webpackDevServer2 = _interopRequireDefault(_webpackDevServer);
 
+var _shelljs = require('shelljs');
+
+var _shelljs2 = _interopRequireDefault(_shelljs);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.launchServer = function () {
   return new Promise(function (resolve) {
     var compiler = void 0;
+
+    // After migrating from webpack 1 to webpack 2
+    // webpack can't find correct devDependencies modules directory
+    // A quick solution is to switch working directory to terra-site
+    // and make all modules available.
+    // Switch to terra site directory
+    var curDir = _path2.default.resolve(process.cwd());
+    _shelljs2.default.cd(_path2.default.resolve(__dirname, '../../terra-site'));
+
     if (process.env.WEBPACK_CONFIG_PATH) {
       /* eslint-disable global-require, import/no-dynamic-require */
       compiler = (0, _webpack2.default)(require(process.env.WEBPACK_CONFIG_PATH));
@@ -22,6 +39,7 @@ exports.launchServer = function () {
       compiler = (0, _webpack2.default)(require('../../terra-site/webpack.config'));
       /* eslint-enable global-require, import/no-dynamic-require */
     }
+
     compiler.plugin('done', resolve);
 
     module.server = new _webpackDevServer2.default(compiler, {
@@ -29,6 +47,8 @@ exports.launchServer = function () {
     });
 
     module.server.listen(8080, '0.0.0.0');
+    // Switch back to previous directory
+    _shelljs2.default.cd(curDir);
   });
 };
 
