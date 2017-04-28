3b4a33cc66ed7ff7a5496eea3df029c2252d1073
Matt Butler <mattwbutler@gmail.com>
2017-04-28 13:28:10 -0500

    add icon nightwatch tests

--- packages/terra-site/src/examples/icon/Index.jsx
+++ packages/terra-site/src/examples/icon/Index.jsx
@@ -10,10 +10,6 @@ import IconSrc from '!raw-loader!terra-icon/src/IconBase';
 
 // Example Files
 import IconAll from './IconAll';
-import IconAppendClass from './IconAppendClass';
-import IconAria from './IconAria';
-import IconBidi from './IconBidi';
-import IconHeightWidth from './IconHeightWidth';
 
 const IconExamples = () => (
   <div>
@@ -22,14 +18,6 @@ const IconExamples = () => (
     <PropsTable id="props" src={IconSrc} />
     <h2 id="all_icons">All Icons</h2>
     <IconAll />
-    <h2 id="bidi_icons">Bidi Icons</h2>
-    <IconBidi />
-    <h2 id="height_width_icons">Icon Height and Width</h2>
-    <IconHeightWidth />
-    <h2 id="aria_icons">Icon Accessibility</h2>
-    <IconAria />
-    <h2 id="append_class_icons">Append Classes</h2>
-    <IconAppendClass />
   </div>
 );
 
