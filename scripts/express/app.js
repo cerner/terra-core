2725fac4b47f15f026edeb1fda387ba64ed0fee6
Tao Zhang <windse7en@gmail.com>
2017-05-08 14:17:28 -0500

    Fixed lint error

--- scripts/express/app.js
+++ scripts/express/app.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+
 const express = require('express');
 const path = require('path');
 const shelljs = require('shelljs');
@@ -9,18 +12,16 @@ const buildCommand = 'npm run compile:prod';
 
 // listen to PORT to avoid Heroku error: R10 boot timeout
 app.listen(port);
-console.log('Listening ' + port);
+console.log(`Listening ${port}`);
 
 if (shelljs.exec(buildCommand).code !== 0) {
-  console.log('Errors in command: ' + buildCommand);
+  console.log(`Errors in command: ${buildCommand}`);
 } else {
-  console.log('Finished command: ' + buildCommand);
+  console.log(`Finished command: ${buildCommand}`);
 
   // path to webpack built path
   const buildPath = path.join(__dirname, '../../packages/terra-site/build');
   app.use('/static', express.static(buildPath));
-  app.get('/', function(req, res) {
-    res.redirect('/static');
-  });
+  app.get('/', (req, res) => res.redirect('/static'));
   console.log('Switched to built assets.');
 }
