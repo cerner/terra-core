db63e29c57ac43d0bf62b20ee5ff692cb5753858
Tao Zhang <windse7en@gmail.com>
2017-05-08 14:46:39 -0500

    Add the logic to check if there is need to rebuild

--- scripts/express/app.js
+++ scripts/express/app.js
@@ -1,27 +1,29 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
-
 const express = require('express');
 const path = require('path');
-const shelljs = require('shelljs');
+const shell = require('shelljs');
 
 const app = express();
 const port = process.env.PORT || 8081;
 // npm script to build assets by webpack
 const buildCommand = 'npm run compile:prod';
+// path to webpack built path
+const buildPath = path.join(__dirname, '../../packages/terra-site/build');
 
 // listen to PORT to avoid Heroku error: R10 boot timeout
 app.listen(port);
 console.log(`Listening ${port}`);
 
-if (shelljs.exec(buildCommand).code !== 0) {
-  console.log(`Errors in command: ${buildCommand}`);
-} else {
-  console.log(`Finished command: ${buildCommand}`);
-
-  // path to webpack built path
-  const buildPath = path.join(__dirname, '../../packages/terra-site/build');
-  app.use('/static', express.static(buildPath));
-  app.get('/', (req, res) => res.redirect('/static'));
-  console.log('Switched to built assets.');
+if (!shell.test('-d', buildPath)) {
+  if (shell.exec(buildCommand).code !== 0) {
+    console.log(`Errors in command: ${buildCommand}`);
+    process.exit(1);
+  } else {
+    console.log(`Finished command: ${buildCommand}`);
+  }
 }
+
+app.use('/static', express.static(buildPath));
+app.get('/', (req, res) => res.redirect('/static'));
+console.log('Switched to built assets.');
