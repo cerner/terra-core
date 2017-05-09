1b428babf541b9b56cc9a4e3960fd89f19487edc
Tao Zhang <windse7en@gmail.com>
2017-05-08 21:25:07 -0500

    Update the app.js

--- scripts/express/app.js
+++ scripts/express/app.js
@@ -2,19 +2,13 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
-const shell = require('shelljs');
 
 const app = express();
 const port = process.env.PORT || 8081;
-// npm script to build assets by webpack
-const buildCommand = 'npm run compile:prod';
 // path to webpack built path
 const buildPath = path.join(__dirname, '../../packages/terra-site/build');
 
-// listen to PORT to avoid Heroku error: R10 boot timeout
-app.listen(port);
-console.log(`Listening ${port}`);
-
 app.use('/static', express.static(buildPath));
 app.get('/', (req, res) => res.redirect('/static'));
-console.log('Switched to built assets.');
+app.listen(port);
+console.log(`Listening ${port}`);
