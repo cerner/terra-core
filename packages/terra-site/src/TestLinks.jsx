720b19b0bb98715e7b96a48378c801de386f3484
Ben Cai <bcai@cerner.com>
2017-05-11 15:25:03 -0500

    terra-time-input - pull out terra-time-input because of an issue in the text-mask component. (#343)

--- packages/terra-site/src/TestLinks.jsx
+++ packages/terra-site/src/TestLinks.jsx
@@ -24,7 +24,6 @@ const TestLinks = () => (
       <li><Link to="/tests/multi-select-list-tests">MultiSelectList Tests</Link></li>
       <li><Link to="/tests/status-tests">Status Tests</Link></li>
       <li><Link to="/tests/table-tests">Table Tests</Link></li>
-      <li><Link to="/tests/time-input-tests">Time Input Tests</Link></li>
       <li><Link to="/tests/modal-tests">Modal Tests</Link></li>
       <li><Link to="/tests/progress-bar-tests">ProgressBar Tests</Link></li>
       <li><Link to="/tests/grid-tests">Grid Tests</Link></li>
