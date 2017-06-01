9f9da0a0905e847137b56e29fcc3d6f108a0e6a9
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-06-01 09:13:40 -0500

    OverlayContainer component.

---
+++ packages/terra-overlay/src/OverlayContainer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import 'terra-base/lib/baseStyles';
+import './Overlay.scss';
+
+const propTypes = {
+ /*
+ * The overlay and elements to be displayed
+ */
+  children: PropTypes.node,
+};
+
+const defaultProps = {
+  children: null,
+};
+
+const OverlayContainer = ({ children, ...customProps }) => {
+  const attributes = Object.assign({}, customProps);
+  const OverlayContainerClassNames = classNames([
+    'terra-OverlayContainer',
+    attributes.className,
+  ]);
+
+  return (
+    <div className={OverlayContainerClassNames} tabIndex="-1" {...customProps} >
+      {children}
+    </div>
+  );
+};
+
+OverlayContainer.propTypes = propTypes;
+OverlayContainer.defaultProps = defaultProps;
+
+export default OverlayContainer;
