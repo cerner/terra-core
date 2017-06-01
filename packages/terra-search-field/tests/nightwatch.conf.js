a901614f61a822f8418bde2ffd4fc95557aab75f
David Schoonover <david.schoonover@cerner.com>
2017-06-01 10:21:29 -0500

    Use nightwatch from toolkit.

--- packages/terra-search-field/tests/nightwatch.conf.js
+++ packages/terra-search-field/tests/nightwatch.conf.js
@@ -2,7 +2,7 @@
 
 const testSettings = require('terra-toolkit').testSettings;
 const resolve = require('path').resolve;
-const nightwatchConfiguration = require('../node_modules/terra-toolkit/lib/nightwatch.json');
+const nightwatchConfiguration = require('terra-toolkit/lib/nightwatch.json');
 
 module.exports = (settings => (
   testSettings(resolve('../../webpack.config'), settings)
