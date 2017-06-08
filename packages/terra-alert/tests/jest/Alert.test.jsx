35c37e3215d235f6486062938b104166ffdefb22
David <david.roth@cerner.com>
2017-06-08 17:14:52 -0400

    Initial Jest tests for Alert

--- packages/terra-alert/tests/jest/Alert.test.jsx
+++ packages/terra-alert/tests/jest/Alert.test.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import Alert from '../../src/Alert';
+import IconAlert from 'terra-icon/lib/icon/IconAlert';
+import IconError from 'terra-icon/lib/icon/IconError';
+import Alert, { AlertTypes } from '../../src/Alert';
 
-describe('Alert', () => {
+describe('Alert with no props', () => {
   const defaultRender = <Alert />;
 
   // Snapshot Tests
@@ -11,14 +13,89 @@ describe('Alert', () => {
   });
 
   // Prop Tests
-  it('should use the default value when no value is given', () => {
+  it('should render an Alert of type alert when no props given including IconAlert icon and default alert title', () => {
     const wrapper = shallow(defaultRender);
-    expect(wrapper.find('.terra-Alert').text()).toEqual('defualt');
+    expect(wrapper.find(IconAlert)).toHaveLength(1);
+    expect(wrapper.childAt(0).childAt(1).childAt(0).text()).toEqual('Alert!');
   });
 
   // Structure Tests
-  it('should have the class terra-Alert', () => {
+  it('should have the class terra-Alert and terra-Alert--alert', () => {
     const wrapper = shallow(defaultRender);
     expect(wrapper.prop('className')).toContain('terra-Alert');
+    expect(wrapper.prop('className')).toContain('terra-Alert--alert');
+  });
+
+  it('default alert should have one child with className terr-Alert-body', () => {
+    const wrapper = shallow(defaultRender);
+    expect(wrapper.children()).toHaveLength(1);
+    expect(wrapper.childAt(0).prop('className')).toContain('terra-Alert-body');
+  });
+
+  it('default alert body should have two children one for the icon section and one for the content section', () => {
+    const wrapper = shallow(defaultRender);
+    expect(wrapper.childAt(0).children()).toHaveLength(2);
+    expect(wrapper.childAt(0).childAt(0).prop('className')).toContain('terra-Alert-icon');
+    expect(wrapper.childAt(0).childAt(1).prop('className')).toContain('terra-Alert-section');
+  });
+
+  it('default alert content section should have two children one for the title and one for the content', () => {
+    const wrapper = shallow(defaultRender);
+    expect(wrapper.childAt(0).childAt(1).children()).toHaveLength(2);
+    expect(wrapper.childAt(0).childAt(1).childAt(0).prop('className')).toContain('terra-Alert-title');
+    expect(wrapper.childAt(0).childAt(1).childAt(0).type()).toEqual('strong');
+    expect(wrapper.childAt(0).childAt(1).childAt(1).prop('className')).toContain('terra-Alert-content');
+  });
+});
+
+describe('Alert of type alert with text content', () => {
+  const alertText = 'This is an alert.';
+  const basicAlertRender = <Alert type={AlertTypes.ALERT} >{alertText}</Alert>;
+
+  // Snapshot Tests
+  it('should render an Alert component of type alert', () => {
+    const wrapper = shallow(basicAlertRender);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  // Prop Tests
+  it('should render an Alert of type alert including IconAlert icon and default alert title', () => {
+    const wrapper = shallow(basicAlertRender);
+    expect(wrapper.find(IconAlert)).toHaveLength(1);
+    expect(wrapper.childAt(0).childAt(1).childAt(0).text()).toEqual('Alert!');
+    expect(wrapper.childAt(0).childAt(1).childAt(1).text()).toEqual(alertText);
+  });
+
+  // Structure Tests
+  it('should have the class terra-Alert and terra-Alert--alert', () => {
+    const wrapper = shallow(basicAlertRender);
+    expect(wrapper.prop('className')).toContain('terra-Alert');
+    expect(wrapper.prop('className')).toContain('terra-Alert--alert');
+  });
+});
+
+describe('Alert of type error with text content', () => {
+  const alertText = 'This is an error.';
+  const basicErrorRender = <Alert type={AlertTypes.ERROR} >{alertText}</Alert>;
+
+  // Snapshot Tests
+  it('should render an Alert component of type error', () => {
+    const wrapper = shallow(basicErrorRender);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  // Prop Tests
+  it('should render an Alert of type error including IconError icon and default error title', () => {
+    const wrapper = shallow(basicErrorRender);
+    expect(wrapper.find(IconError)).toHaveLength(1);
+    expect(wrapper.childAt(0).childAt(1).childAt(0).text()).toEqual('Error!');
+    expect(wrapper.childAt(0).childAt(1).childAt(1).text()).toEqual(alertText);
+  });
+
+  // Structure Tests
+  it('should have the class terra-Alert and terra-Alert--error', () => {
+    const wrapper = shallow(basicErrorRender);
+    expect(wrapper.prop('className')).toContain('terra-Alert');
+    expect(wrapper.prop('className')).toContain('terra-Alert--error');
   });
 });
