c6f5284d28fa8afea896db72677b8e59562c6693
Brett Jankord <brett.jankord@cerner.com>
2017-06-16 16:16:16 -0500

    Update nightwatch tests

--- packages/terra-heading/tests/nightwatch/heading-spec.js
+++ packages/terra-heading/tests/nightwatch/heading-spec.js
@@ -14,48 +14,58 @@ module.exports = {
   'Displays a default heading component': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/heading-tests/default`);
     browser.expect.element('.terra-Heading').text.to.equal('Default');
+    browser.expect.element('.terra-Heading').to.be.an('h1');
   },
 
   'Displays a heading component with a set color': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/heading-tests/color`);
-    browser.expect.element('.terra-Heading').text.to.equal('Color');
+    browser.assert.cssProperty('.terra-Heading', 'color', 'rgba(48, 79, 254, 1)'); // Browser computes #304FFE to rgba(48, 79, 254, 1)
   },
 
   'Displays a heading component with a set size': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/heading-tests/size`);
-    browser.expect.element('.terra-Heading--size-huge').text.to.equal('Huge');
-    browser.expect.element('.terra-Heading--size-large').text.to.equal('Large');
-    browser.expect.element('.terra-Heading--size-medium').text.to.equal('Medium');
-    browser.expect.element('.terra-Heading--size-small').text.to.equal('Small');
-    browser.expect.element('.terra-Heading--size-tiny').text.to.equal('Tiny');
-    browser.expect.element('.terra-Heading--size-mini').text.to.equal('Mini');
+    // Browser computes rem units as pixels when retrieved
+    browser.assert.cssProperty('.terra-Heading--size-huge', 'font-size', '32px');
+    browser.assert.cssProperty('.terra-Heading--size-large', 'font-size', '24px');
+    browser.assert.cssProperty('.terra-Heading--size-medium', 'font-size', '20px');
+    browser.assert.cssProperty('.terra-Heading--size-small', 'font-size', '18px');
+    browser.assert.cssProperty('.terra-Heading--size-tiny', 'font-size', '16px');
+    browser.assert.cssProperty('.terra-Heading--size-mini', 'font-size', '14px');
   },
 
   'Displays a heading component with set italics': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/heading-tests/italics`);
-    browser.expect.element('.terra-Heading').text.to.equal('Italics');
+    browser.assert.cssProperty('.terra-Heading--italic', 'font-style', 'italic');
   },
 
   'Displays a heading component with set levels': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/heading-tests/levels`);
-    browser.expect.element('.terra-Heading--level-1').text.to.equal('Level 1');
-    browser.expect.element('.terra-Heading--level-2').text.to.equal('Level 2');
-    browser.expect.element('.terra-Heading--level-3').text.to.equal('Level 3');
-    browser.expect.element('.terra-Heading--level-4').text.to.equal('Level 4');
-    browser.expect.element('.terra-Heading--level-5').text.to.equal('Level 5');
-    browser.expect.element('.terra-Heading--level-6').text.to.equal('Level 6');
+    // Browser computes rem units as pixels when retrieved
+    browser.assert.cssProperty('.terra-Heading--level-1', 'font-size', '32px');
+    browser.assert.cssProperty('.terra-Heading--level-2', 'font-size', '24px');
+    browser.assert.cssProperty('.terra-Heading--level-3', 'font-size', '20px');
+    browser.assert.cssProperty('.terra-Heading--level-4', 'font-size', '18px');
+    browser.assert.cssProperty('.terra-Heading--level-5', 'font-size', '16px');
+    browser.assert.cssProperty('.terra-Heading--level-6', 'font-size', '14px');
   },
 
   'Displays a heading component with a set weight': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/heading-tests/weight`);
-    browser.expect.element('.terra-Heading--weight-700').text.to.equal('700 Weight');
-    browser.expect.element('.terra-Heading--weight-400').text.to.equal('400 Weight');
-    browser.expect.element('.terra-Heading--weight-200').text.to.equal('200 Weight');
+    browser.assert.cssProperty('.terra-Heading--weight-700', 'font-weight', 'bold'); // Browser computes 700 as bold
+    browser.assert.cssProperty('.terra-Heading--weight-400', 'font-weight', 'normal'); // Browser computes 400 as normal
+    browser.assert.cssProperty('.terra-Heading--weight-200', 'font-weight', '200');
   },
 
   'Displays a heading component with set hidden visibility': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/heading-tests/visually-hidden`);
-    browser.expect.element('.terra-Heading').text.to.equal('Visually Hidden');
+    browser.assert.cssProperty('.terra-Heading', 'border', '0px none rgb(28, 31, 33)'); // Browser computes 0 as 0px none rgb(28, 31, 33)
+    browser.assert.cssProperty('.terra-Heading', 'clip', 'rect(0px 0px 0px 0px)');
+    browser.assert.cssProperty('.terra-Heading', 'height', '1px');
+    browser.assert.cssProperty('.terra-Heading', 'margin', '-1px');
+    browser.assert.cssProperty('.terra-Heading', 'overflow', 'hidden');
+    browser.assert.cssProperty('.terra-Heading', 'padding', '0px');
+    browser.assert.cssProperty('.terra-Heading', 'position', 'absolute');
+    browser.assert.cssProperty('.terra-Heading', 'width', '1px');
   },
 
   'Displays a heading component with all visual props set': (browser) => {
--- packages/terra-text/tests/nightwatch/text-spec.js
+++ packages/terra-text/tests/nightwatch/text-spec.js
@@ -14,41 +14,50 @@ module.exports = {
   'Displays a default text component': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/text-tests/default`);
     browser.expect.element('.terra-Text').text.to.equal('Default');
+    browser.expect.element('.terra-Text').to.be.an('span');
   },
 
   'Displays a text component with a set color': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/text-tests/color`);
-    browser.expect.element('.terra-Text').text.to.equal('Color');
+    browser.assert.cssProperty('.terra-Text', 'color', 'rgba(48, 79, 254, 1)'); // Browser computes #304FFE to rgba(48, 79, 254, 1)
   },
 
   'Displays a text component with a set font size': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/text-tests/font-size`);
-    browser.expect.element('.terra-Text--fontSize-100').text.to.equal('Font Size 100');
-    browser.expect.element('.terra-Text--fontSize-32').text.to.equal('Font Size 32');
-    browser.expect.element('.terra-Text--fontSize-24').text.to.equal('Font Size 24');
-    browser.expect.element('.terra-Text--fontSize-20').text.to.equal('Font Size 20');
-    browser.expect.element('.terra-Text--fontSize-18').text.to.equal('Font Size 18');
-    browser.expect.element('.terra-Text--fontSize-16').text.to.equal('Font Size 16');
-    browser.expect.element('.terra-Text--fontSize-14').text.to.equal('Font Size 14');
-    browser.expect.element('.terra-Text--fontSize-12').text.to.equal('Font Size 12');
-    browser.expect.element('.terra-Text--fontSize-10').text.to.equal('Font Size 10');
+    // Browser computes rem units as pixels when retrieved
+    browser.assert.cssProperty('.terra-Text--fontSize-100', 'font-size', '100px');
+    browser.assert.cssProperty('.terra-Text--fontSize-32', 'font-size', '32px');
+    browser.assert.cssProperty('.terra-Text--fontSize-24', 'font-size', '24px');
+    browser.assert.cssProperty('.terra-Text--fontSize-20', 'font-size', '20px');
+    browser.assert.cssProperty('.terra-Text--fontSize-18', 'font-size', '18px');
+    browser.assert.cssProperty('.terra-Text--fontSize-16', 'font-size', '16px');
+    browser.assert.cssProperty('.terra-Text--fontSize-14', 'font-size', '14px');
+    browser.assert.cssProperty('.terra-Text--fontSize-12', 'font-size', '12px');
+    browser.assert.cssProperty('.terra-Text--fontSize-10', 'font-size', '10px');
   },
 
   'Displays a text component with set italics': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/text-tests/italics`);
-    browser.expect.element('.terra-Text').text.to.equal('Italics');
+    browser.assert.cssProperty('.terra-Text--italic', 'font-style', 'italic');
   },
 
   'Displays a text component with a set weight': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/text-tests/weight`);
-    browser.expect.element('.terra-Text--weight-700').text.to.equal('700 Weight');
-    browser.expect.element('.terra-Text--weight-400').text.to.equal('400 Weight');
-    browser.expect.element('.terra-Text--weight-200').text.to.equal('200 Weight');
+    browser.assert.cssProperty('.terra-Text--weight-700', 'font-weight', 'bold'); // Browser computes 700 as bold
+    browser.assert.cssProperty('.terra-Text--weight-400', 'font-weight', 'normal'); // Browser computes 400 as normal
+    browser.assert.cssProperty('.terra-Text--weight-200', 'font-weight', '200');
   },
 
   'Displays a text component with set hidden visibility': (browser) => {
     browser.url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/text-tests/visually-hidden`);
-    browser.expect.element('.terra-Text').text.to.equal('Visually Hidden');
+    browser.assert.cssProperty('.terra-Text', 'border', '0px none rgb(28, 31, 33)'); // Browser computes 0 as 0px none rgb(28, 31, 33)
+    browser.assert.cssProperty('.terra-Text', 'clip', 'rect(0px 0px 0px 0px)');
+    browser.assert.cssProperty('.terra-Text', 'height', '1px');
+    browser.assert.cssProperty('.terra-Text', 'margin', '-1px');
+    browser.assert.cssProperty('.terra-Text', 'overflow', 'hidden');
+    browser.assert.cssProperty('.terra-Text', 'padding', '0px');
+    browser.assert.cssProperty('.terra-Text', 'position', 'absolute');
+    browser.assert.cssProperty('.terra-Text', 'width', '1px');
   },
 
   'Displays a text component with all visual props set': (browser) => {
