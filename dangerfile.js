16b70afec3473cdb242f565525d9f19d7a8fa793
Brett Jankord <brett.jankord@cerner.com>
2017-07-06 06:45:30 -0500

    Update dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -1,18 +1,33 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { danger, warn, fail } from 'danger';
+// eslint-disable-next-line import/no-extraneous-dependencies, no-unused-vars
+import { danger, warn, fail, message } from 'danger';
 
-const jsModifiedFiles = danger.git.modified_files.filter(path => path.startsWith('src') && path.endsWith('js'));
 
-const hasAppChanges = jsModifiedFiles.length > 0;
+const hasCHANGELOGChanges = danger.git.modified_files.includes('CHANGELOG.md');
+const modifiedSrcFiles = danger.git.modified_files.filter((filePath) => {
+  const srcFilePattern = /^packages\/([a-z-])*\/src/i;
+  return srcFilePattern.test(filePath);
+})
 
+const hasModifiedSrcFiles = modifiedSrcFiles.length > 0;
+
+// danger.git.modified_files.forEach((path) => {
+//   console.log(path);
+//   // var pattern = /^packages\/([a-z-])*\/src/i
+//   //  var result = pattern.test(path);
+//   //  if (result === true) {
+//   //      console.log(pattern, "pattern did match !");
+//   //  } else {
+//   //      console.log(pattern, " pattern did NOT match");
+//   //  }
+// });
+
+// Fail if there are src code changes without a CHANGELOG
+if (hasModifiedSrcFiles && !hasCHANGELOGChanges) {
+  fail('Please include a CHANGELOG entry with this PR.');
+}
 
 // Warn when there is a big PR
 const bigPRThreshold = 100;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-  warn(':exclamation: Big PR');
-}
-
-// Fail if there are app changes without a CHANGELOG
-if (!danger.git.modified_files.includes('CHANGELOG.md') && hasAppChanges) {
-  fail('Please include a CHANGELOG entry.');
+  warn(':exclamation: Big PR. Consider breaking this into smaller PRs if applicaple');
 }
