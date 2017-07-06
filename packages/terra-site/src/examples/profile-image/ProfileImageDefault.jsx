e39c9e2e50cd2d909af5e537c0932b9226b1deab
Schile,Matt <Matt.Schile@Cerner.com>
2017-07-06 11:01:34 -0500

    Removing variant from site examples.

--- packages/terra-site/src/examples/profile-image/ProfileImageDefault.jsx
+++ packages/terra-site/src/examples/profile-image/ProfileImageDefault.jsx
@@ -5,9 +5,9 @@ import exampleProfileImage from '../../assets/150x150.jpg';
 const ProfileImageDefault = () => (
   <div>
     <h2>Successful Profile Image</h2>
-    <ProfileImage src={exampleProfileImage} variant="rounded" width="75" height="75" />
+    <ProfileImage src={exampleProfileImage} width="75" height="75" />
     <h2>Failed Profile Image</h2>
-    <ProfileImage src="invalid.jpg" variant="rounded" width="75" height="75" />
+    <ProfileImage src="invalid.jpg" width="75" height="75" />
   </div>
 );
 
