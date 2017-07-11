f31223d19716be591b7907e8491f166cc5eecf64
Mike Hemesath <mike.hemesath@gmail.com>
2017-07-11 14:24:12 -0500

    Migrated button to CSS modules (#546)

--- packages/terra-button/tests/nightwatch/IconButton.jsx
+++ packages/terra-button/tests/nightwatch/IconButton.jsx
@@ -5,9 +5,9 @@ import Button from '../../lib/Button';
 const iconStyle = { backgroundColor: 'black', height: '1em', width: '1em' };
 const icon = <span style={iconStyle} className="test"><svg width="1em" height="1em"><rect width="1em" height="1em" /></svg></span>;
 
-const IconDefaultButton = () => <Button text="Button with Icon" icon={icon} />;
-const IconOnlyButton = () => <Button icon={icon} />;
-const IconReversedButton = () => <Button text="Button with Icon and reversed" icon={icon} isReversed />;
+const IconDefaultButton = () => <Button id="iconDefaultButton" text="Button with Icon" icon={icon} />;
+const IconOnlyButton = () => <Button id="iconOnlyButton" icon={icon} />;
+const IconReversedButton = () => <Button id="iconReversedButton" text="Button with Icon and reversed" icon={icon} isReversed />;
 
 export {
   IconDefaultButton,
