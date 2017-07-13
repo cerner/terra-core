0b3e7ca1193b4fda214ec26383b4e8636d72b71e
Brett Jankord <brett.jankord@cerner.com>
2017-07-13 06:48:31 -0500

    Update tests

--- packages/terra-modal/tests/nightwatch/modal-spec.js
+++ packages/terra-modal/tests/nightwatch/modal-spec.js
@@ -13,136 +13,121 @@ module.exports = {
   'isOpen prop': (browser) => {
     browser
       .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/is-open`)
-      .waitForElementPresent('.button-open-modal', 1000)
-      .waitForElementNotPresent('#modal > div[role="document"]', 1000)
+      .assert.elementPresent('.button-open-modal')
+      .assert.elementNotPresent('div[role="document"]')
       .click('.button-open-modal')
-      .waitForElementPresent('#modal > div[role="document"]', 1000)
-      .click('#modal > div:fist-child') // overlay
-      .waitForElementNotPresent('#modal > div[role="document"]', 1000);
+      .assert.elementPresent('div[role="document"]')
+      .click('[class*="ModalOverlay"]') // overlay
+      .assert.elementNotPresent('div[role="document"]');
   },
-  //
-  // 'isFullscreen={true}': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/is-fullscreen`)
-  //     .waitForElementPresent('.terra-Modal.terra-Modal--fullscreen', 1000);
-  // },
-  //
-  // 'isFullscreen={false}': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/disable-close-on-esc`)
-  //     .waitForElementNotPresent('.terra-Modal.terra-Modal--fullscreen', 1000);
-  // },
-  //
-  // 'closeOnOutsideClick={true}': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/is-fullscreen`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .click('.terra-Modal-overlay')
-  //     .waitForElementNotPresent('.terra-Modal', 1000);
-  // },
-  //
-  // 'closeOnOutsideClick={false}': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/disable-close-on-outside-click`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .click('.terra-Modal-overlay')
-  //     .waitForElementPresent('.terra-Modal', 1000);
-  // },
-  //
-  // 'closeOnEsc={false}': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/disable-close-on-esc`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .setValue('body', [browser.Keys.ESC])
-  //     .waitForElementPresent('.terra-Modal', 1000);
-  // },
-  //
-  // 'closeOnEsc={true}': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/is-fullscreen`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .setValue('body', [browser.Keys.ESC])
-  //     .waitForElementPresent('.terra-Modal', 1000);
-  // },
-  //
-  // 'set ariaLabel': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/is-fullscreen`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .expect.element('.terra-Modal').to.have.attribute('aria-label').which.contains('Terra Modal');
-  // },
-  //
-  // 'role default value': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/is-fullscreen`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .expect.element('.terra-Modal').to.have.attribute('role').which.contains('document');
-  // },
-  //
-  // 'role is overridable': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/override-role`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .expect.element('.terra-Modal').to.have.attribute('role').which.contains('button');
-  // },
-  //
-  // 'append css class to modal': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/append-class`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .expect.element('.terra-Modal').to.have.attribute('class').which.contains('terra-Modal modal-custom-class');
-  // },
-  //
-  // 'append css class to overlay': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/append-class`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .expect.element('.terra-Modal-overlay').to.have.attribute('class').which.contains('terra-Modal-overlay overlay-custom-class');
-  // },
-  //
-  // 'content overflow example': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/content-overflow`)
-  //     .waitForElementPresent('.terra-Modal', 1000);
-  // },
-  //
-  // 'modal is focusable': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/no-focusable-content`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .expect.element('.terra-Modal').to.have.attribute('tabindex').which.equals('0');
-  // },
-  //
-  // 'background does not scroll when modal is open': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/content-overflow`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .expect.element('html').to.have.attribute('style').which.equals('overflow: hidden;');
-  //
-  //   browser
-  //     .click('.terra-Modal-overlay')
-  //     .waitForElementNotPresent('.terra-Modal', 1000)
-  //     .expect.element('html').to.have.attribute('style').which.equals('');
-  // },
-  //
-  // 'isScrollable default value': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/content-overflow`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .expect.element('.terra-Modal').to.have.attribute('class').which.equals('terra-Modal');
-  // },
-  //
-  // 'isScrollable={false}': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/scrollable-false`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .expect.element('.terra-Modal').to.have.attribute('class').which.equals('terra-Modal');
-  // },
-  //
-  // 'isScrollable={true}': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/scrollable-true`)
-  //     .waitForElementPresent('.terra-Modal', 1000)
-  //     .expect.element('.terra-Modal').to.have.attribute('class').which.equals('terra-Modal terra-Modal--scrollable');
-  // },
+
+  'isFullscreen={true}': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/is-fullscreen`)
+      .assert.elementPresent('div[role="document"]');
+  },
+
+  'isFullscreen={false}': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/disable-close-on-esc`)
+      .assert.elementPresent('div[role="document"]');
+  },
+
+  'closeOnOutsideClick={true}': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/is-fullscreen`)
+      .assert.elementPresent('div[role="document"]')
+      .click('[class*="ModalOverlay"]') // overlay
+      .assert.elementNotPresent('div[role="document"]');
+  },
+
+  'closeOnOutsideClick={false}': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/disable-close-on-outside-click`)
+      .assert.elementPresent('div[role="document"]')
+      .click('[class*="ModalOverlay"]') // overlay
+      .assert.elementPresent('div[role="document"]');
+  },
+
+  'closeOnEsc={false}': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/disable-close-on-esc`)
+      .assert.elementPresent('div[role="document"]')
+      .setValue('body', [browser.Keys.ESC])
+      .assert.elementPresent('div[role="document"]');
+  },
+
+  'closeOnEsc={true}': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/is-fullscreen`)
+      .assert.elementPresent('div[role="document"]')
+      .setValue('body', [browser.Keys.ESC])
+      .assert.elementPresent('div[role="document"]');
+  },
+
+  'set ariaLabel': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/is-fullscreen`)
+      .assert.elementPresent('div[aria-label="Terra Modal"]');
+  },
+
+  'role default value': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/is-fullscreen`)
+      .assert.elementPresent('div[role="document"]');
+  },
+
+  'role is overridable': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/override-role`)
+      .assert.elementPresent('div[role="button"]');
+  },
+
+  'append css class to modal': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/append-class`)
+      .expect.element('div[role="document"]').to.have.attribute('class').which.contains('modal-custom-class');
+  },
+
+  'append css class to overlay': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/append-class`)
+      .expect.element('[class*="ModalOverlay"]').to.have.attribute('class').which.contains('overlay-custom-class');
+  },
+
+  'content overflow example': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/content-overflow`);
+  },
+
+  'modal is focusable': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/no-focusable-content`)
+      .expect.element('div[role="document"]').to.have.attribute('tabindex').which.equals('0');
+  },
+
+  'background does not scroll when modal is open': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/content-overflow`)
+      .expect.element('html').to.have.attribute('style').which.equals('overflow: hidden;');
+
+    browser
+      .click('[class*="ModalOverlay"]')
+      .expect.element('html').to.have.attribute('style').which.equals('');
+  },
+
+  'isScrollable default value': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/content-overflow`);
+  },
+
+  'isScrollable={false}': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/scrollable-false`);
+  },
+
+  'isScrollable={true}': (browser) => {
+    browser
+      .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/modal-tests/scrollable-true`);
+  },
 };
