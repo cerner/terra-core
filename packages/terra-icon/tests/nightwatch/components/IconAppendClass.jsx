c0821aebb087ef84e54d566f46928da087989bb3
Matt Butler <Matt-Butler@users.noreply.github.com>
2017-07-24 13:20:31 -0500

    Migrated Icon to CSS Modules (#598)
    * migrate to css-modules
    
    * make animations global
    
    * delete snapshots
    
    * add snapshots
    
    * update overlay snapshot
    
    * added comment why we need dir
    
    * added comment why we need dir

--- packages/terra-icon/tests/nightwatch/components/IconAppendClass.jsx
+++ packages/terra-icon/tests/nightwatch/components/IconAppendClass.jsx
@@ -4,10 +4,10 @@ import IconAllergy from '../../../lib/icon/IconAllergy';
 const IconAppendClass = () => (
   <div>
     <h3>No classNames added</h3>
-    <IconAllergy />
+    <IconAllergy id="append-class-none" />
     <h3>{'Added className "custom-class"'}</h3>
-    <p>{'className "terra-Icon" should still persist'}</p>
-    <IconAllergy className="custom-class" />
+    <p>{'className "icon-[hash]" should persist'}</p>
+    <IconAllergy id="append-class-custom" className="custom-class" />
   </div>
 );
 
