12878e4c5509984eb18bdabadcec620015fb0042
Dave Kasper <dave.m.kasper@gmail.com>
2017-07-26 07:22:13 -0500

    terra-popup (#606)
    * Squashed terra-popup branch for review and pr.
    
    * Adjusted padding of close button.
    
    * Night watch changes.
    
    * Updated documentation.
    
    * Code review comments.
    
    * Updated test comments.
    
    * Additional documentation.
    
    * Exported default object for utils.
    
    * Fix linter.
    
    * Update bounded width example.
    
    * Add height and width files. Integrate 1.1.0 released components.
    
    * Fix linter errors and update documentation.
    
    * Update tests.
    
    * Correct z-index issue.
    
    * Remove custom props.
    
    * Set right margin to 0.
    
    * Fix linter unused vars.
    
    * Updated to use an Opts object.
    
    * Correct documentation typo.
    
    * Linter errors.

---
+++ packages/terra-popup/tests/nightwatch/components/ClassNamePopup.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Popup from '../../../lib/Popup';
+import './ClassNamePopup.scss';
+
+class ClassNamePopup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.setButtonNode = this.setButtonNode.bind(this);
+    this.getButtonNode = this.getButtonNode.bind(this);
+    this.state = { open: true };
+  }
+
+  componentDidMount() {
+    this.forceUpdate();
+  }
+
+  setButtonNode(node) {
+    this.buttonNode = node;
+  }
+
+  getButtonNode() {
+    return this.buttonNode;
+  }
+
+  handleButtonClick() {
+    this.setState({ open: true });
+  }
+
+  handleRequestClose() {
+    this.setState({ open: false });
+  }
+
+  render() {
+    return (
+      <div>
+        <Popup
+          classNameArrow="terra-TestClass-arrow"
+          classNameContent="terra-TestClass-content"
+          classNameOverlay="terra-TestClass-overlay"
+          contentAttachment="top center"
+          isArrowDisplayed
+          isOpen={this.state.open}
+          onRequestClose={this.handleRequestClose}
+          targetRef={this.getButtonNode}
+        >
+          <p style={{ padding: '5px' }}>The arrow, content, and overlay have classes.</p>
+        </Popup>
+        <button id="class-name-button" onClick={this.handleButtonClick} ref={this.setButtonNode}>
+          Class Name Popup
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ClassNamePopup;
--- packages/terra-site/src/Index.jsx
+++ packages/terra-site/src/Index.jsx
@@ -24,6 +24,7 @@ import ListExamples from './examples/list/Index';
 import ModalExamples from './examples/modal/Index';
 import ModalManagerExamples from './examples/modal-manager/Index';
 import OverlayExamples from './examples/overlay/Index';
+import PopupExamples from './examples/popup/Index';
 import ProfileImageExamples from './examples/profile-image/Index';
 import ProgressBarExamples from './examples/progress-bar/Index';
 import ResponsiveElementExamples from './examples/responsive-element/Index';
@@ -63,6 +64,7 @@ import ModalTestRoutes from 'terra-modal/tests/nightwatch/ModalTestRoutes';
 import ModalManagerTestRoutes from 'terra-modal-manager/tests/nightwatch/ModalManagerTestRoutes';
 import OverlayTestRoutes from 'terra-overlay/tests/nightwatch/Overlay/OverlayTestRoutes';
 import OverlayContainerTestRoutes from 'terra-overlay/tests/nightwatch/OverlayContainer/OverlayContainerTestRoutes';
+import PopupTestRoutes from 'terra-popup/tests/nightwatch/PopupTestRoutes';
 import ProgressBarTestRoutes from 'terra-progress-bar/tests/nightwatch/ProgressBarTestRoutes';
 import ResponsiveElementTestRoutes from 'terra-responsive-element/tests/nightwatch/ResponsiveElementTestRoutes';
 import SearchFieldTestRoutes from 'terra-search-field/tests/nightwatch/SearchFieldTestRoutes';
@@ -101,6 +103,7 @@ ReactDOM.render((
       <Route path="modal" component={ModalExamples} />
       <Route path="modal-manager" component={ModalManagerExamples} />
       <Route path="overlay" component={OverlayExamples} />
+      <Route path="popup" component={PopupExamples} />
       <Route path="profile-image" component={ProfileImageExamples} />
       <Route path="progress-bar" component={ProgressBarExamples} />
       <Route path="responsive-element" component={ResponsiveElementExamples} />
@@ -139,8 +142,8 @@ ReactDOM.render((
     {ModalManagerTestRoutes}
     {ProgressBarTestRoutes}
     {GridTestRoutes}
-    {IconTestRoutes}
     {SearchFieldTestRoutes}
+    {PopupTestRoutes}
     {FormTestRoutes}
     {OverlayTestRoutes}
     {LoadingOverlayTestRoutes}
