689682e73777a9f75c8f5e9aa4a72dfe40608fa6
Jaime Mackey <jaime.mackey@cerner.com>
2017-07-27 09:27:39 -0500

    CSS updates

--- packages/terra-site/src/examples/alert/CustomExample.jsx
+++ packages/terra-site/src/examples/alert/CustomExample.jsx
@@ -21,7 +21,7 @@ const CustomExample = () => (
     </div>
     <br />
     <Alert type={Alert.Opts.Types.CUSTOM} title="Help!" customStatusColor="orange" customIcon={<IconHelp />} >
-      This is a <u>custom</u> alert
+      <span>This is a <i>custom</i> alert</span>
     </Alert>
   </div>
 );
