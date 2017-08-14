1c46f4ca1bc68c795b9297238048c2f4597b4d82
Joseph Fortebuono <Joseph.Fortebuono@cerner.com>
2017-08-14 11:42:48 -0400

    Fixed Tests

--- packages/terra-card/tests/jest/Card.test.jsx
+++ packages/terra-card/tests/jest/Card.test.jsx
@@ -13,7 +13,7 @@ describe('Card', () => {
   // Prop Tests
   it('should use the default value when no value is given', () => {
     const wrapper = shallow(defaultRender);
-    expect(wrapper.find('.card').text()).toEqual('defualt');
+    expect(wrapper.find('.card').text()).toEqual(' ');
   });
 
   // Structure Tests
