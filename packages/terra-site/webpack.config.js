fd95bd20e50a855ed1f40c06e7f48877a6748954
Jake LaCombe <jake.lacombe2@gmail.com>
2017-08-29 13:20:24 -0500

    Add Plugin for Aggregating Themable Variables

--- packages/terra-site/webpack.config.js
+++ packages/terra-site/webpack.config.js
@@ -11,6 +11,7 @@ const I18nAggregatorPlugin = require('terra-i18n-plugin');
 const i18nSupportedLocales = require('terra-i18n/lib/i18nSupportedLocales');
 const CustomProperties = require('postcss-custom-properties');
 const rtl = require('postcss-rtl');
+const ThemingPlugin = require('./theming-plugin');
 
 module.exports = {
   entry: {
@@ -53,6 +54,7 @@ module.exports = {
                   ],
                 }),
                 CustomProperties({ preserve: true }),
+                ThemingPlugin,
                 rtl(),
               ];
             },
