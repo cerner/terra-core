205672d5bd4d59163e3899587fee1cfded292e07
Brett Jankord <brett.jankord@cerner.com>
2017-09-14 18:33:21 -0500

    Update theming-plugin to save JSON in pretty-print format

--- packages/terra-site/theming-plugin.js
+++ packages/terra-site/theming-plugin.js
@@ -33,7 +33,6 @@ module.exports = postcss.plugin('theming-plugin', () => {
         variables[component][matches[1]] = matches[2];
       }
     });
-
-    return fs.writeFileSync(path.resolve(__dirname, 'themeable-variables.json'), JSON.stringify(sortHash(variables)), 'utf8');
+    return fs.writeFileSync(path.resolve(__dirname, 'themeable-variables.json'), JSON.stringify(sortHash(variables), 'utf8', 2));
   };
 });
