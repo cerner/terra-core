f88e530c252500b4ab6206750bebb72724b7ca17
Victor Nava <victor.nava@cerner.com>
2017-09-14 17:18:56 -0500

    Fixing nightwatch test, changing mocked line contents

--- packages/terra-signature/tests/nightwatch/signature-spec.js
+++ packages/terra-signature/tests/nightwatch/signature-spec.js
@@ -1,30 +1,32 @@
 /* eslint-disable no-unused-expressions */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
 // eslint-disable-next-line import/no-extraneous-dependencies
 const resizeTo = require('terra-toolkit/lib/nightwatch/responsive-helpers').resizeTo;
 
 module.exports = resizeTo(['tiny', 'small', 'medium', 'large', 'huge', 'enormous'], {
-
   'Displays a default signature': (browser) => {
     browser.url(`${browser.launchUrl}/#/tests/signature-tests/default`);
     browser.expect.element('canvas[class*="signature"]').to.be.present;
   },
 
   'Draw a line signature and verify canvas': (browser) => {
-    const mockedLine = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGIAAABiCAYAAACrpQYOAAACRElEQVR4Xu3awU0DMRBG4UkXlEAbtMEdCqAQCoA7bdAGJVAGClFEgBBsr2f3n5mXs7W25ttn5bA74ycxgZ3EKTiEASHyEgABhMgERI5BEUCITEDkGBQBhMgERI7RU8SDmb2Y2bvI2VMdoxXizsyezOzNzG7AmP8OtEJcmdmrmV2DMR9h/8RWiP1aMHwMPp/aAwGGEAQYThi9RRyPwTU1GWQUgjKEIMCYiLGkCK4pMQjKmAAyowjKEIOgjAUgM4ugDDEIyhgA8SiCMsQgKKMDxLMIyhCDoIwGkDWKoAwxCMq4ALJmEZQhBkEZZ0C2KIIyxCAo4wRkyyIoQwyCMgY+p2n4Rzy8pPQHCQpX06lcWQw1iLLXlCJESQxViHIYyhClMNQhymBEgCiBEQUiPUYkiNQY0SDSYkSESIkRFSIdRmSIVBjRIdJgZIBIgZEFIjxGJojQGNkgwmJkhAiJkRUiHEZmiFAY2SHCYFSACIFRBUIeoxKENEY1CFmMihCSGFUh5DAqQ0hhVIeQwQDi8C365l+hA3GA2BwDiC+ITTGA+A6xGQYQvyE2wQDiPMTqGED8DbEqBhCXIVbDAOJ/iFUwgGiDcMcAoh3CFQOIPgg3DCD6IVwwgBiDmI4BxDjEVAwglkFMwwBiOcQUDCDmQCzGAGIexCIMIOZCDGMAMR9iCAMIH4huDCD8IH5i3JvZ81/bAeELccS4NbPHS1sB4Q/RtAMQTWPyXwSE/4ybdgCiaUz+i4Dwn3HTDkA0jcl/ERD+M27a4QMUUPJjEwy/jgAAAABJRU5ErkJggg==';
+    const mockedLine = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGIAAABiCAYAAACrpQYOAAACBUlEQVR4Xu3a4W7CMAxF4fL+D71pQtNAMNKktnNtH35HSuSPkwrR28FHYgI3iVNwiAMIkS8BEECITEDkGBQBhMgERI5BEUCITEDkGBSREOLrOPjd4eU2WwQYThKzED/HAMMBYwUCDCEIMIwxVov4PQbXlBHIVQjKEIIAwwDDogiuKTEIyrgAYlkEZYhBUMYCiEcRlCEGQRkTIJ5FUIYYBGWcAIkogjLEICjjA0hkEZQhBkEZb0B2FEEZYhCU8QCyswjKEIOgDLH3lFr/7apwNT0+utpiqEG0vaYUIVpiqEK0w1CGaIWhDtEGIwNEC4wsEOUxMkGUxsgGURYjI0RJjKwQ5TAyQ5TCyA5RBqMCRAmMKhDpMSpBpMaoBpEWoyJESoyqEOkwKkOkwqgOkQajA0QKjC4Q8hidIKQxukHIYnSEkMToCiGH0RlCCqM7hAwGEPd30be/hQ7EHWI7BhB/EFsxgHiG2IYBxCvEFgwg3kOEYwDxP0QoBhCfIcIwgBhDhGAAcQ7CHQOI8xCuGEDMQbhhADEP4YIBxBqEOQYQ6xCmGEBcgzDDAOI6hAkGEDYQlzGAsIO4hAGELcQyBhD2EEsYQPhATGMA4QcxhQGEL8RpDCD8IU5hABEDMdwFiOGIYhYAETPn4S5ADEcUswCImDkPdwFiOKKYBUDEzHm4CxDDEcUs+AZPWVBjp60diQAAAABJRU5ErkJggg==';
 
     browser.url(`${browser.launchUrl}/#/tests/signature-tests/default`);
     browser.expect.element('canvas#drawline').to.be.present;
 
     browser
-      .moveToElement('canvas#drawline', 10, 10) // Move to offset position of 10(x) 10(y)
+      .moveToElement('canvas#drawline', 10, 10)
       .mouseButtonDown(0)
-      .moveToElement('canvas#drawline', 90, 90) // Move to offset position of 10(x) 60(y)
+      .moveToElement('canvas#drawline', 90, 90)
       .mouseButtonUp(0);
 
     browser
-      .execute(() => document.getElementById('drawline').toDataURL(), [], (result) => {
+      .execute(function () {
+        return document.getElementById('drawline').toDataURL();
+      }, [], function (result) {
         browser.assert.equal(result.value, mockedLine);
       });
   },
-
 });
