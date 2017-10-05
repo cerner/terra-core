c700c5781d96317c510459040641a43159dc33ad
Michael Hemesath <mike.hemesath@cerner.com>
2017-10-05 08:04:35 -0500

    Moved postcss config to its own file. Updated webpack dependencies

---
+++ packages/terra-site/postcss.config.js
@@ -0,0 +1,23 @@
+const Autoprefixer = require('autoprefixer');
+const CustomProperties = require('postcss-custom-properties');
+const rtl = require('postcss-rtl');
+const ThemingPlugin = require('./theming-plugin');
+
+module.exports = {
+  plugins() {
+    return [
+      Autoprefixer({
+        browsers: [
+          'ie >= 10',
+          'last 2 versions',
+          'last 2 android versions',
+          'last 2 and_chr versions',
+          'iOS >= 8',
+        ],
+      }),
+      CustomProperties({ preserve: true, warnings: false }),
+      ThemingPlugin,
+      rtl(),
+    ];
+  },
+};
