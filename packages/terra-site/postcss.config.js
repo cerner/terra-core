2153e1f1260beb53868e83ec7dcd7c79a6321df5
Michael Hemesath <mike.hemesath@cerner.com>
2017-10-09 10:33:44 -0500

    update supported browser config

--- packages/terra-site/postcss.config.js
+++ packages/terra-site/postcss.config.js
@@ -8,13 +8,7 @@ module.exports = {
   plugins() {
     return [
       Autoprefixer({
-        browsers: [
-          'ie >= 10',
-          'last 2 versions',
-          'last 2 android versions',
-          'last 2 and_chr versions',
-          'iOS >= 10',
-        ],
+        browsers: ["last 2 versions", "iOS >= 10"],
       }),
       CustomProperties({ preserve: true, warnings: false }),
       ThemingPlugin,
