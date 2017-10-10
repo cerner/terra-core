6a601154b3f6d97d7546cf09f779a9cda6993b09
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2017-10-10 14:38:29 -0500

    Add SelectableTableRows for Managed Selectable Tables (#863)

--- packages/terra-table/tests/nightwatch/single-select-table/single-select-table-spec.js
+++ packages/terra-table/tests/nightwatch/single-select-table/single-select-table-spec.js
@@ -83,14 +83,6 @@ module.exports = resizeTo(['tiny', 'small', 'medium', 'large', 'huge', 'enormous
     browser.expect.element('[class*="row"]:nth-child(3)').to.have.attribute('aria-selected').which.contains('true');
   },
 
-  'Display a selectable table with a subheader': (browser) => {
-    browser.url(`${browser.launchUrl}/#/tests/single-select-table-tests/subheaders`);
-    browser.expect.element('tbody tr:nth-child(1)').to.have.attribute('data-terra-table-subheader-row');
-    browser.assert.containsText('tbody tr:nth-child(1) td:nth-child(1)', 'Single');
-    browser.expect.element('tbody tr:nth-child(4)').to.have.attribute('data-terra-table-subheader-row');
-    browser.assert.containsText('tbody tr:nth-child(4) td:nth-child(1)', 'Married');
-  },
-
   'Triggers onChange for selectable table upon clicking a row': (browser) => {
     browser.url(`${browser.launchUrl}/#/tests/single-select-table-tests/onchange`);
 
@@ -111,27 +103,6 @@ module.exports = resizeTo(['tiny', 'small', 'medium', 'large', 'huge', 'enormous
     browser.expect.element('[class*="row"]:nth-child(2)').to.have.attribute('class').which.not.contains('is-selected');
     browser.expect.element('[class*="row"]:nth-child(3)').to.have.attribute('class').which.contains('is-selected');
     browser.assert.containsText('#selected-index', '2');
-  },
-
-  'Displays a selectable table with only one row': (browser) => {
-    browser.url(`${browser.launchUrl}/#/tests/single-select-table-tests/single-row`);
-    browser.expect.element('[class*="row"]:nth-child(1)').to.be.present;
-    browser.expect.element('[class*="row"]:nth-child(2)').to.not.be.present;
-  },
-
-  'Displays a selectable table with no rows': (browser) => {
-    browser.url(`${browser.launchUrl}/#/tests/single-select-table-tests/no-rows`);
-    browser.expect.element('[class*="row"]:nth-child(1)').to.not.be.present;
-  },
-
-  'Maintains custom classNames when provided': (browser) => {
-    browser.url(`${browser.launchUrl}/#/tests/single-select-table-tests/subheaders`);
-    browser.expect.element('.Table-Custom').to.be.present;
-    browser.expect.element('.Header-Custom').to.be.present;
-    browser.expect.element('.HeaderCell-Custom').to.be.present;
-    browser.expect.element('.Cell-Custom').to.be.present;
-    browser.expect.element('.SingleSelectableRows-Custom').to.be.present;
-    browser.expect.element('.Subheader-Custom').to.be.present;
   },
 
   'Displays a selectable table with a preselected row': (browser) => {
