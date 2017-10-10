5e3d8f5b899dd44e291a630c354074c4c4c85a76
Sai Kaushik Mallela <saikaushik.mallela@gmail.com>
2017-10-10 10:15:00 -0500

    updated content text

--- packages/terra-site/src/examples/content-container/ContentContainerStandard.jsx
+++ packages/terra-site/src/examples/content-container/ContentContainerStandard.jsx
@@ -3,12 +3,42 @@ import React from 'react';
 import ContentContainer from 'terra-content-container';
 import Heading from 'terra-heading';
 
-const display1 = <p key="1">display1display1display1display1display1display1display1display1display1display1display1display1display1display1</p>;
-const display2 = <p key="2">display2display2display2display2display2display2display2display2display2display2display2display2display2display2</p>;
-const display3 = <p key="3">display 3</p>;
-const display4 = <p key="4">display 4</p>;
-const display5 = <p key="5">display 5</p>;
-const display6 = <p key="6">display 6</p>;
+const display1 = (<p key="1">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam lectus quis finibus feugiat. Nullam ut sagittis purus.
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut placerat fringilla aliquam.
+Phasellus in eleifend odio. Nunc maximus sem vel odio lobortis, in accumsan arcu dapibus. Nunc consectetur, nunc in mattis
+pharetra, augue felis aliquet nunc, non facilisis est velit vel elit.
+                  </p>);
+const display2 = (<p key="2">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam lectus quis finibus feugiat. Nullam ut sagittis purus.
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut placerat fringilla aliquam.
+Phasellus in eleifend odio. Nunc maximus sem vel odio lobortis, in accumsan arcu dapibus. Nunc consectetur, nunc in mattis
+pharetra, augue felis aliquet nunc, non facilisis est velit vel elit.
+                 </p>);
+const display3 = (<p key="3">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam lectus quis finibus feugiat. Nullam ut sagittis purus.
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut placerat fringilla aliquam.
+Phasellus in eleifend odio. Nunc maximus sem vel odio lobortis, in accumsan arcu dapibus. Nunc consectetur, nunc in mattis
+pharetra, augue felis aliquet nunc, non facilisis est velit vel elit.
+                 </p>);
+const display4 = (<p key="4">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam lectus quis finibus feugiat. Nullam ut sagittis purus.
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut placerat fringilla aliquam.
+Phasellus in eleifend odio. Nunc maximus sem vel odio lobortis, in accumsan arcu dapibus. Nunc consectetur, nunc in mattis
+pharetra, augue felis aliquet nunc, non facilisis est velit vel elit.
+                 </p>);
+const display5 = (<p key="5">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam lectus quis finibus feugiat. Nullam ut sagittis purus.
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut placerat fringilla aliquam.
+Phasellus in eleifend odio. Nunc maximus sem vel odio lobortis, in accumsan arcu dapibus. Nunc consectetur, nunc in mattis
+pharetra, augue felis aliquet nunc, non facilisis est velit vel elit.
+                 </p>);
+const display6 = (<p key="6">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam lectus quis finibus feugiat. Nullam ut sagittis purus.
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut placerat fringilla aliquam.
+Phasellus in eleifend odio. Nunc maximus sem vel odio lobortis, in accumsan arcu dapibus. Nunc consectetur, nunc in mattis
+pharetra, augue felis aliquet nunc, non facilisis est velit vel elit.
+                 </p>);
 
 const displays1 = [display1, display2, display3, display4, display5, display6];
 const displays2 = displays1;
