bda9be81262f37950837dc2617d178de4a405ff0
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-10-11 09:03:32 -0500

    PR changes.

--- packages/terra-site/src/App.jsx
+++ packages/terra-site/src/App.jsx
@@ -22,7 +22,7 @@ const propTypes = {
 const locale = document.getElementsByTagName('html')[0].getAttribute('lang');
 
 const themes = {
-  'Defualt Theme': '',
+  'Default Theme': '',
   'Consumer Theme': 'cerner-consumer-theme',
   'Mock Theme': 'cerner-mock-theme',
 };
@@ -32,7 +32,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       isOpen: window.innerWidth >= 768,
-      isLtr: true,
+      dir: 'ltr',
       locale,
       theme: 'Default Theme',
     };
@@ -45,7 +45,7 @@ class App extends React.Component {
 
   handleBidiChange(e) {
     document.getElementsByTagName('html')[0].setAttribute('dir', e.currentTarget.id);
-    this.setState({ isLtr: e.currentTarget.id === 'ltr' });
+    this.setState({ dir: e.currentTarget.id });
   }
 
   handleLocaleChange(e) {
@@ -77,8 +77,8 @@ class App extends React.Component {
 
     const bidiContent = (
       <CollapsibleMenuView.ItemGroup key="site-bidi" isSelectable dir="ltr" size="medium">
-        <CollapsibleMenuView.Item id="ltr" text="ltr" key="ltr" isSelected={this.state.isLtr} onClick={this.handleBidiChange} />
-        <CollapsibleMenuView.Item id="rtl" text="rtl" key="rtl" isSelected={!this.state.isLtr} onClick={this.handleBidiChange} />
+        <CollapsibleMenuView.Item id="ltr" text="ltr" key="ltr" isSelected={this.state.dir === 'ltr'} onClick={this.handleBidiChange} />
+        <CollapsibleMenuView.Item id="rtl" text="rtl" key="rtl" isSelected={this.state.dir === 'rtl'} onClick={this.handleBidiChange} />
       </CollapsibleMenuView.ItemGroup>
     );
 
@@ -95,7 +95,7 @@ class App extends React.Component {
           <CollapsibleMenuView.Item id="de" text="de" key="de" onClick={this.handleLocaleChange} />,
           <CollapsibleMenuView.Item id="es" text="es" key="es" onClick={this.handleLocaleChange} />,
           <CollapsibleMenuView.Item id="fr" text="fr" key="fr" onClick={this.handleLocaleChange} />,
-          <CollapsibleMenuView.Item id="pt" text="pt" key="pt"onClick={this.handleLocaleChange} />,
+          <CollapsibleMenuView.Item id="pt" text="pt" key="pt" onClick={this.handleLocaleChange} />,
           <CollapsibleMenuView.Item id="fi-FI" text="fi-FI" key="fi-FI" onClick={this.handleLocaleChange} />,
         ]}
       />
