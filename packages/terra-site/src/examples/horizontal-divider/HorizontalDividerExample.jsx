11d00e7eaa8c624c08fb92d48f3aaefd82eeb319
Sai Kaushik Mallela <saikaushik.mallela@gmail.com>
2017-10-20 09:03:17 -0500

    created site examples

---
+++ packages/terra-site/src/examples/horizontal-divider/HorizontalDividerExample.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import HorizontalDivider from 'terra-horizontal-divider';
+
+const HorizontalDividerExample = () => <HorizontalDivider />;
+
+export default HorizontalDividerExample;
