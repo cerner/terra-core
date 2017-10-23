138eca92c5449f4b6f7e3a6e2a7efb3e28caf5dd
Schuster,Kevin <Kevin.Schuster@Cerner.com>
2017-10-23 13:04:57 -0500

    adding FlexItem

--- packages/terra-flex-container/src/FlexContainer.jsx
+++ packages/terra-flex-container/src/FlexContainer.jsx
@@ -3,18 +3,54 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import 'terra-base/lib/baseStyles';
 import styles from './FlexContainer.scss';
+import FlexItem from './FlexItem';
 
 const cx = classNames.bind(styles);
 
 const propTypes = {
   /**
-   * The children list items passed to the component.
+   * The children flex items passed to the component.
    */
   children: PropTypes.node,
+  /**
+   * This defines a flex container; inline or block depending on the given value.
+   * It enables a flex context for all its direct children.  Default is flex.
+   */
+  display: PropTypes.string,
+  /**
+   * This defines the main-axis, thus defining the direction flex items are
+   * placed in the flex container. Think of flex items as primarily laying out
+   * either in horizontal rows or vertical columns.
+   */
+  direction: PropTypes.string,
+  /**
+   *  This can be set to allow the items to wrap as needed with this property.
+   */
+  wrap: PropTypes.string,
+  /**
+   * This defines the alignment along the main axis.
+   */
+  justifyContent: PropTypes.string,
+  /**
+   * This defines the default behavior for how flex items are laid out along the
+   * cross axis on the current line
+   */
+  alignItems: PropTypes.string,
+  /**
+   * This aligns a flex container's lines within when there is extra space in
+   * the cross-axis.
+   */
+  alignContent: PropTypes.string,
 };
 
 const defaultProps = {
   children: [],
+  direction: 'row',
+  display: 'flex',
+  direction: 'nowrap',
+  justifyContent: 'flex-start',
+  alignItems: 'stretch',
+  alignContent: 'stretch',
 };
 
 const FlexContainer = ({
@@ -32,3 +68,9 @@ const FlexContainer = ({
     </div>
   );
 };
+
+FlexContainer.propTypes = propTypes;
+FlexContainer.defaultProps = defaultProps;
+FlexContainer.Item = FlexItem;
+
+export default FlexContainer;
---
+++ packages/terra-flex-container/src/FlexItem.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import 'terra-base/lib/baseStyles';
+import styles from './FlexItem.scss';
+
+const cx = classNames.bind(styles);
+
+const propTypes = {
+  /**
+   * The content element to be placed inside the flex item for display.
+   */
+  content: PropTypes.element,
+  /**
+   * Controls the order in which the item will appear in the flex container.
+   */
+  order: PropTypes.number,
+  /**
+   * This defines the ability for a flex item to grow if necessary. It accepts
+   * a unitless value that serves as a proportion. It dictates what amount of
+   * the available space inside the flex container the item should take up.
+   */
+  grow: PropTypes.number,
+  /**
+   * This defines the ability for a flex item to shrink if necessary.
+   */
+  shrink: PropTypes.number,
+  /**
+   * This defines the default size of an element before the remaining space is
+   * distributed.
+   */
+  basis: PropTypes.string,
+  /**
+   * This allows the default alignment (or the one specified by align-items) to
+   * be overridden for individual flex items
+   */
+  alignSelf: PropTypes.string,
+};
+
+const defaultProps = {
+  content: undefined,
+  order: 0,
+  grow: 0,
+  shrink: 1,
+  basis: 'auto',
+};
+
+const FlexItem = ({
+    ...customProps
+  }) => {
+  const flexItemClassNames = cx([
+    'flex-item',
+    customProps.className,
+  ]);
+
+  return (
+    <div {...customProps} className={flexItemClassNames}>
+      {content}
+    </div>
+  );
+};
+
+FlexContainer.propTypes = propTypes;
+FlexContainer.defaultProps = defaultProps;
+FlexContainer.Item = FlexItem;
+
+export default FlexContainer;
