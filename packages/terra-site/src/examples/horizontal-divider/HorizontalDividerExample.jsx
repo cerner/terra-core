014293dc4803d517dd3754d3bf8a17dccb7bf76c
Sai Kaushik Mallela <saikaushik.mallela@gmail.com>
2017-10-30 09:04:15 -0500

    deleted horizontal-divider site example and retained divider site example

--- packages/terra-site/src/examples/horizontal-divider/HorizontalDividerExample.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
-import HorizontalDivider from 'terra-horizontal-divider';
-
-const HorizontalDividerExample = () => <HorizontalDivider />;
-
-export default HorizontalDividerExample;
