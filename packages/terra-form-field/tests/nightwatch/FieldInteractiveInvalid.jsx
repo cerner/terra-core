e958e670ffa236397dc69b1312a0fe3a6e851953
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-10-31 07:47:22 -0500

    Oops. Clean up.

--- packages/terra-form-field/tests/nightwatch/FieldInteractiveInvalid.jsx
+++ packages/terra-form-field/tests/nightwatch/FieldInteractiveInvalid.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Base from 'terra-base';
-import Field from '../../src/Field';
+import Field from '../../lib/Field';
 
 const locale = document.getElementsByTagName('html')[0].getAttribute('lang');
 
