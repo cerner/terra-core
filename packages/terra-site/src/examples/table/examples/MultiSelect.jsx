c181f6b24934fb113b10edd89c9242e175558d14
dv045327 <daniel.vu@cerner.com>
2017-11-06 10:56:27 -0600

    Removed extra example

--- packages/terra-site/src/examples/table/examples/MultiSelect.jsx
+++ packages/terra-site/src/examples/table/examples/MultiSelect.jsx
@@ -13,7 +13,7 @@ const table = () => (
         <Table.HeaderCell content={'Phone Number'} key={'PHONE_NUMBER'} minWidth={'large'} />
       </Table.Header>
       <Table.MultiSelectableRows>
-        <Table.Row key={'PERSON_0'}>
+        <Table.Row key={'PERSON_0'} isSelected>
           <Table.Cell content="John Smith" key="NAME" />
           <Table.Cell content="123 Adams Drive" key="ADDRESS" />
           <Table.Cell content="111-222-3333" key="PHONE_NUMBER" />
@@ -39,31 +39,6 @@ const table = () => (
       </Table.Header>
       <Table.MultiSelectableRows maxSelectionCount={2}>
         <Table.Row key={'PERSON_0'}>
-          <Table.Cell content="John Smith" key="NAME" />
-          <Table.Cell content="123 Adams Drive" key="ADDRESS" />
-          <Table.Cell content="111-222-3333" key="PHONE_NUMBER" />
-        </Table.Row>
-        <Table.Row key={'PERSON_1'}>
-          <Table.Cell content="Jane Smith" key="NAME" />
-          <Table.Cell content="321 Drive Street" key="ADDRESS" />
-          <Table.Cell content="111-222-3333" key="PHONE_NUMBER" />
-        </Table.Row>
-        <Table.Row key={'PERSON_2'}>
-          <Table.Cell content="Dave Smith" key="NAME" />
-          <Table.Cell content="213 Raymond Road" key="ADDRESS" />
-          <Table.Cell content="111-222-3333" key="PHONE_NUMBER" />
-        </Table.Row>
-      </Table.MultiSelectableRows>
-    </Table>
-    <h3>Multiple Selectable Rows (First Row Initially Selected)</h3>
-    <Table isStriped={false}>
-      <Table.Header>
-        <Table.HeaderCell content={'Name'} key={'NAME'} minWidth={'small'} />
-        <Table.HeaderCell content={'Address'} key={'ADDRESS'} minWidth={'medium'} />
-        <Table.HeaderCell content={'Phone Number'} key={'PHONE_NUMBER'} minWidth={'large'} />
-      </Table.Header>
-      <Table.MultiSelectableRows>
-        <Table.Row key={'PERSON_0'} isSelected>
           <Table.Cell content="John Smith" key="NAME" />
           <Table.Cell content="123 Adams Drive" key="ADDRESS" />
           <Table.Cell content="111-222-3333" key="PHONE_NUMBER" />
