1218335661fd198b4d611a43544f27d17eb76e43
dv045327 <daniel.vu@cerner.com>
2017-11-07 13:25:32 -0600

    Moved SelectableTable test link

--- packages/terra-site/src/TestLinks.jsx
+++ packages/terra-site/src/TestLinks.jsx
@@ -49,8 +49,8 @@ const TestLinks = () => (
       <li><Link to="/tests/search-field-tests">Search Field</Link></li>
       <li><Link to="/tests/selectable-list-tests">Selectable List Tests</Link></li>
       <li><Link to="/tests/signature-tests">Signature Tests</Link></li>
-      <li><Link to="/tests/selectable-table-tests">SelectableTable Tests</Link></li>
       <li><Link to="/tests/single-select-list-tests">SingleSelectList Tests</Link></li>
+      <li><Link to="/tests/selectable-table-tests">SelectableTable Tests</Link></li>
       <li><Link to="/tests/single-select-table-tests">SingleSelectTable Tests</Link></li>
       <li><Link to="/tests/slide-group-tests">SlideGroup Tests</Link></li>
       <li><Link to="/tests/slide-panel-tests">SlidePanel Tests</Link></li>
