d9955ebed7c40bacd147268ba72b07f87f1483d0
Jake LaCombe <jake.lacombe2@gmail.com>
2017-11-15 20:01:27 -0600

    Implement Textarea for Theming (#982)
    * Implement Textarea for OCS
    
    * Remove extraneous dependencies from package.json file
    
    * Update Changelog
    
    * Remove Migration Guide Steps
    
    * Make Descriptions across files consistent
    
    * Uncomment README features that textarea supports
    
    * Add Hover styles to textarea
    
    * Update Terra Base to 2.6.1
    
    * Update Scripts in package.json to match the latest generator
    
    * Fix Documentation Error
    
    * Adjust styles for ltr and rtl issues
    
    * Fix typo in themeable variable
    
    * Update Styles after UX Sync
    
    * Fix Issue with themeable-variables.json file
    
    * Cleanup Styles
    
    * Style Cleanup
    
    * Move baseheight setting from component mount to onfocus for themability updates
    
    * UX Style Cleanup - Legacy Focus Styles
    
    * Move Mock Theme to Terra Site
    
    * Restrict max textarea width to 100%
    
    * Respect min-height for rows set when resizing with gripper
    
    * Add comment detailing the need to set the minHeight on a textarea
    
    * Fix Issue with the auto resizing of textarea showing a scrollbar
    
    * Prevent Auto Resizing on mobile devices
    
    * Shrink auto-resize example per UX request
    
    * Update Jest Snapshots

---
+++ packages/terra-form-textarea/tests/nightwatch/AutoResizableTextareaMobileMsMaxTouchPoints.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Textarea from '../../lib/Textarea';
+
+export default class textarea extends React.Component {
+  componentWillMount() {
+    if (!navigator.msMaxTouchPoints || navigator.msMaxTouchPoints === 0) {
+      this.resetMsMaxTouchPoints = true;
+      this.previousMsMaxTouchPoints = navigator.msMaxTouchPoints;
+
+      Object.defineProperty(navigator, 'msMaxTouchPoints',
+        {
+          value: 1,
+          configurable: true,
+        },
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.resetMsMaxTouchPoints) {
+      Object.defineProperty(navigator, 'msMaxTouchPoints',
+        {
+          value: this.previousMsMaxTouchPoints,
+          configurable: true,
+        },
+      );
+    }
+  }
+
+  render() {
+    return (
+      <Textarea id="auto-resizable" cols="2" isAutoResizable defaultValue="Default Value" />
+    );
+  }
+}
