fbd084e6dada102d151dd843727d053b54959068
Ryan Manuel <rfmanuel@gmail.com>
2017-11-30 10:54:01 -0600

    Update Spacer.jsx

--- packages/terra-spacer/src/Spacer.jsx
+++ packages/terra-spacer/src/Spacer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import { withBps } from 'react-bps';
 import 'terra-base/lib/baseStyles';
 import styles from './Spacer.scss';
 
