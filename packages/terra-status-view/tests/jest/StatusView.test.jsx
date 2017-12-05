3e2f95b3d3e614761af5b02e9db36abd98f00fd8
Schuster,Kevin <Kevin.Schuster@Cerner.com>
2017-12-05 14:08:12 -0600

    added jest tests

--- packages/terra-status-view/tests/jest/StatusView.test.jsx
+++ packages/terra-status-view/tests/jest/StatusView.test.jsx
@@ -7,9 +7,75 @@ import messages from '../../translations/en-US.json';
 const locale = 'en-US';
 
 // Snapshot Tests
-it('should render an image with a status view', () => {
+it('should render an image with a default status view', () => {
   const defaultRender = (<IntlProvider locale={locale} messages={messages}>
     <StatusView />
+  </IntlProvider>);
+
+  const wrapper = mount(defaultRender);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render an image with a no-data status view with all message props given', () => {
+  const defaultRender = (<IntlProvider locale={locale} messages={messages}>
+    <StatusView variant={StatusView.variants.NODATA} title="Test Title" message="Test Message">
+      <Button text="OK" key="1" />
+      <Button text="Cancel" key="2" />
+    </StatusView>
+  </IntlProvider>);
+
+  const wrapper = mount(defaultRender);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render an image with a no-data status view that will hide the glyph from being shown', () => {
+  const defaultRender = (<IntlProvider locale={locale} messages={messages}>
+    <StatusView variant={StatusView.variants.NODATA} isGlyphHidden />
+  </IntlProvider>);
+
+  const wrapper = mount(defaultRender);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render an image with a no-matching-results status-view that will force the content to align at the top', () => {
+  const defaultRender = (<IntlProvider locale={locale} messages={messages}>
+    <StatusView variant={StatusView.variants.NOMATCHINGRESULTS} isAlignedTop />
+  </IntlProvider>);
+
+  const wrapper = mount(defaultRender);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render an image with a not-authorized status-view', () => {
+  const defaultRender = (<IntlProvider locale={locale} messages={messages}>
+    <StatusView variant={StatusView.variants.NOTAUTHORIZED} />
+  </IntlProvider>);
+
+  const wrapper = mount(defaultRender);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render an image with an error status-view', () => {
+  const defaultRender = (<IntlProvider locale={locale} messages={messages}>
+    <StatusView variant={StatusView.variants.ERROR} />
+  </IntlProvider>);
+
+  const wrapper = mount(defaultRender);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render an image with a no-service status-view', () => {
+  const defaultRender = (<IntlProvider locale={locale} messages={messages}>
+    <StatusView variant={StatusView.variants.NOSERVICE} />
+  </IntlProvider>);
+
+  const wrapper = mount(defaultRender);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render an image with a custom status-view', () => {
+  const defaultRender = (<IntlProvider locale={locale} messages={messages}>
+    <StatusView variant={StatusView.variants.CUSTOM} />
   </IntlProvider>);
 
   const wrapper = mount(defaultRender);
