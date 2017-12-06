88d171f750f90da6ddb82c344a85cbccc8aa10cd
Michael Hemesath <mike.hemesath@cerner.com>
2017-12-06 10:28:59 -0600

    Add comment to spec

--- packages/terra-base/tests/wdio/base-spec.js
+++ packages/terra-base/tests/wdio/base-spec.js
@@ -12,6 +12,9 @@ describe('Base', () => {
     });
   });
 
+  // NOTE: This test is designed to assume that tests won't fail on an error.  If it starts failing,
+  // we can remove it and either deem that this scenario is not needed in night watch or find a
+  // better solution.
   describe('No Translations Placeholder', () => {
     it('Displays a placeholder element', () => {
       browser.url('/#/tests/base-tests/no-translations-loaded');
