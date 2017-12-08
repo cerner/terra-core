4e0fa757e605731454ad8f4232d665eab155e824
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-12-08 15:26:17 -0600

    Updating site example

--- packages/terra-site/src/examples/button/ButtonVariant.jsx
+++ packages/terra-site/src/examples/button/ButtonVariant.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'terra-button';
+import IconEdit from 'terra-icon/lib/icon/IconEdit';
 
 const buttonStyle = { margin: '5px' };
 
@@ -8,6 +9,9 @@ const ButtonVariant = () => (
     <Button text="Neutral" style={buttonStyle} />
     <Button text="Emphasis" variant="emphasis" style={buttonStyle} />
     <Button text="De-emphasis" variant="de-emphasis" style={buttonStyle} />
+    <Button text="Action" variant="action" icon={<IconEdit />} style={buttonStyle} />
+    <Button isIconOnly text="Action" variant="action" icon={<IconEdit />} style={buttonStyle} />
+    <Button text="Utility" variant="utility" icon={<IconEdit />} style={buttonStyle} />
   </div>
 );
 
