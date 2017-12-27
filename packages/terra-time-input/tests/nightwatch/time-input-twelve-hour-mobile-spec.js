3d86c4180c4d911fb3be1a55323b5d4861c3f4c2
Jaime Mackey <jaime.mackey@cerner.com>
2017-12-27 16:16:24 -0600

    Fix button group accessibility issue. (#1100)
    * Fix button group accessibility issue.
    
    * update jest snapshots
    
    * I guess time input jest snapshots needed to be updated too.
    
    * Update nightwatch tests

--- packages/terra-time-input/tests/nightwatch/time-input-twelve-hour-mobile-spec.js
+++ packages/terra-time-input/tests/nightwatch/time-input-twelve-hour-mobile-spec.js
@@ -8,7 +8,7 @@ module.exports = resizeTo(['tiny', 'small', 'medium', 'large'], {
 
     browser.expect.element('#timeInput input[name="terra-time-hour-time-input"]').to.have.attribute('value').equals('');
     browser.expect.element('#timeInput input[name="terra-time-minute-time-input"]').to.have.attribute('value').equals('');
-    browser.expect.element('#timeInput button[aria-selected="true"]').text.to.equal('a.m.');
+    browser.expect.element('#timeInput button[aria-pressed="true"]').text.to.equal('a.m.');
     browser.expect.element('#timeInput label[for="terra-time-hour-time-input"]').text.to.equal('Hours');
     browser.expect.element('#timeInput label[for="terra-time-minute-time-input"]').text.to.equal('Minutes');
   },
@@ -17,7 +17,7 @@ module.exports = resizeTo(['tiny', 'small', 'medium', 'large'], {
 
     browser.expect.element('#timeInput input[name="terra-time-hour-time-input"]').to.have.attribute('value').equals('03');
     browser.expect.element('#timeInput input[name="terra-time-minute-time-input"]').to.have.attribute('value').equals('23');
-    browser.expect.element('#timeInput button[aria-selected="true"]').text.to.equal('p.m.');
+    browser.expect.element('#timeInput button[aria-pressed="true"]').text.to.equal('p.m.');
     browser.expect.element('label[for="terra-time-hour-time-input"]').text.to.equal('Hours');
     browser.expect.element('label[for="terra-time-minute-time-input"]').text.to.equal('Minutes');
   },
@@ -30,12 +30,12 @@ module.exports = resizeTo(['tiny', 'small', 'medium', 'large'], {
   'Clicking the meridiem inputs updates the time': (browser) => {
     browser.url(`${browser.launchUrl}/#/tests/time-input-tests/twelve-hour-mobile-time-provided`);
 
-    browser.click('#timeInput button[aria-selected="false"]');
-    browser.expect.element('#timeInput button[aria-selected="true"]').text.to.equal('a.m.');
+    browser.click('#timeInput button[aria-pressed="false"]');
+    browser.expect.element('#timeInput button[aria-pressed="true"]').text.to.equal('a.m.');
     browser.expect.element('#time-input-value').text.to.contain('03:23');
 
-    browser.click('#timeInput button[aria-selected="false"]');
-    browser.expect.element('#timeInput button[aria-selected="true"]').text.to.equal('p.m.');
+    browser.click('#timeInput button[aria-pressed="false"]');
+    browser.expect.element('#timeInput button[aria-pressed="true"]').text.to.equal('p.m.');
     browser.expect.element('#time-input-value').text.to.contain('15:23');
   },
 });
