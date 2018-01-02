f8076ed327c210a05c48c6ebc9601b2fe250729e
Schuster,Kevin <Kevin.Schuster@Cerner.com>
2018-01-02 11:41:25 -0600

    update tests to attempt to fix build

--- packages/terra-status-view/tests/nightwatch/StatusViewDefault.jsx
+++ packages/terra-status-view/tests/nightwatch/StatusViewDefault.jsx
@@ -6,5 +6,5 @@ const locale = document.getElementsByTagName('html')[0].getAttribute('lang');
 
 export default () =>
   <Base locale={locale}>
-    <StatusView id="statusView" />
+    <StatusView role="main" id="statusView" />
   </Base>;
--- packages/terra-status-view/tests/nightwatch/StatusViewNotAuthorized.jsx
+++ packages/terra-status-view/tests/nightwatch/StatusViewNotAuthorized.jsx
@@ -6,5 +6,5 @@ const locale = document.getElementsByTagName('html')[0].getAttribute('lang');
 
 export default () =>
   <Base locale={locale}>
-    <StatusView id="statusView" variant={StatusView.variants.NOTAUTHORIZED} />
+    <StatusView role="main" id="statusView" variant={StatusView.variants.NOTAUTHORIZED} />
   </Base>;
