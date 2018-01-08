30df187326adf483ab46661c6eea2a7761165d94
Brett Jankord <brett.jankord@cerner.com>
2018-01-08 11:45:18 -0600

    Add logging to post-install script

--- scripts/post-install/index.js
+++ scripts/post-install/index.js
@@ -1,22 +1,30 @@
-console.log(process.cwd());
-// When npm install is run in monorepo
-// /Users/bj031910/repos/terra-core
+/* eslint-disable import/no-extraneous-dependencies */
+const shell = require('shelljs');
 
-// When installed in theme repo
-// /Users/bj031910/repos/cerner-consumer-theme/node_modules/terra-core
+/*
+ The postinstall script is run both when installing packages in the monorepo via `npm install`
+and when the monorepo package is installed into other projects, like the theme repo.
 
-console.log(__dirname);
+The isInstalled variable allows us to detect between to these 2 cases.
 
-// When npm install is run in monorepo
-// /Users/bj031910/repos/terra-core/scripts/post-install
+When `npm install` is run within monorepo, process.cwd() does not include `node_modules`
+example: process.cwd() outputs the follow:
+/Users/computerName/repos/terra-core
 
-// When installed in theme repo
-// /Users/bj031910/repos/cerner-consumer-theme/node_modules/terra-core/scripts/post-install
+When the monorepo is installed in the theme repo, process.cwd() does include `node_modules`
+example: process.cwd() outputs the follow:
+/Users/computerName/repos/cerner-consumer-theme/node_modules/terra-core
 
+*/
 
 const isInstalled = process.cwd().indexOf('node_modules') !== -1;
 
-console.log(isInstalled);
-
-
-// link-parent-bin && npm run compile && npm run bootstrap
+if (isInstalled) {
+  console.log('getting ready to run postinstall:package');
+  shell.exec('npm run postinstall:package');
+  console.log('ran postinstall:package');
+} else {
+  console.log('getting ready to run postinstall:project');
+  shell.exec('npm run postinstall:project');
+  console.log('ran postinstall:project');
+}
