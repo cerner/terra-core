6b9de906c54caef06d9e5d3776ec9ff450635997
Klohr, Dylan <dylan.klohr@cerner.com>
2018-01-20 17:52:41 -0600

    Fixes #1160. Remove unknown `isDisabled` prop from InlineRadiosExample.jsx.

--- packages/terra-site/src/examples/form-radio/InlineRadiosExample.jsx
+++ packages/terra-site/src/examples/form-radio/InlineRadiosExample.jsx
@@ -4,7 +4,7 @@ import Radio from 'terra-form-radio/lib/Radio';
 
 const inlineRadiosExample = () => (
   <div>
-    <Radio id="first-inline" labelText="First Radio" name="inline-example" isInline isDisabled />
+    <Radio id="first-inline" labelText="First Radio" name="inline-example" isInline />
     <Radio id="second-inline" labelText="Second Radio" name="inline-example" isInline />
     <Radio id="third-inline" labelText="Third Radio" name="inline-example" isInline />
   </div>
