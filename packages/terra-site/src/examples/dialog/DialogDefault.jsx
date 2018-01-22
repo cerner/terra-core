d8c7d1694956829d8802501b137ad0371ec6ac35
Perrin,Ryan <ryan.perrin@cerner.com>
2018-01-22 08:55:04 -0600

    Skeleton of component.

--- packages/terra-site/src/examples/dialog/DialogDefault.jsx
+++ packages/terra-site/src/examples/dialog/DialogDefault.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import Dialog from 'terra-dialog/src/Dialog';
+import Dialog from 'terra-dialog';
+
+const header = <span>Header Stuff</span>;
+
+const footer = <span>Footer Stuff</span>;
 
 const DialogDefault = () => (
-  <Dialog />
+  <div>
+    <Dialog header={header} footer={footer} >
+      <p>This is my body content.</p>
+      <p>This is some more content.</p>
+    </Dialog>
+  </div>
 );
 
 export default DialogDefault;
