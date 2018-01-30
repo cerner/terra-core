75ffc58b07a71604f9e18bdd5173d51e0c41a91f
Ryan Perrin <rk.perrin@gmail.com>
2018-01-30 15:52:26 -0600

    Refine and Update Dialog Component (#1173)

--- packages/terra-dialog/src/Dialog.jsx
+++ packages/terra-dialog/src/Dialog.jsx
@@ -2,32 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import 'terra-base/lib/baseStyles';
+import ContentContainer from 'terra-content-container';
 import styles from './Dialog.scss';
 
 const cx = classNames.bind(styles);
 
 const propTypes = {
  /*
- * Content to be displayed as the name
+ * The header element to be placed within the header area of the dialog.
  */
-  name: PropTypes.string,
+  header: PropTypes.node.isRequired,
+  /*
+ * The footer element to be placed within the footer area of the dialog.
+ */
+  footer: PropTypes.node,
+  /*
+ * The children to be placed within the main content area of the dialog.
+ */
+  children: PropTypes.node.isRequired,
 };
 
-const defaultProps = {
-  name: 'default',
-};
-
-const Dialog = ({ name, ...customProps }) => {
-  const attributes = Object.assign({}, customProps);
+const Dialog = ({ header, footer, children }) => {
   const DialogClassNames = cx([
     'dialog',
-    attributes.className,
   ]);
 
-  return (<div {...attributes} className={DialogClassNames}>{name}</div>);
+  return (
+    <div className={DialogClassNames}>
+      <ContentContainer fill header={<div className={cx('header')}>{header}</div>} footer={<div className={cx('footer')}>{footer}</div>}>
+        <div className={cx('body')}>{children}</div>
+      </ContentContainer>
+    </div>
+  );
 };
 
 Dialog.propTypes = propTypes;
-Dialog.defaultProps = defaultProps;
 
 export default Dialog;
