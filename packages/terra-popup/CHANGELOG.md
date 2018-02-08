aae6895720fad091ff89b74b2bf3906f86a0b9ba
Sai Kaushik Mallela <Kaushik.Mallela@cerner.com>
2018-02-07 20:15:57 -0600

    Added wdio tests for popup

--- packages/terra-popup/CHANGELOG.md
+++ packages/terra-popup/CHANGELOG.md
@@ -4,6 +4,9 @@ Changelog
 Unreleased
 ----------
 
+### Added
+* WebDriverIO tests and themable  styles
+
 1.20.0 - (February 1, 2018)
 ------------------
 ### Changed
---
+++ packages/terra-popup/tests/wdio/popup-spec.js
@@ -0,0 +1,17 @@
+/* global  browser,Terra */
+const viewports = Terra.viewports('tiny', 'medium');
+
+describe('Popup', () => {
+  describe('Arrow Popup', () => {
+    beforeEach(() => browser.url('/#/tests/popup-tests/arrow'));
+
+    Terra.should.beAccessible({ viewports });
+    Terra.should.matchScreenshot({ viewports });
+    Terra.should.themeEachCustomProperty({
+      '--terra-popup-content-inner-border': '10px dashed red',
+      '--terra-popup-content-rounded-corners-border-radius': '20px',
+      '--terra-popup-content-inner-box-shadow': '10px 10px 36px blue',
+      '--terra-popup-content-inner-background-color': 'yellow',
+    });
+  });
+});
