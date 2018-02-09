fce74038198e13fb65b0ff246bc2a773774beda3
Kasper,Dave <Dave.Kasper@Cerner.com>
2018-02-08 18:05:35 -0600

    Initial work needs tests.

--- packages/terra-slide-panel/src/SlidePanel.jsx
+++ packages/terra-slide-panel/src/SlidePanel.jsx
@@ -54,42 +54,85 @@ const defaultProps = {
   panelSize: 'small',
 };
 
-const SlidePanel = ({
-  mainContent,
-  panelContent,
-  panelBehavior,
-  panelPosition,
-  panelSize,
-  isFullscreen,
-  isOpen,
-  fill,
-  ...customProps
-  }) => {
-  const slidePanelClassNames = cx([
-    'slide-panel',
-    { 'is-open': isOpen },
-    { 'is-fullscreen': isFullscreen },
-    { fill },
-    customProps.className,
-  ]);
+class SlidePanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setPanelNode = this.setPanelNode.bind(this);
+    this.handleTransitionEnd = this.handleTransitionEnd.bind(this);
 
-  return (
-    <div
-      {...customProps}
-      className={slidePanelClassNames}
-      data-slide-panel-panel-behavior={panelBehavior}
-      data-slide-panel-panel-position={panelPosition}
-      data-slide-panel-panel-size={panelSize}
-    >
-      <div className={cx('panel')} aria-hidden={!isOpen ? 'true' : null}>
-        {panelContent}
+    this.isHidden = !props.isOpen;
+  }
+
+  componentDidMount() {
+    if (this.panelNode) {
+      this.panelNode.addEventListener('transitionend', this.handleTransitionEnd);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.isOpen !== nextProps.isOpen && nextProps.isOpen === true) {
+      this.panelNode.setAttribute('aria-hidden', 'false');
+      this.isHidden = false;
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.panelNode) {
+      this.panelNode.removeEventListener('transitionend', this.handleTransitionEnd);
+    }
+  }
+
+  setPanelNode(node) {
+    this.panelNode = node;
+  }
+
+  handleTransitionEnd() {
+    if (!this.props.isOpen) {
+      this.panelNode.setAttribute('aria-hidden', true);
+      this.isHidden = true;
+    }
+  }
+
+  render() {
+    const {
+      mainContent,
+      panelContent,
+      panelBehavior,
+      panelPosition,
+      panelSize,
+      isFullscreen,
+      isOpen,
+      fill,
+      ...customProps
+    } = this.props;
+
+    const slidePanelClassNames = cx([
+      'slide-panel',
+      { 'is-open': isOpen },
+      { 'is-fullscreen': isFullscreen },
+      { fill },
+      customProps.className,
+    ]);
+
+    return (
+      <div
+        {...customProps}
+        className={slidePanelClassNames}
+        data-slide-panel-panel-behavior={panelBehavior}
+        data-slide-panel-panel-position={panelPosition}
+        data-slide-panel-panel-size={panelSize}
+      >
+        <div className={cx(['panel'])} aria-hidden={this.isHidden ? 'true' : 'false'} ref={this.setPanelNode}>
+          {panelContent}
+        </div>
+        <div className={cx('main')}>
+          {mainContent}
+        </div>
       </div>
-      <div className={cx('main')}>
-        {mainContent}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 
 SlidePanel.propTypes = propTypes;
 SlidePanel.defaultProps = defaultProps;
