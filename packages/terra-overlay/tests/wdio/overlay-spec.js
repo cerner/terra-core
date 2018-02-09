fb0b21f4bc9b1f80c0d88e42f4a6d6a91d5689c7
Sruti Paku <sruti.paku@outlook>
2018-02-08 18:17:52 -0600

    Added tests for clear theme(#1184)

--- packages/terra-overlay/tests/wdio/overlay-spec.js
+++ packages/terra-overlay/tests/wdio/overlay-spec.js
@@ -8,7 +8,7 @@ describe('Overlay', () => {
     Terra.should.matchScreenshot();
     Terra.should.themeEachCustomProperty({
       '--terra-overlay-content-color': 'red',
-      '--terra-overlay-content-padding': '1000px',
+      '--terra-overlay-content-padding': '10px',
       '--terra-overlay-light-background-color': 'green',
       '--terra-overlay-light-background-image': 'linear-gradient(red, green)',
       '--terra-overlay-light-opacity': '0.8',
@@ -21,11 +21,22 @@ describe('Overlay', () => {
     Terra.should.beAccessible();
     Terra.should.matchScreenshot();
     Terra.should.themeEachCustomProperty({
-      '--terra-overlay-content-padding': '1000px',
+      '--terra-overlay-content-padding': '10px',
       '--terra-overlay-dark-background-color': 'blue',
       '--terra-overlay-dark-background-image': 'linear-gradient(blue, green)',
       '--terra-overlay-dark-content-color': 'red',
       '--terra-overlay-dark-opacity': '0.8',
+    });
+  });
+
+  describe('Clear Themed', () => {
+    before(() => browser.url('/#/tests/overlay-tests/clear-themed'));
+
+    Terra.should.beAccessible();
+    Terra.should.matchScreenshot();
+    Terra.should.themeEachCustomProperty({
+      '--terra-overlay-content-color': 'red',
+      '--terra-overlay-content-padding': '10px',
     });
   });
 });
