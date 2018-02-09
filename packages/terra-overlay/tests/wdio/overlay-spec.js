9dd79a89c874c28993e8abfeb228f7ea76c94caf
Sruti Paku <sruti.paku@outlook>
2018-02-09 09:37:48 -0600

    Changes before to beforeEach and updated the screenshots, updated the padding varibales (#1184)

--- packages/terra-overlay/tests/wdio/overlay-spec.js
+++ packages/terra-overlay/tests/wdio/overlay-spec.js
@@ -1,14 +1,17 @@
-/* global before, browser, Terra */
+/* global browser, Terra */
 
 describe('Overlay', () => {
   describe('Light Themed', () => {
-    before(() => browser.url('/#/tests/overlay-tests/light-themed'));
+    beforeEach(() => browser.url('/#/tests/overlay-tests/light-themed'));
 
     Terra.should.beAccessible();
     Terra.should.matchScreenshot();
     Terra.should.themeEachCustomProperty({
       '--terra-overlay-content-color': 'red',
-      '--terra-overlay-content-padding': '10px',
+      '--terra-overlay-content-padding-bottom': '500px',
+      '--terra-overlay-content-padding-left': '500px',
+      '--terra-overlay-content-padding-right': '500px',
+      '--terra-overlay-content-padding-top': '500px',
       '--terra-overlay-light-background-color': 'green',
       '--terra-overlay-light-background-image': 'linear-gradient(red, green)',
       '--terra-overlay-light-opacity': '0.8',
@@ -16,12 +19,15 @@ describe('Overlay', () => {
   });
 
   describe('Dark Themed', () => {
-    before(() => browser.url('/#/tests/overlay-tests/dark-themed'));
+    beforeEach(() => browser.url('/#/tests/overlay-tests/dark-themed'));
 
     Terra.should.beAccessible();
     Terra.should.matchScreenshot();
     Terra.should.themeEachCustomProperty({
-      '--terra-overlay-content-padding': '10px',
+      '--terra-overlay-content-padding-bottom': '500px',
+      '--terra-overlay-content-padding-left': '500px',
+      '--terra-overlay-content-padding-right': '500px',
+      '--terra-overlay-content-padding-top': '500px',
       '--terra-overlay-dark-background-color': 'blue',
       '--terra-overlay-dark-background-image': 'linear-gradient(blue, green)',
       '--terra-overlay-dark-content-color': 'red',
@@ -30,13 +36,16 @@ describe('Overlay', () => {
   });
 
   describe('Clear Themed', () => {
-    before(() => browser.url('/#/tests/overlay-tests/clear-themed'));
+    beforeEach(() => browser.url('/#/tests/overlay-tests/clear-themed'));
 
     Terra.should.beAccessible();
     Terra.should.matchScreenshot();
     Terra.should.themeEachCustomProperty({
       '--terra-overlay-content-color': 'red',
-      '--terra-overlay-content-padding': '10px',
+      '--terra-overlay-content-padding-bottom': '500px',
+      '--terra-overlay-content-padding-left': '500px',
+      '--terra-overlay-content-padding-right': '500px',
+      '--terra-overlay-content-padding-top': '500px',
     });
   });
 });
