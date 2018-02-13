1ba5babd4ccabf1281be2a49620bc6cf4270b601
Dylan Klohr <dylan.sftwr@gmail.com>
2018-02-13 15:10:55 -0600

    Add aria-labels to examples

--- packages/terra-form-input/tests/nightwatch/FormInputPropPermutations.jsx
+++ packages/terra-form-input/tests/nightwatch/FormInputPropPermutations.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import FormInput from '../../lib/Input';
 
-const DefaultFormInput = () => <FormInput id="form-input-default" />;
-const InvalidFormInput = () => <FormInput isInvalid id="form-input-invalid" />;
-const DisabledFormInput = () => <FormInput disabled id="form-input-disabled" />;
+const DefaultFormInput = () => <FormInput aria-label="default form input" id="form-input-default" />;
+const InvalidFormInput = () => <FormInput aria-label="invalid form input" isInvalid id="form-input-invalid" />;
+const DisabledFormInput = () => <FormInput aria-label="disabled form input" disabled id="form-input-disabled" />;
 
 export {
   DefaultFormInput,
