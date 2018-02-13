79f5f2d2e7450c56602e69ab117520337351356d
Perrin,Ryan <ryan.perrin@cerner.com>
2018-02-13 12:24:21 -0600

    Removed action header. Implemented header in Dialog.

--- packages/terra-dialog/src/Dialog.jsx
+++ packages/terra-dialog/src/Dialog.jsx
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import 'terra-base/lib/baseStyles';
 import ContentContainer from 'terra-content-container';
-import ActionHeader from 'terra-clinical-action-header';
+import Button from 'terra-button';
+import IconClose from 'terra-icon/lib/icon/IconClose';
 import styles from './Dialog.scss';
 
 const cx = classNames.bind(styles);
 
 const propTypes = {
   /**
-   * The text string to be placed within the header area of the dialog.
+   * React node to be placed within the header area of the dialog.
    */
-  header: PropTypes.string.isRequired,
+  header: PropTypes.node.isRequired,
   /**
-   * The footer element to be placed within the footer area of the dialog.
+   * React node to be placed within the footer area of the dialog.
+   * Content exceeding the available space will be hidden.
    */
   footer: PropTypes.node.isRequired,
   /**
@@ -38,11 +40,13 @@ const Dialog = ({ header, footer, onClose, children, ...customProps }) => {
     'dialog',
   ]);
 
-  const actionHeader = <div className={cx('dialog-header')}><ActionHeader title={header} onClose={onClose} /></div>;
+  const closeButton = onClose ? <div className={cx('dialog-header-close')}><Button icon={<IconClose />} aria-label="Close Button" onClick={onClose} isIconOnly /></div> : null;
+
+  const dialogHeader = <div className={cx('dialog-header')}><div className={cx('dialog-header-title')}>{header}</div>{closeButton}</div>;
 
   return (
     <div className={dialogClassNames} {...customProps}>
-      <ContentContainer fill header={actionHeader} footer={<div className={cx('dialog-footer')}>{footer}</div>}>
+      <ContentContainer fill header={dialogHeader} footer={<div className={cx('dialog-footer')}>{footer}</div>}>
         <div className={cx('dialog-body')}>{children}</div>
       </ContentContainer>
     </div>
