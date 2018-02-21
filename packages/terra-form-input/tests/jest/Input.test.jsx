9f94d30a4b5e9312e941c2de239d73aeb928a94c
Dylan Klohr <dylan.sftwr@gmail.com>
2018-02-21 15:22:29 -0600

    Implement UX requested color / spacing changes

--- packages/terra-form-input/tests/jest/Input.test.jsx
+++ packages/terra-form-input/tests/jest/Input.test.jsx
@@ -16,6 +16,12 @@ describe('Input', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('should render w/ placeholder text when just a placeholder prop is passed into the Input', () => {
+    const input = <Input placeholder="foo" />;
+    const wrapper = render(input);
+    expect(wrapper).toMatchSnapshot();
+  });
+
   it('should render as controlled when just a default value and `onChange()` is passed into the Input', () => {
     const input = <Input value="foo" onChange={() => {}} />;
     const wrapper = render(input);
