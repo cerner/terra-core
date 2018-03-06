6b2e57c2b048ea43b013faae36f7802bb38ea6aa
Klohr, Dylan <dylan.klohr@cerner.com>
2018-03-06 08:29:07 -0600

    Allow triggering of active styles by keyboard

--- packages/terra-section-header/src/SectionHeader.jsx
+++ packages/terra-section-header/src/SectionHeader.jsx
@@ -7,6 +7,11 @@ import styles from './SectionHeader.scss';
 
 const cx = classNames.bind(styles);
 
+const KEYCODES = {
+  ENTER: 13,
+  SPACE: 32,
+};
+
 const propTypes = {
   /**
    * Text to be displayed on the SectionHeader.
@@ -33,55 +38,101 @@ const defaultProps = {
   level: 2,
 };
 
-const SectionHeader = ({
-  title,
-  onClick,
-  isOpen,
-  level,
-  ...customProps
-}) => {
-  if ((process.env.NODE_ENV !== 'production') && (!onClick && isOpen)) {
-    // eslint-disable-next-line no-console
-    console.warn('\'isOpen\' are intended to be used only when \'onClick\' is provided.');
+const isRecognizedKeyPress = event => ((event.nativeEvent.keyCode === KEYCODES.ENTER) || (event.nativeEvent.keyCode === KEYCODES.SPACE));
+
+class SectionHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isActive: false };
+    this.wrapOnKeyDown = this.wrapOnKeyDown.bind(this);
+    this.wrapOnKeyUp = this.wrapOnKeyUp.bind(this);
   }
 
-  const attributes = Object.assign({}, customProps);
+  wrapOnKeyDown(onKeyDown) {
+    return ((event) => {
+      if (isRecognizedKeyPress(event) && !this.state.isActive) {
+        this.setState({ isActive: true });
 
-  if (onClick) {
-    attributes.tabIndex = '0';
+        // Call the same function the user provides for a click event
+        if (this.props.onClick) {
+          this.props.onClick(event);
+        }
+      }
+
+      if (onKeyDown) {
+        onKeyDown(event);
+      }
+    });
   }
 
-  const iconClassNames = cx([
-    'accordion-icon',
-    { 'is-open': onClick && isOpen },
-  ]);
+  wrapOnKeyUp(onKeyUp) {
+    return ((event) => {
+      if (isRecognizedKeyPress(event) && this.state.isActive) {
+        this.setState({ isActive: false });
+      }
 
-  const accordionIcon = (
-    <div className={cx('accordion-icon-wrapper')}>
-      <span className={iconClassNames} />
-    </div>
-  );
+      if (onKeyUp) {
+        onKeyUp(event);
+      }
+    });
+  }
 
-  const sectionHeaderClassNames = cx([
-    'section-header',
-    { 'is-interactable': onClick },
-    customProps.className,
-  ]);
+  render() {
+    const {
+      title,
+      onClick,
+      isOpen,
+      level,
+      ...customProps
+    } = this.props;
 
-  const Element = `h${level}`;
+    if ((process.env.NODE_ENV !== 'production') && (!onClick && isOpen)) {
+      // eslint-disable-next-line no-console
+      console.warn('\'isOpen\' are intended to be used only when \'onClick\' is provided.');
+    }
 
-  // allows us to set an onClick on the div
-  /* eslint-disable jsx-a11y/no-static-element-interactions */
-  return (
-    <div {...attributes} onClick={onClick} className={sectionHeaderClassNames}>
-      <Arrange
-        fitStart={onClick && accordionIcon}
-        fill={<Element className={cx('title')}>{title}</Element>}
-      />
-    </div>
-  );
-  /* eslint-enable jsx-a11y/no-static-element-interactions */
-};
+    const attributes = Object.assign({}, customProps);
+
+    if (onClick) {
+      attributes.tabIndex = '0';
+      attributes.onKeyDown = this.wrapOnKeyDown(attributes.onKeyDown);
+      attributes.onKeyUp = this.wrapOnKeyUp(attributes.onKeyUp);
+    }
+
+    const iconClassNames = cx([
+      'accordion-icon',
+      { 'is-open': onClick && isOpen },
+    ]);
+
+    const accordionIcon = (
+      <div className={cx('accordion-icon-wrapper')}>
+        <span className={iconClassNames} />
+      </div>
+    );
+
+    const sectionHeaderClassNames = cx([
+      'section-header',
+      { 'is-interactable': onClick },
+      { 'is-active': this.state.isActive },
+      customProps.className,
+    ]);
+
+    const Element = `h${level}`;
+
+    // allows us to set an onClick on the div
+    /* eslint-disable jsx-a11y/no-static-element-interactions */
+    return (
+      <div {...attributes} onClick={onClick} className={sectionHeaderClassNames}>
+        <Arrange
+          fitStart={onClick && accordionIcon}
+          fill={<Element className={cx('title')}>{title}</Element>}
+        />
+      </div>
+    );
+    /* eslint-enable jsx-a11y/no-static-element-interactions */
+  }
+}
 
 SectionHeader.propTypes = propTypes;
 SectionHeader.defaultProps = defaultProps;
