4bc55448dcd94c02cd6bdce5a504fc2b891e4c50
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-03-12 15:37:30 -0500

    And this.

--- packages/terra-i18n/src/I18n.js
+++ packages/terra-i18n/src/I18n.js
@@ -1,9 +1,7 @@
 import I18nProvider from './I18nProvider';
 import i18nLoader from './i18nLoader';
-import aggregateTranslations from '../scripts/aggregate-translations';
 
 module.exports = {
   I18nProvider,
   i18nLoader,
-  aggregateTranslations,
 };
