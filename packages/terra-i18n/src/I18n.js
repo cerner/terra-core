483d4404d0d16a1b10d0fa710279a57f006a3404
Tyler Biethman <tbiethman@users.noreply.github.com>
2018-03-14 22:26:20 -0500

    Update I18n.js

--- packages/terra-i18n/src/I18n.js
+++ packages/terra-i18n/src/I18n.js
@@ -1,4 +1,4 @@
-import { injectIntl } from 'react-intl';
+import { injectIntl, intlShape } from 'react-intl';
 import I18nProvider from './I18nProvider';
 import i18nLoader from './i18nLoader';
 
@@ -6,4 +6,5 @@ module.exports = {
   I18nProvider,
   i18nLoader,
   injectIntl,
+  intlShape,
 };
