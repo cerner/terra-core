2c7aa2539fc3c12484cc409096d505a3c697daac
Pfeiffer,Neil <neil.pfeiffer@cerner.com>
2018-03-23 13:37:58 -0500

    Terra-Tag Example Fixes
    Changed Tag examples and added new section for Fallback Styles with
    alert message for non-interactive tags. Latest: Refactored examples,
    resolved lint errors.
    
    Resolves #1368

--- packages/terra-site/src/examples/tag/Index.jsx
+++ packages/terra-site/src/examples/tag/Index.jsx
@@ -3,7 +3,6 @@ import PropsTable from 'terra-props-table';
 import Markdown from 'terra-markdown';
 import ReadMe from 'terra-tag/docs/README.md';
 import { version } from 'terra-tag/package.json';
-import Alert from 'terra-alert';
 
 // Component Source
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/first, import/no-unresolved, import/extensions
@@ -13,14 +12,6 @@ import TagSrc from '!raw-loader!terra-tag/src/Tag';
 import TagDefault from './TagDefault';
 import TagHref from './HrefTag';
 import TagFallbacks from './TagFallbacks';
-
-// Fallback Styles Message
-const alertTitle = 'Not Reccomended Usage.';
-const alertText = 'These styles are provided for when this compoment is missing recomended elements for best practice use.';
-
-const TagHeadingMarginStyle = {
-  marginTop: '200px',
-};
 
 const TagExamples = () => (
   <div>
@@ -33,12 +24,7 @@ const TagExamples = () => (
     <h2 id="href">Tag with href</h2>
     <TagHref />
     <br />
-    <div className="markdown-body" style={TagHeadingMarginStyle}>
-      <h2 id="fallbacks-header">Fallback Styles</h2>
-    </div>
-    <Alert type={Alert.Opts.Types.INFO} title={alertTitle}>{alertText}</Alert>
     <br />
-    <h3 id="fallbacks">Tag with no onClick or href</h3>
     <TagFallbacks />
   </div>
 );
--- packages/terra-site/src/examples/tag/TagDefault.jsx
+++ packages/terra-site/src/examples/tag/TagDefault.jsx
@@ -4,8 +4,10 @@ import Tag from 'terra-tag';
 
 const TagDefault = () => (
   <div>
+    {/* eslint-disable no-alert */}
     <Tag text="Default OnClick Tag" onClick={() => window.alert('Tag has been clicked!')} />
     <Tag icon={<IconTag />} onClick={() => window.alert('Tag has been clicked!')} text="Icon &amp; Text OnClick Tag" />
+    {/* eslint-enable no-alert */}
   </div>
 );
 
