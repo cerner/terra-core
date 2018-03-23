0b0c669e4cbd64d0dcb23c61aca0b349138a5c72
Pfeiffer,Neil <neil.pfeiffer@cerner.com>
2018-03-23 08:17:27 -0500

    Terra-Tag Example Fixes
    Changed Tag examples and added new section for Fallback Styles with
    alert message for non-interactive tags, fixed linting errors

--- packages/terra-site/src/examples/tag/Index.jsx
+++ packages/terra-site/src/examples/tag/Index.jsx
@@ -3,6 +3,7 @@ import PropsTable from 'terra-props-table';
 import Markdown from 'terra-markdown';
 import ReadMe from 'terra-tag/docs/README.md';
 import { version } from 'terra-tag/package.json';
+import Alert from 'terra-alert';
 
 // Component Source
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/first, import/no-unresolved, import/extensions
@@ -14,7 +15,6 @@ import TagHref from './HrefTag';
 import TagFallbacks from './TagFallbacks';
 
 // Fallback Styles Message
-import Alert from 'terra-alert';
 const alertTitle = 'Not Reccomended Usage.';
 const alertText = 'These styles are provided for when this compoment is missing recomended elements for best practice use.';
 
@@ -33,7 +33,7 @@ const TagExamples = () => (
     <h2 id="href">Tag with href</h2>
     <TagHref />
     <br />
-    <div class="markdown-body" style={TagHeadingMarginStyle}>
+    <div className="markdown-body" style={TagHeadingMarginStyle}>
       <h2 id="fallbacks-header">Fallback Styles</h2>
     </div>
     <Alert type={Alert.Opts.Types.INFO} title={alertTitle}>{alertText}</Alert>
