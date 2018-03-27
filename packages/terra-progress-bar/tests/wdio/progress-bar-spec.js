05ac8d2918ac30e330a245cbeda9ffe06610f48f
‘Umesh <Umesh.Shimpi@cerner.com>
2018-03-27 11:27:41 -0400

    adding an empty line as requested on review

--- packages/terra-progress-bar/tests/wdio/progress-bar-spec.js
+++ packages/terra-progress-bar/tests/wdio/progress-bar-spec.js
@@ -2,6 +2,7 @@
 
 describe('Progress Bar Gradient', () => {
   beforeEach(() => browser.url('/#/raw/tests/progress-bar/progress-bar-gradient'));
+
   Terra.should.beAccessible();
   Terra.should.matchScreenshot();
 });
