fa3c82a9aeff18e0aef2be0d034566e096bd958e
Kasper, Dave <dave.m.kasper@gmail.com>
2018-03-28 12:44:26 -0500

    Remove references to hookshot and popup.

--- packages/terra-popup/tests/jest/Popup.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PopupDefaultExample from './PopupDefaultExample';
-
-// Snapshot Tests
-it('should shallow a default component', () => {
-  const popup = shallow(<PopupDefaultExample />);
-  expect(popup).toMatchSnapshot();
-});
