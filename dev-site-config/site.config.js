fc746614cb7d3d4bc04d98cad290a933dd0642fe
Matt Henkes <mjhenkes@gmail.com>
2018-04-01 00:21:33 -0500

    Consume wrapped webpack-serve server. Move generated component config. Use defaults in site.config

--- dev-site-config/site.config.js
+++ dev-site-config/site.config.js
@@ -1,20 +1,15 @@
 import homeReadMe from '../README.md';
-import componentConfig from '../generatedComponentConfig';
+import componentConfig from './generatedComponentConfig';
 import '../packages/terra-site/src/cerner-mock-theme.scss';
 
 const siteConfig = {
   /* The component configuration. */
   componentConfig,
 
-  /* The logo to display as the placeholder. */
-  placeholderSrc: 'https://github.com/cerner/terra-core/raw/master/terra.png',
-
   /* The markdown content to display on the home page. */
   readMeContent: homeReadMe,
 
   appConfig: {
-    /* The logo for the site header. */
-    logoSrc: 'https://github.com/cerner/terra-core/raw/master/terra.png',
 
     /* The title for the site header. */
     title: 'Terra Core',
