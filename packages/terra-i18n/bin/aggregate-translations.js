58e6685fd98353c62b4b32101507c49e191e2b39
Matt Henkes <mjhenkes@gmail.com>
2018-04-05 16:03:47 -0500

    remove aggregator script, alias to the terra-toolkit version for passivity.

--- packages/terra-i18n/bin/aggregate-translations.js
+++ packages/terra-i18n/bin/aggregate-translations.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
-require('../scripts/aggregate-translations/aggregate-translations-cli');
+// eslint-disable-next-line no-console
+console.warn('WARNING: The terra-i18n aggregate-translations command has moved to terra terra-toolkit. Please use the "tt:aggregate-translations" command instead.');
+
+require('terra-toolkit/scripts/aggregate-translations/aggregate-translations-cli');
