f5876d58255dd11abdacab6aff27c34b102ff85f
Matt Henkes <mjhenkes@gmail.com>
2018-04-10 07:17:30 -0500

    Revert "remove aggregator script, alias to the terra-toolkit version for passivity."
    This reverts commit 58e6685fd98353c62b4b32101507c49e191e2b39.

--- packages/terra-i18n/bin/aggregate-translations.js
+++ packages/terra-i18n/bin/aggregate-translations.js
@@ -1,6 +1,3 @@
 #!/usr/bin/env node
 
-// eslint-disable-next-line no-console
-console.warn('WARNING: The terra-i18n aggregate-translations command has moved to terra terra-toolkit. Please use the "tt:aggregate-translations" command instead.');
-
-require('terra-toolkit/scripts/aggregate-translations/aggregate-translations-cli');
+require('../scripts/aggregate-translations/aggregate-translations-cli');
