9438092020cab57c786fae693e3a839f2d2e3782
Matt Henkes <mjhenkes@gmail.com>
2018-04-12 10:24:18 -0500

    Remove more web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,8 @@
 const path = require('path');
 const webpackConfig = require('terra-dev-site/src/webpack/webpack.config');
-const aggregateTranslations = require('./packages/terra-i18n/scripts/aggregate-translations/aggregate-translations');
-
-aggregateTranslations({ baseDirectory: __dirname });
 
 const processPath = process.cwd();
 const rootPath = processPath.includes('packages') ? processPath.split('packages')[0] : processPath;
-
-// Remove the terra-i18n-plugin from the configuration
-// const startPlugins = webpackConfig.plugins.splice(0, 2);
-// const endPlugins = webpackConfig.plugins.splice(1, webpackConfig.plugins.length);
-// webpackConfig.plugins = startPlugins.concat(endPlugins);
 
 const momentAlias = path.resolve(path.join(rootPath, 'packages', 'terra-date-time-picker', 'node_modules', 'moment'));
 webpackConfig.resolve.alias.moment = momentAlias;
