5be0247512eb55dcaea146692d47de1b22af5aee
Jaime Mackey <jaime.mackey@cerner.com>
2018-04-13 13:21:48 -0500

    Remove outdated screenshots

--- packages/terra-text/tests/nightwatch/TextLineHeight.jsx
+++ packages/terra-text/tests/nightwatch/TextLineHeight.jsx
@@ -6,13 +6,13 @@ const TextLineHeight = () => (
     <div style={{ fontSize: '100px' }}>
       Parent font size: 100px
       <Text fontSize={16}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at ex tellus. Duis eget maximus ante, vitae bibendum libero. Integer.
       </Text>
     </div>
     <div style={{ 'font-size': '8px' }}>
       Parent font size: 8px
       <Text id="lineHeight" fontSize={16}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at ex tellus. Duis eget maximus ante, vitae bibendum libero. Integer.
       </Text>
     </div>
   </div>
