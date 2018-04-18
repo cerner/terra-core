10a2ea8ed4c0db8eb6c6f4766c4ccd9a196ad871
Perrin,Ryan <ryan.perrin@cerner.com>
2018-04-18 12:00:28 -0500

    Removed old file. Added functions for page sequence and total pages.

--- packages/terra-paginator/src/Paginator.jsx
+++ packages/terra-paginator/src/Paginator.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import Button from 'terra-button';
+import IconNext from 'terra-icon/lib/icon/IconNext';
+import IconPrevious from 'terra-icon/lib/icon/IconPrevious';
+
 import 'terra-base/lib/baseStyles';
 import styles from './Paginator.scss';
 
@@ -24,6 +28,36 @@ const propTypes = {
    */
   itemCountPerPage: PropTypes.number,
 };
+
+const calculatePages = (totalCount, itemCountPerPage) => Math.ceil(totalCount / itemCountPerPage);
+
+const pageSequence = (index, totalPages) => {
+  var sequence = [];
+  var numberShiftPoint = 7;
+  var maxPagesDisplayed = 10;
+  var previousSequenceCount = 5;
+  var nextSequenceCount = 4;
+
+  if (index < numberShiftPoint) {
+    for (var i = 1; i <= 10; i++) {
+      sequence.push(i);
+    }
+  } else if (index <= totalPages && index > totalPages - previousSequenceCount) {
+    for (var i = totalPages; i > totalPages - maxPagesDisplayed; i--) {
+      sequence.push(i);
+    }
+  } else {
+    for (var i = index; i >= index - previousSequenceCount; i--) {
+      sequence.push(i);
+    }
+
+    for (var i = index + 1; i <= index + nextSequenceCount; i++) {
+      sequence.push(i);
+    }
+  }
+
+  return sequence.sort((a, b) => a - b);
+}
 
 class Paginator extends React.Component {
   constructor(props) {
