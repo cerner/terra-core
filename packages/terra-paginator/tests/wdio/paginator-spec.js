dae74d49440a1f5cb8a3d94af3b4793913011d6a
Perrin,Ryan <ryan.perrin@cerner.com>
2018-04-24 10:07:24 -0500

    Updated theme variables. Updated wdio test.

--- packages/terra-paginator/tests/wdio/paginator-spec.js
+++ packages/terra-paginator/tests/wdio/paginator-spec.js
@@ -7,15 +7,17 @@ describe('Paginator', () => {
 
     Terra.should.beAccessible({ viewports });
     Terra.should.matchScreenshot({ viewports });
-    Terra.should.themeEachCustomProperty(
-      {
+    Terra.should.themeCombinationOfCustomProperties({
+      testName: 'themed',
+      properties: {
         "--terra-paginator-nav-link-foreground-color": "red",
+        "--terra-paginator-nav-link-foreground-color-hover": "#001F67",
+        "--terra-paginator-nav-link-foreground-color-disabled": "purple",
         "--terra-paginator-nav-link-margin-left": "0.5px",
         "--terra-paginator-nav-link-margin-right": "0.5px",
         "--terra-paginator-nav-link-min-width": "2rem",
         "--terra-paginator-nav-link-padding": "2px",
-        "--terra-button-disabled-opacity": "0.1"
       },
-    );
+    });
   });
 });
