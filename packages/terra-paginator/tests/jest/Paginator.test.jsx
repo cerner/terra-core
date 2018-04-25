a4e3d39ec3695d7a970c6a257a27a13ea7dd30aa
Perrin,Ryan <ryan.perrin@cerner.com>
2018-04-25 11:07:38 -0500

    More lint!

--- packages/terra-paginator/tests/jest/Paginator.test.jsx
+++ packages/terra-paginator/tests/jest/Paginator.test.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Paginator from '../../src/Paginator';
 
 describe('Paginator', () => {
-  const defaultRender = <Paginator onPageChange={(e) => typeof(e)} selectedPage={1} totalCount={2234} itemCountPerPage={20} />;
-  const noPagesRender = <Paginator onPageChange={(e) => typeof(e)} />;
+  const defaultRender = <Paginator onPageChange={e => typeof e} selectedPage={1} totalCount={2234} itemCountPerPage={20} />;
+  const noPagesRender = <Paginator onPageChange={e => typeof e} />;
 
   // Snapshot Tests
   it('should render a default paginator', () => {
--- packages/terra-paginator/tests/jest/ProgressivePaginator.test.jsx
+++ packages/terra-paginator/tests/jest/ProgressivePaginator.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProgressivePaginator from '../../src/ProgressivePaginator';
 
 describe('ProgressivePaginator', () => {
-  const defaultRender = <ProgressivePaginator onPageChange={(e) => typeof(e)} selectedPage={1} totalCount={2234} itemCountPerPage={20} />;
+  const defaultRender = <ProgressivePaginator onPageChange={e => typeof e} selectedPage={1} totalCount={2234} itemCountPerPage={20} />;
 
   // Snapshot Tests
   it('should render a default ProgressivePaginator', () => {
