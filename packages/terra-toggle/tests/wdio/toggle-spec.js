976b8565629b9325d7cc04dfc2d0148c81365389
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-04-27 12:49:45 -0500

    Nightwatch -> Wide.

---
+++ packages/terra-toggle/tests/wdio/toggle-spec.js
@@ -0,0 +1,47 @@
+/* global browser, Terra, before */
+
+describe('Toggle', () => {
+  before(() => browser.setViewportSize(Terra.viewports('medium')[0]));
+
+  describe('Default', () => {
+    before(() => browser.url('/#/raw/tests/toggle/default-toggle'));
+
+    Terra.should.beAccessible();
+    Terra.should.matchScreenshot('closed');
+
+    it('has aria-hidden=true when collaped', () => {
+      expect(browser.getAttribute('#toggle', 'aria-hidden')).to.equal('true');
+    });
+
+    it('has aria-hidden=false when expanded', () => {
+      browser.click('#trigger-toggle');
+      expect(browser.getAttribute('#toggle', 'aria-hidden')).to.equal('false');
+    });
+
+    Terra.should.matchScreenshot('opened');
+  });
+
+  describe('Opened', () => {
+    beforeEach(() => browser.url('/#/raw/tests/toggle/open-toggle'));
+
+    Terra.should.beAccessible();
+    Terra.should.matchScreenshot();
+  });
+
+  describe('Animated', () => {
+    beforeEach(() => browser.url('/#/raw/tests/toggle/animated-toggle'));
+
+    it('disables focusable elements when closed', () => {
+      expect(browser.getAttribute('#toggle', 'aria-hidden')).to.equal('true');
+      expect(browser.getCssProperty('#toggle', 'visibility').value).to.equal('hidden');
+    });
+
+    it('enables focusable elements when opened', () => {
+      browser.click('#trigger-toggle'); // Open toggle
+      browser.waitForVisible('#toggle', 1000);
+
+      expect(browser.getAttribute('#toggle', 'aria-hidden')).to.equal('false');
+      expect(browser.getCssProperty('#toggle', 'visibility').value).to.equal('visible');
+    });
+  });
+});
