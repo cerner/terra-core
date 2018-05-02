c143318067b974762ce7458a1f44c4bf6765f3cb
rakshith91 <rakshith.bhyravabhotla@gmail.com>
2018-05-02 14:51:55 -0500

    Add 7000 and 9000 z-index for abstract_modal

--- packages/terra-abstract-modal/src/_ModalContent.jsx
+++ packages/terra-abstract-modal/src/_ModalContent.jsx
@@ -8,7 +8,7 @@ import styles from './AbstractModal.scss';
 
 const cx = classNames.bind(styles);
 
-const zIndexes = ['6000', '8000'];
+const zIndexes = ['6000', '7000', '8000', '9000'];
 
 const propTypes = {
   /**
