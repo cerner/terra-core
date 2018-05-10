86bbe12ed430dddf49fc8c4cc511418fc5de1c12
Dylan Klohr <dylan.sftwr@gmail.com>
2018-05-09 20:20:10 -0500

    Spacer shorthand enhancement (#1457)

--- packages/terra-spacer/tests/jest/Spacer.test.jsx
+++ packages/terra-spacer/tests/jest/Spacer.test.jsx
@@ -29,6 +29,11 @@ describe('Spacer', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('should render a spacer component with large vertical and small horizontal margin', () => {
+    const wrapper = render(<Spacer margin="large small"><p>Test</p></Spacer>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
   it('should render a spacer component with all margin props set to the given values', () => {
     const wrapper =
     render(
@@ -50,6 +55,47 @@ describe('Spacer', () => {
   // Custom Prop Test
   it('it should pass in a custom prop', () => {
     const wrapper = render(<Spacer className="spacer"><p>Test</p></Spacer>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  // Shorthand Mapping Tests
+  it('should render a spacer component with top, right, bottom, and left margin set to large', () => {
+    const wrapper = render(<Spacer margin="large"><p>Test</p></Spacer>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a spacer component with vertical margin set to large and horizontal margin set to small', () => {
+    const wrapper = render(<Spacer margin="large small"><p>Test</p></Spacer>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a spacer component with vertical top margin set to large, horizontal margin set to medium, and bottom margin set to small', () => {
+    const wrapper = render(<Spacer margin="large medium small"><p>Test</p></Spacer>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a spacer component with vertical top margin set to large, right margin set to medium, bottom margin set to small, and left margin set to small-1 ', () => {
+    const wrapper = render(<Spacer margin="large medium small small-1"><p>Test</p></Spacer>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a spacer component with top, right, bottom, and left padding set to large', () => {
+    const wrapper = render(<Spacer padding="large"><p>Test</p></Spacer>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a spacer component with vertical padding set to large and horizontal padding set to small', () => {
+    const wrapper = render(<Spacer padding="large small"><p>Test</p></Spacer>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a spacer component with vertical top padding set to large, horizontal padding set to medium, and bottom padding set to small', () => {
+    const wrapper = render(<Spacer padding="large medium small"><p>Test</p></Spacer>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a spacer component with vertical top padding set to large, right padding set to medium, bottom padding set to small, and left padding set to small-1 ', () => {
+    const wrapper = render(<Spacer padding="large medium small small-1"><p>Test</p></Spacer>);
     expect(wrapper).toMatchSnapshot();
   });
 });
