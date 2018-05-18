bc119b3992e0e1a3d2cb6900d2d5469c2570ede6
Matt Henkes <mjhenkes@gmail.com>
2018-05-18 10:46:03 -0500

    update config

--- dev-site-config/site.config.js
+++ dev-site-config/site.config.js
@@ -1,33 +1,20 @@
-import homeReadMe from '../README.md';
-import componentConfig from './generatedComponentConfig';
-import '../packages/terra-site/src/cerner-mock-theme.scss';
-import '../packages/terra-legacy-theme/src/scoped-theme';
 
 const siteConfig = {
-  /* The component configuration. */
-  componentConfig,
 
-  /* The markdown content to display on the home page. */
-  readMeContent: homeReadMe,
+  themeImports: [
+    '../packages/terra-legacy-theme/src/scoped-theme',
+  ],
 
   appConfig: {
-
-    /* The title for the site header. */
-    title: 'Terra Core',
 
     defaultTheme: 'Default Theme',
 
     /* The themes to toggle in the site. */
     themes: {
       'Default Theme': '',
-      'Legacy Theme': 'terra-legacy-theme',
       'Mock Theme': 'cerner-mock-theme',
-    },
-
-    extensions: {
-      gitHubUrl: 'https://github.com/cerner/terra-core',
     },
   },
 };
 
-export default siteConfig;
+module.exports = siteConfig;
