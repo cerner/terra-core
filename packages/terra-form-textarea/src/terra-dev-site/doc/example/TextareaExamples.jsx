f5fb6ee3ff7378e4c10d56237277f3ea8addc167
Matt Henkes <mjhenkes@gmail.com>
2018-05-22 13:14:41 -0500

    fix text area

--- packages/terra-form-textarea/src/terra-dev-site/doc/example/TextareaExamples.jsx
+++ packages/terra-form-textarea/src/terra-dev-site/doc/example/TextareaExamples.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // eslint-disable-next-line import/no-unresolved, import/extensions
-import Textarea from 'terra-form-textarea/lib/FormTextArea';
+import Textarea from 'terra-form-textarea/lib/TextArea';
 
 class TextareaExamples extends React.Component {
   constructor(props) {
