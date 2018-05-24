5923c9d521b2cbffb8c7d350809e4c66ec98ea44
Esser,Stephen <Stephen.Esser@Cerner.com>
2018-05-24 16:08:22 -0500

    Set the select field example to have a max width

--- packages/terra-site/src/examples/form-select/examples/SelectField.jsx
+++ packages/terra-site/src/examples/form-select/examples/SelectField.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SelectField from 'terra-form-select/lib/SelectField';
 
 const SelectFieldExample = () => (
-  <SelectField label="T-shirt size" placeholder="Select a size" selectId="select-field" required style={{ width: '300px' }}>
+  <SelectField label="T-shirt size" placeholder="Select a size" selectId="select-field" required style={{ maxWidth: '300px' }}>
     <SelectField.Option value="xSmall" display="Extra Small" />
     <SelectField.Option value="small" display="Small" />
     <SelectField.Option value="medium" display="Medium" />
