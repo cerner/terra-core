bca1a061e7db7728678fc059c25c3ff8a8cbf2d5
Matt Henkes <mjhenkes@gmail.com>
2018-05-24 11:14:57 -0500

    some more

--- packages/terra-overlay/tests/wdio/overlay-container-spec.js
+++ packages/terra-overlay/tests/wdio/overlay-container-spec.js
@@ -1,7 +1,7 @@
 /* global before, browser, Terra */
 
 describe('Overlay Container Default', () => {
-  before(() => browser.url('/#/raw/tests/overlay/overlay-container/default-overlay-container'));
+  before(() => browser.url('/#/raw/tests/terra-overlay/overlay/overlay-container/default-overlay-container'));
 
   Terra.should.beAccessible();
   Terra.should.matchScreenshot();
