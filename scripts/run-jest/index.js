d2b55f745b5937892998c6369c6e1680b2ceff9c
Boersma <BB060837@cerner.net>
2018-05-25 12:33:59 -0500

    Linting the newly created script

--- scripts/run-jest/index.js
+++ scripts/run-jest/index.js
@@ -1,36 +1,19 @@
-var exec = require('child_process').exec;
-var spawn = require('child_process').spawn;
-var path = require("path");
-var os = require('os');
+const spawn = require('child_process').spawn;
+const os = require('os');
 
-function log(error, stdout, stderr) {
-	if (error) {
-		console.error(`exec error: ${error}`);
-		return;
-	}
-	console.log(`stdout: ${stdout}`);
-	console.log(`stderr: ${stderr}`);
-}
-
-var regex = "";
-var jestcmd = "";
+let regex = '';
+let jestcmd = '';
 if (os.type() === 'Linux') {
-	regex = process.env.npm_package_name + "/tests";
-	jestcmd = "jest";
+  regex = `${process.env.npm_package_name}/tests`;
+  jestcmd = 'jest';
+} else if (os.type() === 'Darwin') {
+  regex = `${process.env.npm_package_name}/tests`;
+  jestcmd = 'jest';
+} else if (os.type() === 'Windows_NT') {
+  regex = `${process.env.npm_package_name}\\\\tests`;
+  jestcmd = 'jest.cmd';
+} else {
+  throw new Error(`Unsupported OS found: ${os.type()}`);
 }
-else if (os.type() === 'Darwin') {
-	regex = process.env.npm_package_name + "/tests";
-	jestcmd = "jest";
-}
-else if (os.type() === 'Windows_NT') {
-	regex = process.env.npm_package_name + "\\\\tests";
-	jestcmd = "jest.cmd";
-}
-else
-   throw new Error("Unsupported OS found: " + os.type());
 
-var jest = spawn(jestcmd, [regex, "--config", "../../jestconfig.json"], {stdio: 'inherit'});
-jest.on('error', function (err) {
-  console.log('jest error', err);
-});
-
+spawn(jestcmd, [regex, '--config', '../../jestconfig.json'], { stdio: 'inherit' });
