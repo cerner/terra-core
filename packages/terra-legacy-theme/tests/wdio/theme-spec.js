d2de62f4dea88d75644fc5c60993e0434115af37
Kasper, Dave <dave.m.kasper@gmail.com>
2018-05-25 15:14:53 -0500

    Update legacy theme tests.

--- packages/terra-legacy-theme/tests/wdio/theme-spec.js
+++ packages/terra-legacy-theme/tests/wdio/theme-spec.js
@@ -1,50 +1,25 @@
 /* global browser, Terra, before */
 describe('Legacy Theme', () => {
-  before(() => browser.setViewportSize(Terra.viewports('large')[0]));
+  before(() => {
+    browser.setViewportSize(Terra.viewports('large')[0]);
+    browser.url('/#/raw/tests/terra-legacy-theme/legacy-theme/legecy-theme-navigation-side-menu');
+  });
 
   describe('Navigation Side Menu', () => {
-    beforeEach(() => {
-      browser.url('/#/tests');
-      browser.execute('document.body.classList.add("terra-legacy-theme");');
-    });
-
-    Terra.should.beAccessible({ context: '[data-routing-menu]' });
-    Terra.should.matchScreenshot({ selector: '[data-routing-menu]' });
-  });
-
-  describe('Navigation Side Menu Hover', () => {
-    beforeEach(() => {
-      browser.url('/#/tests');
-      browser.execute('document.body.classList.add("terra-legacy-theme");');
-      browser.moveToObject('[data-menu-item="Action Footer"]');
-    });
-
-    Terra.should.beAccessible({ context: '[data-routing-menu]' });
-    Terra.should.matchScreenshot({ selector: '[data-routing-menu]' });
-  });
-
-  describe('Navigation Side Menu Selected', () => {
-    beforeEach(() => {
-      browser.url('/#/tests');
-      browser.click('[data-menu-item="Action Footer"]');
-      browser.click('button[aria-label="Back"]');
-      browser.execute('document.body.classList.add("terra-legacy-theme");');
-    });
-
-    Terra.should.beAccessible({ context: '[data-routing-menu]' });
-    Terra.should.matchScreenshot({ selector: '[data-routing-menu]' });
-  });
-
-  describe('Navigation Side Menu Selected Hover', () => {
-    beforeEach(() => {
-      browser.url('/#/tests');
-      browser.click('[data-menu-item="Action Footer"]');
-      browser.click('button[aria-label="Back"]');
-      browser.execute('document.body.classList.add("terra-legacy-theme");');
-      browser.moveToObject('[data-menu-item="Action Footer"]');
-    });
-
-    Terra.should.beAccessible({ context: '[data-routing-menu]' });
-    Terra.should.matchScreenshot({ selector: '[data-routing-menu]' });
+    Terra.should.matchScreenshot('list-padding-top', { selector: '.legacy-nav-menu-list-padding-top' });
+    Terra.should.matchScreenshot('list-background-color', { selector: '.legacy-nav-menu-list-background-color' });
+    Terra.should.matchScreenshot('item-border-bottom', { selector: '.legacy-nav-menu-item-border-bottom' });
+    Terra.should.matchScreenshot('item-color', { selector: '.legacy-nav-menu-item-color' });
+    Terra.should.matchScreenshot('item-chevron-color', { selector: '.legacy-nav-menu-item-chevron-color' });
+    Terra.should.matchScreenshot('item-hover-background-color', { selector: '.legacy-nav-menu-item-hover-background-color' });
+    Terra.should.matchScreenshot('item-hover-color', { selector: '.legacy-nav-menu-item-hover-color' });
+    Terra.should.matchScreenshot('item-selected-border-left', { selector: '.legacy-nav-menu-item-selected-border-left' });
+    Terra.should.matchScreenshot('item-selected-font-weight', { selector: '.legacy-nav-menu-item-selected-font-weight' });
+    Terra.should.matchScreenshot('item-selected-background-color', { selector: '.legacy-nav-menu-item-selected-background-color' });
+    Terra.should.matchScreenshot('item-selected-color', { selector: '.legacy-nav-menu-item-selected-color' });
+    Terra.should.matchScreenshot('item-selected-hover-color', { selector: '.legacy-nav-menu-item-selected-hover-color' });
+    Terra.should.matchScreenshot('action-header-background-color', { selector: '.legacy-nav-menu-action-header-background-color' });
+    Terra.should.matchScreenshot('action-header-border-bottom-color', { selector: '.legacy-nav-menu-action-header-border-bottom-color' });
+    Terra.should.matchScreenshot('action-header-color', { selector: '.legacy-nav-menu-action-header-color' });
   });
 });
