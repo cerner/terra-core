547c585a5a24820c3462e84427f1be635442a67d
Kasper, Dave <dave.m.kasper@gmail.com>
2018-05-30 10:44:06 -0500

    Add doc example.

--- packages/terra-scrollable-container/src/terra-dev-site/doc/ScrollableContainer.doc.jsx
+++ packages/terra-scrollable-container/src/terra-dev-site/doc/ScrollableContainer.doc.jsx
@@ -3,14 +3,14 @@ import DocTemplate from 'terra-doc-template';
 import ReadMe from '../../../docs/README.md';
 import { name } from '../../../package.json';
 
-/* eslint-disable import/no-extraneous-dependencies, import/no-webpack-loader-syntax, import/first, import/no-unresolved, import/extensions */
+/* eslint-disable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
 // Component Source
 import ScrollableContainerSrc from '!raw-loader!terra-scrollable-container/src/ScrollableContainer';
 
 // Example Files
 import ScrollableContainerExample from './ScrollableContainerExample';
 import ScrollableContainerExampleSrc from '!raw-loader!../../../src/terra-dev-site/doc/example/ScrollableContainerExample';
-/* eslint-enable import/no-extraneous-dependencies, import/no-webpack-loader-syntax, import/first, import/no-unresolved, import/extensions */
+/* eslint-enable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
 
 const DocPage = () => (
   <DocTemplate
