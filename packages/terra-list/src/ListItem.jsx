0f3848ec1f730855203959aa109b87b9c7772e8e
Nathan Faltermeier <nefalt2@gmail.com>
2018-06-15 08:59:04 -0500

    Greatly simplify solution

--- packages/terra-list/src/ListItem.jsx
+++ packages/terra-list/src/ListItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import 'terra-base/lib/baseStyles';
+import Arrange from 'terra-arrange';
 import ChevronRight from 'terra-icon/lib/icon/IconChevronRight';
 import styles from './List.scss';
 
@@ -37,78 +38,42 @@ const defaultProps = {
   hasChevron: undefined,
 };
 
-class ListItem extends React.Component {
-  constructor(props) {
-    super(props);
+const ListItem = ({
+    content,
+    isSelected,
+    isSelectable,
+    hasChevron,
+    refCallback,
+    ...customProps
+  }) => {
+  const listItemClassNames = cx([
+    'list-item',
+    { selected: isSelected },
+    { 'is-selectable': isSelectable },
+    customProps.className,
+  ]);
 
-    this.refHandler = this.refHandler.bind(this);
-    this.adjustWidths = this.adjustWidths.bind(this);
-
-    this.state = { contentMaxWidth: '100%' };
-  }
-
-  componentDidMount() {
-    if (this.innerNode && this.chevron) {
-      this.adjustWidths();
-    }
-  }
-
-  adjustWidths() {
-    const maxWidth = this.innerNode.offsetWidth - this.chevron.offsetWidth;
-    this.setState({ contentMaxWidth: maxWidth });
-  }
-
-  refHandler(element) {
-    this.innerNode = element;
-
-    if (this.props.refCallback) {
-      this.props.refCallback(element);
-    }
-  }
-
-  render() {
-    const {
-      content,
-      isSelected,
-      isSelectable,
-      hasChevron,
-      refCallback,
-      ...customProps
-    } = this.props;
-
-    const listItemClassNames = cx([
-      'list-item',
-      { selected: isSelected },
-      { 'is-selectable': isSelectable },
-      customProps.className,
-    ]);
-
-    if (hasChevron) {
-      const chevron = (
-        <span ref={(element) => { this.chevron = element; }} className={cx('chevron')}>
-          <ChevronRight height="0.8em" width="0.8em" />
-        </span>
-      );
-
-      return (
-        <li {...customProps} role="option" aria-selected={isSelected} className={listItemClassNames} ref={this.refHandler}>
-          <div className={cx('chevron-parent')}>
-            {chevron}
-            <div style={{ maxWidth: this.state.contentMaxWidth }}>
-              {content}
-            </div>
-          </div>
-        </li>
-      );
-    }
+  if (hasChevron) {
+    const chevron = <span className={cx('chevron')}><ChevronRight height="0.8em" width="0.8em" /></span>;
 
     return (
-      <li {...customProps} className={listItemClassNames} ref={refCallback}>
-        {content}
+      <li {...customProps} role="option" aria-selected={isSelected} className={listItemClassNames} ref={refCallback}>
+        <Arrange
+          fill={content}
+          fitEnd={chevron}
+          align={'center'}
+          fillAttributes={{ className: cx('arrange-content') }}
+        />
       </li>
     );
   }
-}
+
+  return (
+    <li {...customProps} className={listItemClassNames} ref={refCallback}>
+      {content}
+    </li>
+  );
+};
 
 ListItem.propTypes = propTypes;
 ListItem.defaultProps = defaultProps;
