400e3b3aab6e48283d79d59d81f7a8a21454902b
Sai Kaushik Mallela <Kaushik.Mallela@cerner.com>
2018-06-22 10:11:10 -0500

    form-fieldset, form-input, form-radio

--- packages/terra-form-fieldset/src/terra-dev-site/doc/form-fieldset/FormFieldset.doc.jsx
+++ packages/terra-form-fieldset/src/terra-dev-site/doc/form-fieldset/FormFieldset.doc.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import DocTemplate from 'terra-doc-template';
-import ReadMe from '../../../docs/README.md';
-import { name } from '../../../package.json';
+import ReadMe from '../../../../docs/README.md';
+import { name } from '../../../../package.json';
 
 /* eslint-disable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
 // Component Source
-import FieldsetSrc from '!raw-loader!../../../src/Fieldset';
+import FieldsetSrc from '!raw-loader!../../../../src/Fieldset';
 
 // Example Files
-import FieldsetExamples from './example/FieldsetExamples';
+import FieldsetExamples from '../example/FieldsetExamples';
 /* eslint-enable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
 
 const DocPage = () => (
