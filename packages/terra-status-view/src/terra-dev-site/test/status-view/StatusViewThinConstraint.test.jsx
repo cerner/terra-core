49bc4026c19da13e8cb2814fd8f2ba83e624ad06
Nathan Faltermeier <nefalt2@gmail.com>
2018-06-28 10:05:26 -0500

    Contents wrap, buttons don't

--- packages/terra-status-view/src/terra-dev-site/test/status-view/StatusViewThinConstraint.test.jsx
+++ packages/terra-status-view/src/terra-dev-site/test/status-view/StatusViewThinConstraint.test.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Button from 'terra-button';
 import StatusView from '../../../StatusView';
 
-const ipsum = 'Lorem ipsum dolor';
+// const ipsum = 'Lore\u200Bm ip\u00ADsum dolor';
+const ipsum = 'Oh mama mia how could this happen, this is one realllllllllllllllyyyyyyyyyylongwordrighthereyesitis';
 
 export default () =>
-  <div style={{ width: '70px', border: '1px black solid' }}>
+  <div style={{ width: '140px', border: '1px black solid' }}>
     <StatusView id="statusView" variant="error" title={ipsum} message={ipsum}>
       <Button text={ipsum} key="1" id="button1" />
       <Button text={ipsum} key="2" id="button2" />
