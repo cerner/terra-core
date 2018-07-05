dd7da91ac14038917338aaf1721380d72a9a59e0
Nathan Faltermeier <nefalt2@gmail.com>
2018-07-05 14:54:59 -0500

    Status view won't unnecessarily break words (#1668)

--- packages/terra-status-view/src/terra-dev-site/test/status-view/StatusViewTextWrapping.test.jsx
+++ packages/terra-status-view/src/terra-dev-site/test/status-view/StatusViewTextWrapping.test.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Button from 'terra-button';
 import StatusView from '../../../StatusView';
 
-const ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+const ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. AnIncrediblyIncrediblyIncrediblyIncrediblyIncrediblyIncrediblyLongWord.';
 
-export default () =>
-  <div>
+export default () => (
+  <div style={{ maxWidth: '500px', border: '1px black solid' }}>
     <StatusView id="statusView" variant="error" title={ipsum} message={ipsum}>
       <Button text={ipsum} key="1" id="button1" />
       <Button text={ipsum} key="2" id="button2" />
     </StatusView>
-  </div>;
+  </div>);
