f46612e7fea6ac525279c8736ec73a02d5914702
Brett Jankord <bjankord@gmail.com>
2018-07-09 17:43:56 -0500

    Update OCS icons to v1.19.1 (#1675)

---
+++ packages/terra-icon/src/icon/IconCriticalInverseLowLight.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import React from 'react';
+import IconBase from '../IconBase';
+
+const SvgIcon = (customProps) => {
+  const attributes = Object.assign({}, customProps);
+
+  return (
+    <IconBase {...attributes}>
+      <path fill="#B1B5B6" d="M47.7 23.2L24.8.3c-.4-.4-1.1-.4-1.6 0L.3 23.2c-.4.4-.4 1.1 0 1.6l22.9 22.9c.4.4 1.1.4 1.6 0l22.9-22.9c.4-.4.4-1.2 0-1.6zM27 39h-6v-6h6v6zm0-11h-6V8h6v20z" ></path>
+    </IconBase>
+  );
+};
+
+SvgIcon.displayName = "IconCriticalInverseLowLight";
+SvgIcon.defaultProps = {"viewBox":"0 0 48 48","xmlns":"http://www.w3.org/2000/svg"};
+
+export default SvgIcon;
+/* eslint-enable */
