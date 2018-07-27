c55d3fc05de52f7288dc7747742c879c2126dbdd
Nathan Faltermeier <nefalt2@gmail.com>
2018-07-27 10:11:37 -0500

    Update test pages

--- packages/terra-button/src/terra-dev-site/test/button/IconButton.test.jsx
+++ packages/terra-button/src/terra-dev-site/test/button/IconButton.test.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
 import Button from '../../../Button';
+import IconSquare from 'terra-icon/lib/icon/IconSquareSymbol';
 
 const spacingStyle = { padding: '20px' };
 
-const iconStyle = { backgroundColor: 'black', height: '1em', width: '1em' };
-const icon = <span style={iconStyle} className="test"><svg width="1em" height="1em"><rect width="1em" height="1em" /></svg></span>;
-
-const IconNeutralButton = () => <Button id="iconNeutralButton" text="Button with Icon" icon={icon} />;
-const IconOnlyButton = () => <Button id="iconOnlyButton" text="iconOnlyButton" isIconOnly icon={icon} />;
-const IconReversedButton = () => <Button id="iconReversedButton" text="Button with Icon and reversed" icon={icon} isReversed />;
+const IconNeutralButton = () => <Button id="iconNeutralButton" text="Button with Icon" icon={<IconSquare />} />;
+const IconOnlyButton = () => <Button id="iconOnlyButton" text="iconOnlyButton" isIconOnly icon={<IconSquare />} />;
+const IconReversedButton = () => <Button id="iconReversedButton" text="Button with Icon and reversed" icon={<IconSquare />} isReversed />;
 
 
 export default () => (
--- packages/terra-button/src/terra-dev-site/test/button/Variants/UtilityButton.test.jsx
+++ packages/terra-button/src/terra-dev-site/test/button/Variants/UtilityButton.test.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import Button from '../../../../Button';
-
-const iconStyle = { backgroundColor: 'black' };
-const icon = <svg width="1em" height="1em" style={iconStyle} className="test"><rect width="1em" height="1em" /></svg>;
+import IconSquare from 'terra-icon/lib/icon/IconSquareSymbol';
 
 const UtilityButton = () => (
   <div id="utility" style={{ padding: '5px' }}>
-    <Button id="utilityButton" text="Utility" icon={icon} variant="utility" />
-    <Button id="utilityButtonDisabled" text="Utility Disabled" icon={icon} variant="utility" isDisabled />
+    <Button id="utilityButton" text="Utility" icon={<IconSquare />} variant="utility" />
+    <Button id="utilityButtonDisabled" text="Utility Disabled" icon={<IconSquare />} variant="utility" isDisabled />
   </div>);
 
 export default UtilityButton;
