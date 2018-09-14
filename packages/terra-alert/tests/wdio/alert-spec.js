39f4423115c2287b9f241e5539ea741baa919822
Derek Yu <yuderekyu@gmail.com>
2018-09-14 15:28:44 -0500

    [terra-menu] make padding for section css class themeable.

--- packages/terra-alert/tests/wdio/alert-spec.js
+++ packages/terra-alert/tests/wdio/alert-spec.js
@@ -25,6 +25,7 @@ describe('Alert', () => {
       testName: 'themed',
       properties: {
         '--terra-alert-min-height': '5rem',
+
         '--terra-alert-title-display': 'block',
         '--terra-alert-title-left': 'none',
         '--terra-alert-title-margin-bottom': '10px',
