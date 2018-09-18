39981757acfa3efda1a078345122abe2519a339e
Derek Yu <yuderekyu@gmail.com>
2018-09-18 11:01:34 -0500

    [terra-action-header] update margin theme var

--- packages/terra-action-header/tests/wdio/action-header-spec.js
+++ packages/terra-action-header/tests/wdio/action-header-spec.js
@@ -105,8 +105,6 @@ describe('ActionHeader - Themeable', () => {
         '--terra-action-header-box-shadow': '5px 5px 5px #551A8B',
         '--terra-action-header-color': '#FFC0CB',
         '--terra-action-header-font-size': '0.5rem',
-        '--terra-action-header-title-padding-left': '0.5rem',
-        '--terra-action-header-title-padding-right': '0.5rem',
         '--terra-action-header-title-padding-top': '.2rem',
         '--terra-action-header-padding': '1rem',
       },
