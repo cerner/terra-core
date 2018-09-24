15d020b449c6d7141b82f7c0c41c251adafa3cc6
Jake LaCombe <jake.lacombe2@gmail.com>
2018-09-24 15:43:01 -0500

    Add valueText prop onto progress bar for readability. (#1851)
    * Add valueText prop onto progress bar for readability.
    
    * Fix Changelog
    
    * Add link describing aria-valuetext
    
    * Update Site Examples

--- packages/terra-progress-bar/src/terra-dev-site/doc/example/ProgressBarSize.jsx
+++ packages/terra-progress-bar/src/terra-dev-site/doc/example/ProgressBarSize.jsx
@@ -4,19 +4,19 @@ import ProgressBar from 'terra-progress-bar/lib/ProgressBar';
 
 const ProgressBarSize = () => (
   <div>
-    <ProgressBar heightSize="tiny" value={15} color="#8ccc62" />
+    <ProgressBar heightSize="tiny" valueText="15%" value={15} color="#8ccc62" />
     <br />
     <br />
-    <ProgressBar heightSize="small" value={30} color="#8ccc62" />
+    <ProgressBar heightSize="small" valueText="30%" value={30} color="#8ccc62" />
     <br />
     <br />
-    <ProgressBar heightSize="medium" value={45} color="#8ccc62" />
+    <ProgressBar heightSize="medium" valueText="45%" value={45} color="#8ccc62" />
     <br />
     <br />
-    <ProgressBar heightSize="large" value={60} color="#8ccc62" />
+    <ProgressBar heightSize="large" valueText="60%" value={60} color="#8ccc62" />
     <br />
     <br />
-    <ProgressBar heightSize="huge" value={75} color="#8ccc62" />
+    <ProgressBar heightSize="huge" valueText="75%" value={75} color="#8ccc62" />
   </div>
 );
 
