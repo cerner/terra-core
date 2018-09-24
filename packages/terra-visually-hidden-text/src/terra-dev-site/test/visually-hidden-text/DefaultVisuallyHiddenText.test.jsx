e3dd700304dcc9ca7994b0f9a9328e9fd8258ef3
Jake LaCombe <jake.lacombe2@gmail.com>
2018-09-24 12:59:21 -0500

    Create Component for Visually Hidden Text (#1912)

---
+++ packages/terra-visually-hidden-text/src/terra-dev-site/test/visually-hidden-text/DefaultVisuallyHiddenText.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import VisuallyHiddenText from '../../../VisuallyHiddenText';
+
+export default () => (
+  <p>
+    No Screen Reader text is added on this page, and should be blank other than this line.
+    <VisuallyHiddenText />
+  </p>
+);
