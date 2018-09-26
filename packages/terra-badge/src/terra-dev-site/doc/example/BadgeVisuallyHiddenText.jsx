e8f3f3abdf86b5c909679164ff8a482b0ae3b66e
JakeLaCombe <jake.lacombe2@gmail.com>
2018-09-26 09:37:31 -0500

    Clean up text in examples

--- packages/terra-badge/src/terra-dev-site/doc/example/BadgeVisuallyHiddenText.jsx
+++ packages/terra-badge/src/terra-dev-site/doc/example/BadgeVisuallyHiddenText.jsx
@@ -4,13 +4,13 @@ import Badge from 'terra-badge/lib/Badge';
 
 const BadgeVisuallyHiddenText = () => (
   <div>
-    <Badge text="Low" visuallyHiddenText="Rick Factor" />
+    <Badge text="Low" visuallyHiddenText="Risk Factor" />
     {' '}
-    <Badge text="Medium" visuallyHiddenText="Rick Factor" />
+    <Badge text="Medium" visuallyHiddenText="Risk Factor" />
     {' '}
-    <Badge text="High" visuallyHiddenText="Rick Factor" />
+    <Badge text="High" visuallyHiddenText="Risk Factor" />
     {' '}
-    <Badge text="Critical" visuallyHiddenText="Rick Factor" />
+    <Badge text="Critical" visuallyHiddenText="Risk Factor" />
   </div>
 );
 
