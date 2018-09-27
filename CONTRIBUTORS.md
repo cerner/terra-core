29a23fa372e8b37cf3e5d3edb9e8b67fd1edf99f
kyleroush <roush.kyle@gmail.com>
2018-09-27 14:31:41 -0500

    Update jest missing locale tests (#1393) (#1932)

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -70,6 +70,7 @@ Cerner Corporation
 - Matthew Carr [@Matthematic]
 - Mahesh S [@m-samkc]
 - Alexander Brisimitzakis [@AlexBrizi]
+- Kyle Roush [@kyleroush]
 
 [@ryanthemanuel]: https://github.com/ryanthemanuel
 [@Matt-Butler]: https://github.com/Matt-Butler
@@ -142,3 +143,4 @@ Cerner Corporation
 [@Matthematic]: https://github.com/Matthematic
 [@m-samkc]: https://github.com/m-samkc
 [@AlexBrizi]: https://github.com/AlexBrizi
+[@kyleroush]: https://github.com/kyleroush
--- packages/terra-demographics-banner/tests/jest/DemographicsBanner.test.jsx
+++ packages/terra-demographics-banner/tests/jest/DemographicsBanner.test.jsx
@@ -35,9 +35,10 @@ it('renders the banner wrapper with all props', () => {
 });
 
 it('throws error on missing locale prop in Base', () => {
-  try {
+  global.console = { error: jest.fn() };
+
+  expect(() => {
     render(<DemographicsBanner />);
-  } catch (e) {
-    expect(e.message).toContain('Component is internationalized, and must be wrapped in terra-base');
-  }
+  }).toThrowError();
+  expect(console.error).toBeCalledWith(expect.stringContaining('Component is internationalized, and must be wrapped in terra-base'));
 });
