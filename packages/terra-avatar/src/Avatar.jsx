01049a1ea10449c00eb2d1c97e15a89f4b7f7520
Adam Parker <amichaelparker@gmail.com>
2018-10-10 13:49:46 -0500

    generated attributes updated

--- packages/terra-avatar/src/Avatar.jsx
+++ packages/terra-avatar/src/Avatar.jsx
@@ -55,7 +55,7 @@ class Avatar extends React.Component {
   }
 
   static generateImage(image, variant, alt) {
-    const icon = Avatar.generateImagePlaceholder(variant);
+    const icon = Avatar.generateImagePlaceholder(variant, alt);
 
     return <TerraImage className={cx('avatar-image')} src={image} placeholder={icon} alt={alt} />;
   }
