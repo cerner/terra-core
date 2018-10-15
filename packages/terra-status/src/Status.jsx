5170eeca854d95bcff8821bbedc92fbb30a7d84b
Alex Brisimitzakis <alex.brisimitzakis@cerner.com>
2018-10-15 12:54:12 -0500

    Fixed lint errors

--- packages/terra-status/src/Status.jsx
+++ packages/terra-status/src/Status.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import 'terra-base/lib/baseStyles';
 import styles from './Status.module.scss';
-import VisuallyHiddenText from '../../terra-visually-hidden-text'
+import VisuallyHiddenText from '../../terra-visually-hidden-text';
 
 const cx = classNames.bind(styles);
 
@@ -19,7 +19,12 @@ const propTypes = {
   visuallyHiddenText: PropTypes.string,
 };
 
-const Status = ({ color, children, visuallyHiddenText, ...customProps }) => (
+const Status = ({
+  color,
+  children,
+  visuallyHiddenText,
+  ...customProps
+}) => (
   <div {...customProps} style={{ borderColor: color }} className={cx('status', customProps.className)}>
     <VisuallyHiddenText text={visuallyHiddenText} />
     {children}
