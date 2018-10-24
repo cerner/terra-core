6d7d0eb0ee227073468ab424148cd837e98c65c7
Brett Jankord <bjankord@gmail.com>
2018-10-24 13:19:41 -0500

    Improve docs UI (#1986)

--- packages/terra-doc-template/src/ExampleTemplate.jsx
+++ packages/terra-doc-template/src/ExampleTemplate.jsx
@@ -45,8 +45,17 @@ class ExampleTemplate extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { isExpanded: false };
+    this.state = {
+      isExpanded: false,
+      isBackgroundTransparent: false,
+    };
+
+    this.handleBgToggle = this.handleBgToggle.bind(this);
     this.handleCodeToggle = this.handleCodeToggle.bind(this);
+  }
+
+  handleBgToggle() {
+    this.setState(prevState => ({ isBackgroundTransparent: !prevState.isBackgroundTransparent }));
   }
 
   handleCodeToggle() {
@@ -63,6 +72,14 @@ class ExampleTemplate extends React.Component {
       ...customProps
     } = this.props;
 
+    let dynamicContentStyle = {};
+
+    if (this.state.isBackgroundTransparent) {
+      dynamicContentStyle = {
+        backgroundColor: 'rgba(0,0,0,0)',
+      };
+    }
+
     return (
       <div {...customProps} className={cx('template', customProps.className)}>
         {title
@@ -73,7 +90,7 @@ class ExampleTemplate extends React.Component {
             </h2>
           </div>
           )}
-        <div className={cx('content')}>
+        <div className={cx('content')} style={dynamicContentStyle}>
           {description
             && (
             <div className={cx('description')}>
@@ -86,11 +103,16 @@ class ExampleTemplate extends React.Component {
         {exampleSrc
           && (
           <div className={cx('footer')}>
-            <button type="button" className={cx('toggle')} onClick={this.handleCodeToggle}>
-              <span className={cx('chevron-left')} />
-              <span>Code</span>
-              <span className={cx('chevron-right')} />
-            </button>
+            <div className={cx('button-container')}>
+              <button type="button" className={cx('bg-toggle')} onClick={this.handleBgToggle}>
+                Toggle Background
+              </button>
+              <button type="button" className={cx('code-toggle')} onClick={this.handleCodeToggle}>
+                <span className={cx('chevron-left')} />
+                <span>Code</span>
+                <span className={cx('chevron-right')} />
+              </button>
+            </div>
             <div className={cx('code', { 'is-expanded': this.state.isExpanded })} aria-hidden={!this.state.isExpanded}>
               <SyntaxHighlighter language="jsx" style={theme} customStyle={{ margin: '0', borderRadius: '0' }}>
                 {exampleSrc}
