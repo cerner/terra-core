6405a6556104fdc69807e9f9863427d5dc8ba7bf
Stephen Esser <Stephen.Esser@gmail.com>
2018-11-01 13:30:31 -0500

    [terra-base] - Restructure theme tests (#2009)
    * [terra-base] - Restructure theme tests
    
    * Add a change log entry

---
+++ packages/terra-base/tests/wdio/theme/base-theme-spec.js
@@ -0,0 +1,36 @@
+describe('Base theme', () => {
+  before(() => browser.setViewportSize(Terra.viewports('tiny')[0]));
+
+  describe('Background color', () => {
+    before(() => browser.url('/#/raw/tests/terra-base/base/default-base'));
+
+    Terra.should.themeEachCustomProperty(
+      '#root',
+      {
+        '--terra-base-background-color': 'red',
+      },
+    );
+  });
+
+  describe('Background image', () => {
+    before(() => browser.url('/#/raw/tests/terra-base/base/default-base'));
+
+    Terra.should.themeCombinationOfCustomProperties({
+      testName: 'Background Image',
+      properties: {
+        '--terra-base-background-image': 'url("https://github.com/cerner/terra-core/raw/master/terra.png")',
+        '--terra-base-background-repeat': 'no-repeat',
+        '--terra-base-background-size': '100%',
+        '--terra-base-background-position': 'top 70px left 45px',
+        '--terra-base-background-attachment': 'fixed',
+      },
+      selector: '#root',
+    });
+
+    it('should scroll to the bottom', () => {
+      browser.moveToObject('#bottom');
+    });
+
+    Terra.should.matchScreenshot('scrolled down', { selector: '#bottom-section' });
+  });
+});
