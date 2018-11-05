2a53cb2cfb9701895261977d1285046b262b26f6
Derek Yu <yuderekyu@gmail.com>
2018-11-05 15:40:25 -0600

    update wdio test

---
+++ packages/terra-avatar/src/terra-dev-site/test/avatar/avatar/IsDeceasedAvatar.test.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Avatar } from '../../../../Avatar';
+
+export default () => <Avatar alt="user" isDeceased />;
--- packages/terra-avatar/tests/wdio/theme/avatar-theme-spec.js
+++ packages/terra-avatar/tests/wdio/theme/avatar-theme-spec.js
@@ -9,7 +9,8 @@ describe('Avatar', () => {
         '--terra-avatar-border': '0.07143rem solid rgb(0, 0, 255)',
         '--terra-avatar-box-shadow': 'inset 0 0.07143rem 0.2143rem 5px rgba(255, 0, 0, 0.3)',
         '--terra-avatar-icon-user': 'rgb(0, 100, 0)',
-        '--terra-avatar-size': '5rem',
+        '--terra-avatar-height': '5rem',
+        '--terra-avatar-width': '5rem',
       },
     });
   });
@@ -26,7 +27,39 @@ describe('Avatar', () => {
         '--terra-avatar-box-shadow': 'inset 0 0.07143rem 0.2143rem 5px rgba(255, 0, 0, 0.3)',
         '--terra-avatar-color': 'rgb(0, 0, 255)',
         '--terra-avatar-font-size-large': '2rem',
-        '--terra-avatar-size': '5rem',
+        '--terra-avatar-height': '5rem',
+        '--terra-avatar-width': '5rem',
+      },
+    });
+  });
+
+  describe('Image', () => {
+    before(() => browser.url('/#/raw/tests/terra-avatar/avatar/image-avatar'));
+
+    Terra.should.themeCombinationOfCustomProperties({
+      testName: 'themed',
+      selector: '#image-avatar',
+      properties: {
+        '--terra-avatar-background-color': 'rgba(0, 0, 255, 0.5)',
+        '--terra-avatar-border': '0.07143rem solid rgb(0, 0, 255)',
+        '--terra-avatar-box-shadow': 'inset 0 0.07143rem 0.2143rem 5px rgba(255, 0, 0, 0.3)',
+        '--terra-avatar-color': 'rgb(0, 0, 255)',
+        '--terra-avatar-font-size-large': '2rem',
+        '--terra-avatar-height': '5rem',
+        '--terra-avatar-width': '5rem',
+      },
+    });
+  });
+
+  describe('IsDeceased', () => {
+    before(() => browser.url('/#/raw/tests/terra-avatar/avatar/is-deceased-avatar'));
+
+    Terra.should.themeCombinationOfCustomProperties({
+      testName: 'themed',
+      selector: '#is-deceased-avatar',
+      properties: {
+        '--terra-avatar-is-deceased-background-color': 'purple',
+        '--terra-avatar-image-is-deceased-opacity': '0.5',
       },
     });
   });
