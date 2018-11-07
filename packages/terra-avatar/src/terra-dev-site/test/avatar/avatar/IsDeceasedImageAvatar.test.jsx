ad658b0afdd2a219e896dcac2a7f371178cd9778
Derek Yu <yuderekyu@gmail.com>
2018-11-07 11:54:05 -0600

    update examples

--- packages/terra-avatar/src/terra-dev-site/test/avatar/avatar/IsDeceasedImageAvatar.test.jsx
+++ packages/terra-avatar/src/terra-dev-site/test/avatar/avatar/IsDeceasedImageAvatar.test.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import { Avatar } from '../../../../Avatar';
 import exampleAvatarImage from '../../../assets/150x150.jpg';
 
-export default () => <Avatar id="is-deceased-avatar" alt="user" image={exampleAvatarImage} isDeceased />;
+export default () => <Avatar id="is-deceased-image-avatar" alt="user" image={exampleAvatarImage} isDeceased />;
--- packages/terra-avatar/src/terra-dev-site/test/avatar/facility/DefaultFacility.test.jsx
+++ packages/terra-avatar/src/terra-dev-site/test/avatar/facility/DefaultFacility.test.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import { Facility } from '../../../../Avatar';
 
-export default () => <Facility id="facility" alt="facility" color="three" />;
+export default () => <Facility id="default-facility" alt="facility" color="three" />;
