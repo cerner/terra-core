f182f4cdd8bbfa7be79a7eee2a49d92dd6f4bc05
Derek Yu <yuderekyu@gmail.com>
2018-11-30 09:53:43 -0600

    [terra-signature] update theme test to use helper method, rather than using browser execute.

--- packages/terra-signature/tests/wdio/signature-spec.js
+++ packages/terra-signature/tests/wdio/signature-spec.js
@@ -27,16 +27,19 @@ describe('Signature', () => {
     before(() => {
       browser.url('/#/raw/tests/terra-signature/signature/signature-default');
       browser.waitForVisible('#drawline');
-      // Theme variables need to be set before drawing on canvas for theme test
-      browser.execute('document.documentElement.style.setProperty("--terra-signature-background-color", "#a7dc9e")');
-      browser.execute('document.documentElement.style.setProperty("--terra-signature-line-color", "#8b0a0a")');
       browser.moveToObject('#drawline', 0, 0);
       browser.buttonDown(0);
       browser.moveToObject('#drawline', 90, 90);
       browser.buttonUp(0);
     });
 
-    Terra.should.matchScreenshot({ viewports });
+    Terra.should.themeCombinationOfCustomProperties({
+      testName: 'Themed',
+      properties: {
+        '--terra-signature-background-color': '#a7dc9e',
+        '--terra-signature-line-color': '#8b0a0a',
+      },
+    });
   });
 
   describe('Right Click does not draw lines', () => {
