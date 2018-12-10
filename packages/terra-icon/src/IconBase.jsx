733a95db30604606df94c3ab9d74a3c316fed1bb
Brett Jankord <brett.jankord@cerner.com>
2018-12-10 16:04:20 -0600

    Move CSS animation to global styles outside of CSS Modules

--- packages/terra-icon/src/IconBase.jsx
+++ packages/terra-icon/src/IconBase.jsx
@@ -50,41 +50,68 @@ const defaultProps = {
   focusable: 'false',
 };
 
-const IconBase = ({
-  isBidi,
-  isSpin,
-  children,
-  height,
-  width,
-  ariaLabel,
-  focusable,
-  ...customProps
-}) => {
-  const attributes = Object.assign({}, customProps);
-
-  // append to existing classNames
-  const classes = cx(
-    'icon',
-    { 'is-bidi': isBidi },
-    { 'is-spin': isSpin },
-    attributes.className,
-  );
-
-  // aria-label is present, remove aria-hidden, set role to img
-  if (ariaLabel) {
-    attributes['aria-label'] = ariaLabel;
-    attributes.role = 'img';
-    attributes['aria-hidden'] = null;
-  } else {
-    attributes['aria-hidden'] = 'true';
+class IconBase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addAnimationStyles();
   }
 
-  attributes.height = height;
-  attributes.width = width;
-  attributes.focusable = focusable;
+  addAnimationStyles() {
+    const { isSpin } = this.props;
 
-  return <svg {...attributes} className={classes}>{children}</svg>;
-};
+    if (isSpin) {
+      const css = '@-webkit-keyframes tui-spin-ltr{0%{-webkit-transform:rotate(0deg);transform:rotate(0deg)}100%{-webkit-transform:rotate(359deg);transform:rotate(359deg)}}@keyframes tui-spin-ltr{0%{-webkit-transform:rotate(0deg);transform:rotate(0deg)}100%{-webkit-transform:rotate(359deg);transform:rotate(359deg)}}@-webkit-keyframes tui-spin-rtl{0%{-webkit-transform:rotate(0deg);transform:rotate(0deg)}100%{-webkit-transform:rotate(-359deg);transform:rotate(-359deg)}}@keyframes tui-spin-rtl{0%{-webkit-transform:rotate(0deg);transform:rotate(0deg)}100%{-webkit-transform:rotate(-359deg);transform:rotate(-359deg)}}[dir=ltr] .tui-Icon.is-spin {-webkit-animation: tui-spin-ltr 2s infinite linear;animation: tui-spin-ltr 2s infinite linear;}[dir=rtl] .tui-Icon.is-spin {-webkit-animation: tui-spin-rtl 2s infinite linear;animation: tui-spin-rtl 2s infinite linear;}';
+      const head = document.head || document.getElementsByTagName('head')[0];
+      const style = document.createElement('style');
+      style.type = 'text/css';
+      style.id = 'terra-icon-animation';
+      // If we haven't written the styles to the DOM yet, add them, otherwise don't do this for subsequent spinner icons.
+      if (!document.getElementById(style.id)) {
+        style.appendChild(document.createTextNode(css));
+        head.appendChild(style);
+      }
+    }
+  }
+
+  render() {
+    const {
+      isBidi,
+      isSpin,
+      children,
+      height,
+      width,
+      ariaLabel,
+      focusable,
+      ...customProps
+    } = this.props;
+
+    const attributes = Object.assign({}, customProps);
+
+    // append to existing classNames
+    const classes = cx(
+      'tui-Icon', // Namespaced class since it will be global, needed for global spin animation
+      'icon',
+      { 'is-bidi': isBidi },
+      { 'is-spin': isSpin },
+      attributes.className,
+    );
+
+    // aria-label is present, remove aria-hidden, set role to img
+    if (ariaLabel) {
+      attributes['aria-label'] = ariaLabel;
+      attributes.role = 'img';
+      attributes['aria-hidden'] = null;
+    } else {
+      attributes['aria-hidden'] = 'true';
+    }
+
+    attributes.height = height;
+    attributes.width = width;
+    attributes.focusable = focusable;
+
+    return <svg {...attributes} className={classes}>{children}</svg>;
+  }
+}
 
 IconBase.propTypes = propTypes;
 IconBase.defaultProps = defaultProps;
