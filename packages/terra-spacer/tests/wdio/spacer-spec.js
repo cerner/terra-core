ac1228ea08845793b154c2f5e5c6377e29804096
Derek Yu <yuderekyu@gmail.com>
2018-12-20 10:26:59 -0600

    Revert "[spacer] refactor theme vars. update tests. yikes"
    This reverts commit b7049fdb4d6a97b97532b9738642e6cb27ea448d.

--- packages/terra-spacer/tests/wdio/spacer-spec.js
+++ packages/terra-spacer/tests/wdio/spacer-spec.js
@@ -16,14 +16,7 @@ describe('Spacer', () => {
     Terra.should.themeCombinationOfCustomProperties({
       testName: 'themed',
       properties: {
-        '--terra-spacer-small-minus-2-margin-top': '39px',
-        '--terra-spacer-small-minus-2-margin-right': '39px',
-        '--terra-spacer-small-minus-2-margin-bottom': '39px',
-        '--terra-spacer-small-minus-2-margin-left': '39px',
-        '--terra-spacer-small-minus-2-padding-top': '39px',
-        '--terra-spacer-small-minus-2-padding-right': '39px',
-        '--terra-spacer-small-minus-2-padding-bottom': '39px',
-        '--terra-spacer-small-minus-2-padding-left': '39px',
+        '--terra-spacer-small-minus-2': '39px',
       },
     });
   });
@@ -36,14 +29,7 @@ describe('Spacer', () => {
     Terra.should.themeCombinationOfCustomProperties({
       testName: 'themed',
       properties: {
-        '--terra-spacer-small-minus-1-margin-top': '36px',
-        '--terra-spacer-small-minus-1-margin-right': '36px',
-        '--terra-spacer-small-minus-1-margin-bottom': '36px',
-        '--terra-spacer-small-minus-1-margin-left': '36px',
-        '--terra-spacer-small-minus-1-padding-top': '36px',
-        '--terra-spacer-small-minus-1-padding-right': '36px',
-        '--terra-spacer-small-minus-1-padding-bottom': '36px',
-        '--terra-spacer-small-minus-1-padding-left': '36px',
+        '--terra-spacer-small-minus-1': '36px',
       },
     });
   });
@@ -56,14 +42,7 @@ describe('Spacer', () => {
     Terra.should.themeCombinationOfCustomProperties({
       testName: 'themed',
       properties: {
-        '--terra-spacer-small-margin-top': '33px',
-        '--terra-spacer-small-margin-right': '33px',
-        '--terra-spacer-small-margin-bottom': '33px',
-        '--terra-spacer-small-margin-left': '33px',
-        '--terra-spacer-small-padding-top': '33px',
-        '--terra-spacer-small-padding-right': '33px',
-        '--terra-spacer-small-padding-bottom': '33px',
-        '--terra-spacer-small-padding-left': '33px',
+        '--terra-spacer-small': '33px',
       },
     });
   });
@@ -76,14 +55,7 @@ describe('Spacer', () => {
     Terra.should.themeCombinationOfCustomProperties({
       testName: 'themed',
       properties: {
-        '--terra-spacer-medium-margin-top': '30px',
-        '--terra-spacer-medium-margin-right': '30px',
-        '--terra-spacer-medium-margin-bottom': '30px',
-        '--terra-spacer-medium-margin-left': '30px',
-        '--terra-spacer-medium-padding-top': '30px',
-        '--terra-spacer-medium-padding-right': '30px',
-        '--terra-spacer-medium-padding-bottom': '30px',
-        '--terra-spacer-medium-padding-left': '30px',
+        '--terra-spacer-medium': '30px',
       },
     });
   });
@@ -96,14 +68,7 @@ describe('Spacer', () => {
     Terra.should.themeCombinationOfCustomProperties({
       testName: 'themed',
       properties: {
-        '--terra-spacer-large-margin-top': '26px',
-        '--terra-spacer-large-margin-right': '26px',
-        '--terra-spacer-large-margin-bottom': '26px',
-        '--terra-spacer-large-margin-left': '26px',
-        '--terra-spacer-large-padding-top': '26px',
-        '--terra-spacer-large-padding-right': '26px',
-        '--terra-spacer-large-padding-bottom': '26px',
-        '--terra-spacer-large-padding-left': '26px',
+        '--terra-spacer-large': '26px',
       },
     });
   });
@@ -116,14 +81,7 @@ describe('Spacer', () => {
     Terra.should.themeCombinationOfCustomProperties({
       testName: 'themed',
       properties: {
-        '--terra-spacer-large-plus-1-margin-top': '23px',
-        '--terra-spacer-large-plus-1-margin-right': '23px',
-        '--terra-spacer-large-plus-1-margin-bottom': '23px',
-        '--terra-spacer-large-plus-1-margin-left': '23px',
-        '--terra-spacer-large-plus-1-padding-top': '23px',
-        '--terra-spacer-large-plus-1-padding-right': '23px',
-        '--terra-spacer-large-plus-1-padding-bottom': '23px',
-        '--terra-spacer-large-plus-1-padding-left': '23px',
+        '--terra-spacer-large-plus-1': '23px',
       },
     });
   });
@@ -136,14 +94,7 @@ describe('Spacer', () => {
     Terra.should.themeCombinationOfCustomProperties({
       testName: 'themed',
       properties: {
-        '--terra-spacer-large-plus-2-margin-top': '20px',
-        '--terra-spacer-large-plus-2-margin-right': '20px',
-        '--terra-spacer-large-plus-2-margin-bottom': '20px',
-        '--terra-spacer-large-plus-2-margin-left': '20px',
-        '--terra-spacer-large-plus-2-padding-top': '20px',
-        '--terra-spacer-large-plus-2-padding-right': '20px',
-        '--terra-spacer-large-plus-2-padding-bottom': '20px',
-        '--terra-spacer-large-plus-2-padding-left': '20px',
+        '--terra-spacer-large-plus-2': '20px',
       },
     });
   });
@@ -156,14 +107,7 @@ describe('Spacer', () => {
     Terra.should.themeCombinationOfCustomProperties({
       testName: 'themed',
       properties: {
-        '--terra-spacer-large-plus-3-margin-top': '13px',
-        '--terra-spacer-large-plus-3-margin-right': '13px',
-        '--terra-spacer-large-plus-3-margin-bottom': '13px',
-        '--terra-spacer-large-plus-3-margin-left': '13px',
-        '--terra-spacer-large-plus-3-padding-top': '13px',
-        '--terra-spacer-large-plus-3-padding-right': '13px',
-        '--terra-spacer-large-plus-3-padding-bottom': '13px',
-        '--terra-spacer-large-plus-3-padding-left': '13px',
+        '--terra-spacer-large-plus-3': '13px',
       },
     });
   });
@@ -176,14 +120,7 @@ describe('Spacer', () => {
     Terra.should.themeCombinationOfCustomProperties({
       testName: 'themed',
       properties: {
-        '--terra-spacer-large-plus-4-margin-top': '10px',
-        '--terra-spacer-large-plus-4-margin-right': '10px',
-        '--terra-spacer-large-plus-4-margin-bottom': '10px',
-        '--terra-spacer-large-plus-4-margin-left': '10px',
-        '--terra-spacer-large-plus-4-padding-top': '10px',
-        '--terra-spacer-large-plus-4-padding-right': '10px',
-        '--terra-spacer-large-plus-4-padding-bottom': '10px',
-        '--terra-spacer-large-plus-4-padding-left': '10px',
+        '--terra-spacer-large-plus-4': '10px',
       },
     });
   });
