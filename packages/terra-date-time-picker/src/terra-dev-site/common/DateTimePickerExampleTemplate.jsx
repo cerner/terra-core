d9d8425894e1be1b8967992a8da7a545c966ce08
Jaime Mackey <jaime.mackey@cerner.com>
2018-12-20 14:03:39 -0600

    Code review change fix date time picker tests

--- packages/terra-date-time-picker/src/terra-dev-site/common/DateTimePickerExampleTemplate.jsx
+++ packages/terra-date-time-picker/src/terra-dev-site/common/DateTimePickerExampleTemplate.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Field from 'terra-form-field';
 // eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
 import DateTimePicker from 'terra-date-time-picker/lib/DateTimePicker';
 
@@ -34,14 +33,11 @@ class DatePickerExample extends React.Component {
 Selected ISO Date Time:
           {this.state.dateTime}
         </p>
-        <Field label="Enter Date/Time" htmlFor="default">
-          <DateTimePicker
-            name="date-time-picker-example"
-            id="default"
-            onChange={this.handleDateTimeChange}
-            {...this.props}
-          />
-        </Field>
+        <DateTimePicker
+          name="date-time-picker-example"
+          onChange={this.handleDateTimeChange}
+          {...this.props}
+        />
       </div>
     );
   }
