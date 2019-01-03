8d89917b804edb178baaadd1947aa47cc26901dc
Alex Brisimitzakis <42747444+AlexBrizi@users.noreply.github.com>
2019-01-03 12:39:15 -0600

    Added doc site examples to Alert and Status-View components (#2130)

--- packages/terra-alert/src/terra-dev-site/doc/example/WarningExample.jsx
+++ packages/terra-alert/src/terra-dev-site/doc/example/WarningExample.jsx
@@ -2,24 +2,8 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
 import Alert from 'terra-alert/lib/Alert';
 
-const alertStartTagStr = '<Alert type="warning" >';
-const alertEndTagStr = '</Alert>';
-const alertText = 'This is a warning';
-
 const WarningExample = () => (
-  <div>
-    <div dir="ltr">
-      <h3>Basic Alert of type warning with default title</h3>
-      <br />
-      <code>
-        {alertStartTagStr}
-        {alertText}
-        {alertEndTagStr}
-      </code>
-    </div>
-    <br />
-    <Alert type="warning">{alertText}</Alert>
-  </div>
+  <Alert type="warning">This is a warning</Alert>
 );
 
 export default WarningExample;
