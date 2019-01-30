567bfd3dfaa8c69fec581959b095ad4a9a92c429
Kasper,Dave <dave.m.kasper@gmail.com>
2019-01-30 15:07:36 -0600

    Correct lint error.

--- packages/terra-action-header/src/terra-dev-site/doc/example/MinimizeCustomButtonActionHeader.jsx
+++ packages/terra-action-header/src/terra-dev-site/doc/example/MinimizeCustomButtonActionHeader.jsx
@@ -1,15 +1,8 @@
 /* eslint-disable no-alert */
 import React from 'react';
-import IconTable from 'terra-icon/lib/icon/IconTable';
-import IconFlowsheet from 'terra-icon/lib/icon/IconFlowsheet';
-import IconVisualization from 'terra-icon/lib/icon/IconVisualization';
-import IconSend from 'terra-icon/lib/icon/IconSend';
-import IconPrinter from 'terra-icon/lib/icon/IconPrinter';
-import IconFolder from 'terra-icon/lib/icon/IconFolder';
-import IconTrash from 'terra-icon/lib/icon/IconTrash';
 // eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
 import ActionHeader from 'terra-action-header/lib/ActionHeader';
-import Placeholder from 'terra-doc-template/lib/Placeholder'
+import Placeholder from 'terra-doc-template/lib/Placeholder';
 
 const ActionHeaderExample = () => (
   <div>
