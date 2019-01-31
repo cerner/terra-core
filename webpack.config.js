8ba1baacacbe18eaac9fb70ba280734d89b19f84
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2019-01-31 15:28:38 -0600

    remove date picker

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,10 @@ const coreConfig = () => {
   const processPath = process.cwd();
   const rootPath = processPath.includes('packages') ? processPath.split('packages')[0] : processPath;
 
-  const momentAlias = path.resolve(path.join(rootPath, 'packages', 'terra-date-picker', 'node_modules', 'moment'));
-
   const i18nAlias = path.resolve(path.join(rootPath, 'packages', 'terra-i18n'));
   return {
     resolve: {
       alias: {
-        moment: momentAlias,
         'terra-i18n': i18nAlias,
       },
     },
