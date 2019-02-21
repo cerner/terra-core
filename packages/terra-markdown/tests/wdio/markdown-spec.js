b4fbdb3e3f13db64e20051da1837a30a0572ae3a
Alex Brisimitzakis <alex.brisimitzakis@cerner.com>
2019-02-21 14:12:02 -0600

    Added a wdio test

--- packages/terra-markdown/tests/wdio/markdown-spec.js
+++ packages/terra-markdown/tests/wdio/markdown-spec.js
@@ -1,18 +1,18 @@
-describe('Markdown', () => {
-  before(() => browser.setViewportSize(Terra.viewports('tiny')[0]));
+const viewports = Terra.viewports('large');
 
+describe('Markdown', () => {
   describe('Default', () => {
     before(() => browser.url('/#/raw/tests/terra-markdown/markdown/markdown'));
 
-    Terra.should.beAccessible({ selector: '.markdown-body blockquote' });
-    Terra.should.matchScreenshot();
+    Terra.should.beAccessible({ viewports });
+    Terra.should.matchScreenshot({ viewports });
   });
 
   describe('Block Quote', () => {
     before(() => browser.url('/#/raw/tests/terra-markdown/markdown/markdown'));
 
-    Terra.should.beAccessible({ selector: '.markdown-body blockquote' });
-    Terra.should.matchScreenshot();
+    Terra.should.beAccessible({ viewports });
+    Terra.should.matchScreenshot({ viewports });
     Terra.should.themeCombinationOfCustomProperties({
       testName: 'themed',
       properties: {
