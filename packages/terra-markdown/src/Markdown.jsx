ff1541f406fd263483bb357f27d512fd3dea677a
Alex Brisimitzakis <alex.brisimitzakis@cerner.com>
2019-02-21 18:27:08 -0600

    Made style fixes

--- packages/terra-markdown/src/Markdown.jsx
+++ packages/terra-markdown/src/Markdown.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames/bind';
 import marked from 'marked';
 /* github-markdown-css's main entry in package.json resolves to a CSS file and this seems to be causing issues with eslint */
 /* eslint-disable import/extensions */
 import 'github-markdown-css/github-markdown.css';
 /* eslint-enable import/extensions */
-import styles from './Markdown.scss';
-
-const cx = classNames.bind(styles);
+import './Markdown.scss';
 
 marked.setOptions({
   headerIds: false,
@@ -25,16 +22,11 @@ const defaultProps = {
   src: '',
 };
 
-const markdownClasses = cx([
-  'markdown-body',
-]);
-
-
 /* eslint react/no-danger:0 */
 const Markdown = props => (
   <div
     dir="ltr"
-    className={markdownClasses}
+    className="markdown-body"
     style={{ listStyle: 'disc outside none' }}
     dangerouslySetInnerHTML={{ __html: marked(props.src) }}
   />
