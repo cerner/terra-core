31936fc9e592f059a585cfd2896eb9efb7c0ac35
Adam Parker <amichaelparker@gmail.com>
2019-03-19 16:49:56 -0500

    moved divider into a div, added custom theming to scss

--- packages/terra-divider/src/Divider.jsx
+++ packages/terra-divider/src/Divider.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
 import 'terra-base/lib/baseStyles';
 import styles from './Divider.module.scss';
+
+const propTypes = {
+  text: PropTypes.string,
+};
 
 const cx = classNames.bind(styles);
 
 const Divider = ({ ...customProps }) => {
+  const { text } = customProps;
+
   const dividerClassNames = cx([
     'divider',
+    'divider-text',
     customProps.className,
   ]);
 
-  return <hr {...customProps} className={dividerClassNames} aria-hidden="true" />;
+  return <div {...customProps} className={dividerClassNames} aria-hidden="true" content="Hello There" />;
 };
 
+Divider.propTypes = propTypes;
 export default Divider;
