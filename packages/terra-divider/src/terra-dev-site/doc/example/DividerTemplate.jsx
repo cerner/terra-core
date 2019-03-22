92f2c89435ba1f49d9db3d034fda867dcf0efeac
Adam Parker <amichaelparker@gmail.com>
2019-03-22 15:14:02 -0500

    update template component

--- packages/terra-divider/src/terra-dev-site/doc/example/DividerTemplate.jsx
+++ packages/terra-divider/src/terra-dev-site/doc/example/DividerTemplate.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 /* eslint-disable react/prop-types */
-class DividerTemplate extends Component {
+class DividerTemplate extends React.Component {
   render() {
     return (
       <div>
