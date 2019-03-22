6ad30396c0b8badd228cfb0757ac0b45339f4693
Adam Parker <amichaelparker@gmail.com>
2019-03-22 15:25:52 -0500

    exmaple updates

--- packages/terra-divider/src/terra-dev-site/doc/example/DividerCustomExample.jsx
+++ packages/terra-divider/src/terra-dev-site/doc/example/DividerCustomExample.jsx
@@ -6,9 +6,21 @@ import DividerTemplate from './DividerTemplate';
 const content = 'Custom divider text';
 
 const DividerCustomExample = () => (
-  <DividerTemplate>
+  <div>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industrys standard
+      dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+      book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+      unchanged.
+    </p>
     <Divider text={content} />
-  </DividerTemplate>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industrys standard
+      dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+      book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+      unchanged.
+    </p>
+  </div>
 );
 
 export default DividerCustomExample;
--- packages/terra-divider/src/terra-dev-site/doc/example/DividerExample.jsx
+++ packages/terra-divider/src/terra-dev-site/doc/example/DividerExample.jsx
@@ -4,9 +4,21 @@ import Divider from 'terra-divider/lib/Divider';
 import DividerTemplate from './DividerTemplate';
 
 const DividerExample = () => (
-  <DividerTemplate>
+  <div>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industrys standard
+      dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+      book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+      unchanged.
+    </p>
     <Divider />
-  </DividerTemplate>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industrys standard
+      dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+      book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+      unchanged.
+    </p>
+</div>
 );
 
 export default DividerExample;
