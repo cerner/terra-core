aa792964b721b27641fe881781c8135f98ca7a13
Brett Jankord <brett.jankord@cerner.com>
2019-03-22 12:56:50 -0500

    Remove supported locales import/logic from i18nLoader

--- packages/terra-i18n/src/i18nLoader.js
+++ packages/terra-i18n/src/i18nLoader.js
@@ -2,21 +2,17 @@
 import Intl from './intl';
 import loadIntl from './intlLoaders';
 import loadTranslations from './translationsLoaders';
-import supportedLocales from './i18nSupportedLocales';
 
 const hasIntl = typeof (Intl) !== 'undefined';
 
-const permitParams = (locale, callback) => {
-  if (process.env.NODE_ENV !== 'production' && supportedLocales.indexOf(locale) < 0) {
-    console.warn(`${locale} is not a supported locale, supported locales include: ${supportedLocales.join(', ')}.`);
-  }
+const permitParams = (callback) => {
   if (typeof (callback) !== 'function') {
     throw new Error('Second argument must be function');
   }
 }
 
 module.exports = (locale, callback, scope) => {
-  permitParams(locale, callback);
+  permitParams(callback);
   if (!hasIntl) {
       loadIntl(locale);
       loadTranslations(locale, callback, scope);
