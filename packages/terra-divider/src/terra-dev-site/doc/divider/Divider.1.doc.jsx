770501690859e6fc38b3132c478be73a3d742450
Adam Parker <amichaelparker@gmail.com>
2019-03-23 19:11:41 -0500

    corrected source path

--- packages/terra-divider/src/terra-dev-site/doc/divider/Divider.1.doc.jsx
+++ packages/terra-divider/src/terra-dev-site/doc/divider/Divider.1.doc.jsx
@@ -3,15 +3,16 @@ import DocTemplate from 'terra-doc-template';
 import ReadMe from '../../../../docs/README.md';
 import { name } from '../../../../package.json';
 
-/* eslint-disable import/no-webpack-loader-syntax, import/no-unresolved */
-// Component source
-import DividerSrc from '!raw-loader!../../../Divider';
+/* eslint-disable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
+// Component Source
+import DividerSrc from '!raw-loader!../../../../src/Divider';
 
 // Example Files
 import DividerExample from '../example/DividerExample';
 import DividerExampleSrc from '!raw-loader!../../../../src/terra-dev-site/doc/example/DividerExample';
 import DividerCustomExample from '../example/DividerCustomExample';
 import DividerCustomExampleSrc from '!raw-loader!../../../../src/terra-dev-site/doc/example/DividerCustomExample';
+/* eslint-enable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
 
 const DocPage = () => (
   <DocTemplate
@@ -20,12 +21,12 @@ const DocPage = () => (
     srcPath={`https://github.com/cerner/terra-core/tree/master/packages/${name}`}
     examples={[
       {
-        title: 'Divider - Default',
+        title: 'Default',
         example: <DividerExample />,
         source: DividerExampleSrc,
       },
       {
-        title: 'Divider - Custom',
+        title: 'Custom Text',
         example: <DividerCustomExample />,
         source: DividerCustomExampleSrc,
       },
