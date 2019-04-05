ab752c5be28d7c9db5332db850293f7d46589cd8
Brett Jankord <bjankord@gmail.com>
2019-04-05 15:53:52 -0500

    Update to OCS icons v1.21.1 (#2311)

--- packages/terra-icon/src/icon/IconDateOfManufacture.jsx
+++ packages/terra-icon/src/icon/IconDateOfManufacture.jsx
@@ -7,12 +7,12 @@ const SvgIcon = (customProps) => {
 
   return (
     <IconBase {...attributes}>
-      <path d="M14.6 18L9 12.4C12.5 7.9 17.9 5 24 5c10.5 0 19 8.5 19 19s-8.5 19-19 19c-5.6 0-10.5-2.3-14-6.1l-3.5 3.5C10.9 45.1 17.1 48 24 48c13.3 0 24-10.7 24-24S37.3 0 24 0C16.5 0 9.8 3.4 5.4 8.8L0 3.4V18h14.6z" ></path>
+      <path d="M34 0v26l-5.773-8-5.613 7.778L17 18l-5.613 7.778L5.773 18 0 26v22h48V0H34zm11 45H3V26.969l2.773-3.843 3.181 4.407 2.433 3.371 2.433-3.371L17 23.126l3.181 4.407 2.433 3.371 2.433-3.371 3.181-4.407 3.341 4.629L33.909 31H37V3h8v42z" ></path>
     </IconBase>
   );
 };
 
-SvgIcon.displayName = "IconReload";
+SvgIcon.displayName = "IconDateOfManufacture";
 SvgIcon.defaultProps = {"viewBox":"0 0 48 48","xmlns":"http://www.w3.org/2000/svg"};
 
 export default SvgIcon;
--- packages/terra-icon/src/icon/IconManufacturer.jsx
+++ packages/terra-icon/src/icon/IconManufacturer.jsx
@@ -7,13 +7,13 @@ const SvgIcon = (customProps) => {
 
   return (
     <IconBase {...attributes}>
-      <path d="M0 35h48v6H0v-6zm0-8h33v-6H0v6zm0-14h20V7H0v6z" ></path>
+      <path d="M34 0v26l-5.773-8-5.614 7.778L17 18l-5.613 7.778L5.773 18 0 26v22h48V0z" ></path>
     </IconBase>
   );
 };
 
-SvgIcon.displayName = "IconSortAscending";
-SvgIcon.defaultProps = {"className":"","viewBox":"0 0 48 48","xmlns":"http://www.w3.org/2000/svg","isBidi":true};
+SvgIcon.displayName = "IconManufacturer";
+SvgIcon.defaultProps = {"viewBox":"0 0 48 48","xmlns":"http://www.w3.org/2000/svg"};
 
 export default SvgIcon;
 /* eslint-enable */
