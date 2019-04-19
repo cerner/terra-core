73a254806983b45941caf6a05d601cca8d5a0689
Adam Parker <amichaelparker@gmail.com>
2019-04-19 09:38:53 -0500

    added examples to checkboxfield and radiofield for inline and invalid behaviors

---
+++ packages/terra-form-checkbox/src/terra-dev-site/doc/example/field/InlineCheckboxField.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
+import Checkbox from 'terra-form-checkbox/lib/Checkbox';
+// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
+import CheckboxField from 'terra-form-checkbox/lib/CheckboxField';
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedAnswers: [],
+      toggleInline: false,
+    };
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnChange(e) {
+    const { selectedAnswers } = this.state;
+
+    if (e.currentTarget.checked) {
+      selectedAnswers.push(e.currentTarget.value);
+    } else {
+      selectedAnswers.splice(selectedAnswers.indexOf(e.currentTarget.value), 1);
+    }
+
+    this.setState({ selectedAnswers });
+  }
+
+  handleOnClick() {
+    if (this.state.toggleInline) {
+      this.setState({ toggleInline: false });
+    } else {
+      this.setState({ toggleInline: true });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button style={{ marginBottom: '5px' }} type="button" aria-label="Toggle Inline" onClick={this.handleOnClick}>Toggle Inline</button>
+        <div>
+          <CheckboxField
+            isInline={this.state.toggleInline}
+            legend="Do you have experience with any of the following?"
+          >
+            <Checkbox id="inline-experience-indesign" name="experience[]" labelText="InDesign" />
+            <Checkbox id="inline-experience-photoshop" name="experience[]" labelText="Photoshop" />
+            <Checkbox id="inline-experience-illustrator" name="experience[]" labelText="Illustrator" />
+          </CheckboxField>
+          <CheckboxField
+            isInline={this.state.toggleInline}
+            legend="Do you also have experience with any of the cutting edge technologies?"
+          >
+            <Checkbox id="experience-ie9" name="experience[]" labelText="IE9" />
+            <Checkbox id="experience-flase" name="experience[]" labelText="Flash" />
+            <Checkbox id="experience-punchcards" name="experience[]" labelText="Punch Cards" />
+          </CheckboxField>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ packages/terra-form-radio/src/terra-dev-site/doc/example/field/InlineRadioField.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
+import Radio from 'terra-form-radio/lib/Radio';
+// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
+import RadioField from 'terra-form-radio/lib/RadioField';
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedAnswer: undefined,
+      toggleInline: false,
+    };
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnChange(e) {
+    this.setState({ selectedAnswer: e.currentTarget.value });
+  }
+
+  handleOnClick() {
+    if (this.state.toggleInline) {
+      this.setState({ toggleInline: false });
+    } else {
+      this.setState({ toggleInline: true });
+    }
+  }
+
+  render() {
+    let errorMessage = 'An answer must be chosen';
+    let isInvalid;
+
+    if (this.state.selectedAnswer === 'mcdonalds') {
+      errorMessage = 'Invalid option selected.';
+      isInvalid = true;
+    } else if (this.state.selectedAnswer === undefined) {
+      isInvalid = true;
+    }
+
+    return (
+      <div>
+        <button style={{ marginBottom: '5px' }} type="button" aria-label="Toggle Inline" onClick={this.handleOnClick}>Toggle Inline</button>
+        <hr />
+        <div>
+          <RadioField
+            legend="Which Type of Meal are you looking for?"
+            help="This cannot be changed when submitted"
+            isInline={this.state.toggleInline}
+            isInvalid={isInvalid}
+            error={errorMessage}
+            required
+          >
+            <Radio id="inline-chicken-meal" name="meal" labelText="Chicken" onChange={this.handleOnChange} value="chicken" />
+            <Radio id="inline-salmon-meal" name="meal" labelText="Salmon" onChange={this.handleOnChange} value="salmon" />
+            <Radio id="inline-mcdonalds-meal" name="meal" labelText="McDonalds (Not a valid choice)" onChange={this.handleOnChange} value="mcdonalds" />
+          </RadioField>
+          <RadioField
+            legend="Which Type of Sides do you want?"
+            help="This cannot be changed when submitted"
+            isInline={this.state.toggleInline}
+            required
+          >
+            <Radio id="fries" name="side" labelText="Fries" value="fries" />
+            <Radio id="fruit" name="side" labelText="Fruit" value="fruit" />
+            <Radio id="salad" name="side" labelText="Salad" value="salad" />
+          </RadioField>
+        </div>
+      </div>
+    );
+  }
+}
