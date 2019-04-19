d941c990de24a3f4c628af90e94d5d5a7f0ba265
Adam Parker <amichaelparker@gmail.com>
2019-04-19 15:32:25 -0500

    [terra-icon] Add Regression Test for All Icons (#2350)

---
+++ packages/terra-icon/src/IconExports.js
@@ -0,0 +1,517 @@
+import IconAbnormal from './icon/IconAbnormal';
+import IconAbnormalLowLight from './icon/IconAbnormalLowLight';
+import IconAdd from './icon/IconAdd';
+import IconAddPerson from './icon/IconAddPerson';
+import IconAlert from './icon/IconAlert';
+import IconAlertLowLight from './icon/IconAlertLowLight';
+import IconAllergy from './icon/IconAllergy';
+import IconAnalytics from './icon/IconAnalytics';
+import IconAnnouncement from './icon/IconAnnouncement';
+import IconArchive from './icon/IconArchive';
+import IconAttachment from './icon/IconAttachment';
+import IconAudio from './icon/IconAudio';
+import IconAvailable from './icon/IconAvailable';
+import IconAway from './icon/IconAway';
+import IconBedAssigned from './icon/IconBedAssigned';
+import IconBedAssignedLowLight from './icon/IconBedAssignedLowLight';
+import IconBedRequested from './icon/IconBedRequested';
+import IconBookmark from './icon/IconBookmark';
+import IconBriefcase from './icon/IconBriefcase';
+import IconBusy from './icon/IconBusy';
+import IconCalculator from './icon/IconCalculator';
+import IconCalendar from './icon/IconCalendar';
+import IconCamera from './icon/IconCamera';
+import IconCancel from './icon/IconCancel';
+import IconCaretDown from './icon/IconCaretDown';
+import IconCaretLeft from './icon/IconCaretLeft';
+import IconCaretRight from './icon/IconCaretRight';
+import IconCaretUp from './icon/IconCaretUp';
+import IconCatalogueNumber from './icon/IconCatalogueNumber';
+import IconCaution from './icon/IconCaution';
+import IconChecklist from './icon/IconChecklist';
+import IconCheckmark from './icon/IconCheckmark';
+import IconChevron from './icon/IconChevron';
+import IconChevronDown from './icon/IconChevronDown';
+import IconChevronLeft from './icon/IconChevronLeft';
+import IconChevronRight from './icon/IconChevronRight';
+import IconChevronUp from './icon/IconChevronUp';
+import IconCircleLinesSymbol from './icon/IconCircleLinesSymbol';
+import IconCircleSymbol from './icon/IconCircleSymbol';
+import IconClear from './icon/IconClear';
+import IconClipboard from './icon/IconClipboard';
+import IconClock from './icon/IconClock';
+import IconClose from './icon/IconClose';
+import IconCollapseRow from './icon/IconCollapseRow';
+import IconComment from './icon/IconComment';
+import IconCommit from './icon/IconCommit';
+import IconComplete from './icon/IconComplete';
+import IconCompose from './icon/IconCompose';
+import IconConsultInstructionsForUse from './icon/IconConsultInstructionsForUse';
+import IconCritical from './icon/IconCritical';
+import IconCriticalInverse from './icon/IconCriticalInverse';
+import IconCriticalInverseLowLight from './icon/IconCriticalInverseLowLight';
+import IconCriticalLowLight from './icon/IconCriticalLowLight';
+import IconDateOfManufacture from './icon/IconDateOfManufacture';
+import IconDevice from './icon/IconDevice';
+import IconDeviceAlert from './icon/IconDeviceAlert';
+import IconDeviceAlertLowLight from './icon/IconDeviceAlertLowLight';
+import IconDeviceCheck from './icon/IconDeviceCheck';
+import IconDeviceCheckLowLight from './icon/IconDeviceCheckLowLight';
+import IconDiamond from './icon/IconDiamond';
+import IconDiamondLowLight from './icon/IconDiamondLowLight';
+import IconDiamondSymbol from './icon/IconDiamondSymbol';
+import IconDischargeComingDue from './icon/IconDischargeComingDue';
+import IconDischargeComingDueLowLight from './icon/IconDischargeComingDueLowLight';
+import IconDischargeComplete from './icon/IconDischargeComplete';
+import IconDischargeCompleteLowLight from './icon/IconDischargeCompleteLowLight';
+import IconDischargeOverDue from './icon/IconDischargeOverDue';
+import IconDischargeOverDueLowLight from './icon/IconDischargeOverDueLowLight';
+import IconDoNotDisturb from './icon/IconDoNotDisturb';
+import IconDocuments from './icon/IconDocuments';
+import IconDoorOpen from './icon/IconDoorOpen';
+import IconDoubleChevronLeft from './icon/IconDoubleChevronLeft';
+import IconDoubleChevronRight from './icon/IconDoubleChevronRight';
+import IconDown from './icon/IconDown';
+import IconDownload from './icon/IconDownload';
+import IconDrugDrug from './icon/IconDrugDrug';
+import IconDrugDrugLowLight from './icon/IconDrugDrugLowLight';
+import IconDue from './icon/IconDue';
+import IconDueSoon from './icon/IconDueSoon';
+import IconDueSoonLowLight from './icon/IconDueSoonLowLight';
+import IconEdit from './icon/IconEdit';
+import IconEllipses from './icon/IconEllipses';
+import IconEnvelope from './icon/IconEnvelope';
+import IconError from './icon/IconError';
+import IconErrorDark from './icon/IconErrorDark';
+import IconErrorLowLight from './icon/IconErrorLowLight';
+import IconExclamation from './icon/IconExclamation';
+import IconExpandLess from './icon/IconExpandLess';
+import IconExpandMore from './icon/IconExpandMore';
+import IconExpandRow from './icon/IconExpandRow';
+import IconExternalLink from './icon/IconExternalLink';
+import IconFeatured from './icon/IconFeatured';
+import IconFeaturedOff from './icon/IconFeaturedOff';
+import IconFeaturedOutline from './icon/IconFeaturedOutline';
+import IconFirst from './icon/IconFirst';
+import IconFitScale from './icon/IconFitScale';
+import IconFitWidth from './icon/IconFitWidth';
+import IconFlag from './icon/IconFlag';
+import IconFlipHorizontal from './icon/IconFlipHorizontal';
+import IconFlipVertical from './icon/IconFlipVertical';
+import IconFlowsheet from './icon/IconFlowsheet';
+import IconFolder from './icon/IconFolder';
+import IconForward from './icon/IconForward';
+import IconFunnel from './icon/IconFunnel';
+import IconGapChecking from './icon/IconGapChecking';
+import IconGlasses from './icon/IconGlasses';
+import IconHalfRating from './icon/IconHalfRating';
+import IconHelp from './icon/IconHelp';
+import IconHelpInverse from './icon/IconHelpInverse';
+import IconHelpInverseLowLight from './icon/IconHelpInverseLowLight';
+import IconHelpLowLight from './icon/IconHelpLowLight';
+import IconHide from './icon/IconHide';
+import IconHigh from './icon/IconHigh';
+import IconHighInverse from './icon/IconHighInverse';
+import IconHighInverseLowLight from './icon/IconHighInverseLowLight';
+import IconHighLowLight from './icon/IconHighLowLight';
+import IconHighPriority from './icon/IconHighPriority';
+import IconHold from './icon/IconHold';
+import IconHospital from './icon/IconHospital';
+import IconHouse from './icon/IconHouse';
+import IconIPass from './icon/IconIPass';
+import IconImage from './icon/IconImage';
+import IconImplant from './icon/IconImplant';
+import IconIncomingCall from './icon/IconIncomingCall';
+import IconIncomplete from './icon/IconIncomplete';
+import IconIncompleteInverse from './icon/IconIncompleteInverse';
+import IconIncompleteInverseLowLight from './icon/IconIncompleteInverseLowLight';
+import IconInformation from './icon/IconInformation';
+import IconInformationInverse from './icon/IconInformationInverse';
+import IconInformationInverseLowLight from './icon/IconInformationInverseLowLight';
+import IconInformationLowLight from './icon/IconInformationLowLight';
+import IconItalicI from './icon/IconItalicI';
+import IconKnurling from './icon/IconKnurling';
+import IconLast from './icon/IconLast';
+import IconLeft from './icon/IconLeft';
+import IconLeftPane from './icon/IconLeftPane';
+import IconLightbulb from './icon/IconLightbulb';
+import IconLink from './icon/IconLink';
+import IconList from './icon/IconList';
+import IconLookback from './icon/IconLookback';
+import IconLow from './icon/IconLow';
+import IconLowLowLight from './icon/IconLowLowLight';
+import IconLowRisk from './icon/IconLowRisk';
+import IconLowRiskLowLight from './icon/IconLowRiskLowLight';
+import IconManufacturer from './icon/IconManufacturer';
+import IconMax from './icon/IconMax';
+import IconMaximize from './icon/IconMaximize';
+import IconMenu from './icon/IconMenu';
+import IconMerge from './icon/IconMerge';
+import IconMicrophone from './icon/IconMicrophone';
+import IconMicrophoneDisabled from './icon/IconMicrophoneDisabled';
+import IconMin from './icon/IconMin';
+import IconMinimize from './icon/IconMinimize';
+import IconMinus from './icon/IconMinus';
+import IconMissedCall from './icon/IconMissedCall';
+import IconModerate from './icon/IconModerate';
+import IconModerateLowLight from './icon/IconModerateLowLight';
+import IconModified from './icon/IconModified';
+import IconNext from './icon/IconNext';
+import IconNoRisk from './icon/IconNoRisk';
+import IconNoRiskLowLight from './icon/IconNoRiskLowLight';
+import IconNoSignal from './icon/IconNoSignal';
+import IconNotMet from './icon/IconNotMet';
+import IconNotification from './icon/IconNotification';
+import IconNotificationOff from './icon/IconNotificationOff';
+import IconOutgoingCall from './icon/IconOutgoingCall';
+import IconOverDue from './icon/IconOverDue';
+import IconOverDueLowLight from './icon/IconOverDueLowLight';
+import IconPadlock from './icon/IconPadlock';
+import IconPanelLeft from './icon/IconPanelLeft';
+import IconPanelRight from './icon/IconPanelRight';
+import IconPaperFolded from './icon/IconPaperFolded';
+import IconPaperPencil from './icon/IconPaperPencil';
+import IconPartiallyMet from './icon/IconPartiallyMet';
+import IconPause from './icon/IconPause';
+import IconPending from './icon/IconPending';
+import IconPerson from './icon/IconPerson';
+import IconPersonHospital from './icon/IconPersonHospital';
+import IconPersonnelPerson from './icon/IconPersonnelPerson';
+import IconPharmacyReject from './icon/IconPharmacyReject';
+import IconPharmacyRejectLowLight from './icon/IconPharmacyRejectLowLight';
+import IconPharmacyReview from './icon/IconPharmacyReview';
+import IconPhone from './icon/IconPhone';
+import IconPhoneDown from './icon/IconPhoneDown';
+import IconPill from './icon/IconPill';
+import IconPinDiagonal from './icon/IconPinDiagonal';
+import IconPinDown from './icon/IconPinDown';
+import IconPlusSymbol from './icon/IconPlusSymbol';
+import IconPrevious from './icon/IconPrevious';
+import IconPrinter from './icon/IconPrinter';
+import IconProjects from './icon/IconProjects';
+import IconProtocol from './icon/IconProtocol';
+import IconProvider from './icon/IconProvider';
+import IconQuestion from './icon/IconQuestion';
+import IconQuestionOutline from './icon/IconQuestionOutline';
+import IconRectangleSymbol from './icon/IconRectangleSymbol';
+import IconRecurringEvent from './icon/IconRecurringEvent';
+import IconRedo from './icon/IconRedo';
+import IconRefresh from './icon/IconRefresh';
+import IconReload from './icon/IconReload';
+import IconReply from './icon/IconReply';
+import IconReplyAll from './icon/IconReplyAll';
+import IconRequired from './icon/IconRequired';
+import IconRight from './icon/IconRight';
+import IconRollup from './icon/IconRollup';
+import IconRotateLeft from './icon/IconRotateLeft';
+import IconRotateRight from './icon/IconRotateRight';
+import IconSave from './icon/IconSave';
+import IconScheduled from './icon/IconScheduled';
+import IconScheduledLowLight from './icon/IconScheduledLowLight';
+import IconScratchPad from './icon/IconScratchPad';
+import IconSearch from './icon/IconSearch';
+import IconSecondaryProvider from './icon/IconSecondaryProvider';
+import IconSend from './icon/IconSend';
+import IconSeparate from './icon/IconSeparate';
+import IconSettings from './icon/IconSettings';
+import IconSimulation from './icon/IconSimulation';
+import IconSortAscending from './icon/IconSortAscending';
+import IconSortDescending from './icon/IconSortDescending';
+import IconSpinner from './icon/IconSpinner';
+import IconSquareSymbol from './icon/IconSquareSymbol';
+import IconSuccess from './icon/IconSuccess';
+import IconSuccessInverse from './icon/IconSuccessInverse';
+import IconSuccessInverseLowLight from './icon/IconSuccessInverseLowLight';
+import IconSuccessLowLight from './icon/IconSuccessLowLight';
+import IconSwap from './icon/IconSwap';
+import IconTable from './icon/IconTable';
+import IconTag from './icon/IconTag';
+import IconTeardropSymbol from './icon/IconTeardropSymbol';
+import IconTeardropUpSymbol from './icon/IconTeardropUpSymbol';
+import IconThinDiamondSymbol from './icon/IconThinDiamondSymbol';
+import IconTile from './icon/IconTile';
+import IconTrash from './icon/IconTrash';
+import IconTreemap from './icon/IconTreemap';
+import IconTriangleDownSymbol from './icon/IconTriangleDownSymbol';
+import IconTriangleSymbol from './icon/IconTriangleSymbol';
+import IconTrophy from './icon/IconTrophy';
+import IconUnavailable from './icon/IconUnavailable';
+import IconUndo from './icon/IconUndo';
+import IconUnexpected from './icon/IconUnexpected';
+import IconUnexpectedLowLight from './icon/IconUnexpectedLowLight';
+import IconUnknown from './icon/IconUnknown';
+import IconUnlock from './icon/IconUnlock';
+import IconUnread from './icon/IconUnread';
+import IconUnspecified from './icon/IconUnspecified';
+import IconUp from './icon/IconUp';
+import IconUpload from './icon/IconUpload';
+import IconUsers from './icon/IconUsers';
+import IconVideoCamera from './icon/IconVideoCamera';
+import IconVideoCameraDisabled from './icon/IconVideoCameraDisabled';
+import IconVisualization from './icon/IconVisualization';
+import IconWarning from './icon/IconWarning';
+import IconWarningLowLight from './icon/IconWarningLowLight';
+import IconXSymbol from './icon/IconXSymbol';
+import IconZoomIn from './icon/IconZoomIn';
+import IconZoomOut from './icon/IconZoomOut';
+
+const Icons = {
+  IconAbnormal,
+  IconAbnormalLowLight,
+  IconAdd,
+  IconAddPerson,
+  IconAlert,
+  IconAlertLowLight,
+  IconAllergy,
+  IconAnalytics,
+  IconAnnouncement,
+  IconArchive,
+  IconAttachment,
+  IconAudio,
+  IconAvailable,
+  IconAway,
+  IconBedAssigned,
+  IconBedAssignedLowLight,
+  IconBedRequested,
+  IconBookmark,
+  IconBriefcase,
+  IconBusy,
+  IconCalculator,
+  IconCalendar,
+  IconCamera,
+  IconCancel,
+  IconCaretDown,
+  IconCaretLeft,
+  IconCaretRight,
+  IconCaretUp,
+  IconCatalogueNumber,
+  IconCaution,
+  IconChecklist,
+  IconCheckmark,
+  IconChevron,
+  IconChevronDown,
+  IconChevronLeft,
+  IconChevronRight,
+  IconChevronUp,
+  IconCircleLinesSymbol,
+  IconCircleSymbol,
+  IconClear,
+  IconClipboard,
+  IconClock,
+  IconClose,
+  IconCollapseRow,
+  IconComment,
+  IconCommit,
+  IconComplete,
+  IconCompose,
+  IconConsultInstructionsForUse,
+  IconCritical,
+  IconCriticalInverse,
+  IconCriticalInverseLowLight,
+  IconCriticalLowLight,
+  IconDateOfManufacture,
+  IconDevice,
+  IconDeviceAlert,
+  IconDeviceAlertLowLight,
+  IconDeviceCheck,
+  IconDeviceCheckLowLight,
+  IconDiamond,
+  IconDiamondLowLight,
+  IconDiamondSymbol,
+  IconDischargeComingDue,
+  IconDischargeComingDueLowLight,
+  IconDischargeComplete,
+  IconDischargeCompleteLowLight,
+  IconDischargeOverDue,
+  IconDischargeOverDueLowLight,
+  IconDoNotDisturb,
+  IconDocuments,
+  IconDoorOpen,
+  IconDoubleChevronLeft,
+  IconDoubleChevronRight,
+  IconDown,
+  IconDownload,
+  IconDrugDrug,
+  IconDrugDrugLowLight,
+  IconDue,
+  IconDueSoon,
+  IconDueSoonLowLight,
+  IconEdit,
+  IconEllipses,
+  IconEnvelope,
+  IconError,
+  IconErrorDark,
+  IconErrorLowLight,
+  IconExclamation,
+  IconExpandLess,
+  IconExpandMore,
+  IconExpandRow,
+  IconExternalLink,
+  IconFeatured,
+  IconFeaturedOff,
+  IconFeaturedOutline,
+  IconFirst,
+  IconFitScale,
+  IconFitWidth,
+  IconFlag,
+  IconFlipHorizontal,
+  IconFlipVertical,
+  IconFlowsheet,
+  IconFolder,
+  IconForward,
+  IconFunnel,
+  IconGapChecking,
+  IconGlasses,
+  IconHalfRating,
+  IconHelp,
+  IconHelpInverse,
+  IconHelpInverseLowLight,
+  IconHelpLowLight,
+  IconHide,
+  IconHigh,
+  IconHighInverse,
+  IconHighInverseLowLight,
+  IconHighLowLight,
+  IconHighPriority,
+  IconHold,
+  IconHospital,
+  IconHouse,
+  IconIPass,
+  IconImage,
+  IconImplant,
+  IconIncomingCall,
+  IconIncomplete,
+  IconIncompleteInverse,
+  IconIncompleteInverseLowLight,
+  IconInformation,
+  IconInformationInverse,
+  IconInformationInverseLowLight,
+  IconInformationLowLight,
+  IconItalicI,
+  IconKnurling,
+  IconLast,
+  IconLeft,
+  IconLeftPane,
+  IconLightbulb,
+  IconLink,
+  IconList,
+  IconLookback,
+  IconLow,
+  IconLowLowLight,
+  IconLowRisk,
+  IconLowRiskLowLight,
+  IconManufacturer,
+  IconMax,
+  IconMaximize,
+  IconMenu,
+  IconMerge,
+  IconMicrophone,
+  IconMicrophoneDisabled,
+  IconMin,
+  IconMinimize,
+  IconMinus,
+  IconMissedCall,
+  IconModerate,
+  IconModerateLowLight,
+  IconModified,
+  IconNext,
+  IconNoRisk,
+  IconNoRiskLowLight,
+  IconNoSignal,
+  IconNotMet,
+  IconNotification,
+  IconNotificationOff,
+  IconOutgoingCall,
+  IconOverDue,
+  IconOverDueLowLight,
+  IconPadlock,
+  IconPanelLeft,
+  IconPanelRight,
+  IconPaperFolded,
+  IconPaperPencil,
+  IconPartiallyMet,
+  IconPause,
+  IconPending,
+  IconPerson,
+  IconPersonHospital,
+  IconPersonnelPerson,
+  IconPharmacyReject,
+  IconPharmacyRejectLowLight,
+  IconPharmacyReview,
+  IconPhone,
+  IconPhoneDown,
+  IconPill,
+  IconPinDiagonal,
+  IconPinDown,
+  IconPlusSymbol,
+  IconPrevious,
+  IconPrinter,
+  IconProjects,
+  IconProtocol,
+  IconProvider,
+  IconQuestion,
+  IconQuestionOutline,
+  IconRectangleSymbol,
+  IconRecurringEvent,
+  IconRedo,
+  IconRefresh,
+  IconReload,
+  IconReply,
+  IconReplyAll,
+  IconRequired,
+  IconRight,
+  IconRollup,
+  IconRotateLeft,
+  IconRotateRight,
+  IconSave,
+  IconScheduled,
+  IconScheduledLowLight,
+  IconScratchPad,
+  IconSearch,
+  IconSecondaryProvider,
+  IconSend,
+  IconSeparate,
+  IconSettings,
+  IconSimulation,
+  IconSortAscending,
+  IconSortDescending,
+  IconSpinner,
+  IconSquareSymbol,
+  IconSuccess,
+  IconSuccessInverse,
+  IconSuccessInverseLowLight,
+  IconSuccessLowLight,
+  IconSwap,
+  IconTable,
+  IconTag,
+  IconTeardropSymbol,
+  IconTeardropUpSymbol,
+  IconThinDiamondSymbol,
+  IconTile,
+  IconTrash,
+  IconTreemap,
+  IconTriangleDownSymbol,
+  IconTriangleSymbol,
+  IconTrophy,
+  IconUnavailable,
+  IconUndo,
+  IconUnexpected,
+  IconUnexpectedLowLight,
+  IconUnknown,
+  IconUnlock,
+  IconUnread,
+  IconUnspecified,
+  IconUp,
+  IconUpload,
+  IconUsers,
+  IconVideoCamera,
+  IconVideoCameraDisabled,
+  IconVisualization,
+  IconWarning,
+  IconWarningLowLight,
+  IconXSymbol,
+  IconZoomIn,
+  IconZoomOut,
+};
+
+export default Icons;
