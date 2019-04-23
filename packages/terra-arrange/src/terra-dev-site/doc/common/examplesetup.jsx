ee3bc4e6c51fb6e974e2b3abdb6d2ed5a3fb5e21
Adam Parker <amichaelparker@gmail.com>
2019-04-23 09:53:28 -0500

    adjust example div arrangement

--- packages/terra-arrange/src/terra-dev-site/doc/common/examplesetup.jsx
+++ packages/terra-arrange/src/terra-dev-site/doc/common/examplesetup.jsx
@@ -5,10 +5,21 @@ import IconAlert from 'terra-icon/lib/icon/IconAlert';
 const icon = <IconAlert width="40px" height="40px" />;
 // Block style is required since images are inline by default. This could be resolved by someones normalize.css
 const image = <img style={{ display: 'block' }} height="40px" width="40px" src="https://github.com/cerner/terra-core/raw/master/terra.png" alt="a happy panda" />;
-const alignExampleDiv = <div style={{ backgroundColor: '#000', display: 'block', height: '20px', margin: '1px', width: '40px' }} />;
 const ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 const simpleText = <div>{ipsum}</div>;
 const textWithPadding = <div style={{ paddingLeft: '10px', paddingRight: '10px', border: '1px solid red' }}>{ipsum}</div>;
+
+const alignExampleDiv = (
+  <div
+    style={{
+      backgroundColor: '#000',
+      display: 'block',
+      height: '20px',
+      margin: '1px',
+      width: '40px',
+    }}
+  />
+);
 
 const ArrangeWrapper = props => (
   <div style={{ width: '500px', height: '50px', border: '1px solid black' }}>
