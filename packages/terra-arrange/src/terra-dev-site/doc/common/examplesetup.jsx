efc8d44844856176f96854fc41c36ab7b98f895b
Adam Parker <amichaelparker@gmail.com>
2019-04-23 15:07:23 -0500

    removed more dead code, added wdio test from old doc site example

--- packages/terra-arrange/src/terra-dev-site/doc/common/examplesetup.jsx
+++ packages/terra-arrange/src/terra-dev-site/doc/common/examplesetup.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import IconAlert from 'terra-icon/lib/icon/IconAlert';
 
-const icon = <IconAlert width="40px" height="40px" />;
-// Block style is required since images are inline by default. This could be resolved by someones normalize.css
-const image = <img style={{ display: 'block' }} height="40px" width="40px" src="https://github.com/cerner/terra-core/raw/master/terra.png" alt="a happy panda" />;
 const ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 const textWithBorder = <div style={{ border: '1px solid #000' }}>{ipsum}</div>;
 const textWithBlueBorder = <div style={{ border: '1px solid #0079BE' }}>{ipsum}</div>;
@@ -48,8 +44,6 @@ export {
   alignEndExampleDiv,
   alignExampleDiv,
   alignStartExampleDiv,
-  icon,
-  image,
   textWithBlueBorder,
   textWithBorder,
 };
