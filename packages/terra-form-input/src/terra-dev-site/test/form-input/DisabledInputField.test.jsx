52c45e369a96e8d1f8c4bcaf78c024ad89543605
Alex Brisimitzakis <alex.brisimitzakis@cerner.com>
2019-04-25 14:48:40 -0500

    Fixed lint errors

--- packages/terra-form-input/src/terra-dev-site/test/form-input/DisabledInputField.test.jsx
+++ packages/terra-form-input/src/terra-dev-site/test/form-input/DisabledInputField.test.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import InputField from '../../../InputField';
 
-const DisabledInputFieldExample = () => {
-  return (
-      <InputField
-        disabled
-        inputId="test-input"
-        defaultValue="Value"
-        error="Text"
-        help="Help"
-        hideRequired
-        inputAttrs={{
-          name: 'test',
-          type: 'text',
-        }}
-        label="Label Text"
-        labelAttrs={{
-          className: 'label',
-        }}
-        showOptional
-        maxWidth="40%"
-      />
-  );
-};
+const DisabledInputFieldExample = () => (
+  <InputField
+    disabled
+    inputId="test-input"
+    defaultValue="Value"
+    error="Text"
+    help="Help"
+    hideRequired
+    inputAttrs={{
+      name: 'test',
+      type: 'text',
+    }}
+    label="Label Text"
+    labelAttrs={{
+      className: 'label',
+    }}
+    showOptional
+    maxWidth="40%"
+  />
+);
 
 export default DisabledInputFieldExample;
