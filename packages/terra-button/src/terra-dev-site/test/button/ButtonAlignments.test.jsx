bbc6d66b3599bbfd0fb66d14ca580d76f0218f1e
Adam Parker <amichaelparker@gmail.com>
2019-05-03 12:17:47 -0500

    added variety button alignment test page

---
+++ packages/terra-button/src/terra-dev-site/test/button/ButtonAlignments.test.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import IconCaretUp from 'terra-icon/lib/icon/IconCaretUp';
+import IconCaretDown from 'terra-icon/lib/icon/IconCaretDown';
+import IconEdit from 'terra-icon/lib/icon/IconEdit';
+import IconSuccess from 'terra-icon/lib/icon/IconSuccess';
+import IconTag from 'terra-icon/lib/icon/IconTag';
+import ActionHeader from '../../../../../terra-action-header/src/ActionHeader';
+import Button from '../../../Button';
+import ButtonGroup from '../../../../../terra-button-group/src/ButtonGroup';
+import Dialog from '../../../../../terra-dialog/src/Dialog';
+import SearchField from '../../../../../terra-search-field/src/SearchField';
+import SectionHeader from '../../../../../terra-section-header/src/SectionHeader';
+import ShowHide from '../../../../../terra-show-hide/src/ShowHide';
+import Tag from '../../../../../terra-tag/src/Tag';
+import ToggleButton from '../../../../../terra-toggle-button/src/ToggleButton';
+import ToggleSectionHeader from '../../../../../terra-toggle-section-header';
+
+const spacingStyle = { padding: '10px' };
+const buttonStyle = { margin: '5px' };
+const cellStyle = { padding: '0.5rem' };
+const constraintStyle = { width: '20%' };
+
+export default () => (
+  <div style={{ paddingLeft: '1em' }}>
+    <Button icon={<IconEdit />} text="icon" style={buttonStyle} />
+    <div style={spacingStyle} />
+    <ActionHeader
+      title="tester"
+      onClose={() => alert('You clicked close!')}
+      onBack={() => alert('You clicked back!')}
+      style={constraintStyle}
+    />
+    <div style={spacingStyle} />
+    <ButtonGroup>
+      <ButtonGroup.Button text="Up" icon={<IconCaretUp />} key="upIcon" />
+      <ButtonGroup.Button text="Down" icon={<IconCaretDown />} key="downIcon" />
+    </ButtonGroup>
+    <div style={spacingStyle} />
+    <Dialog
+      header="le header"
+      onClose={() => alert('Close')}
+      style={constraintStyle}
+    />
+    <div style={spacingStyle} />
+    <tbody>
+      <tr style={{ backgroundColor: '#EEEEEE' }}>
+        <td style={cellStyle}><IconSuccess height="2em" width="2em" /></td>
+      </tr>
+    </tbody>
+    <div style={spacingStyle} />
+    <SearchField />
+    <div style={spacingStyle} />
+    <SectionHeader
+      title="Closed"
+      onClick={() => {}}
+      style={constraintStyle}
+    />
+    <div style={spacingStyle} />
+    <ShowHide style={{ width: '20%', backgroundColor: '#eee' }} />
+    <div style={spacingStyle} />
+    <Tag icon={<IconTag />} text="Icon &amp; Text Tag" />
+    <div style={spacingStyle} />
+    <ToggleButton closedButtonText="ToggleButton" />
+    <div style={spacingStyle} />
+    <ToggleSectionHeader style={constraintStyle} />
+  </div>
+);
