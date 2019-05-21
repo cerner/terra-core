a007ca0b2e48bf7fc33cc35cfeb81a7d791adfd9
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2019-05-21 16:16:09 -0500

    fix section header toggle pics

--- packages/terra-toggle-section-header/tests/wdio/toggle-section-header-spec.js
+++ packages/terra-toggle-section-header/tests/wdio/toggle-section-header-spec.js
@@ -1,13 +1,19 @@
 describe('ToggleSectionHeader', () => {
   describe('Default', () => {
-    beforeEach(() => browser.url('/#/raw/tests/terra-toggle-section-header/toggle-section-header/default-toggle-section-header'));
+    before(() => {
+      browser.url('/#/raw/tests/terra-toggle-section-header/toggle-section-header/default-toggle-section-header');
+      browser.moveToObject('#root', 0, 900); // move to prevent hover styles
+    });
 
     Terra.should.beAccessible();
     Terra.should.matchScreenshot();
   });
 
   describe('Open', () => {
-    before(() => browser.url('/#/raw/tests/terra-toggle-section-header/toggle-section-header/open-toggle-section-header'));
+    before(() => {
+      browser.url('/#/raw/tests/terra-toggle-section-header/toggle-section-header/open-toggle-section-header');
+      browser.moveToObject('#root', 0, 900); // move to prevent hover styles
+    });
 
     Terra.should.beAccessible();
     Terra.should.matchScreenshot();
