7258e1dc8e69f321bbdf4ec2b86a8ee58ff368bb
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2019-05-23 14:23:09 -0500

    tweaks to configs

--- dangerfile.js
+++ dangerfile.js
@@ -1,38 +1,37 @@
 // eslint-disable-next-line import/no-extraneous-dependencies, no-unused-vars, object-curly-newline
-import { danger, warn, fail, message } from 'danger';
-import path from 'path';
+import { danger, warn, fail } from 'danger';
 
-const newChangelog = danger.git.created_files.filter((filePath) => {
-  const srcFilePattern = /^packages\/terra-([a-z-])*\/CHANGELOG.md/i;
-  return srcFilePattern.test(filePath);
-});
+const determinePackages = (changedList, path) => {
+  const path1 = path.split('packages/')[1];
+  const packageName = path1.split('/')[0];
+  return changedList.includes(packageName) ? changedList : changedList.concat([packageName]);
+};
 
-const modifiedChangelog = danger.git.modified_files.filter((filePath) => {
-  const srcFilePattern = /^packages\/terra-([a-z-])*\/CHANGELOG.md/i;
-  return srcFilePattern.test(filePath);
-});
+const newChangelogs = danger.git.created_files.filter((filePath) => {
+  const changelogPattern = /^packages\/terra-([a-z-])*\/CHANGELOG.md/i;
+  return changelogPattern.test(filePath);
+}).reduce(determinePackages, []);
 
-const modifiedPackageFiles = danger.git.modified_files.filter((filePath) => {
-  const packagePattern = /^packages\/terra-([a-z-])*\//;
-  return packagePattern.test(filePath);
-});
+const modifiedChangelogs = danger.git.modified_files.filter((filePath) => {
+  const changelogPattern = /^packages\/terra-([a-z-])*\/CHANGELOG.md/i;
+  return changelogPattern.test(filePath);
+}).reduce(determinePackages, []);
 
-console.log()
+const modifiedPackages = danger.git.modified_files.filter((filePath) => {
+  const changelogPattern = /^packages\/terra-([a-z-])*\/CHANGELOG.md/i;
+  const packagePattern = /^packages\/terra-([a-z-])*\//i;
+  return packagePattern.test(filePath) && !changelogPattern.test(filePath);
+}).reduce(determinePackages, []);
 
-const hasCHANGELOGChanges = modifiedChangelog.length > 0 || newChangelog.length > 0;
-const hasModifiedPackages = modifiedPackages.length > 0;
+const missingChangelogs = modifiedPackages.filter(packageName => !modifiedChangelogs.includes(packageName) || newChangelogs.includes(packageName));
 
-modifiedPackages.forEach((package) => {
-
-})
-
-// Fail if there are src code changes without a CHANGELOG update
-if (hasModifiedPackages && !hasCHANGELOGChanges) {
-  fail('Please include a CHANGELOG entry with this PR.');
+// Fail if there are package changes without a CHANGELOG update
+if (missingChangelogs.length > 0) {
+  fail(`Please include a CHANGELOG entry for each changed package this PR. Looks like a CHANGELOG is missing for: \n - ${missingChangelogs.join('\n - ')}`);
 }
 
 // Warn when there is a big PR
-// const bigPRThreshold = 1000;
-// if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-//   warn(':exclamation: Big PR. Consider breaking this into smaller PRs if applicable');
-// }
+const bigPRThreshold = 1000;
+if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+  warn(':exclamation: Big PR. Consider breaking this into smaller PRs if applicable');
+}
