d5716b204e50ae1cfea576fdb96f96af19d10905
Nathan Faltermeier <nathan.faltermeier@cerner.com>
2019-05-29 11:54:30 -0500

    changelog required for any changes in packages

--- dangerfile.js
+++ dangerfile.js
@@ -3,12 +3,11 @@ import { danger, warn, fail } from 'danger';
 
 const BIG_PR_THRESHOLD = 1000;
 const CHANGELOG_PATTERN = /^packages\/terra-([a-z-])*\/CHANGELOG\.md/i;
-const PACKAGE_SOURCE_PATTERN = /^packages\/terra-([a-z-])*\/src/i;
 
 const changedFiles = danger.git.created_files.concat(danger.git.modified_files);
 
 const changedChangelogs = new Set();
-const changedSourceFiles = new Set();
+const changedPackages = new Set();
 
 changedFiles.forEach((file) => {
   // file isn't in a package so it has no changelog, skip further processing
@@ -20,12 +19,12 @@ changedFiles.forEach((file) => {
 
   if (CHANGELOG_PATTERN.test(file)) {
     changedChangelogs.add(packageName);
-  } else if (PACKAGE_SOURCE_PATTERN.test(file)) {
-    changedSourceFiles.add(packageName);
+  } else { // file is in a package and was changed - we need a changelog
+    changedPackages.add(packageName);
   }
 });
 
-const missingChangelogs = [...changedSourceFiles].filter(packageName => !changedChangelogs.has(packageName));
+const missingChangelogs = [...changedPackages].filter(packageName => !changedChangelogs.has(packageName));
 
 // Fail if there are package changes without a CHANGELOG update
 if (missingChangelogs.length > 0) {
