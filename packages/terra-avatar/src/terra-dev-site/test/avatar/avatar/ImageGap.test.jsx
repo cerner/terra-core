f906291f6f169476a5abb18e13d0cb215902893e
Pranav Agarwal <agarwal.pranav300@gmail.com>
2019-06-17 23:36:41 +0530

    [terra-core] Removed inline style for dev-site components Part 1 (#2427)

--- packages/terra-avatar/src/terra-dev-site/test/avatar/avatar/ImageGap.test.jsx
+++ packages/terra-avatar/src/terra-dev-site/test/avatar/avatar/ImageGap.test.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import Avatar, { Facility } from '../../../../index';
 import exampleAvatarImage from '../../../assets/150x150.jpg';
+import styles from './ImageGap.test.scss';
+
+const cx = classNames.bind(styles);
 
 export default () => (
   <div>
-    <div style={{ border: '5px solid #f00' }}>
+    <div className={cx('avatar-wrapper')}>
       <Avatar id="image-avatar" image={exampleAvatarImage} alt="placeholder" />
     </div>
-    <div style={{ border: '5px solid #f00' }}>
+    <div className={cx('avatar-wrapper')}>
       <Facility id="facility-avatar" alt="facility" />
     </div>
   </div>
