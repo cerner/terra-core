3645237fcedb348d89a97e9e2eeb0b2036e70c0d
Avinash Gupta <50138162+avinashg1994@users.noreply.github.com>
2019-06-26 01:13:34 +0530

    [terra-core] Removed inline style for dev-site components Part 7 (#2473)

--- packages/terra-spacer/src/terra-dev-site/test/spacer/SpacerSmall.test.jsx
+++ packages/terra-spacer/src/terra-dev-site/test/spacer/SpacerSmall.test.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
-import './SpacerTest.module.scss';
+import classNames from 'classnames/bind';
 import Spacer from '../../../Spacer';
+import styles from './SpacerTest.module.scss';
+
+const cx = classNames.bind(styles);
 
 export default () => (
   <div>
     <h3>Background color added for context. The light orange background shows margin, the green background shows padding.</h3>
     <div>Margin with value small applied</div>
-    <div className="spacing-demo spacer-demo-margin">
-      <Spacer id="spacerMarginSmall" className="spacer" marginTop="small" marginBottom="small" marginLeft="small" marginRight="small">
-        <div style={{ background: 'rgb(141, 182, 193)' }}>Test component</div>
+    <div className={cx(['spacing-demo', 'spacer-demo-margin'])}>
+      <Spacer id="spacerMarginSmall" className={cx('spacer')} marginTop="small" marginBottom="small" marginLeft="small" marginRight="small">
+        <div className={cx('spacer-content-wrapper')}>Test component</div>
       </Spacer>
     </div>
     <div>Padding with value small applied</div>
-    <div className="spacing-demo spacer-demo-margin">
-      <Spacer id="spacerPaddingSmall" className="spacer" paddingTop="small" paddingBottom="small" paddingLeft="small" paddingRight="small">
-        <div style={{ background: 'rgb(141, 182, 193)' }}>Test component</div>
+    <div className={cx(['spacing-demo', 'spacer-demo-margin'])}>
+      <Spacer id="spacerPaddingSmall" className={cx('spacer')} paddingTop="small" paddingBottom="small" paddingLeft="small" paddingRight="small">
+        <div className={cx('spacer-content-wrapper')}>Test component</div>
       </Spacer>
     </div>
     <div>Margin and Padding with value small applied</div>
-    <div className="spacing-demo spacer-demo-margin">
-      <Spacer id="spacerMarginPaddingSmall" className="spacer" marginTop="small" marginBottom="small" marginLeft="small" marginRight="small" paddingTop="small" paddingBottom="small" paddingLeft="small" paddingRight="small">
-        <div style={{ background: 'rgb(141, 182, 193)' }}>Test component</div>
+    <div className={cx(['spacing-demo', 'spacer-demo-margin'])}>
+      <Spacer id="spacerMarginPaddingSmall" className={cx('spacer')} marginTop="small" marginBottom="small" marginLeft="small" marginRight="small" paddingTop="small" paddingBottom="small" paddingLeft="small" paddingRight="small">
+        <div className={cx('spacer-content-wrapper')}>Test component</div>
       </Spacer>
     </div>
     <div>Margin, Padding with value small and isInlineBlock prop applied</div>
-    <div className="spacing-demo spacer-demo-margin">
-      <Spacer id="spacerMarginPaddingSmallInlineBlock" className="spacer" marginTop="small" marginBottom="small" marginLeft="small" marginRight="small" paddingTop="small" paddingBottom="small" paddingLeft="small" paddingRight="small" isInlineBlock>
-        <div style={{ background: 'rgb(141, 182, 193)' }}>Test component</div>
+    <div className={cx(['spacing-demo', 'spacer-demo-margin'])}>
+      <Spacer id="spacerMarginPaddingSmallInlineBlock" className={cx('spacer')} marginTop="small" marginBottom="small" marginLeft="small" marginRight="small" paddingTop="small" paddingBottom="small" paddingLeft="small" paddingRight="small" isInlineBlock>
+        <div className={cx('spacer-content-wrapper')}>Test component</div>
       </Spacer>
     </div>
   </div>
--- packages/terra-status-view/src/terra-dev-site/doc/example/NoPropsExample.jsx
+++ packages/terra-status-view/src/terra-dev-site/doc/example/NoPropsExample.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
 import StatusView from 'terra-status-view/lib/StatusView';
+import classNames from 'classnames/bind';
+import styles from './StatusViewDocCommon.module.scss';
+
+const cx = classNames.bind(styles);
 
 const NoPropsExample = (
-  <StatusView style={{ width: '500px' }} />
+  <StatusView className={cx('status-view-width')} />
 );
 
 export default NoPropsExample;
