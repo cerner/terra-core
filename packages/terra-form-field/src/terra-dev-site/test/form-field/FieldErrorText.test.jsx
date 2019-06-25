fabc278bd6e721576e6a558b13f6fbf84d914083
Avinash Gupta <50138162+avinashg1994@users.noreply.github.com>
2019-06-25 17:05:23 +0530

    Form Field, List and Responsive Element inline style cleanup (#2472)

--- packages/terra-form-field/src/terra-dev-site/test/form-field/FieldErrorText.test.jsx
+++ packages/terra-form-field/src/terra-dev-site/test/form-field/FieldErrorText.test.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import Field from '../../../Field';
+import styles from './FormFieldTestCommon.module.scss';
+
+const cx = classNames.bind(styles);
 
 const FieldErrorText = () => (
   <div>
     <h3>Field - Error Text</h3>
     <Field
       id="error-text"
-      style={{ border: 'dashed 1px lightGrey', padding: '10px' }}
+      className={cx('form-field')}
       label="Field Label"
       isLabelHidden
       error="Error Message"
       isInvalid
     >
-      <div style={{ border: 'dashed 1px lightGrey' }}>Control Placeholder</div>
+      <div className={cx('field-content')}>Control Placeholder</div>
     </Field>
   </div>
 );
