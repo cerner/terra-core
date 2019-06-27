f635a6e44e518ae193889ae665f2fc2a90f4643f
Pranav Agarwal <agarwal.pranav300@gmail.com>
2019-06-28 01:40:04 +0530

    [terra-alert]Change `customStatusColor` prop to themeable `customColorClass` prop. (#2407)
    * updated source code.
    
    * Added customColorClass prop and jest test.
    
    * updated source code, wdio test.
    
    * updated changelog.
    
    * updated source code.
    
    Added customColorClass prop and jest test.
    
    updated source code, wdio test.
    
    updated changelog.
    
    * update source code to use const for wide and narrow.
    
    * updated source code comment for the prop.
    
    * updated moreinformation file.
    
    * removed moreinformation, updated examples for Alert and readme.
    
    * updated changelog.
    
    * button changelog.
    
    * Updated button changelog
    
    * updated changelog
    
    * Resolved Review Comments.
    
    * added end line in scss file.
    
    * Added default color to customColorClass and updated wdio screenshots.
    
    * updated snapshots
    
    * Resolved review comments
    
    * updated doc-site documents
    
    * updated package.json
    
    * updated wdio screenshots
    
    * Fix default class
    
    * Fix default class

--- packages/terra-alert/src/terra-dev-site/doc/example/CustomExample.jsx
+++ packages/terra-alert/src/terra-dev-site/doc/example/CustomExample.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import IconHelp from 'terra-icon/lib/icon/IconHelp';
+import classNames from 'classnames/bind';
 // eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
 import Alert from 'terra-alert/lib/Alert';
+import styles from './colors.module.scss';
 
+const cx = classNames.bind(styles);
 
 const CustomExample = () => (
-  <Alert type="custom" title="Help!" customStatusColor="orange" customIcon={<IconHelp />}>
+  <Alert type="custom" title="Help!" customColorClass={cx(['my-app-alert-help-example'])} customIcon={<IconHelp />}>
     <span>
       This is a
-      <b>custom</b>
+      <b> custom</b>
       {' '}
       alert
     </span>
