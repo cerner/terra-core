faf011aad0b10c18dfba6765565250c9b9efa221
Lokesh P <lokesh.p@cerner.com>
2019-07-08 16:27:45 +0530

    Updated Alert to use react-inlt's injectIntl

--- packages/terra-alert/src/Alert.jsx
+++ packages/terra-alert/src/Alert.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ResponsiveElement from 'terra-responsive-element';
 import Button from 'terra-button';
+import { injectIntl, intlShape } from 'react-intl';
 import IconAlert from 'terra-icon/lib/icon/IconAlert';
 import IconError from 'terra-icon/lib/icon/IconError';
 import IconWarning from 'terra-icon/lib/icon/IconWarning';
@@ -42,6 +43,10 @@ const propTypes = {
    * Adding `var(--my-app...` CSS variables is required for proper re-themeability when creating custom color styles _(see included examples)_.
    */
   customColorClass: PropTypes.string,
+  /**
+   * The intl object containing translations. This is retrieved from the context automatically by injectIntl.
+   */
+  intl: intlShape.isRequired,
   /**
    * Callback function triggered when Dismiss button is clicked. The presence of this prop will cause the Dismiss button to be included on the alert.
    */
@@ -96,26 +101,16 @@ const getAlertIcon = (type, customIcon) => {
   }
 };
 
-const contextTypes = {
-  /* eslint-disable consistent-return */
-  intl: (context) => {
-    if (context.intl === undefined) {
-      return new Error('Component is internationalized, and must be wrapped in terra-base');
-    }
-  },
-};
-
 const Alert = ({
-  type,
+  action,
   children,
-  title,
   customIcon,
   customColorClass,
-  onDismiss,
-  action,
-  ...customProps
-}, {
   intl,
+  onDismiss,
+  title,
+  type,
+  ...customProps
 }) => {
   const defaultTitle = type === AlertTypes.CUSTOM ? '' : intl.formatMessage({ id: `Terra.alert.${type}` });
   const attributes = Object.assign({}, customProps);
@@ -191,8 +186,7 @@ const Alert = ({
 };
 Alert.propTypes = propTypes;
 Alert.defaultProps = defaultProps;
-Alert.contextTypes = contextTypes;
 Alert.Opts = {};
 Alert.Opts.Types = AlertTypes;
 
-export default Alert;
+export default injectIntl(Alert);
--- packages/terra-alert/tests/jest/Alert.test.jsx
+++ packages/terra-alert/tests/jest/Alert.test.jsx
@@ -10,16 +10,8 @@ describe('Alert with no props', () => {
   // Snapshot Tests
   it('should render a default component', () => {
     const wrapper = mountWithIntl(<Alert />);
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it('throws error on missing locale prop in Base', () => {
-    global.console = { error: jest.fn() };
-
-    expect(() => {
-      render(<Alert />);
-    }).toThrowError();
-    expect(console.error).toBeCalledWith(expect.stringContaining('Component is internationalized, and must be wrapped in terra-base'));
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
 
@@ -27,7 +19,8 @@ describe('Dismissible Alert that includes actions section', () => {
   // Snapshot Tests
   it('should render an alert component with a dismiss button', () => {
     const wrapper = mountWithIntl(<Alert onDismiss={() => { }}>This is a test</Alert>);
-    expect(wrapper).toMatchSnapshot();
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
 
@@ -35,7 +28,8 @@ describe('Alert of type alert with text content', () => {
   // Snapshot Tests
   it('should render an Alert component of type alert', () => {
     const wrapper = mountWithIntl(<Alert type={Alert.Opts.Types.ALERT}>This is a test</Alert>);
-    expect(wrapper).toMatchSnapshot();
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
 
@@ -43,7 +37,8 @@ describe('Alert of type error with text content', () => {
   // Snapshot Tests
   it('should render an Alert component of type error', () => {
     const wrapper = mountWithIntl(<Alert type={Alert.Opts.Types.ERROR}>This is an error.</Alert>);
-    expect(wrapper).toMatchSnapshot();
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
 
@@ -51,7 +46,8 @@ describe('Alert of type warning with text content', () => {
   // Snapshot Tests
   it('should render an Alert component of type warning', () => {
     const wrapper = mountWithIntl(<Alert type={Alert.Opts.Types.WARNING}>This is an warning.</Alert>);
-    expect(wrapper).toMatchSnapshot();
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
 
@@ -59,7 +55,8 @@ describe('Alert of type advisory with text content', () => {
   // Snapshot Tests
   it('should render an Alert component of type advisory', () => {
     const wrapper = mountWithIntl(<Alert type={Alert.Opts.Types.ADVISORY}>This is an advisory alert.</Alert>);
-    expect(wrapper).toMatchSnapshot();
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
 
@@ -67,7 +64,8 @@ describe('Alert of type info with text content', () => {
   // Snapshot Tests
   it('should render an Alert component of type info', () => {
     const wrapper = mountWithIntl(<Alert type={Alert.Opts.Types.INFO}>This is an information alert.</Alert>);
-    expect(wrapper).toMatchSnapshot();
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
 
@@ -75,7 +73,8 @@ describe('Alert of type success with text content', () => {
   // Snapshot Tests
   it('should render an Alert component of type success', () => {
     const wrapper = mountWithIntl(<Alert type={Alert.Opts.Types.SUCCESS}>This is a success alert.</Alert>);
-    expect(wrapper).toMatchSnapshot();
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
 
@@ -83,7 +82,8 @@ describe('Alert of type custom with custom title and text content', () => {
   // Snapshot Tests
   it('should render an Alert component of type custom', () => {
     const wrapper = mountWithIntl(<Alert type={Alert.Opts.Types.CUSTOM} title="Help!" customIcon={<IconHelp />} customColorClass="terra-alert-custom-orange-color">This is a custom alert.</Alert>);
-    expect(wrapper).toMatchSnapshot();
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
 
@@ -91,7 +91,8 @@ describe('Alert of type info with custom title and HTML content', () => {
   // Snapshot Tests
   it('should render an Alert component of type info with custom title and HTML content', () => {
     const wrapper = mountWithIntl(<Alert type={Alert.Opts.Types.INFO} title="Gettysburg Address"><span>Four score and seven years ago . . .</span></Alert>);
-    expect(wrapper).toMatchSnapshot();
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
 
@@ -99,7 +100,8 @@ describe('Alert of type success with an action button text content', () => {
   // Snapshot Tests
   it('should render an Alert component of type success with an action button', () => {
     const wrapper = mountWithIntl(<Alert type={Alert.Opts.Types.SUCCESS} action={<Button text="Action" variant={Button.Opts.Variants.EMPHASIS} onClick={() => { }} />}>This is a success alert.</Alert>);
-    expect(wrapper).toMatchSnapshot();
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
 
@@ -107,6 +109,7 @@ describe('Dismissable Alert of type custom with action button, custom title and 
   // Snapshot Tests
   it('should render an Alert component of type custom with an action button', () => {
     const wrapper = mountWithIntl(<Alert type={Alert.Opts.Types.CUSTOM} onDismiss={() => { }} title="Help!" customIcon={<IconHelp />} customColorClass="terra-alert-custom-orange-color" action={<Button text="Action" variant={Button.Opts.Variants.EMPHASIS} onClick={() => { }} />}>This is a custom alert.</Alert>);
-    expect(wrapper).toMatchSnapshot();
+    const testComponent = wrapper.children();
+    expect(testComponent).toMatchSnapshot();
   });
 });
