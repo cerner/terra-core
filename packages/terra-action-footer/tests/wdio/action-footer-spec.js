b60d3efac84015003de1a98cd85a2bf31e560522
Nathan Faltermeier <nathan.faltermeier@cerner.com>
2019-07-08 09:42:37 -0500

    Change to use Terra.describeViewports part 1 (#2498)

--- packages/terra-action-footer/tests/wdio/action-footer-spec.js
+++ packages/terra-action-footer/tests/wdio/action-footer-spec.js
@@ -1,38 +1,31 @@
-const { viewports } = require('./common');
-
-describe('ActionFooter', () => {
+Terra.describeViewports('ActionFooter', ['small', 'medium'], () => {
   describe('Multiple End Actions', () => {
     before(() => browser.url('/#/raw/tests/terra-action-footer/action-footer/multiple-end-action-footer'));
 
-    Terra.it.isAccessible({ viewports });
-    Terra.it.matchesScreenshot({ viewports });
+    Terra.it.validatesElement();
   });
 
   describe('Multiple Start Actions', () => {
     before(() => browser.url('/#/raw/tests/terra-action-footer/action-footer/multiple-start-action-footer'));
 
-    Terra.it.isAccessible({ viewports });
-    Terra.it.matchesScreenshot({ viewports });
+    Terra.it.validatesElement();
   });
 
   describe('Single End Action', () => {
     before(() => browser.url('/#/raw/tests/terra-action-footer/action-footer/single-end-action-footer'));
 
-    Terra.it.isAccessible({ viewports });
-    Terra.it.matchesScreenshot({ viewports });
+    Terra.it.validatesElement();
   });
 
   describe('Single Start Action', () => {
     before(() => browser.url('/#/raw/tests/terra-action-footer/action-footer/single-start-action-footer'));
 
-    Terra.it.isAccessible({ viewports });
-    Terra.it.matchesScreenshot({ viewports });
+    Terra.it.validatesElement();
   });
 
   describe('Single Start and End Action', () => {
     before(() => browser.url('/#/raw/tests/terra-action-footer/action-footer/single-start-end-action-footer'));
 
-    Terra.it.isAccessible({ viewports });
-    Terra.it.matchesScreenshot({ viewports });
+    Terra.it.validatesElement();
   });
 });
