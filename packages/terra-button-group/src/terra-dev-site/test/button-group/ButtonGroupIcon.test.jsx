3ded2642e4d65fa543725d17b92161b7047f044b
Bajaj,Saket <saket.bajaj@cerner.com>
2019-07-11 11:51:04 +0530

    IE svg fix

--- packages/terra-button-group/src/terra-dev-site/test/button-group/ButtonGroupIcon.test.jsx
+++ packages/terra-button-group/src/terra-dev-site/test/button-group/ButtonGroupIcon.test.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import classNames from 'classnames/bind';
 import ButtonGroup from '../../../ButtonGroup';
-import styles from './ButtonGroupIcon.test.scss';
 
-const cx = classNames.bind(styles);
-
-const icon = <span className={cx(['test', 'icon'])}><svg width="1em" height="1em"><rect width="1em" height="1em" /></svg></span>;
+const icon = <svg width="1em" height="1em"><rect width="1em" height="1em" fill="#000" /></svg>;
 
 const IconButtonGroup = () => (
   <ButtonGroup id="button-group-icon">
