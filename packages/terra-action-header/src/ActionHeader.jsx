9a16a0d8b03be14e47647a3649ffdb2590bdd689
Nathan Faltermeier <nathan.faltermeier@cerner.com>
2019-07-16 09:11:17 -0500

    Apply suggestions from code review
    Co-Authored-By: Brett Jankord <bjankord@gmail.com>

--- packages/terra-action-header/src/ActionHeader.jsx
+++ packages/terra-action-header/src/ActionHeader.jsx
@@ -31,7 +31,7 @@ const propTypes = {
    * The expand button will not display if this is not set or on small viewports.
    * Only the expand button will be rendered if onMaximize and onMinimize are set.
    *
-   * *Note: If `onBack` is set the maximize button will not appear and must be provided as a child inside
+   * *Note: If `onBack` is set, the maximize button will not appear and must be provided as a child inside
    * a `Collapsible Menu View`. See examples for an example.*
    */
   onMaximize: PropTypes.func,
@@ -40,7 +40,7 @@ const propTypes = {
    * The minimize button will not display if this is not set or on small viewports.
    * Only the expand button will be rendered if both onMaximize and onMinimize are set.
    *
-   * *Note: If `onBack` is set the minimize button will not appear and must be provided as a child inside
+   * *Note: If `onBack` is set, the minimize button will not appear and must be provided as a child inside
    * a `Collapsible Menu View`. See examples for an example.*
    */
   onMinimize: PropTypes.func,
