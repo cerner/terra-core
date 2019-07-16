b0d2c7642f0f8573835ac9235f89ca58b4001ffe
Pranav Agarwal <Pranav.Agarwal@cerner.com>
2019-07-16 16:22:14 +0530

    resolved review comments.

--- packages/terra-form-select/src/_Frame.jsx
+++ packages/terra-form-select/src/_Frame.jsx
@@ -726,7 +726,7 @@ class Frame extends React.Component {
           <span id={labelId}>{this.ariaLabel()}</span>
           <span id={descriptionId}>{this.renderDescriptionText()}</span>
         </div>
-        <div className={cx('display')} aria-label={this.props.intl.formatMessage({ id: 'Terra.form.select.display' })} role={variant === Variants.DEFAULT ? 'textbox' : null}>
+        <div className={cx('display')} aria-label={this.ariaLabel()} role={variant === Variants.DEFAULT ? 'textbox' : null}>
           {this.getDisplay(displayId, placeholderId, ariaDescribedBy)}
         </div>
         {this.renderToggleButton()}
