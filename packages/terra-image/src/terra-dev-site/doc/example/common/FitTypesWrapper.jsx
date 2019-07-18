7dfb5138d61bc6c48c800e42512b33babc53c732
Pranav Agarwal <Pranav.Agarwal@cerner.com>
2019-07-18 15:08:01 +0530

    updated FitTypesWrapper.jsx

--- packages/terra-image/src/terra-dev-site/doc/example/common/FitTypesWrapper.jsx
+++ packages/terra-image/src/terra-dev-site/doc/example/common/FitTypesWrapper.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function FitTypesWrapper(WrappedComponent) {
-  return class FitTypes extends React.Component {
+  return class fitTypes extends React.Component {
     constructor(props) {
       super(props);
       this.handleOnSelect = this.handleOnSelect.bind(this);
-      this.state = { FitType: 'fill', VariantType: 'default' };
+      this.state = { fitType: 'fill', variantType: 'default' };
     }
 
     handleOnSelect(event) {
@@ -15,17 +15,17 @@ function FitTypesWrapper(WrappedComponent) {
     render() {
       return (
         <div>
-          <WrappedComponent fit={this.state.FitType} variant={this.state.VariantType} {...this.props} />
-          <p><label htmlFor="FitType">Select a Fit Type:</label></p>
-          <select id="FitType" name="FitType" value={this.state.FitType} onChange={this.handleOnSelect}>
+          <WrappedComponent fit={this.state.fitType} variant={this.state.variantType} {...this.props} />
+          <p><label htmlFor="fitType">Select a Fit Type:</label></p>
+          <select id="fitType" name="fitType" value={this.state.fitType} onChange={this.handleOnSelect}>
             <option value="fill">fill</option>
             <option value="cover">cover</option>
             <option value="contain">contain</option>
             <option value="scale-down">scale-down</option>
             <option value="none">none</option>
           </select>
-          <p><label htmlFor="VariantType">Select a Variant Type:</label></p>
-          <select id="VariantType" name="VariantType" value={this.state.VariantType} onChange={this.handleOnSelect}>
+          <p><label htmlFor="variantType">Select a Variant Type:</label></p>
+          <select id="variantType" name="variantType" value={this.state.variantType} onChange={this.handleOnSelect}>
             <option value="default">default</option>
             <option value="circle">circle</option>
             <option value="rounded">rounded</option>
