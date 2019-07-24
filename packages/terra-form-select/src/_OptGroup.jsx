49cf563f69586b087d71ec26310bea07ecee4e06
Pranav Agarwal <Pranav.Agarwal@cerner.com>
2019-07-24 16:55:26 +0530

    added translations.

--- packages/terra-form-select/src/_OptGroup.jsx
+++ packages/terra-form-select/src/_OptGroup.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import { injectIntl, intlShape } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import styles from './_OptGroup.module.scss';
 
 const cx = classNames.bind(styles);
@@ -16,11 +16,6 @@ const propTypes = {
    */
   disabled: PropTypes.bool,
   /**
-   * @private
-   * The intl object to be injected for translations.
-   */
-  intl: intlShape.isRequired,
-  /**
    * The group label.
    */
   label: PropTypes.node.isRequired,
@@ -31,16 +26,20 @@ const defaultProps = {
 };
 
 const OptGroup = ({
-  children, disabled, intl, label,
+  children, disabled, label,
 }) => (
   <li className={cx('opt-group', { 'is-disabled': disabled })} role="group">
     <div className={cx('label')}>
       {label}
     </div>
-    <ul className={cx('options')} aria-label={intl.formatMessage({ id: 'Terra.form.select.option' })} role="listbox">
-      {React.Children.map(children, child => (
-        React.cloneElement(child, { disabled: disabled || !!child.props.disabled })))}
-    </ul>
+    <FormattedMessage id="Terra.form.select.option">
+      {option => (
+        <ul className={cx('options')} role="listbox" aria-label={option}>
+          {React.Children.map(children, child => (
+            React.cloneElement(child, { disabled: disabled || !!child.props.disabled })))}
+        </ul>
+      )}
+    </FormattedMessage>
   </li>
 );
 
@@ -49,4 +48,4 @@ OptGroup.propTypes = propTypes;
 OptGroup.defaultProps = defaultProps;
 OptGroup.isOptGroup = true;
 
-export default injectIntl(OptGroup);
+export default OptGroup;
