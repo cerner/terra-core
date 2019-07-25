043c147c518288d27ada4722769b2468fcac25bf
Pranav Agarwal <Pranav.Agarwal@cerner.com>
2019-07-25 19:27:51 +0530

    updated jest

--- packages/terra-form-select/tests/jest/OptGroup.test.jsx
+++ packages/terra-form-select/tests/jest/OptGroup.test.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-/* eslint-disable-next-line import/no-extraneous-dependencies */
-import { shallowWithIntl } from 'terra-enzyme-intl';
 import OptGroup from '../../src/_OptGroup';
 
 describe('OptGroup', () => {
   it('should render a default Option', () => {
-    const wrapper = shallowWithIntl(<OptGroup label="label" />).dive();
+    const wrapper = shallow(<OptGroup label="label" />);
     expect(wrapper).toMatchSnapshot();
   });
 
