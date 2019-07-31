e6c19962a21987a962ed83ad88ec2124077dd216
Manuel,Ryan <Ryan.Manuel@Cerner.com>
2019-07-31 17:47:46 -0500

    Fix Field

--- packages/terra-form-field/tests/jest/mocks/Select.jsx
+++ packages/terra-form-field/tests/jest/mocks/Select.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Select extends React.Component {
   render() {
     const {
+      isInvalid,
       ...customProps
     } = this.props;
 
     return (
       <select
+        invalid={isInvalid && 'true'}
         {...customProps}
       />
     );
   }
 }
 
+Select.propTypes = { isInvalid: PropTypes.bool };
 Select.isSelect = true;
 
 export default Select;
