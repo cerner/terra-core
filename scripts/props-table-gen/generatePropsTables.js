f19ad9bec8e7e82a3acdf3604ee3c4fcc5d9dbb8
Adam Parker <Adam.M.Parker2@cerner.com>
2019-08-05 12:38:17 -0500

    cleaned up logic, added comments, improved handling of files

--- scripts/props-table-gen/generatePropsTables.js
+++ scripts/props-table-gen/generatePropsTables.js
@@ -6,28 +6,17 @@ const reactDocs = require('react-docgen');
 /* eslint-enable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
 
-const fileExclusions = [
-  'ActiveBreakpointContext.jsx',
-  'withActiveBreakpoint.jsx',
-  'ButtonGroupUtils.jsx',
-];
-
 const packagesDir = path.resolve(
   path.dirname(__dirname), '../packages',
 );
 
 const tableHeaderTemplate = '<thead><tr><th>Prop Name</th><th>Type</th><th>Is Required</th><th>Default Value</th><th>Description</th></tr></thead>';
 
-const generateTable = (rowArray) => {
-  const generatedTable = `<table>${tableHeaderTemplate}<tbody>${rowArray.join('')}</tbody><table>`;
-
-  return generatedTable;
-};
-
 const createTableRow = (propValues) => {
+  const rowValues = Object.values(propValues);
   let currentRow = '<tr>';
 
-  propValues.forEach((value) => {
+  rowValues.forEach((value) => {
     if (value && value !== undefined) {
       currentRow += `<td>${value}</td>`;
     }
@@ -37,22 +26,25 @@ const createTableRow = (propValues) => {
   return currentRow;
 };
 
-const formatPropTable = (propTable) => {
-  const [name, value] = propTable;
+const formatPropShape = (propShape) => {
+  const [name, value] = propShape;
   const props = {};
   const customType = name === 'customMessages' ? 'custom' : name;
   const specialTypes = ['enum', 'union', 'arrayOf'];
 
+  // Check for special prop types and handle them.
   const typeCheck = specialTypes.indexOf(Object.values(value.type)[0][0]) > -1
     || customType
     ? Object.values(value.type)[0]
     : Object.values(value.type).join('');
 
+  // Check if prop type is an object shape.
   const shapeHandler = value.type.name === 'shape'
     ? `An object shaped like:<br /><pre>${JSON.stringify(value.type.value, null, 1)}</pre>`
     : undefined;
 
-  propTable.forEach(() => {
+  // Construct props for use in constructing table rows.
+  propShape.forEach(() => {
     props.name = name;
     props.type = shapeHandler || typeCheck;
     props.required = Object.values(value.required) === true ? 'required' : 'optional';
@@ -62,13 +54,13 @@ const formatPropTable = (propTable) => {
   return props;
 };
 
-const formatMarkdown = (formattedProps) => {
-  const propValues = Object.values(formattedProps);
-  const tableRow = createTableRow(propValues);
+const generateTable = (rowArray) => {
+  const generatedTable = `<table>${tableHeaderTemplate}<tbody>${rowArray.join('')}</tbody><table>`;
 
-  return tableRow;
+  return generatedTable;
 };
 
+// Generate props table markdown files in each component's docs folder
 const generateMarkdownFile = (component, dir, formattedProps) => {
   const componentPrefix = component.substring(0, component.length - 4);
   const fileName = `${dir}../docs/${componentPrefix}-props-table.md`;
@@ -76,26 +68,36 @@ const generateMarkdownFile = (component, dir, formattedProps) => {
 };
 
 const generateMarkdown = (component, dir, propTable) => {
-  const formattedMarkdown = [];
+  const formattedMarkdownRows = [];
   const propsArray = Object.entries(propTable);
 
+  // Push completely formatted table rows to an array.
   propsArray.forEach((propSet) => {
     if (propSet.indexOf('intl') > -1) return;
-    formattedMarkdown.push(formatMarkdown(formatPropTable(propSet)));
+    formattedMarkdownRows.push(createTableRow(formatPropShape(propSet)));
   });
-  const completeTable = generateTable(formattedMarkdown);
+
+  // Generate complete props table
+  const completeTable = generateTable(formattedMarkdownRows);
 
   return generateMarkdownFile(component, dir, completeTable);
 };
 
 const processFile = (dir, component) => {
-  if (fileExclusions.indexOf(component) > -1 || !component.match(/^[^_]/g)) return;
-  const currentFile = fs.readFileSync(dir + component);
+  if (!component.match(/^[^_]/g)) return;
+  const currentFile = fs.readFileSync(dir + component, 'utf-8');
+
+  // Check if imported file has a propTypes object.
+  const propsCheck = JSON.stringify(currentFile);
+  if (!propsCheck.includes('propTypes')) return;
+
+  // Use react-docgen to extract props from the current component.
   const parsedProps = reactDocs.parse(currentFile);
 
   generateMarkdown(component, dir, parsedProps.props);
 };
 
+// Uses fs and glob to traverse the directory tree looking for .jsx files.
 const processPackageList = () => fs.readdirSync(packagesDir).map(component => path.join(packagesDir, `${component}/src/`))
   .forEach(dir => glob('*.jsx', { cwd: dir }, (err, files) => {
     if (err) console.log(err);
