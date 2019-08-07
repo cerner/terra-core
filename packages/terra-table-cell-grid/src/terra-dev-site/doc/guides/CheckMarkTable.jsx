d01333b1d0274574ec57799d14ec4382f3dcebf0
Kasper,Dave <dave.m.kasper@gmail.com>
2019-08-07 18:21:54 -0500

    Small tweaks for new example.

--- packages/terra-table-cell-grid/src/terra-dev-site/doc/guides/CheckMarkTable.jsx
+++ packages/terra-table-cell-grid/src/terra-dev-site/doc/guides/CheckMarkTable.jsx
@@ -2,34 +2,34 @@ import React, {
   useState,
 } from 'react';
 import Table, {
-  Row, Cell, HeaderCell, HeaderCheckMarkCell,
+  Row, Cell, HeaderCell, Utils, HeaderCheckMarkCell,
 } from 'terra-table-cell-grid/lib/index'; // eslint-disable-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
 import mockData from './mock-data/mock-select';
+
+const maxSectionCount = 3;
 
 const createCell = cell => <Cell isPadded key={cell.key}>{cell.title}</Cell>;
 
 const createCellsForRow = cells => cells.map(cell => createCell(cell));
 
 const CheckMarkTable = () => {
-  const [selectedKey, setSelectedKey] = useState([]);
+  const [selectedKeys, setSelectedKeys] = useState([]);
 
   const handleRowSelection = (event, metaData) => {
     event.preventDefault();
-    if (selectedKey !== metaData.key) {
-      setSelectedKey(metaData.key);
-    }
+    setSelectedKeys(Utils.updatedMultiSelectedKeys(selectedKeys, metaData.key));
   };
 
-  const createTableRow = itemData => (
+  const createTableRow = rowData => (
     <Row
       selectionStyle="checkmark"
-      key={itemData.key}
-      isSelectable
-      isSelected={selectedKey === itemData.key}
-      metaData={{ key: itemData.key }}
+      key={rowData.key}
+      isSelectable={Utils.shouldBeMultiSelectable(maxSectionCount, selectedKeys, rowData.key)}
+      isSelected={selectedKeys.indexOf(rowData.key) >= 0}
+      metaData={{ key: rowData.key }}
       onSelect={handleRowSelection}
     >
-      {createCellsForRow(itemData.cells)}
+      {createCellsForRow(rowData.cells)}
     </Row>
   );
 
@@ -37,9 +37,10 @@ const CheckMarkTable = () => {
 
   return (
     <Table
+      aria-multiselectable
       paddingStyle="standard"
       headerCells={[
-        <HeaderCheckMarkCell isPadded key="check-0" />,
+        <HeaderCheckMarkCell isPadded key="derp" />,
         <HeaderCell isPadded key="cell-0">Column 0</HeaderCell>,
         <HeaderCell isPadded key="cell-1">Column 1</HeaderCell>,
         <HeaderCell isPadded key="cell-2">Column 2</HeaderCell>,
