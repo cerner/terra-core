d4a571b7a0d1eccbfa56836711cbf7fc6d3fa574
Nathan Faltermeier <nathan.faltermeier@cerner.com>
2019-08-08 14:36:01 -0500

    [terra-button] Show focus styles when programmatically focused (#2531)

---
+++ packages/terra-button/src/terra-dev-site/test/button/ProgrammaticFocus.test.jsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+
+import Button from '../../../Button';
+
+export default () => {
+  const button1 = useRef(null);
+  const button2 = useRef(null);
+
+  return (
+    <>
+      <Button
+        refCallback={(ref) => { button1.current = ref; }}
+        text="Button 1 (Press to focus button 2)"
+        onClick={() => { button2.current.focus(); }}
+        id="button1"
+      />
+      <br />
+      <br />
+      <Button
+        refCallback={(ref) => { button2.current = ref; }}
+        text="Button 2 (Press to focus button 1)"
+        onClick={() => { button1.current.focus(); }}
+        id="button2"
+      />
+    </>
+  );
+};
