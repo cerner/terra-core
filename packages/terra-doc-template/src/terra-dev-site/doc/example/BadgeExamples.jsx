7c620def694144a791f69662b1b34b3bf196bbea
Adam Parker <amichaelparker@gmail.com>
2019-08-09 08:22:53 -0500

    updated css, example version

--- packages/terra-doc-template/src/terra-dev-site/doc/example/BadgeExamples.jsx
+++ packages/terra-doc-template/src/terra-dev-site/doc/example/BadgeExamples.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
 import NpmBadge from '../../../NpmBadge';
-import { name } from '../../../../package.json';
+import { name, version } from '../../../../package.json';
 
 const packageUrl = 'https://engineering.cerner.com/terra-ui/components/terra-doc-template/doc-template/doc-template';
-const packageVersion = '2.13.0';
 
 const BadgeExamples = () => (
   <div>
     <p>Dynamically generated npm badge:</p>
     <NpmBadge packageName={name} />
     <p>Custom Badge with packageVersion prop set:</p>
-    <NpmBadge packageName={name} packageVersion={packageVersion} />
+    <NpmBadge packageName={name} packageVersion={version} />
     <p>Custom Badge with packageUrl and packageVersion props set:</p>
-    <NpmBadge packageName={name} packageUrl={packageUrl} packageVersion={packageVersion} />
+    <NpmBadge packageName={name} packageUrl={packageUrl} packageVersion={version} />
   </div>
 );
 
