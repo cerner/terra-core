fd74d787bb3a9885127528ce7ba4285e2426deff
Matt Henkes <mjhenkes@gmail.com>
2019-08-14 14:16:11 -0500

    it block those waits

--- packages/terra-doc-template/tests/wdio/npm-badge-spec.js
+++ packages/terra-doc-template/tests/wdio/npm-badge-spec.js
@@ -1,7 +1,9 @@
 Terra.describeViewports('NpmBadge', ['medium'], () => {
   describe('Npm Badge Variants', () => {
-    before(() => browser.url('/#/raw/tests/terra-doc-template/doc-template/npm-badge'));
-    browser.pause(50);
+    before(() => {
+      browser.url('/#/raw/tests/terra-doc-template/doc-template/npm-badge');
+      browser.pause(50);
+    });
     Terra.it.matchesScreenshot('Badge variants display correctly');
   });
 });
