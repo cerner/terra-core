4282fd165652604b76e7aae1036a6a8c0c3051b6
Kasper,Dave <dave.m.kasper@gmail.com>
2019-09-06 16:25:22 -0500

    Html snapshots.

--- packages/terra-html-table/tests/wdio/html-table-spec.js
+++ packages/terra-html-table/tests/wdio/html-table-spec.js
@@ -1,31 +1,31 @@
 Terra.describeViewports('HTML Table', ['medium'], () => {
   describe('Non Striped Table', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/no-striped-table'));
+    before(() => browser.url('/#/raw/tests/terra-html-table/html-table/no-striped-table'));
     Terra.it.validatesElement();
   });
 
   describe('Striped', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/striped-table'));
+    before(() => browser.url('/#/raw/tests/terra-html-table/html-table/striped-table'));
     Terra.it.validatesElement();
   });
 
   describe('Non Padding', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/no-padding-table'));
+    before(() => browser.url('/#/raw/tests/terra-html-table/html-table/no-padding-table'));
     Terra.it.validatesElement();
   });
 
   describe('Sort Indicator', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/table-with-sort-indicator'));
+    before(() => browser.url('/#/raw/tests/terra-html-table/html-table/table-with-sort-indicator'));
     Terra.it.validatesElement();
   });
 
   describe('Single Row', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/single-row-table'));
+    before(() => browser.url('/#/raw/tests/terra-html-table/html-table/single-row-table'));
     Terra.it.validatesElement();
   });
 
   describe('No Rows', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/table-no-rows'));
+    before(() => browser.url('/#/raw/tests/terra-html-table/html-table/table-no-rows'));
     Terra.it.validatesElement();
   });
 });
