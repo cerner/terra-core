8d0b21ccc2a4fb45137c918c525fa74d8d2506f9
Kasper,Dave <dave.m.kasper@gmail.com>
2019-09-09 16:03:33 -0500

    Update html table screens.

--- packages/terra-cell-grid/tests/wdio/cell-grid-spec.js
+++ packages/terra-cell-grid/tests/wdio/cell-grid-spec.js
@@ -1,11 +1,11 @@
-Terra.describeViewports('Table', ['medium'], () => {
+Terra.describeViewports('CellGrid', ['medium'], () => {
   describe('Cell Test', () => {
-    before(() => browser.url('/#/raw/tests/terra-html-table/html-table/cell'));
+    before(() => browser.url('/#/raw/tests/terra-cell-grid/cell-grid/cell'));
     Terra.it.validatesElement();
   });
 
   describe('CellGrid Test', () => {
-    before(() => browser.url('/#/raw/tests/terra-html-table/html-table/cell-grid'));
+    before(() => browser.url('/#/raw/tests/terra-cell-grid/cell-grid/cell-grid'));
     Terra.it.validatesElement();
   });
 });
