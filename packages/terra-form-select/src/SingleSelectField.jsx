2357038c94e038c5e27821bc102d457143eb17c4
Cody Price <34115417+dev-cprice@users.noreply.github.com>
2019-09-17 13:03:39 -0500

    [terra-form-select] Restore passivity with prop-types (#2646)

--- packages/terra-form-select/src/SingleSelectField.jsx
+++ packages/terra-form-select/src/SingleSelectField.jsx
@@ -19,9 +19,9 @@ const propTypes = {
    */
   label: PropTypes.node.isRequired,
   /**
-   * The default value of the select.
+   * The default value of the select. Can be a string, number, or array of strings/numbers.
    */
-  defaultValue: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array]),
   /**
    * Whether the input is disabled.
    */
@@ -86,9 +86,9 @@ const propTypes = {
    */
   showOptional: PropTypes.bool,
   /**
-   * The value of the select.
+   * The value of the select. Can be a string, number, or array of strings/numbers.
    */
-  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array]),
 };
 
 const defaultProps = {
