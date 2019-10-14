8086910d743bb98cb2a40810e0a108e5ed11fe3a
Ryan Manuel <rfmanuel@gmail.com>
2019-10-14 12:08:41 -0500

    Update packages/terra-form-select/src/terra-dev-site/doc/form-select/SearchSelect.11.doc.jsx
    Co-Authored-By: Neil Pfeiffer <neil.pfeiffer@cerner.com>

--- packages/terra-form-select/src/terra-dev-site/doc/form-select/SearchSelect.11.doc.jsx
+++ packages/terra-form-select/src/terra-dev-site/doc/form-select/SearchSelect.11.doc.jsx
@@ -73,7 +73,7 @@ const Index = () => (
       source: InvalidExampleSrc,
     }, {
       title: 'Incomplete',
-      description: 'Applies theme-specific styling for incomplete.',
+      description: 'Applies theme-specific styling for incomplete. ***Note: Only use incomplete if given specific guidance, reserved for specific applications when no value has been provided. Not for general use.***',
       example: <IncompleteExample />,
       source: IncompleteExampleSrc,
     }, {
