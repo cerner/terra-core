87c8588d616d28719761f4836dba89ac95063d99
Saket Bajaj <42207428+saket2403@users.noreply.github.com>
2019-10-22 21:33:42 +0530

    Duplicate IDs in examples changed (#2711)

--- packages/terra-form-select/src/terra-dev-site/doc/example/tag/RequiredTagSelectField.jsx
+++ packages/terra-form-select/src/terra-dev-site/doc/example/tag/RequiredTagSelectField.jsx
@@ -6,7 +6,7 @@ import styles from '../FormSelectDocCommon.module.scss';
 const cx = classNames.bind(styles);
 
 const TagSelectFieldExample = () => (
-  <TagSelectField label="T-shirt size" placeholder="Select a size" selectId="tshirt-size-field" required className={cx('form-select')}>
+  <TagSelectField label="T-shirt size" placeholder="Select a size" selectId="tshirt-size-field-2" required className={cx('form-select')}>
     <TagSelectField.Option value="xSmall" display="Extra Small" />
     <TagSelectField.Option value="small" display="Small" />
     <TagSelectField.Option value="medium" display="Medium" />
