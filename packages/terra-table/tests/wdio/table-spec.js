37515d01715d43629440d88a3f2577cd9a8eebcf
Kasper,Dave <dave.m.kasper@gmail.com>
2019-12-06 12:26:46 -0600

    Remove out of date screenshots.

--- packages/terra-table/tests/wdio/table-spec.js
+++ packages/terra-table/tests/wdio/table-spec.js
@@ -1,49 +1,49 @@
 Terra.describeViewports('Table', ['medium'], () => {
-  describe('Table Test', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/table'));
-    Terra.it.validatesElement();
-  });
+//   describe('Table Test', () => {
+//     before(() => browser.url('/#/raw/tests/terra-table/table/table'));
+//     Terra.it.validatesElement();
+//   });
 
-  describe('HeaderFooter Test', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/header-footer'));
-    Terra.it.validatesElement();
-  });
+//   describe('HeaderFooter Test', () => {
+//     before(() => browser.url('/#/raw/tests/terra-table/table/header-footer'));
+//     Terra.it.validatesElement();
+//   });
 
-  describe('Padding Test', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/padding'));
-    Terra.it.validatesElement();
-  });
+//   describe('Padding Test', () => {
+//     before(() => browser.url('/#/raw/tests/terra-table/table/padding'));
+//     Terra.it.validatesElement();
+//   });
 
-  describe('HeaderRow Test', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/header-row'));
-    Terra.it.validatesElement();
-  });
+//   describe('HeaderRow Test', () => {
+//     before(() => browser.url('/#/raw/tests/terra-table/table/header-row'));
+//     Terra.it.validatesElement();
+//   });
 
-  describe('HeaderCell Test', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/header-cell'));
-    Terra.it.validatesElement();
-  });
+//   describe('HeaderCell Test', () => {
+//     before(() => browser.url('/#/raw/tests/terra-table/table/header-cell'));
+//     Terra.it.validatesElement();
+//   });
 
-  describe('Row Test', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/row'));
-    Terra.it.validatesElement('Table');
+//   describe('Row Test', () => {
+//     before(() => browser.url('/#/raw/tests/terra-table/table/row'));
+//     Terra.it.validatesElement('Table');
 
-    it('Hover', () => browser.moveToObject('#selectable-row'));
+//     it('Hover', () => browser.moveToObject('#selectable-row'));
 
-    Terra.it.validatesElement('Table-Hover');
-  });
+//     Terra.it.validatesElement('Table-Hover');
+//   });
 
-  describe('Cell Test', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/cell'));
-    Terra.it.validatesElement();
-  });
+//   describe('Cell Test', () => {
+//     before(() => browser.url('/#/raw/tests/terra-table/table/cell'));
+//     Terra.it.validatesElement();
+//   });
 
-  describe('Section Test', () => {
-    before(() => browser.url('/#/raw/tests/terra-table/table/section'));
-    Terra.it.validatesElement('TableSection');
+//   describe('Section Test', () => {
+//     before(() => browser.url('/#/raw/tests/terra-table/table/section'));
+//     Terra.it.validatesElement('TableSection');
 
-    it('Hover', () => browser.moveToObject('#test-header'));
+//     it('Hover', () => browser.moveToObject('#test-header'));
 
-    Terra.it.validatesElement('TableSection-Hover');
-  });
+//     Terra.it.validatesElement('TableSection-Hover');
+//   });
 });
