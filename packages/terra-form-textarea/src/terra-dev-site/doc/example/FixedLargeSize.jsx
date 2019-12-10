7a356664e7f75d538ae854b2f2c9581a2a1e8464
Saket Bajaj <42207428+saket2403@users.noreply.github.com>
2019-12-11 03:21:55 +0530

    [terra-form-textarea]Disallowed Resize when size or rows are provided. (#2764)

--- packages/terra-form-textarea/src/terra-dev-site/doc/example/FixedLargeSize.jsx
+++ packages/terra-form-textarea/src/terra-dev-site/doc/example/FixedLargeSize.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import Field from 'terra-form-field';
 import Textarea from 'terra-form-textarea';
-import classNames from 'classnames/bind';
-import styles from './FormTextareaDocCommon.module.scss';
-
-const cx = classNames.bind(styles);
 
 const FixedLargeSizeExample = () => (
   <Field label="Fixed Textarea - Large Size" htmlFor="large">
     <Textarea
       size="large"
-      className={cx('textarea')}
       id="large"
+      disableResize
     />
   </Field>
 );
