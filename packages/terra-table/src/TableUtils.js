0785a7c6d484977f56921fe8846b4edd5f7ab4fc
Kasper,Dave <dave.m.kasper@gmail.com>
2019-12-10 00:04:55 -0600

    Fix test and typos.

--- packages/terra-table/src/TableUtils.js
+++ packages/terra-table/src/TableUtils.js
@@ -1,9 +1,7 @@
 /**
  * Returns whether or not the provided value can be added or removed from the array given a max key count and presence checks.
  */
-const canToggleArrayValue = (arrayMaxLength, currentArray, value) => {
-  return arrayMaxLength < 0 || currentArray.indexOf(value) >= 0 || currentArray.length < arrayMaxLength;
-};
+const canToggleArrayValue = (arrayMaxLength, currentArray, value) => (arrayMaxLength < 0 || currentArray.indexOf(value) >= 0 || currentArray.length < arrayMaxLength);
 
 /**
  * Returns a new array, updated with the value being added or removed from the existing key array.
@@ -12,7 +10,7 @@ const toggleArrayValue = (currentArray, value) => {
   if (currentArray.length) {
     if (currentArray.indexOf(value) >= 0) {
       const newArray = currentArray.slice();
-      newArray.splice(newKeys.indexOf(value), 1);
+      newArray.splice(newArray.indexOf(value), 1);
       return newArray;
     }
     return currentArray.concat([value]);
--- packages/terra-table/tests/jest/TableUtils.test.jsx
+++ packages/terra-table/tests/jest/TableUtils.test.jsx
@@ -1,31 +1,31 @@
 import {
-  shouldBeMultiSelectable,
-  updatedMultiSelectedKeys,
+  canToggleArrayValue,
+  toggleArrayValue,
 } from '../../src/TableUtils';
 
 describe('TableUtils', () => {
-  it('shouldBeMultiSelectable return correct values', () => {
-    let result = shouldBeMultiSelectable(-1, ['1', '2', '3'], '4');
+  it('canToggleArrayValue return correct values', () => {
+    let result = canToggleArrayValue(-1, ['1', '2', '3'], '4');
     expect(result).toEqual(true);
 
-    result = shouldBeMultiSelectable(4, ['1', '2', '3'], '4');
+    result = canToggleArrayValue(4, ['1', '2', '3'], '4');
     expect(result).toEqual(true);
 
-    result = shouldBeMultiSelectable(3, ['1', '2', '3'], '3');
+    result = canToggleArrayValue(3, ['1', '2', '3'], '3');
     expect(result).toEqual(true);
 
-    result = shouldBeMultiSelectable(3, ['1', '2', '3'], '4');
+    result = canToggleArrayValue(3, ['1', '2', '3'], '4');
     expect(result).toEqual(false);
   });
 
-  it('updatedMultiSelectedKeys return correct values', () => {
-    let result = updatedMultiSelectedKeys(['1', '2', '3'], '4');
+  it('toggleArrayValue return correct values', () => {
+    let result = toggleArrayValue(['1', '2', '3'], '4');
     expect(result).toEqual(['1', '2', '3', '4']);
 
-    result = updatedMultiSelectedKeys(['1', '2', '3'], '2');
+    result = toggleArrayValue(['1', '2', '3'], '2');
     expect(result).toEqual(['1', '3']);
 
-    result = updatedMultiSelectedKeys([], '3');
+    result = toggleArrayValue([], '3');
     expect(result).toEqual(['3']);
   });
 });
