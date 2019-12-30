1080fb854fd31d6cb2e512367ab0f6d30d33838b
Supreeth MR <supreeth.mr1990@gmail.com>
2019-12-30 19:42:27 +0530

    tabIndex reverted to undefined

--- packages/terra-dropdown-button/src/DropdownButton.jsx
+++ packages/terra-dropdown-button/src/DropdownButton.jsx
@@ -163,7 +163,7 @@ class DropdownButton extends React.Component {
           onClick={this.handleDropdownButtonClick}
           onKeyDown={this.handleKeyDown}
           onKeyUp={this.handleKeyUp}
-          disabled={isDisabled ? '-1' : 0}
+          disabled={isDisabled ? '-1' : undefined}
           aria-disabled={isDisabled}
           aria-expanded={isOpen}
           aria-haspopup="menu"
