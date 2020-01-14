704ff6d41d7428d72e8786a13f6a3cf6550e9194
Kasper,Dave <dave.m.kasper@gmail.com>
2020-01-14 15:41:23 -0600

    Update doc.

--- packages/terra-cell-grid/src/terra-dev-site/doc/example/CellGridBoth.jsx
+++ packages/terra-cell-grid/src/terra-dev-site/doc/example/CellGridBoth.jsx
@@ -7,7 +7,7 @@ const CellGridBoth = () => (
   <CellGrid
     dividerStyle="both"
   >
-    <Cell key="cell-0" width={{ static: { value: 80, unit: 'px' } }}>60px</Cell>
+    <Cell key="cell-0" width={{ static: { value: 80, unit: 'px' } }}>80px</Cell>
     <Cell key="cell-1" width={{ percentage: 10 }}>10%</Cell>
     <Cell key="cell-2">Scalar 1</Cell>
     <Cell key="cell-3" width={{ scalar: 3 }}>Scalar 3</Cell>
