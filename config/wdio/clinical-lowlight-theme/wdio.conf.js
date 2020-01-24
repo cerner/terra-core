f0b4184ca3d941e38b02ceb6ee8fb5f386d9efd2
Avinash Gupta <Avinash.Gupta@cerner.com>
2020-01-24 11:16:15 +0530

    addressed review comments

--- config/wdio/clinical-lowlight-theme/wdio.conf.js
+++ config/wdio/clinical-lowlight-theme/wdio.conf.js
@@ -1,5 +1,7 @@
 const defaultConfig = require('../../../node_modules/terra-toolkit/config/wdio/wdio.conf');
 
+// Disabling color contrast rule for clinical-lowlight-theme. May not be addressed, since these rules
+// are designed for normal lighting situations and NOT for specialized lighting situations (lowlight)
 const config = {
   ...defaultConfig.config,
   axe: {
