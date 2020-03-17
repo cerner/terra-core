d8e3eeae2860a2991c739c238f5f6a6de8be1c77
saket2403 <saket.bajaj1998@gmail.com>
2020-03-17 13:31:51 +0530

    Add Example

---
+++ packages/terra-alert/src/terra-dev-site/test/alert/AlertPopup.test.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from 'terra-button';
+import Alert from '../../../Alert';
+
+class AlertActionButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      actionButtonClickCount: 0,
+      showAlert: false,
+    };
+    this.actionFunc = this.actionFunc.bind(this);
+    this.popAlert = this.popAlert.bind(this);
+    this.alert = (
+      <Alert id="actionAlert" type="warning" action={<Button text="Action" onClick={this.actionFunc} />}>
+    This is a warning. It is configured with a custom Action button. Action button has been
+    clicked
+        {' '}
+        <span id="actionButtonClickCount">{this.state.actionButtonClickCount}</span>
+        {' '}
+    times.
+      </Alert>
+    );
+  }
+
+  actionFunc() {
+    this.setState(prevState => ({ actionButtonClickCount: prevState.actionButtonClickCount + 1 }));
+  }
+
+  popAlert() {
+    this.setState(prevState => ({ showAlert: !prevState.showAlert }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Button text="Action" onClick={this.popAlert} />
+        {(this.state.showAlert)
+          ? this.alert : ''}
+      </div>
+    );
+  }
+}
+
+export default AlertActionButton;
