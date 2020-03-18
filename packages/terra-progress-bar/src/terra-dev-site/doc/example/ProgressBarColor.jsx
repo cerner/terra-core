472d51bddd42b2532b44b54fce5943f9da57ee9d
pranav agarwal <agarwal.pranav300@gmail.com>
2020-03-18 17:07:47 +0530

    Added css documentation

--- packages/terra-progress-bar/src/terra-dev-site/doc/example/ProgressBarColor.jsx
+++ packages/terra-progress-bar/src/terra-dev-site/doc/example/ProgressBarColor.jsx
@@ -3,40 +3,14 @@ import ProgressBar from 'terra-progress-bar';
 import classNames from 'classnames/bind';
 import styles from './colors.module.scss';
 
-// By passing in a colorClass, authors can manually set the bar color of any `<ProgressBar />` component.
-
-// colors.module.css
-// :root {
-//   --my-app-progress-bar-example-1-background-color: rgb(255, 165, 0);
-//   --my-app-progress-bar-example-2-background-color: #8ccc62;
-//   --my-app-progress-bar-example-3-background-color: rgb(255, 0, 0);
-// }
-
-// .color-bar-example-1 {
-//   color: var(--my-app-progress-bar-example-1-background-color, rgb(255, 165, 0));
-//   &::-webkit-progress-value { background-color: var(--my-app-progress-bar-example-1-background-color, rgb(255, 165, 0)); }
-//   &::-moz-progress-bar { background-color: var(--my-app-progress-bar-example-1-background-color, rgb(255, 165, 0)); }
-//   &::-ms-fill { background-color: var(--my-app-progress-bar-example-1-background-color, rgb(255, 165, 0)); }
-// }
-
-// .color-bar-example-2 {
-//   color: var(--my-app-progress-bar-example-2-background-color, #8ccc62);
-//   &::-webkit-progress-value { background-color: var(--my-app-progress-bar-example-2-background-color, #8ccc62); }
-//   &::-moz-progress-bar { background-color: var(--my-app-progress-bar-example-2-background-color, #8ccc62); }
-//   &::-ms-fill { background-color: var(--my-app-progress-bar-example-2-background-color, #8ccc62); }
-// }
-
-// .color-bar-example-3 {
-//   color: var(--my-app-progress-bar-example-3-background-color, rgb(255, 0, 0));
-//   &::-webkit-progress-value { background-color: var(--my-app-progress-bar-example-3-background-color, rgb(255, 0, 0)); }
-//   &::-moz-progress-bar { background-color: var(--my-app-progress-bar-example-3-background-color, rgb(255, 0, 0)); }
-//   &::-ms-fill { background-color: var(--my-app-progress-bar-example-3-background-color, rgb(255, 0, 0)); }
-// }
+// eslint-disable-next-line import/order
+import StylesDoc from 'terra-progress-bar/docs/colors.doc.scss?dev-site-codeblock';
 
 const cx = classNames.bind(styles);
 
 const ProgressBarColor = () => (
   <div>
+    <StylesDoc />
     <ProgressBar valueText="5%" value={5} max={10} colorClass={cx(['color-bar-example-1'])} />
     <br />
     <br />
