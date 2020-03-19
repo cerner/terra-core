cb5da3877ddaef52ed7972d0b2c26896703ec14e
pranav agarwal <agarwal.pranav300@gmail.com>
2020-03-19 21:26:35 +0530

    updated docs to not show css for examples

--- packages/terra-heading/src/terra-dev-site/doc/example/HeadingColors.jsx
+++ packages/terra-heading/src/terra-dev-site/doc/example/HeadingColors.jsx
@@ -3,14 +3,10 @@ import Heading from 'terra-heading';
 import classNames from 'classnames/bind';
 import styles from './colors.module.scss';
 
-// eslint-disable-next-line import/order
-import StylesDoc from 'terra-heading/docs/colors.doc.scss?dev-site-codeblock';
-
 const cx = classNames.bind(styles);
 
 const HeadingColors = () => (
   <div>
-    <StylesDoc />
     <Heading level={3} colorClass={cx(['attention'])}>{'Font Color: \'attention\' class'}</Heading>
     <Heading level={3} colorClass={cx(['success'])}>{'Font Color: \'success\' class'}</Heading>
     <Heading level={3} colorClass={cx(['info'])}>{'Font Color: \'info\' class'}</Heading>
