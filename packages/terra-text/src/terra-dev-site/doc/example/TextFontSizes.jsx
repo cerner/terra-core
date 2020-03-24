88d7739a51d61d65cec97299064fe301fcee4ce5
pranav agarwal <agarwal.pranav300@gmail.com>
2020-03-24 11:19:42 +0530

    resolved review comments

--- packages/terra-text/src/terra-dev-site/doc/example/TextFontSizes.jsx
+++ packages/terra-text/src/terra-dev-site/doc/example/TextFontSizes.jsx
@@ -3,15 +3,6 @@ import Text from 'terra-text';
 
 const TextFontSizes = () => (
   <div>
-    <p>
-      The `fontSize` prop can be used set the `font-size` on any `Text` component. The `fontSize` prop accepts one of the following numbers:
-      <br />
-      <br />
-      `10`, `12`, `14`, `16`, `18`, `20`, `24`, `32`, `36`, `46`, `50`, `56`, `64`, `72`, `92`, `100`
-      <br />
-      <br />
-      These values represent the pixel values of our UX defined font-scale. The `Text` component renders these font-sizes with `rem` units.
-    </p>
     <Text fontSize={100}>100px (7.143rem)</Text>
     <br />
     <Text fontSize={92}>92px (2.286rem)</Text>
--- packages/terra-text/src/terra-dev-site/doc/example/TextVisuallyHidden.jsx
+++ packages/terra-text/src/terra-dev-site/doc/example/TextVisuallyHidden.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Text from 'terra-text';
 
 const TextVisuallyHidden = () => (
   <div>
-    <p>
-      The `isVisuallyHidden` prop can be used to hide the content visually, yet provide the content to screen readers.
-      More info on this approach can be found here:
-      {' '}
-      <Link to="http://a11yproject.com/posts/how-to-hide-content/">http://a11yproject.com/posts/how-to-hide-content/</Link>
-    </p>
     <p>
 The example below includes a
       <code> &lt;Text /&gt;</code>
