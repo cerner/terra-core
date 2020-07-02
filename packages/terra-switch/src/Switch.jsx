353bec2f0e080426345898d5721636816ba537c3
Supreeth MR <supreeth.mr1990@gmail.com>
2020-07-03 00:55:54 +0530

    doc update

--- packages/terra-switch/src/Switch.jsx
+++ packages/terra-switch/src/Switch.jsx
@@ -59,7 +59,7 @@ const Switch = (props) => {
 
   const handleOnClick = useCallback((event) => {
     // See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Button#Clicking_and_focus
-    // Button on Firefox, Safari and IE running on OS X does not receive focus when clicked.
+    // Button on IE running on OS X does not receive focus when clicked.
     // This will set focus on the button when clicked.
     sliderButton.current.focus();
     if (onChange) {
