93a13ec7ca1d625d5804fabb69b818c704c57d29
Matt Henkes <mjhenkes@gmail.com>
2020-07-09 09:02:53 -0500

    fix test url

--- packages/terra-docs/tests/wdio/docs-spec.js
+++ packages/terra-docs/tests/wdio/docs-spec.js
@@ -1,6 +1,6 @@
 Terra.describeViewports('Docs', ['tiny', 'medium', 'large'], () => {
   describe('Default', () => {
-    before(() => browser.url('/#/raw/tests/terra-docs/default-docs'));
+    before(() => browser.url('/raw/tests/terra-docs/docs/default-docs'));
 
     it('validates the element', () => {
       Terra.validates.element();
