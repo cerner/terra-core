467c3a5669402fd6a62f5e7c74ec09e1fa31e373
Matt Henkes <mjhenkes@gmail.com>
2020-07-10 13:52:24 -0500

    extend wait time for the docker service

--- wdio.conf.js
+++ wdio.conf.js
@@ -8,4 +8,9 @@ if (process.env.npm_package_name !== 'terra-core') {
   wdioConfig.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
 }
 
+wdioConfig.seleniumDocker = {
+  retries: 10,
+  retryInterval: 5000,
+};
+
 exports.config = wdioConfig;
