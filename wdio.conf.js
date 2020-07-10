93d47be21ca8eb450755fd8cda57a2d9f6a49dd8
Matt Henkes <mjhenkes@gmail.com>
2020-07-10 16:47:56 -0500

    please stop

--- wdio.conf.js
+++ wdio.conf.js
@@ -8,9 +8,4 @@ if (process.env.npm_package_name !== 'terra-core') {
   wdioConfig.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
 }
 
-wdioConfig.seleniumDocker = {
-  retries: 10,
-  retryInterval: 5000,
-};
-
 exports.config = wdioConfig;
