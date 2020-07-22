a801092c904dcf0d1ab6ee760cba0a761bda7890
pranav agarwal <agarwal.pranav300@gmail.com>
2020-07-23 01:07:26 +0530

    minor changes

--- packages/terra-form-select/src/terra-dev-site/test/form-select/EmptyPlaceholder.test.jsx
+++ packages/terra-form-select/src/terra-dev-site/test/form-select/EmptyPlaceholder.test.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import classNames from 'classnames/bind';
-import Variants from '../../../shared/_variants';
-import Select from '../../../SingleSelect';
+import Select from '../../../Select';
 import styles from './common/Select.test.module.scss';
 
 const cx = classNames.bind(styles);
 
 const EmptyPlaceholder = () => (
   <div className={cx('content-wrapper')}>
-    <Select placeholder="njjjjj" id="default">
+    <Select placeholder="" id="default">
       <Select.Option value="blue" display="Blue" />
       <Select.Option value="green" display="Green" />
       <Select.Option value="purple" display="Purple" />
