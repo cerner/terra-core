06cc077fead184327838ca74c9c1911276a64091
pranav agarwal <agarwal.pranav300@gmail.com>
2020-08-19 23:04:23 +0530

    minor changes

--- packages/terra-form-input/src/terra-dev-site/doc/common/BlankExample.jsx
+++ packages/terra-form-input/src/terra-dev-site/doc/common/BlankExample.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import Input from 'terra-form-input';
 
 const BlankExample = () => (
-  <div>
-    <Input name="default blank input" id="blank" ariaLabel="Blank" />
-  </div>
+  <Input name="default blank input" id="blank" ariaLabel="Blank" />
 );
 
 export default BlankExample;
--- packages/terra-form-input/src/terra-dev-site/doc/common/NumberInputExample.jsx
+++ packages/terra-form-input/src/terra-dev-site/doc/common/NumberInputExample.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import Input from 'terra-form-input';
 
 const NumberInputExample = () => (
-  <div>
-    <Input name="number input" id="numeric" type="number" ariaLabel="Numeric Input" />
-  </div>
+  <Input name="number input" id="numeric" type="number" ariaLabel="Numeric Input" />
 );
 
 export default NumberInputExample;
