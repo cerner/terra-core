41e8a1db9e7afa4be2d50d14c675e3265e70c946
Cai,Ben <BCAI@CERNER.COM>
2020-08-25 11:03:58 -0500

    Fix jest tests

--- packages/terra-demographics-banner/tests/jest/DemographicsBannerDisplay.test.jsx
+++ packages/terra-demographics-banner/tests/jest/DemographicsBannerDisplay.test.jsx
@@ -12,7 +12,10 @@ it('renders a banner that has basic information', () => {
   const banner = shallowWithIntl(<DemographicsBannerDisplay
     age={{ value: '24', ariaLabel: '24 Years Old.' }}
     dateOfBirth={{ value: 'March 30, 2017', ariaLabel: 'March 30, 2017' }}
+    dateOfBirthLabel="DOB"
+    dateOfBirthFullText="Date of Birth"
     administrativeSex={{ value: 'F', ariaLabel: 'Female', isBirthSexMismatch: false }}
+    administrativeSexFullText="Administrative Sex"
     personName="Terra Demographics Banner"
   />);
 
@@ -24,7 +27,10 @@ it('renders the banner that contains additional information', () => {
     applicationContent={<span className="risk-score">5%</span>}
     age={{ value: '25 Years', ariaLabel: '25 Years Old.' }}
     dateOfBirth={{ value: 'May 9, 1993', ariaLabel: 'May 9, 1993' }}
-    administrativeSex={{ value: 'Male', ariaLabel: 'Male', isBirthSexMismatch: false }}
+    dateOfBirthLabel="DOB"
+    dateOfBirthFullText="Date of Birth"
+    administrativeSex={{ value: 'Male', ariaLabel: 'Male', isBirthSexMismatch: true }}
+    administrativeSexFullText="Administrative Sex"
     identifiers={[
       {
         label: 'MRN',
@@ -40,6 +46,7 @@ it('renders the banner that contains additional information', () => {
     photo=""
     personName="Johnathon Doe"
     preferredFirstName="John"
+    sexMismatchFullText="The documented sex values are not identical."
   />);
 
   expect(banner).toMatchSnapshot();
@@ -49,8 +56,12 @@ it('renders the banner appropriately for a deceased person', () => {
   const banner = shallowWithIntl(<DemographicsBannerDisplay
     age={{ value: '24', ariaLabel: '24 Years Old.' }}
     administrativeSex={{ value: 'F', ariaLabel: 'Female', isBirthSexMismatch: false }}
+    administrativeSexFullText="Administrative Sex"
     dateOfBirth={{ value: 'June 15, 2014', ariaLabel: 'June 15, 2014' }}
+    dateOfBirthLabel="DOB"
+    dateOfBirthFullText="Date of Birth"
     deceasedDate={{ value: 'March 30, 2017', ariaLabel: 'March 30, 2017' }}
+    deceasedDateLabel="Deceased"
     personName="Old Terra Demographics Banner"
   />);
 
@@ -62,9 +73,13 @@ it('renders the banner appropriately for a person with gestational and post mens
     applicationContent={<span className="risk-score">5%</span>}
     age={{ value: '25 Years', ariaLabel: '25 Years Old.' }}
     dateOfBirth={{ value: 'May 9, 1993', ariaLabel: 'May 9, 1993' }}
+    dateOfBirthLabel="DOB"
+    dateOfBirthFullText="Date of Birth"
     administrativeSex={{ value: 'Male', ariaLabel: 'Male', isBirthSexMismatch: false }}
+    administrativeSexFullText="Administrative Sex"
     gestationalAge={{ value: 'April 5, 2016', ariaLabel: 'April 5, 2016' }}
     gestationalAgeLabel="GA"
+    gestationalAgeFullText="Gestational Age"
     identifiers={[
       {
         label: 'MRN',
@@ -77,23 +92,32 @@ it('renders the banner appropriately for a person with gestational and post mens
         ariaLabel: 'R E A 3JSDA',
       },
     ]}
-    photo=""
     personName="Johnathon Doe"
+    photo=""
     postMenstrualAge={{ value: 'April 7, 2016', ariaLabel: 'April 7, 2016' }}
     postMenstrualAgeLabel="PMA"
+    postMenstrualAgeFullText="Post Menstrual Age"
     preferredFirstName="John"
   />);
 
   expect(banner).toMatchSnapshot();
 });
 
-it('renders gestational date with a default label when the label is not provided', () => {
-  const banner = shallowWithIntl(<DemographicsBannerDisplay gestationalAge={{ value: 'April 5, 2016', ariaLabel: 'April 5, 2016' }} />);
+it('renders gestational date with a default label', () => {
+  const banner = shallowWithIntl(<DemographicsBannerDisplay
+    gestationalAge={{ value: 'April 5, 2016', ariaLabel: 'April 5, 2016' }}
+    gestationalAgeLabel="GA"
+    gestationalAgeFullText="Gestational Age"
+  />);
   expect(banner).toMatchSnapshot();
 });
 
-it('renders post menstrural date with a default label when the label is not provided', () => {
-  const banner = shallowWithIntl(<DemographicsBannerDisplay postMenstrualAge={{ value: 'April 5, 2016', ariaLabel: 'April 5, 2016' }} />);
+it('renders post menstrual date with a default label', () => {
+  const banner = shallowWithIntl(<DemographicsBannerDisplay
+    postMenstrualAge={{ value: 'April 5, 2016', ariaLabel: 'April 5, 2016' }}
+    postMenstrualAgeLabel="PMA"
+    postMenstrualAgeFullText="Post Menstrual Age"
+  />);
   expect(banner).toMatchSnapshot();
 });
 
@@ -102,8 +126,12 @@ it('renders the banner properly for a deceased person with additional applicatio
     applicationContent={<span className="risk-score">5%</span>}
     age={{ value: '25 Years', ariaLabel: '25 Years Old.' }}
     dateOfBirth={{ value: 'May 9, 1993', ariaLabel: 'May 9, 1993' }}
+    dateOfBirthLabel="DOB"
+    dateOfBirthFullText="Date of Birth"
     deceasedDate={{ value: 'March 5, 2016', ariaLabel: 'March 5, 2016' }}
+    deceasedDateLabel="Deceased"
     administrativeSex={{ value: 'Male', ariaLabel: 'Male', isBirthSexMismatch: false }}
+    administrativeSexFullText="Administrative Sex"
     identifiers={[
       {
         label: 'MRN',
@@ -129,7 +157,10 @@ it('renders the banner with confidential icon', () => {
     applicationContent={<span className="risk-score">5%</span>}
     age={{ value: '25 Years', ariaLabel: '25 Years Old.' }}
     dateOfBirth={{ value: 'May 9, 1993', ariaLabel: 'May 9, 1993' }}
+    dateOfBirthLabel="DOB"
+    dateOfBirthFullText="Date of Birth"
     administrativeSex={{ value: 'Male', ariaLabel: 'Male', isBirthSexMismatch: false }}
+    administrativeSexFullText="Administrative Sex"
     identifiers={[
       {
         label: 'MRN',
