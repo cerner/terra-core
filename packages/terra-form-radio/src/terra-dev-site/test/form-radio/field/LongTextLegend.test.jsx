33a809d073a91b4776bff4c68608814858248c91
pranav agarwal <agarwal.pranav300@gmail.com>
2020-09-10 01:47:00 +0530

    minor changes

--- packages/terra-form-radio/src/terra-dev-site/test/form-radio/field/LongTextLegend.test.jsx
+++ packages/terra-form-radio/src/terra-dev-site/test/form-radio/field/LongTextLegend.test.jsx
@@ -7,7 +7,6 @@ const LongTextLegend = () => (
     id="testing-radio-field"
     legend="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     help="Help Message"
-    error="One must be selected"
   >
     <Radio id="default" labelText="Default Radio" />
   </RadioField>
