77d60c05447867a9b1ffefa11a5405a165ff15d0
Emily Rohrbough <emily.rohrbough@cerner.com>
2020-09-28 13:11:59 -0500

    fix lint

--- packages/terra-i18n/src/i18nLoader.js
+++ packages/terra-i18n/src/i18nLoader.js
@@ -33,12 +33,14 @@ export default (locale, callback, scope) => {
     /**
      * Intl polyfill attempts to cache and restore static RegExp properties before executing any regular expressions in order
      * to comply with ECMA-402. There are times this results in regex syntax error so we are disabling this feature.
-     * 
+     *
      * Reference: https://github.com/andyearnshaw/Intl.js#regexp-cache--restore
      */
+    /* eslint-disable no-underscore-dangle */
     if (Intl.__disableRegExpRestore && typeof Intl.__disableRegExpRestore === 'function') {
-      Intl.__disableRegExpRestore(); 
-     }
+      Intl.__disableRegExpRestore();
+    }
+    /* eslint-enable no-underscore-dangle */
 
     loadIntl(locale);
   }
