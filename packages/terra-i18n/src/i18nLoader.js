86ca855cd307d44ef8659515734e5aa348f11c14
Jaime Mackey <jaime.mackey@cerner.com>
2020-10-26 16:22:28 -0500

    revers changes to terra-i18n

--- packages/terra-i18n/src/i18nLoader.js
+++ packages/terra-i18n/src/i18nLoader.js
@@ -1,12 +1,6 @@
 /* eslint-disable global-require, import/no-named-as-default */
-import { shouldPolyfill as shouldPolyfillGetCanonicalLocales } from '@formatjs/intl-getcanonicallocales/should-polyfill';
-import { shouldPolyfill as shouldPolyfillPluralRules } from '@formatjs/intl-pluralrules/should-polyfill';
-import { shouldPolyfill as shouldPolyfillRelativeTimeFormat } from '@formatjs/intl-relativetimeformat/should-polyfill';
-import { shouldPolyfill as shouldPolyfillNumberFormat } from '@formatjs/intl-numberformat/should-polyfill';
-import { shouldPolyfill as shouldPolyfillDateTimeFormat } from '@formatjs/intl-datetimeformat/should-polyfill';
 import loadIntl from './intlLoaders';
 import loadTranslations from './translationsLoaders';
-
 
 const permitParams = (callback) => {
   if (typeof (callback) !== 'function') {
@@ -16,29 +10,6 @@ const permitParams = (callback) => {
 
 export default (locale, callback, scope) => {
   permitParams(callback);
-
-  const polyfills = {
-    'getCanonicalLocales': {
-      hasIntl: shouldPolyfillGetCanonicalLocales,
-      polyfills: ['@formatjs/intl-getcanonicallocales'],
-    },
-    'PluralRules': {
-      hasIntl: shouldPolyfillPluralRules,
-      polyfills: ['@formatjs/intl-pluralrules'],
-    },
-    'RelativeTimeFormat': {
-      hasIntl: shouldPolyfillRelativeTimeFormat,
-      polyfills: ['@formatjs/intl-relativetimeformat'],
-    },
-    'NumberFormat': {
-      hasIntl: shouldPolyfillNumberFormat,
-      polyfills: ['@formatjs/intl-numberformat'],
-    },
-    'DateTimeFormat': {
-      hasIntl: shouldPolyfillDateTimeFormat,
-      polyfills: ['@formatjs/intl-datetimeformat'],
-    },
-  },
 
   /**
    * Use try-catch to check if Intl is provided by the browser. In some instances checking Intl will throw an
