fde4e7e455b1fce9af3717fd4b7075315fd170c3
Avinash Gupta <50138162+avinashg1994@users.noreply.github.com>
2020-10-28 23:10:41 +0530

    [terra-toggle] updated examples to use hooks (#3202)
    Co-authored-by: Avinash Gupta <Avinash.Gupta@cerner.com>
    Co-authored-by: Emily Rohrbough <emilyrohrbough@users.noreply.github.com>

--- packages/terra-toggle/src/terra-dev-site/test/toggle/OpenToggle.test.jsx
+++ packages/terra-toggle/src/terra-dev-site/test/toggle/OpenToggle.test.jsx
@@ -1,29 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Toggle from '../../../Toggle';
 
-class OpenToggle extends React.Component {
-  constructor() {
-    super();
-    this.state = ({ isOpen: true });
+const OpenToggle = () => {
+  const [isOpen, setIsOpen] = useState(true);
 
-    this.handleOnClick = this.handleOnClick.bind(this);
-  }
+  const handleOnClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-  handleOnClick() {
-    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
-  }
-
-  render() {
-    const { isOpen } = this.state;
-    return (
-      <div>
-        <button type="button" id="trigger-toggle" onClick={this.handleOnClick} aria-expanded={isOpen} aria-controls="toggle">Open Toggle</button>
-        <Toggle id="toggle" isOpen={isOpen}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </Toggle>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <button type="button" id="trigger-toggle" onClick={handleOnClick} aria-expanded={isOpen} aria-controls="toggle">Open Toggle</button>
+      <Toggle id="toggle" isOpen={isOpen}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </Toggle>
+    </div>
+  );
+};
 
 export default OpenToggle;
