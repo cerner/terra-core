34bc0e68bfb082e88a9c830eb07ca9a9c985cb36
Matt Henkes <mjhenkes@gmail.com>
2020-11-05 08:27:50 -0600

    setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,20 @@ const merge = require('webpack-merge');
 const {
   TerraDevSite,
   TerraDevSiteEntrypoints,
-  DirectorySwitcherPlugin,
-  LocalPackageAliasPlugin,
-} = require('terra-dev-site');
+} = require('@cerner/terra-dev-site');
 
 const WebpackConfigTerra = require('@cerner/webpack-config-terra');
 
-const coreConfig = (env = {}, argv = { p: false }) => {
-  const production = argv.p;
+const coreConfig = () => {
   const processPath = process.cwd();
-  const rootPath = processPath.includes('packages') ? processPath.split('packages')[0] : processPath;
 
-  const i18nAlias = path.resolve(path.join(rootPath, 'packages', 'terra-i18n'));
+  const i18nAlias = path.resolve(path.join(processPath, 'packages', 'terra-i18n'));
   return {
     entry: TerraDevSiteEntrypoints,
     plugins: [
-      new TerraDevSite({ env }),
+      new TerraDevSite(),
     ],
     resolve: {
-      plugins: [
-        new DirectorySwitcherPlugin({
-          shouldSwitch: !production,
-          rootDirectories: [
-            processPath,
-            path.resolve(processPath, 'packages', '*'),
-          ],
-        }),
-        new LocalPackageAliasPlugin({
-          rootDirectories: [
-            processPath,
-            path.resolve(processPath, 'packages', '*'),
-          ],
-        }),
-      ],
       alias: {
         'terra-i18n': i18nAlias,
       },
