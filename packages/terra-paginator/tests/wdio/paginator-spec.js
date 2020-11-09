b7c3d93640693248b8725509d2238dfab842502b
Matt Henkes <mjhenkes@gmail.com>
2020-11-09 14:30:32 -0600

    Apply suggestions from code review

--- packages/terra-paginator/tests/wdio/paginator-spec.js
+++ packages/terra-paginator/tests/wdio/paginator-spec.js
@@ -1,21 +1,21 @@
 Terra.describeViewports('Paginator', ['tiny', 'medium', 'large'], () => {
   describe('Paginator', () => {
     it('default', () => {
-      browser.url('/#/raw/tests/terra-paginator/paginator/paginator');
+      browser.url('/raw/tests/terra-paginator/paginator/paginator');
       Terra.validates.element();
     });
   });
 
   describe('PaginatorNoPages', () => {
     it('default', () => {
-      browser.url('/#/raw/tests/terra-paginator/paginator/paginator-no-pages');
+      browser.url('/raw/tests/terra-paginator/paginator/paginator-no-pages');
       Terra.validates.element();
     });
   });
 
   describe('ProgressivePaginator', () => {
     it('default', () => {
-      browser.url('/#/raw/tests/terra-paginator/paginator/progressive-paginator');
+      browser.url('/raw/tests/terra-paginator/paginator/progressive-paginator');
       Terra.validates.element();
     });
   });
@@ -24,7 +24,7 @@ Terra.describeViewports('Paginator', ['tiny', 'medium', 'large'], () => {
 Terra.describeViewports('Controlled Paginator', ['large'], () => {
   describe('Controlled Paginator', () => {
     it('displays initial selected page', () => {
-      browser.url('/#/raw/tests/terra-paginator/paginator/controlled-paginator');
+      browser.url('/raw/tests/terra-paginator/paginator/controlled-paginator');
       browser.moveToObject('#root', 0, 700); // move cursor to remove focus styles
       Terra.validates.element('0');
     });
@@ -47,7 +47,7 @@ Terra.describeViewports('Controlled Paginator', ['large'], () => {
 
   describe('Controlled Progressive Paginator', () => {
     it('displays initial selected page', () => {
-      browser.url('/#/raw/tests/terra-paginator/paginator/controlled-progressive-paginator');
+      browser.url('/raw/tests/terra-paginator/paginator/controlled-progressive-paginator');
       Terra.validates.element('0');
     });
 
@@ -69,7 +69,7 @@ Terra.describeViewports('Controlled Paginator', ['large'], () => {
 
   describe('Controlled Progressive Paginator without total count', () => {
     it('displays the initial selected page', () => {
-      browser.url('/#/raw/tests/terra-paginator/paginator/controlled-progressive-paginator-without-total-count');
+      browser.url('/raw/tests/terra-paginator/paginator/controlled-progressive-paginator-without-total-count');
       Terra.validates.element('0');
     });
 
@@ -91,14 +91,14 @@ Terra.describeViewports('Controlled Paginator', ['large'], () => {
 
   describe('Progressive paginator without total count', () => {
     it('renders without total count', () => {
-      browser.url('/#/raw/tests/terra-paginator/paginator/progressive-paginator-without-total-count');
+      browser.url('/raw/tests/terra-paginator/paginator/progressive-paginator-without-total-count');
       Terra.validates.element();
     });
   });
 
   describe('Paginator without item count', () => {
     it('renders without item count', () => {
-      browser.url('/#/raw/tests/terra-paginator/paginator/paginator-without-item-count');
+      browser.url('/raw/tests/terra-paginator/paginator/paginator-without-item-count');
       Terra.validates.element();
     });
   });
