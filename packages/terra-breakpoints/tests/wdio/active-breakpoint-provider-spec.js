78108777e598daf9d59af42ee2cbebc7d2679448
pranav agarwal <agarwal.pranav300@gmail.com>
2020-11-12 00:21:22 +0530

    updated wdio

--- packages/terra-breakpoints/tests/wdio/active-breakpoint-provider-spec.js
+++ packages/terra-breakpoints/tests/wdio/active-breakpoint-provider-spec.js
@@ -1,5 +1,6 @@
-Terra.describeViewports('ActiveBreakpointProvider',
-  ['tiny', 'small', 'medium', 'large', 'huge', 'enormous'], () => {
-    before(() => browser.url('/#/raw/tests/terra-breakpoints/breakpoints/active-breakpoint-provider-test'));
-    Terra.it.validatesElement();
+Terra.describeViewports('ActiveBreakpointProvider', ['tiny', 'small', 'medium', 'large', 'huge', 'enormous'], () => {
+  it('displays default Breakpoints', () => {
+    browser.url('/#/raw/tests/terra-breakpoints/breakpoints/active-breakpoint-provider-test');
+    Terra.validates.element();
   });
+});
