85f452662ab0602ace7b2b5c36b71896167f5733
pranav agarwal <agarwal.pranav300@gmail.com>
2020-11-17 23:03:45 +0530

    minor changes

--- packages/terra-cell-grid/tests/wdio/cell-grid-spec.js
+++ packages/terra-cell-grid/tests/wdio/cell-grid-spec.js
@@ -1,15 +1,15 @@
 Terra.describeViewports('CellGrid', ['medium'], () => {
-  it('Cell test', () => {
+  it('should render grid cell', () => {
     browser.url('/raw/tests/terra-cell-grid/cell-grid/cell');
     Terra.validates.element('cell');
   });
 
-  it('CellGrid test', () => {
+  it('should render a cell-grid', () => {
     browser.url('/raw/tests/terra-cell-grid/cell-grid/cell-grid');
     Terra.validates.element('cell-grid');
   });
 
-  it('CellGridDivider test', () => {
+  it('should render cell-grid divider', () => {
     browser.url('/raw/tests/terra-cell-grid/cell-grid/cell-grid-divider');
     Terra.validates.element('cell-grid-divider');
   });
