43363c43ee6d90d5fa3a6afbce1593e9648d28f6
Matt Henkes <mjhenkes@gmail.com>
2021-01-21 13:48:29 -0600

    remove dev-site config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const {
   TerraDevSite,
-  TerraDevSiteEntrypoints,
 } = require('@cerner/terra-dev-site');
 
 const WebpackConfigTerra = require('@cerner/webpack-config-terra');
 
 const coreConfig = (env = {}) => ({
-  entry: TerraDevSiteEntrypoints,
   plugins: [
     new TerraDevSite({
       defaultLocale: env.defaultLocale,
