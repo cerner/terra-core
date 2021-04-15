db630994a8b4de534507b50b9f22908607d406c7
Neil Pfeiffer <neil.pfeiffer@cerner.com>
2021-04-15 00:03:15 -0500

    Adding supported and unsupported input types and tests

---
+++ packages/terra-form-input/src/terra-dev-site/test/form-input/SupportedTypesInputField.test.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import InputField from '../../../InputField';
+import styles from './common/Input.test.module.scss';
+
+const cx = classNames.bind(styles);
+
+const supportedTypes = ['text', 'number', 'password', 'email', 'search', 'tel', 'url', 'hidden'];
+
+class SupportedTypesInputField extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isInvalid: false,
+      isIncomplete: false,
+      required: false,
+    };
+
+    this.toggleInvalid = this.toggleInvalid.bind(this);
+    this.toggleIncomplete = this.toggleIncomplete.bind(this);
+  }
+
+  createInputFieldTypeExamples(arr) {
+    const inputFieldTypeExamples = [];
+    for (let i = 0; i < arr.length; i += 1) {
+      inputFieldTypeExamples.push((
+        <div className={cx('content-wrapper')}>
+          <InputField
+            inputId={`unsupported-${arr[i]}-input`}
+            error="Error message."
+            help="Help message."
+            isInvalid={this.state.isInvalid}
+            isIncomplete={this.state.isIncomplete}
+            required={this.state.required}
+            inputAttrs={{
+              name: `supported-${arr[i]}-input`,
+              type: arr[i],
+            }}
+            label={`Input type=${arr[i]}`}
+          />
+        </div>
+      ));
+    }
+    return inputFieldTypeExamples;
+  }
+
+  toggleInvalid() {
+    this.setState(prevState => ({
+      isInvalid: !prevState.isInvalid,
+    }));
+  }
+
+  toggleIncomplete() {
+    this.setState(prevState => ({
+      isIncomplete: !prevState.isIncomplete,
+      required: !prevState.required,
+    }));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className={cx('content-wrapper')}>
+          <button type="button" id="validity-toggle" onClick={this.toggleInvalid}>Toggle All: isInvalid</button>
+          <button type="button" id="incomplete-toggle" onClick={this.toggleIncomplete}>Toggle All: isIncomplete + required</button>
+          <div>
+            {`isInvalid: ${this.state.isInvalid}`}
+          </div>
+          <div>
+            {`isIncomplete: ${this.state.isIncomplete}`}
+          </div>
+          <div>
+            {`required: ${this.state.required}`}
+          </div>
+        </div>
+        {this.createInputFieldTypeExamples(supportedTypes)}
+      </React.Fragment>
+    );
+  }
+}
+
+export default SupportedTypesInputField;
