edecbe8562f66dd8044dffbb9db0a27ad5c67253
Matt Henkes <mjhenkes@gmail.com>
2021-04-30 10:18:16 -0500

    Introducing terra-core-docs

---
+++ packages/terra-core-docs/src/terra-dev-site/test/action-footer/MultipleStartEndActionFooter.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from 'terra-button';
+import ActionFooter from 'terra-action-footer/lib/ActionFooter';
+
+export default () => (
+  <ActionFooter
+    start={(
+      <div>
+        Start Text
+        <Button text="Start Action" />
+      </div>
+)}
+    end={(
+      <div>
+        End Text
+        <Button text="End Action" />
+      </div>
+)}
+  />
+);
