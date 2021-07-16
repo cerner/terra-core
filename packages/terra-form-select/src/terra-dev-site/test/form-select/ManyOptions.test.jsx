f62507fd77e88bfa3492864f176106e69dc8aafa
pranav agarwal <agarwal.pranav300@gmail.com>
2021-07-17 00:39:19 +0530

    reverted test ManyOptions and wdio snapshots

--- packages/terra-form-select/src/terra-dev-site/test/form-select/ManyOptions.test.jsx
+++ packages/terra-form-select/src/terra-dev-site/test/form-select/ManyOptions.test.jsx
@@ -8,22 +8,13 @@ const cx = classNames.bind(styles);
 const ManyOptionsExample = () => {
   const options = [];
 
-  for (let index = 1; index <= 1000; index += 1) {
+  for (let index = 1; index <= 100; index += 1) {
     options.push(<Select.Option key={index} value={index} display={`${index}`} />);
   }
 
   return (
     <div className={cx('content-wrapper')}>
-      <Select placeholder="Select a number for Multiple" variant="multiple" maxHeight={200}>
-        {options}
-      </Select>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <Select placeholder="Select a number For Search" variant="search" maxHeight={200}>
+      <Select placeholder="Select a number" variant="combobox" maxHeight={200}>
         {options}
       </Select>
       <p>
@@ -38,7 +29,7 @@ const ManyOptionsExample = () => {
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
-      <Select id="frmSelectSingle" placeholder="Select a number for Tag" variant="tag" maxHeight={200}>
+      <Select id="frmSelectSingle" placeholder="Select a number" variant="default" maxHeight={200}>
         {options}
       </Select>
       <p>
@@ -50,7 +41,7 @@ const ManyOptionsExample = () => {
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
-      <Select placeholder="Select a number for Combobox" variant="combobox" maxHeight={200}>
+      <Select placeholder="Select a number" variant="search" maxHeight={200}>
         {options}
       </Select>
     </div>
