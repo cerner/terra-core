79d782599cab5bfd2553d4813c856361cd7a66e4
Pranav Agarwal <agarwal.pranav300@gmail.com>
2021-08-12 10:33:09 +0530

    Update packages/terra-paginator/src/PageLabel.js
    Co-authored-by: Matt Henkes <mjhenkes@gmail.com>

--- packages/terra-paginator/src/PageLabel.js
+++ packages/terra-paginator/src/PageLabel.js
@@ -1,22 +1,23 @@
 export default function getPageLabel(pageLabel, selectedPage, totalPages) {
-  let messageId;
-  let messageAttributes;
-
-  if (totalPages) {
-    if (pageLabel) {
-      messageId = 'Terra.paginator.pageCountWithLabel';
-      messageAttributes = { pageLabel, pageNumber: selectedPage, pageNumberTotal: totalPages };
-    } else {
-      messageId = 'Terra.paginator.pageCount';
-      messageAttributes = { pageNumber: selectedPage, pageNumberTotal: totalPages };
+  if (totalPages && pageLabel) {
+    return ({
+      messageId: 'Terra.paginator.pageCountWithLabel',
+      messageAttributes: { pageLabel, pageNumber: selectedPage, pageNumberTotal: totalPages },
+    })
+   if (totalPages) {
+      return ({
+        messageId: 'Terra.paginator.pageCount',
+        messageAttributes: { pageNumber: selectedPage, pageNumberTotal: totalPages },
+      )}
     }
-  } else if (pageLabel) {
-    messageId = 'Terra.paginator.pageIndexWithLabel';
-    messageAttributes = { pageLabel, pageNumber: selectedPage };
-  } else {
-    messageId = 'Terra.paginator.pageIndex';
-    messageAttributes = { pageNumber: selectedPage };
+  if (pageLabel) {
+    return ({
+      messageId: 'Terra.paginator.pageIndexWithLabel',
+      messageAttributes: { pageLabel, pageNumber: selectedPage },
+    })
   }
-
-  return { messageId, messageAttributes };
+  return ({
+    messageId: 'Terra.paginator.pageIndex',
+    messageAttributes: { pageNumber: selectedPage },
+  });
 }
