9c8e4e18598efb0a2ac50a71a465e02687ac220e
pranav agarwal <agarwal.pranav300@gmail.com>
2021-08-12 10:50:43 +0530

    minor changes

--- packages/terra-paginator/src/PageLabel.js
+++ packages/terra-paginator/src/PageLabel.js
@@ -3,18 +3,19 @@ export default function getPageLabel(pageLabel, selectedPage, totalPages) {
     return ({
       messageId: 'Terra.paginator.pageCountWithLabel',
       messageAttributes: { pageLabel, pageNumber: selectedPage, pageNumberTotal: totalPages },
-    })
-   if (totalPages) {
-      return ({
-        messageId: 'Terra.paginator.pageCount',
-        messageAttributes: { pageNumber: selectedPage, pageNumberTotal: totalPages },
-      )}
-    }
+    });
+  }
+  if (totalPages) {
+    return ({
+      messageId: 'Terra.paginator.pageCount',
+      messageAttributes: { pageNumber: selectedPage, pageNumberTotal: totalPages },
+    });
+  }
   if (pageLabel) {
     return ({
       messageId: 'Terra.paginator.pageIndexWithLabel',
       messageAttributes: { pageLabel, pageNumber: selectedPage },
-    })
+    });
   }
   return ({
     messageId: 'Terra.paginator.pageIndex',
