cc0bf6ad6d7d445f1f51344358688a72bc770ef0
saket2403 <saket.bajaj1998@gmail.com>
2021-10-27 11:24:58 +0530

    Migrate Docs

--- packages/terra-divider/tests/wdio/divider-spec.js
+++ packages/terra-divider/tests/wdio/divider-spec.js
@@ -1,12 +1,12 @@
 Terra.describeViewports('Divider', ['medium'], () => {
   it('displays default Divider', () => {
-    browser.url('/raw/tests/terra-divider/divider/default-divider');
+    browser.url('/raw/tests/cerner-terra-core-docs/divider/default-divider');
 
     Terra.validates.element('default');
   });
 
   it('displays Divider with custom text', () => {
-    browser.url('/raw/tests/terra-divider/divider/divider-with-text');
+    browser.url('/raw/tests/cerner-terra-core-docs/divider/divider-with-text');
 
     Terra.validates.element('custom text');
   });
