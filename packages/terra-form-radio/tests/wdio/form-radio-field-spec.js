7d4d407c2451ba7fd05e71eae1b67a6204d7ef3e
saket2403 <saket.bajaj1998@gmail.com>
2021-11-16 14:35:45 +0530

    Migrate docs

--- packages/terra-form-radio/tests/wdio/form-radio-field-spec.js
+++ packages/terra-form-radio/tests/wdio/form-radio-field-spec.js
@@ -1,24 +1,24 @@
 Terra.describeViewports('RadioField', ['tiny', 'large'], () => {
   it('should display valid RadioField', () => {
-    browser.url('/raw/tests/terra-form-radio/form-radio/field/controlled-radio-field');
+    browser.url('/raw/tests/cerner-terra-core-docs/form-radio/field/controlled-radio-field');
 
     Terra.validates.element('valid');
   });
 
   it('should display optional RadioField', () => {
-    browser.url('/raw/tests/terra-form-radio/form-radio/field/optional-radio-field');
+    browser.url('/raw/tests/cerner-terra-core-docs/form-radio/field/optional-radio-field');
 
     Terra.validates.element('optional');
   });
 
   it('should display RadioField with hidden legend', () => {
-    browser.url('/raw/tests/terra-form-radio/form-radio/field/hidden-legend');
+    browser.url('/raw/tests/cerner-terra-core-docs/form-radio/field/hidden-legend');
 
     Terra.validates.element('hidden legend');
   });
 
   it('should display RadioField with long text legend', () => {
-    browser.url('/raw/tests/terra-form-radio/form-radio/field/long-text-legend');
+    browser.url('/raw/tests/cerner-terra-core-docs/form-radio/field/long-text-legend');
 
     Terra.validates.element('long text legend');
   });
@@ -26,7 +26,7 @@ Terra.describeViewports('RadioField', ['tiny', 'large'], () => {
 
 Terra.describeViewports('RadioField Interactions', ['tiny'], () => {
   it('should display RadioField interactions - valid state', () => {
-    browser.url('/raw/tests/terra-form-radio/form-radio/field/controlled-radio-field');
+    browser.url('/raw/tests/cerner-terra-core-docs/form-radio/field/controlled-radio-field');
     $('#testing-radio-field').waitForDisplayed();
     $('[for="website-dept"]').click();
     $('[for="ux-dept"]').click();
@@ -37,7 +37,7 @@ Terra.describeViewports('RadioField Interactions', ['tiny'], () => {
   });
 
   it('should display RadioField interactions - valid state with hideRequired', () => {
-    browser.url('/raw/tests/terra-form-radio/form-radio/field/hide-required-invalid-radio-field');
+    browser.url('/raw/tests/cerner-terra-core-docs/form-radio/field/hide-required-invalid-radio-field');
     $('#testing-radio-field').waitForDisplayed();
     $('[for="website-dept"]').click();
     $('[for="ux-dept"]').click();
@@ -46,7 +46,7 @@ Terra.describeViewports('RadioField Interactions', ['tiny'], () => {
   });
 
   it('should display RadioField Interactions - invalid state with hideRequired', () => {
-    browser.url('/raw/tests/terra-form-radio/form-radio/field/hide-required-invalid-radio-field');
+    browser.url('/raw/tests/cerner-terra-core-docs/form-radio/field/hide-required-invalid-radio-field');
     $('#testing-radio-field').waitForDisplayed();
     $('[for="website-dept"]').click();
     $('[for="ux-dept"]').click();
