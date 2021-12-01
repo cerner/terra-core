2253af113f8a78bdb861f9c67c5a07fa020cee52
Elliott Hoffman <elliott.hoffman@cerner.com>
2021-12-01 12:33:30 -0600

    added npm command

--- scripts/check-changelogs/index.js
+++ scripts/check-changelogs/index.js
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
+
+/*
+* This script scans the changelogs of all the packages and prints any unreleased changes noted in those logs.
+*/
 const fs = require('fs');
-const { release } = require('os');
 const path = require('path');
 const packagePaths = require('../common/getPackagePaths');
 
+// Looking for something like:
+//
+//     ## unreleased
+//
+//     Something something ...
+//
+//     ## 2.04.2
+//
 const unreleasedRegex = /#+\s+unreleased\s+([^#]+)##/im;
 
 const released = [];
@@ -14,6 +25,15 @@ packagePaths.forEach((packPath) => {
   const changelogFile = path.resolve(packPath, 'CHANGELOG.md');
   if (fs.existsSync(changelogFile)) {
     const changelog = fs.readFileSync(changelogFile, { encoding: 'utf8', flag: 'r' });
+
+    // Looking for something like:
+    //
+    //     ## unreleased
+    //
+    //     <nothing>
+    //
+    //     ## 2.04.2
+    //
     if (/#+\s+unreleased\s+#+\s+\d+\.\d+\.\d+/im.test(changelog)) {
       released.push(`${packPath} has no unreleased changes.`);
     } else if (unreleasedRegex.test(changelog)) {
