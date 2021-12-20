c2b43b5a5317e0928b3f8c17ebe775fb707b74a1
saket2403 <saket.bajaj1998@gmail.com>
2021-12-20 11:07:18 +0530

    Updates

--- packages/terra-profile-image/tests/wdio/profile-image-spec.js
+++ packages/terra-profile-image/tests/wdio/profile-image-spec.js
@@ -1,18 +1,18 @@
 Terra.describeViewports('Profile Image', ['medium'], () => {
   it('should display default Profile Image', () => {
-    browser.url('/raw/tests/terra-profile-image/profile-image/profile-image-default');
+    browser.url('/raw/tests/cerner-terra-core-docs/profile-image/profile-image-default');
 
     Terra.validates.element('default');
   });
 
   it('should display failed Profile Image', () => {
-    browser.url('/raw/tests/terra-profile-image/profile-image/profile-image-failed');
+    browser.url('/raw/tests/cerner-terra-core-docs/profile-image/profile-image-failed');
 
     Terra.validates.element('failed');
   });
 
   it('should display Profile Image with source not provided', () => {
-    browser.url('/raw/tests/terra-profile-image/profile-image/profile-image-source-not-provided');
+    browser.url('/raw/tests/cerner-terra-core-docs/profile-image/profile-image-source-not-provided');
 
     Terra.validates.element('source not provided');
   });
