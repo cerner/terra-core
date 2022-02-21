3d6e808f32065bea8ca1f99107264c1d068d3b18
Eric Wilson <eawww0@gmail.com>
2022-02-21 12:58:45 -0600

    Move icon test data into test folder

--- packages/terra-core-docs/src/terra-dev-site/doc/icon/IconCollectionTable/index.js
+++ packages/terra-core-docs/src/terra-dev-site/doc/icon/IconCollectionTable/index.js
@@ -1,5 +1,5 @@
 import IconCollectionTable from './IconCollectionTable';
-import iconData from './iconData';
+import iconData from '../../../test/icon-library-table/iconData';
 
 export {IconCollectionTable, iconData};
 export default IconCollectionTable;
