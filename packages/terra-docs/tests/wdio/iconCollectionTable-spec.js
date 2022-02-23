03f46802d1dc8b246914bd5f1d28f2f1f7d05643
Eric Wilson <eawww0@gmail.com>
2022-02-23 16:49:00 -0600

    Add large wdio tests for IconCollectionTable

--- packages/terra-docs/tests/wdio/iconCollectionTable-spec.js
+++ packages/terra-docs/tests/wdio/iconCollectionTable-spec.js
@@ -1,6 +1,15 @@
 Terra.describeViewports('IconCollectionTable', ['medium'], () => {
   describe('IconCollectionTable', () => {
-    it('is valid', () => {
+    it('is valid and scrolls at medium width', () => {
+      browser.url('/raw/tests/cerner-terra-core-docs/icon-collection-table/icon-collection-table');
+      Terra.validates.element('icon-library-table');
+    });
+  });
+});
+
+Terra.describeViewports('IconCollectionTable', ['large'], () => {
+  describe('IconCollectionTable', () => {
+    it('is valid at large width', () => {
       browser.url('/raw/tests/cerner-terra-core-docs/icon-collection-table/icon-collection-table');
       Terra.validates.element('icon-library-table');
     });
