f5c1cfe9e6b96c058ce2ebf618a1e72bc6884bd5
Eric Wilson <eawww0@gmail.com>
2022-02-23 05:57:09 -0600

    Add IconCollectionTable to exports for terra-docs

--- packages/terra-docs/src/index.js
+++ packages/terra-docs/src/index.js
@@ -1,3 +1,4 @@
 export { default as Notice } from './Notice';
 export { default as Placeholder } from './Placeholder';
 export { default as Button } from './Button';
+export { default as IconCollectionTable } from './IconCollectionTable';
