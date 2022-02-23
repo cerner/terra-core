efbd9d866852a51da014f8754e8f59c886cd6573
Eric Wilson <eawww0@gmail.com>
2022-02-23 05:56:41 -0600

    Add tests for IconCollectionRow

---
+++ packages/terra-docs/tests/jest/IconCollectionRow.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import IconCollectionRow from '../../src/IconCollectionTable/IconCollectionRow';
+
+import { bareMinimum, bareMaximum } from './iconCollectionData'
+
+describe('IconCollectionTable', () => {
+  it('should render a default component with minimum props', () => {
+    const wrapper = shallow(<IconCollectionRow
+      {...bareMinimum[0]}
+    />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should render a default component with maximum props', () => {
+    const wrapper = shallow(<IconCollectionRow
+      {...bareMaximum[0]}
+    />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should render a check icon for each function of the icon', () => {
+    const wrapper = shallow(<IconCollectionRow
+      {...bareMinimum[0]}
+      isAction
+      isStatus
+      isToggle
+    />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
