9c76d68e4971c3b5d3520a3496a182698641a83d
Elliott Hoffman <elliott.hoffman@cerner.com>
2022-03-07 13:24:29 -0600

    Added draft content

---
+++ packages/terra-core-docs/src/terra-dev-site/doc/icon/example/DecorativeIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Image from 'terra-image';
+import img from '../assets/decorative-icon.png';
+
+const DecorativeIcon = () => (
+  <Image src={img} alt="" />
+);
+
+export default DecorativeIcon;
---
+++ packages/terra-core-docs/src/terra-dev-site/doc/icon/example/MeaningfulIIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Image from 'terra-image';
+import img from '../assets/meaningful-icon.png';
+
+const MeaningfulIcon = () => (
+  <Image src={img} alt="" />
+);
+
+export default MeaningfulIcon;
