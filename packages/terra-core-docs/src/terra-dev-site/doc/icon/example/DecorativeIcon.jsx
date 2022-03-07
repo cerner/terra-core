415864f61730fe96a2d8a5bbff0bb4096c089f52
Elliott Hoffman <elliott.hoffman@cerner.com>
2022-03-07 17:04:43 -0600

    Better examples

--- packages/terra-core-docs/src/terra-dev-site/doc/icon/example/DecorativeIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Image from 'terra-image';
-import img from '../assets/decorative-icon.png';
-
-const DecorativeIcon = () => (
-  <Image src={img} alt="" />
-);
-
-export default DecorativeIcon;
--- packages/terra-core-docs/src/terra-dev-site/doc/icon/example/MeaningfulIIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Image from 'terra-image';
-import img from '../assets/meaningful-icon.png';
-
-const MeaningfulIcon = () => (
-  <Image src={img} alt="" />
-);
-
-export default MeaningfulIcon;
