0e5c1dbcf0b0da5a82ce82d9edcb9aa45527e038
Elliott Hoffman <elliott.hoffman@cerner.com>
2022-03-09 14:45:24 -0600

    Better examples

---
+++ packages/terra-core-docs/src/terra-dev-site/doc/icon/example/MeaningfulIconGroupExample.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import IconFeatured from 'terra-icon/lib/icon/IconFeatured';
+import IconHalfRating from 'terra-icon/lib/icon/IconHalfRating';
+import IconFeaturedOff from 'terra-icon/lib/icon/IconFeaturedOff';
+
+import styles from './IconDocCommon.module.scss';
+
+const cx = classNames.bind(styles);
+const className = cx('example');
+
+const MeaningfulIconGroupExample = () => (
+  <dl className={className}>
+    <dt>Rating:</dt>
+    <dd>
+      <IconFeatured title="Three and a half stars out of five" />
+      <IconFeatured role="presentation" />
+      <IconFeatured role="presentation" />
+      <IconHalfRating role="presentation" />
+      <IconFeaturedOff role="presentation" />
+    </dd>
+  </dl>
+);
+
+export default MeaningfulIconGroupExample;
