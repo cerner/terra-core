2ff0b734d19923e7af92d9ade4d41b23ab333698
Elliott Hoffman <52507228+xenoworf@users.noreply.github.com>
2022-04-04 13:51:56 -0500

    Icon a11y guide 2 UXPLATFORM-6244 (#3604)
    Added a guide for how to use terra-icon in an accessible way.
    
    Co-authored-by: Jeremy Fuksa <hello@orangefla.me>
    Co-authored-by: Ben Cai <benbcai@gmail.com>

---
+++ packages/terra-core-docs/src/terra-dev-site/doc/icon/example/A11yGroupInteractive.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import Star from 'terra-icon/lib/icon/IconFeaturedOutlineYellow';
+import EmptyStar from 'terra-icon/lib/icon/IconFeaturedOutline';
+import VisuallyHiddenText from 'terra-visually-hidden-text';
+import Card from 'terra-card';
+import { width, height } from './common';
+
+const A11yGroupInteractive = () => (
+  <Card>
+    <Card.Body>
+      Rating:&nbsp;
+      <VisuallyHiddenText text="Four out of five stars" />
+      <button type="button">
+        <Star a11yLabel="Rate one star" width={width} height={height} />
+      </button>
+      <button type="button">
+        <Star a11yLabel="Rate two stars" width={width} height={height} />
+      </button>
+      <button type="button">
+        <Star a11yLabel="Rate three stars" width={width} height={height} />
+      </button>
+      <button type="button">
+        <Star a11yLabel="Rate four stars" width={width} height={height} />
+      </button>
+      <button type="button">
+        <EmptyStar a11yLabel="Rate five stars" width={width} height={height} />
+      </button>
+    </Card.Body>
+  </Card>
+);
+
+export default A11yGroupInteractive;
