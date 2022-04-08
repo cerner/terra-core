e1b50a786615868e7109b439293c5d578d5b1193
Neil Pfeiffer <neil.pfeiffer@cerner.com>
2022-04-08 05:26:44 -0500

    HyperlinkButton docs
    Updating HyperlinkButton doc examples with its own page

---
+++ packages/terra-core-docs/src/terra-dev-site/doc/hyperlink/example/HyperlinkButton/DisabledHyperlinkButton.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import HyperlinkButton from 'terra-hyperlink/lib/HyperlinkButton';
+
+export default () => <HyperlinkButton text="Disabled hyperlink Button" onClick={() => {}} isDisabled />;
---
+++ packages/terra-core-docs/src/terra-dev-site/doc/hyperlink/example/HyperlinkButton/HyperlinkButton.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import HyperlinkButton from 'terra-hyperlink/lib/HyperlinkButton';
+
+export default () => <HyperlinkButton onClick={() => {}} text="Hyperlink Button" />;
