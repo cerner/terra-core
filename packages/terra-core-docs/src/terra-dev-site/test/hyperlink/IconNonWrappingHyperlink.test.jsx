529c6e768188c127aa5bbc05ddf207f10bf476f9
Neil Pfeiffer <neil.pfeiffer@cerner.com>
2022-04-08 05:43:04 -0500

    Update IconNonWrappingHyperlink.test.jsx

--- packages/terra-core-docs/src/terra-dev-site/test/hyperlink/IconNonWrappingHyperlink.test.jsx
+++ packages/terra-core-docs/src/terra-dev-site/test/hyperlink/IconNonWrappingHyperlink.test.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import Hyperlink from 'terra-hyperlink';
 import HyperlinkButton from 'terra-hyperlink/lib/HyperlinkButton';
 
-const exampleHyperlink = <Hyperlink text="Document hyperlink" variant="document" href="https://www.cerner.com" />;
-const exampleHyperlinkButton = <HyperlinkButton text="Document hyperlink" variant="document" onClick={() => {}} />;
-
 export default () => (
   <div role="main" id="link-example">
     <p>
       Example of a hyperlink inside some content that will wrap. With this, the icon should not wrap onto a separate line without the link text. I am a
-      {` ${exampleHyperlink} `}
+      {' '}
+      <Hyperlink text="Document hyperlink" href="https://www.cerner.com" variant="document" />
+      {' '}
       that is forced to wrap due to space constraints. My icon wraps with me.
     </p>
     <p>
       Example of a hyperlink inside some content that will wrap. With this, the icon should not wrap onto a separate line without the link text. I am a
-      {` ${exampleHyperlinkButton} `}
+      {' '}
+      <HyperlinkButton text="Document hyperlink button" onClick={() => {}} variant="document" />
+      {' '}
       that is forced to wrap due to space constraints. My icon wraps with me.
     </p>
   </div>
