196286628669633ed9a4351a045facdc99bf9fb1
Saket Bajaj <42207428+saket2403@users.noreply.github.com>
2022-04-25 15:58:00 +0530

    [terra-hyperlink] Add Hyperlink Button component (#3608)
    * Add Button
    
    * Docs and Tests
    
    * Changelog
    
    * Fix path
    
    * Examples

---
+++ packages/terra-hyperlink/src/index.js
@@ -0,0 +1,5 @@
+import Hyperlink from './Hyperlink';
+import HyperlinkButton from './HyperlinkButton';
+
+export default Hyperlink;
+export { HyperlinkButton };
--- packages/terra-hyperlink/tests/jest/Hyperlink.test.jsx
+++ packages/terra-hyperlink/tests/jest/Hyperlink.test.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { mountWithIntl, renderWithIntl } from 'terra-enzyme-intl';
 import ThemeContextProvider from 'terra-theme-context/lib/ThemeContextProvider';
 
 import Hyperlink from '../../src/Hyperlink';
@@ -6,57 +8,57 @@ import Hyperlink from '../../src/Hyperlink';
 describe('Hyperlink', () => {
   // Snapshot Tests
   it('should render a default hyperlink', () => {
-    const wrapper = render(<Hyperlink text="Default hyperlink" href="https://www.cerner.com" />);
+    const wrapper = renderWithIntl(<Hyperlink text="Default hyperlink" href="https://www.cerner.com" />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render a disabled hyperlink', () => {
-    const wrapper = render(<Hyperlink text="Disabled hyperlink" href="https://www.cerner.com" isDisabled />);
+    const wrapper = renderWithIntl(<Hyperlink text="Disabled hyperlink" href="https://www.cerner.com" isDisabled />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render a audio hyperlink', () => {
-    const wrapper = render(<Hyperlink text="Audio hyperlink" href="https://www.cerner.com" variant="audio" />);
+    const wrapper = renderWithIntl(<Hyperlink text="Audio hyperlink" href="https://www.cerner.com" variant="audio" />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render a document hyperlink', () => {
-    const wrapper = render(<Hyperlink text="Document hyperlink" href="https://www.cerner.com" variant="document" />);
+    const wrapper = renderWithIntl(<Hyperlink text="Document hyperlink" href="https://www.cerner.com" variant="document" />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render a external hyperlink', () => {
-    const wrapper = render(<Hyperlink text="External hyperlink" href="https://www.cerner.com" variant="external" />);
+    const wrapper = renderWithIntl(<Hyperlink text="External hyperlink" href="https://www.cerner.com" variant="external" />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render a image hyperlink', () => {
-    const wrapper = render(<Hyperlink text="Image hyperlink" href="https://www.cerner.com" variant="image" />);
+    const wrapper = renderWithIntl(<Hyperlink text="Image hyperlink" href="https://www.cerner.com" variant="image" />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render a underline hidden hyperlink', () => {
-    const wrapper = render(<Hyperlink text="Underline hidden hyperlink" href="https://www.cerner.com" isUnderlineHidden />);
+    const wrapper = renderWithIntl(<Hyperlink text="Underline hidden hyperlink" href="https://www.cerner.com" isUnderlineHidden />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render a video hyperlink', () => {
-    const wrapper = render(<Hyperlink text="Video hyperlink" href="https://www.cerner.com" variant="video" />);
+    const wrapper = renderWithIntl(<Hyperlink text="Video hyperlink" href="https://www.cerner.com" variant="video" />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render with custom props', () => {
-    const wrapper = render(<Hyperlink text="Custom props hyperlink" href="https://www.cerner.com" target="_self" rel="nofollow" />);
+    const wrapper = renderWithIntl(<Hyperlink text="Custom props hyperlink" href="https://www.cerner.com" target="_self" rel="nofollow" />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render with custom props with external variant', () => {
-    const wrapper = render(<Hyperlink text="Custom target and rel props external hyperlink" href="https://www.cerner.com" target="_parent" rel="external" variant="external" />);
+    const wrapper = renderWithIntl(<Hyperlink text="Custom target and rel props external hyperlink" href="https://www.cerner.com" target="_parent" rel="external" variant="external" />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('correctly applies the theme context className', () => {
-    const wrapper = mount(
+    const wrapper = mountWithIntl(
       <ThemeContextProvider theme={{ className: 'orion-fusion-theme' }}>
         <Hyperlink text="Default hyperlink" href="https://www.cerner.com" />
       </ThemeContextProvider>,
