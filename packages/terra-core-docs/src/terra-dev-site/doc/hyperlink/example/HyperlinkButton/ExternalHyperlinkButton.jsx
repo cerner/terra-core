65fe38a158d3ca689636b19d420fa6cfe96cbb27
Saket Bajaj <42207428+saket2403@users.noreply.github.com>
2022-04-25 15:58:00 +0530

    [terra-hyperlink] Add Hyperlink Button component (#3608)
    * Add Button
    
    * Docs and Tests
    
    * Changelog
    
    * Fix path
    
    * Examples

---
+++ packages/terra-core-docs/src/terra-dev-site/doc/hyperlink/example/HyperlinkButton/ExternalHyperlinkButton.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import HyperlinkButton from 'terra-hyperlink/lib/HyperlinkButton';
+
+export default () => <HyperlinkButton text="External hyperlink button" onClick={() => {}} variant="external" />;
