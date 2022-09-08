abea03809b1beaf008c6fd212edf893396e123dd
Neil Pfeiffer <neil.pfeiffer@cerner.com>
2022-09-07 23:37:30 -0500

    testing zero page index and zero count bug

--- packages/terra-core-docs/src/terra-dev-site/test/paginator/ProgressivePaginator.test.jsx
+++ packages/terra-core-docs/src/terra-dev-site/test/paginator/ProgressivePaginator.test.jsx
@@ -3,7 +3,8 @@ import ProgressivePaginator from 'terra-paginator/lib/ProgressivePaginator';
 
 const ProgressivePaginatorExample = () => (
   // eslint-disable-next-line no-console
-  <ProgressivePaginator onPageChange={i => console.log(i)} selectedPage={1} totalCount={2234} itemCountPerPage={20} />
+  <ProgressivePaginator onPageChange={i => console.log(i)} selectedPage={0} totalCount={0} itemCountPerPage={0} />
 );
+//   <ProgressivePaginator onPageChange={i => console.log(i)} selectedPage={1} totalCount={2234} itemCountPerPage={20} />
 
 export default ProgressivePaginatorExample;
