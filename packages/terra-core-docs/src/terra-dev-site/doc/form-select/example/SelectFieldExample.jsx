bad100f4f386f567977eb62c7797692a428c0fad
udaymattam <udaym2942@gmail.com>
2022-12-02 15:38:22 +0530

    added examples

--- packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/SelectFieldExample.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/SelectFieldExample.jsx
@@ -9,12 +9,11 @@ const cx = classNames.bind(styles);
 const SelectFieldExample = () => (
   <Card>
     <Card.Body>
-      <SelectField label="T-shirt size" placeholder="Select a size" selectId="tshirt-size-field-3" className={cx('form-select')}>
-        <SelectField.Option value="xSmall" display="Extra Small" />
-        <SelectField.Option value="small" display="Small" />
-        <SelectField.Option value="medium" display="Medium" />
-        <SelectField.Option value="large" display="Large" />
-        <SelectField.Option value="xLarge" display="Extra Large" />
+      <SelectField label="Allergies Hospital Locations" placeholder="Select clinic for location" selectId="Hospital-location-field" className={cx('form-select')}>
+        <SelectField.Option value="Kansas,MO" display="Cerner,Kansas City, MO" />
+        <SelectField.Option value="Riverport Campus, MO" display="Cerner Corporation - Riverport Campus" />
+        <SelectField.Option value="Innovations Campus, MO" display="Cerner Corporation - Innovations Campus" />
+        <SelectField.Option value="Realization Campus, MO" display="Cerner Corporation - Realization Campus" />
       </SelectField>
     </Card.Body>
   </Card>
