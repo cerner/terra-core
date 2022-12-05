aafbf36dbbeca029b52295bfffcf7bd920e321fd
saket2403 <saket.bajaj1998@gmail.com>
2022-12-05 17:29:56 +0530

    Minor Change

--- packages/terra-html-table/src/TableUtils.jsx
+++ packages/terra-html-table/src/TableUtils.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 // Returns the children after applying the required scope attribute for table headers
 
 const addScope = (children, scope) => {
-  const cloneChildren = children ? [] : undefined;
+  const cloneChildren = [];
   React.Children.forEach(children, (child) => {
     cloneChildren.push(React.cloneElement(child, { scope: child.type.name === 'TableHeaderCell' ? scope : undefined }));
   });
