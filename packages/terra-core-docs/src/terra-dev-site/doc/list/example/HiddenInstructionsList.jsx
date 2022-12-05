d3b2360e516444d13d0b1136b53207a3b0f858f4
sycombs <sydneyycombs@gmail.com>
2022-12-05 15:16:41 -0600

    Add examples

---
+++ packages/terra-core-docs/src/terra-dev-site/doc/list/example/HiddenInstructionsList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import List, { Item } from 'terra-list/lib/index';
+import { Placeholder } from '@cerner/terra-docs';
+import VisuallyHiddenText from 'terra-visually-hidden-text';
+import classNames from 'classnames/bind';
+import styles from './ListDocCommon.module.scss';
+
+const cx = classNames.bind(styles);
+
+const HiddenInstructionsList = () => (
+  <>
+    <VisuallyHiddenText
+      id="list-help"
+      text="Here are some instructions for using this list."
+    />
+    <List instructions="list-help">
+      <Item key="123">
+        <Placeholder title="Item 0" className={cx('placeholder')} />
+      </Item>
+      <Item key="124">
+        <Placeholder title="Item 1" className={cx('placeholder')} />
+      </Item>
+      <Item key="125">
+        <Placeholder title="Item 2" className={cx('placeholder')} />
+      </Item>
+    </List>
+  </>
+);
+
+export default HiddenInstructionsList;
---
+++ packages/terra-core-docs/src/terra-dev-site/doc/list/example/InstructionTextList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import List, { Item } from 'terra-list/lib/index';
+import { Placeholder } from '@cerner/terra-docs';
+import classNames from 'classnames/bind';
+import styles from './ListDocCommon.module.scss';
+
+const cx = classNames.bind(styles);
+
+const InstructionTextList = () => (
+  <List instructionText="Here are some instructions for using this list.">
+    <Item key="123">
+      <Placeholder title="Item 0" className={cx('placeholder')} />
+    </Item>
+    <Item key="124">
+      <Placeholder title="Item 1" className={cx('placeholder')} />
+    </Item>
+    <Item key="125">
+      <Placeholder title="Item 2" className={cx('placeholder')} />
+    </Item>
+  </List>
+);
+
+export default InstructionTextList;
