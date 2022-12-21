d3426b053fe6eef4cc329cef8fe7ec808917bd3f
Eric Wilson <eawww0@gmail.com>
2022-12-21 09:55:08 -0600

    Update config file for temp icon data source

--- packages/terra-icon/scripts/src/config.js
+++ packages/terra-icon/scripts/src/config.js
@@ -1,9 +1,13 @@
 export const CernerOneIcons = {
+  //this line is temporary local copy until https://github.com/cerner/one-cerner-style-icons/pulls is merged
+  jsonFile: 'scripts/src/CernerOneIcons.json',
+  // jsonFile: 'node_modules/one-cerner-style-icons/src/CernerOneIcons.json',
   csvFile: 'node_modules/one-cerner-style-icons/src/CernerOneIcons-CSV.csv',
   svgDir: 'node_modules/one-cerner-style-icons/src/icons/',
 };
 
 export const TerraIcon = {
+  jsonFile: 'src/cerner-one-icons.json',
   csvFile: 'src/cerner-one-icons.csv',
   svgDir: 'src/svg/',
   iconDir: 'src/icon/',
