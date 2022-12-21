215322faddd6e024d5f3d5fd47b35b15898dba44
Eric Wilson <eawww0@gmail.com>
2022-12-21 13:32:05 -0600

    Convert Icon class to function

--- packages/terra-icon/scripts/src/generate-icons/Icon.js
+++ packages/terra-icon/scripts/src/generate-icons/Icon.js
@@ -4,14 +4,11 @@ import transformChildren from './transformChildren';
 import htmlToReactAttributes from './htmlToReactAttributes';
 import classNamesToAttributes from './classNamesToAttributes';
 
-class Icon {
-  constructor(name, node) {
-    this.name = `Icon${_.upperFirst(_.camelCase(name))}`;
-    this.children = transformChildren(node);
-    this.attributes = classNamesToAttributes(Array.prototype.slice.call(node.attributes)
-      .map(x => ({ name: htmlToReactAttributes(x.name), value: x.value }))
-      .reduce((attrs, x) => ({ [x.name]: x.value, ...attrs }), {}));
-  }
-}
+const Icon = (node) => ({
+  children: transformChildren(node),
+  attributes: classNamesToAttributes(Array.prototype.slice.call(node.attributes)
+    .map(x => ({ name: htmlToReactAttributes(x.name), value: x.value }))
+    .reduce((attrs, x) => ({ [x.name]: x.value, ...attrs }), {}))
+});
 
 export default Icon;
