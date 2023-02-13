59e271ac10ad0240397c0fb5df94f0762711678c
udaymattam <udaym2942@gmail.com>
2023-02-13 14:10:57 +0530

    wdio Update

--- packages/terra-form-fieldset/tests/wdio/fieldset-spec.js
+++ packages/terra-form-fieldset/tests/wdio/fieldset-spec.js
@@ -14,6 +14,6 @@ Terra.describeViewports('Fieldset', ['tiny', 'medium'], () => {
   it('should display populated Fieldset without legend and helptext', () => {
     browser.url('/raw/tests/cerner-terra-core-docs/form-fieldset/fieldset-without-legend-and-helptext');
 
-    Terra.validates.element('populated Fieldset without legend and helptext');
+    Terra.validates.accessibility();
   });
 });
