aa946cb4ec63def2801845aaa57193af424c5e57
udaymattam <udaym2942@gmail.com>
2023-02-13 13:56:52 +0530

    Updated Examples

--- packages/terra-core-docs/src/terra-dev-site/doc/toggle/example/ToggleLabResults.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/toggle/example/ToggleLabResults.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import Toggle from 'terra-toggle';
 import IconInformation from 'terra-icon/lib/icon/IconInformation';
-import { KEY_TAB } from 'keycode-js';
+import { KEY_TAB, KEY_SPACE, KEY_RETURN } from 'keycode-js';
 import './ToggleExample.module.scss';
-import Card from 'terra-card';
+import Table from 'terra-table';
 
 const ToggleLabResults = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,28 +15,63 @@ const ToggleLabResults = () => {
     if (event.nativeEvent.keyCode === KEY_TAB) {
       setFocus(true);
     }
+    if (event.nativeEvent.keyCode === KEY_SPACE || event.nativeEvent.keyCode === KEY_RETURN) {
+      setIsOpen(!isOpen);
+      event.preventDefault();
+    }
   };
 
   return (
-    <Card>
-      <Card.Body>
-        <div>
-          <label htmlFor="Icon-label">
-            <IconInformation data-show-focus-styles={focused} focusable onKeyDown={handleOnKeyDown} onMouseDown={() => { setFocus(false); }} onClick={handleOnClick} a11yLabel="information Icon" tabIndex="0" role="button" aria-expanded={isOpen} aria-controls="toggle" id="Icon-label" />
-            <span id="Icon-label"> Lab Results</span>
-          </label>
-          <Toggle isOpen={isOpen} isAnimated>
-            <p>
-              Cerner offers a suite of solutions targeted toward the key segments of the complex laboratory
-              <a href="#test">blood sciences, microbiology, cellular pathology, molecular diagnostics, and laboratory logistical services.</a>
-              {' '}
-              Cerner also offers a reference lab network partner programme that allows you to collaborate with Cerner and other reference labs to support the sharing of information across your own lab network.
-            </p>
-          </Toggle>
-        </div>
-      </Card.Body>
-    </Card>
-
+    <div>
+      <label htmlFor="Icon-label">
+        <IconInformation data-show-focus-styles={focused} focusable onKeyDown={handleOnKeyDown} onMouseDown={() => { setFocus(false); }} onClick={handleOnClick} a11yLabel="Lab Results, Information Icon" tabIndex="0" role="button" aria-expanded={isOpen} aria-controls="toggle" id="Icon-label" />
+        <span id="Icon-label"> Lab Results</span>
+      </label>
+      <Toggle isOpen={isOpen} isAnimated>
+        <Table
+          numberOfColumns={3}
+          cellPaddingStyle="standard"
+          dividerStyle="horizontal"
+          headerData={{
+            cells: [
+              { id: 'header-Component', key: 'Component', children: 'BLOOD PRESSURE CATEGORY' },
+              { id: 'header-Normal range', key: 'Normal range', children: 'SYSTOLIC mm Hg (upper number)' },
+              { id: 'header-Results', key: 'Results', children: 'DIASTOLIC mm Hg (lower number)' },
+            ],
+          }}
+          bodyData={[
+            {
+              rows: [
+                {
+                  key: 'row-0',
+                  cells: [
+                    { key: 'cell-0', children: 'NORMAL' },
+                    { key: 'cell-1', children: 'LESS THAN 120' },
+                    { key: 'cell-2', children: 'LESS THAN 80' },
+                  ],
+                },
+                {
+                  key: 'row-1',
+                  cells: [
+                    { key: 'cell-0', children: 'ELEVATED' },
+                    { key: 'cell-1', children: '120 – 129' },
+                    { key: 'cell-2', children: 'LESS THAN 80' },
+                  ],
+                },
+                {
+                  key: 'row-2',
+                  cells: [
+                    { key: 'cell-0', children: 'HIGH BLOOD PRESSURE (HYPERTENSION) STAGE 1' },
+                    { key: 'cell-1', children: '130 – 139' },
+                    { key: 'cell-2', children: '80 – 89' },
+                  ],
+                },
+              ],
+            },
+          ]}
+        />
+      </Toggle>
+    </div>
   );
 };
 
--- packages/terra-core-docs/src/terra-dev-site/doc/toggle/example/TogglePatient.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/toggle/example/TogglePatient.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import Toggle from 'terra-toggle';
 import IconInformation from 'terra-icon/lib/icon/IconInformation';
-import { KEY_TAB } from 'keycode-js';
+import { KEY_TAB, KEY_SPACE, KEY_RETURN } from 'keycode-js';
 import './ToggleExample.module.scss';
-import Card from 'terra-card';
+import Table from 'terra-table';
 
 const TogglePatient = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,28 +15,55 @@ const TogglePatient = () => {
     if (event.nativeEvent.keyCode === KEY_TAB) {
       setFocus(true);
     }
+    if (event.nativeEvent.keyCode === KEY_SPACE || event.nativeEvent.keyCode === KEY_RETURN) {
+      setIsOpen(!isOpen);
+      event.preventDefault();
+    }
   };
 
   return (
-    <Card>
-      <Card.Body>
-        <div>
-          <label htmlFor="Icon-label">
-            <IconInformation data-show-focus-styles={focused} focusable onKeyDown={handleOnKeyDown} onMouseDown={() => { setFocus(false); }} onClick={handleOnClick} a11yLabel="information Icon" tabIndex="0" role="button" aria-expanded={isOpen} aria-controls="toggle" id="Icon-label" />
-            <span id="Icon-label"> Patients</span>
-          </label>
-          <Toggle isOpen={isOpen} isAnimated>
-            <p>
-              Cerner Patient
-              <a href="https://www.cerner.com/solutions/patient"> Cerner recognizes the digitization of health care requires improved focus around care coordination and communication, as well as support, empowerment and access to promote patient-centered care.</a>
-              {' '}
-              Leveraging an ecosystem of virtual, remote and mobile services and technology, your provider is able to provide you greater access to care and enable collaboration among your care team. Through use of Cerner’s patient engagement tools, your care team is able to track your progress and proactively engage with you, enabling the delivery of timely, quality and cost-effective care anywhere.
-            </p>
-          </Toggle>
-        </div>
-      </Card.Body>
-    </Card>
-
+    <div>
+      <label htmlFor="Icon-label">
+        <IconInformation data-show-focus-styles={focused} focusable onKeyDown={handleOnKeyDown} onMouseDown={() => { setFocus(false); }} onClick={handleOnClick} a11yLabel="Patient, Information Icon" tabIndex="0" role="button" aria-expanded={isOpen} aria-controls="toggle" id="Icon-label" />
+        <span id="Icon-label"> Patients</span>
+      </label>
+      <Toggle isOpen={isOpen} isAnimated>
+        <Table
+          numberOfColumns={3}
+          cellPaddingStyle="standard"
+          dividerStyle="horizontal"
+          headerData={{
+            cells: [
+              { id: 'header-name', key: 'name', children: 'Patient is allergic TO:' },
+            ],
+          }}
+          bodyData={[
+            {
+              rows: [
+                {
+                  key: 'row-0',
+                  cells: [
+                    { key: 'cell-0', children: 'dogs' },
+                  ],
+                },
+                {
+                  key: 'row-1',
+                  cells: [
+                    { key: 'cell-0', children: 'dust' },
+                  ],
+                },
+                {
+                  key: 'row-2',
+                  cells: [
+                    { key: 'cell-0', children: 'peanuts' },
+                  ],
+                },
+              ],
+            },
+          ]}
+        />
+      </Toggle>
+    </div>
   );
 };
 
