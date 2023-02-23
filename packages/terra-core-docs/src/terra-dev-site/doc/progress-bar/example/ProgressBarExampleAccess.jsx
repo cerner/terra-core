3bcf5d43d5332cfb044ef87a4a901e609ed840b7
Sugan G <Sugan.G@cerner.com>
2023-02-23 11:53:25 +0530

    added guide, example

---
+++ packages/terra-core-docs/src/terra-dev-site/doc/progress-bar/example/ProgressBarExampleAccess.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from 'terra-card';
+import ProgressBar from 'terra-progress-bar';
+
+const ProgressBarExampleAccess = () => (
+    <div>
+        <Card>
+            <Card.Body>
+                <ProgressBar valueText="Progress is 15%" value={15} />
+            </Card.Body>
+        </Card>
+    </div>
+);
+
+export default ProgressBarExampleAccess;
