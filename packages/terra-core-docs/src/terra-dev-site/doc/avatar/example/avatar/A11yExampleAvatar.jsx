cfe80c3f2d4ea2943c5921b41d4c3ac20b94b5b5
Chetan Sharma <Chetan.Sharma@cerner.com>
2023-03-03 21:07:57 +0530

    Fixed lint issues

--- packages/terra-core-docs/src/terra-dev-site/doc/avatar/example/avatar/A11yExampleAvatar.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/avatar/example/avatar/A11yExampleAvatar.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import classNamesBind from "classnames/bind";
-import Card from "terra-card";
+import React from 'react';
+import classNamesBind from 'classnames/bind';
+import Card from 'terra-card';
 import Avatar from 'terra-avatar';
 import exampleAvatarImage from '../../assets/150x150.jpg';
-import styles from "../common/A11yExamples.module.scss";
+import styles from '../common/A11yExamples.module.scss';
 
 const cx = classNamesBind.bind(styles);
 
 const A11yExampleAvatar = () => (
-  <Card classNames={cx("card")}>
+  <Card classNames={cx('card')}>
     <Card.Body>
-      <Avatar image={exampleAvatarImage} initials="JS" alt="Deep Space" size="1.5em"/>
-      <span className={cx("add-left-margin")}>
+      <Avatar image={exampleAvatarImage} initials="JS" alt="Deep Space" size="1.5em" />
+      <span className={cx('add-left-margin')}>
         Avatar with meaningful image assigned with proper alternative text
       </span>
     </Card.Body>
