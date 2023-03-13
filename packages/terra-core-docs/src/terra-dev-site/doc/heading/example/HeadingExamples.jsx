b0366be799ca3585acf208f04b7daed092ff8b45
Chandan Theja R C <cr062319@cerner.com>
2023-03-13 17:47:20 +0530

    lint fixed

--- packages/terra-core-docs/src/terra-dev-site/doc/heading/example/HeadingExamples.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/heading/example/HeadingExamples.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Arrange from 'terra-arrange';
 import Heading from 'terra-heading';
 import classNames from 'classnames/bind';
-import styles from './HeadingDocCommon.module.scss';
 import { Notice } from '@cerner/terra-docs';
+import styles from './HeadingDocCommon.module.scss';
 
 const cx = classNames.bind(styles);
 
@@ -12,65 +12,65 @@ const HeadingExamples = () => (
     <Notice variant="important" ariaLevel="5">
 
       <Arrange
-      align="center"
-      fill={(
-        <div className={cx('heading-wrapper')}>
-          <Heading level={1}>
-          Allergies Hospitals Locations
-          </Heading>
-        </div>
+        align="center"
+        fill={(
+          <div className={cx('heading-wrapper')}>
+            <Heading level={1}>
+              Allergies Hospitals Locations
+            </Heading>
+          </div>
 )}
-    />
-    <Arrange
-      align="center"
-      fill={(
-        <div className={cx('heading-wrapper')}>
-          <Heading level={2}>
-            Kansas,MO
-          </Heading>
-        </div>
+      />
+      <Arrange
+        align="center"
+        fill={(
+          <div className={cx('heading-wrapper')}>
+            <Heading level={2}>
+              Kansas,MO
+            </Heading>
+          </div>
 )}
-    />
-    <Arrange
-      align="center"
-      fill={(
-        <div className={cx('heading-wrapper')}>
-          <Heading level={3}>
-            Riverport Campus, MO
-          </Heading>
-        </div>
+      />
+      <Arrange
+        align="center"
+        fill={(
+          <div className={cx('heading-wrapper')}>
+            <Heading level={3}>
+              Riverport Campus, MO
+            </Heading>
+          </div>
 )}
-    />
-    <Arrange
-      align="center"
-      fill={(
-        <div className={cx('heading-wrapper')}>
-          <Heading level={4}>
-            Innovations Campus, MO
-          </Heading>
-        </div>
+      />
+      <Arrange
+        align="center"
+        fill={(
+          <div className={cx('heading-wrapper')}>
+            <Heading level={4}>
+              Innovations Campus, MO
+            </Heading>
+          </div>
 )}
-    />
-    <Arrange
-      align="center"
-      fill={(
-        <div className={cx('heading-wrapper')}>
-          <Heading level={5}>
-            Realization Campus, MO
-          </Heading>
-        </div>
+      />
+      <Arrange
+        align="center"
+        fill={(
+          <div className={cx('heading-wrapper')}>
+            <Heading level={5}>
+              Realization Campus, MO
+            </Heading>
+          </div>
 )}
-    />
-    <Arrange
-      align="center"
-      fill={(
-        <div className={cx('heading-wrapper')}>
-          <Heading level={6}>
-            Kansas HQ,MO
-          </Heading>
-        </div>
+      />
+      <Arrange
+        align="center"
+        fill={(
+          <div className={cx('heading-wrapper')}>
+            <Heading level={6}>
+              Kansas HQ,MO
+            </Heading>
+          </div>
 )}
-    />
+      />
     </Notice>
   </div>
 );
