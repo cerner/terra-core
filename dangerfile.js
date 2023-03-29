5fa3086c6427a55dd730e75071a27f409e6843d8
Saad Adnan <38024451+sdadn@users.noreply.github.com>
2023-03-29 12:09:57 -0500

    Update dangerfile config (#3763)

--- dangerfile.js
+++ dangerfile.js
@@ -4,29 +4,37 @@ import { danger, fail } from 'danger';
 const CHANGELOG_PATTERN = /^packages\/terra-([a-z-0-9])*\/CHANGELOG\.md/i;
 
 const changedFiles = danger.git.created_files.concat(danger.git.modified_files);
+const allowedFilepaths = ['package.json', 'src', 'translations'];
 
 const changedChangelogs = new Set();
 const changedPackages = new Set();
 
 changedFiles.forEach((file) => {
-  // file isn't in a package so it has no changelog, skip further processing
   if (file.substring(0, 9) !== 'packages/') {
+    // file isn't in a package so it has no changelog, skip further processing
+    return;
+  }
+
+  if (!allowedFilepaths.some(filepath => file.includes(filepath))) {
+    // skip further processing if the changed file is not among the allowed filepaths
     return;
   }
 
   const packageName = file.split('packages/')[1].split('/')[0];
-  const fileName = file.split('packages/')[1].split('/')[1].split('/')[0];
 
   if (CHANGELOG_PATTERN.test(file)) {
+    // changed file is the CHANGELOG itself
     changedChangelogs.add(packageName);
-  } else if (fileName !== 'README.md') { // file is in a package and was changed ( ignores changes made for readme file ) - we need a changelog
-    changedPackages.add(packageName);
+    return;
   }
+
+  // for other allowed files
+  changedPackages.add(packageName);
 });
 
 const missingChangelogs = [...changedPackages].filter(packageName => !changedChangelogs.has(packageName));
 
 // Fail if there are package changes without a CHANGELOG update
 if (missingChangelogs.length > 0) {
-  fail(`Please include a CHANGELOG entry for each changed package this PR. Looks like a CHANGELOG is missing for: \n\n - ${missingChangelogs.join('\n - ')}`);
+  fail(`Please include a CHANGELOG entry for each changed package this PR. Looks like a CHANGELOG entry is missing for: \n\n - ${missingChangelogs.join('\n - ')}`);
 }
