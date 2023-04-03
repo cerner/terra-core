11d5c3141768494b4c48a1bea1d15acc1b2dec61
Saad Adnan <38024451+sdadn@users.noreply.github.com>
2023-04-03 14:50:38 -0500

    updated dangerfile (#3769)

--- dangerfile.js
+++ dangerfile.js
@@ -4,7 +4,7 @@ import { danger, fail } from 'danger';
 const CHANGELOG_PATTERN = /^packages\/terra-([a-z-0-9])*\/CHANGELOG\.md/i;
 
 const changedFiles = danger.git.created_files.concat(danger.git.modified_files);
-const allowedFilepaths = ['package.json', 'src', 'translations'];
+const allowedFilepaths = ['package.json', 'CHANGELOG.md', 'src', 'translations', 'terra-core-docs'];
 
 const changedChangelogs = new Set();
 const changedPackages = new Set();
