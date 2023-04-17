00cb15d728348da92eedeadc47e38133952c376a
Pravinkumar K S <pk106552@cerner.net>
2023-04-17 17:39:19 +0530

    Fixed lint issues

--- packages/terra-core-docs/src/terra-dev-site/doc/status-view/example/Example.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/status-view/example/Example.jsx
@@ -1,18 +1,16 @@
-import React, { useState} from 'react'; 
+import React from 'react';
 import StatusView from 'terra-status-view';
 import ToggleButton from 'terra-toggle-button';
 
-const Example = () => {
-    return ( 
-      <div>
-        <ToggleButton closedButtonText="ToggleButton">
-          <StatusView 
-            role="alert" 
-            aria-live="polite" 
-            message="Testing"
-            variant="no-data"
-          />
-        </ToggleButton>
-      </div> ); 
-}; 
+const Example = () => (
+  <ToggleButton closedButtonText="ToggleButton">
+    <StatusView
+      role="alert"
+      aria-live="polite"
+      message="Testing"
+      variant="no-data"
+    />
+  </ToggleButton>
+);
+
 export default Example;
