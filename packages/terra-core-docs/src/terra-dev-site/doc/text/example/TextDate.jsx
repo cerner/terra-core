d5eee82881fd004359a7bd8160fd557afae81bdb
swetasingh9714 <swetasingh9714@gmail.com>
2023-04-19 16:50:58 +0530

    Use semantic tag

--- packages/terra-core-docs/src/terra-dev-site/doc/text/example/TextDate.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/text/example/TextDate.jsx
@@ -1,32 +1,11 @@
 import React from 'react';
 import Text from 'terra-text';
 import moment from 'moment';
-import VisuallyHiddenText from 'terra-visually-hidden-text';
 
-const dateVar = moment('01-10-2022', 'MM/DD/YYYY').format('MM/DD/YYYY');
-
-const dateParts = dateVar.split('/');
-const monthVal = dateParts[0];
-const dayVal = dateParts[1];
-const yearVal = dateParts[2];
+const dateVar = moment('01-10-2022', 'MM-DD-YYYY').format('MM-DD-YYYY');
 
 const valueToDisplay = (
-  <>
-    <VisuallyHiddenText text="Month" />
-    <React.Fragment>
-      {monthVal}
-      /
-    </React.Fragment>
-
-    <VisuallyHiddenText text="Day" />
-    <React.Fragment>
-      {dayVal}
-      /
-    </React.Fragment>
-
-    <VisuallyHiddenText text="Year" />
-    <React.Fragment>{yearVal}</React.Fragment>
-  </>
+  <time datetime={dateVar}>{dateVar}</time>
 );
 
 const TextDate = () => (
--- packages/terra-core-docs/src/terra-dev-site/doc/text/example/TextTime.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/text/example/TextTime.jsx
@@ -1,32 +1,11 @@
 import React from 'react';
 import Text from 'terra-text';
 import moment from 'moment';
-import VisuallyHiddenText from 'terra-visually-hidden-text';
 
 const timeVar = moment('10:22:30', 'hh:mm:ss').format('hh:mm:ss');
 
-const timeParts = timeVar.split(':');
-const hourVal = timeParts[0];
-const minuteVal = timeParts[1];
-const secondVal = timeParts[2];
-
 const valueToDisplay = (
-  <>
-    <VisuallyHiddenText text="Hour" />
-    <React.Fragment>
-      {hourVal}
-      :
-    </React.Fragment>
-
-    <VisuallyHiddenText text="Minute" />
-    <React.Fragment>
-      {minuteVal}
-      :
-    </React.Fragment>
-
-    <VisuallyHiddenText text="Second" />
-    <React.Fragment>{secondVal}</React.Fragment>
-  </>
+  <time datetime={timeVar}>{timeVar}</time>
 );
 
 const TextTime = () => (
