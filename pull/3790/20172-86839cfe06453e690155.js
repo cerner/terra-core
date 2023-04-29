457f98b6e0876855f408fac01eeefbc23591b28d
AH106586Harika <AH106586Harika@users.noreply.github.com>
2023-04-29 18:11:30 +0000

    deploy: 1477d11c574c4e50183c038daa5473c778ec43f4

---
+++ pull/3790/20172-86839cfe06453e690155.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkterra_core=self.webpackChunkterra_core||[]).push([[20172],{20172:function(e,t,a){Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var u=r(a(67294)),n=r(a(89755));function r(e){return e&&e.__esModule?e:{default:e}}t.default=function(){return u.default.createElement(n.default,{id:"one-initial-avatar",initials:"J",alt:"John"})}}}]);
