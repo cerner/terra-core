3c7c42aafac8cb8f72e3e6387d9788131943dcbb
swetasingh9714 <swetasingh9714@gmail.com>
2023-05-04 11:53:46 +0530

    Update Examples for date and time

--- packages/terra-core-docs/src/terra-dev-site/doc/text/example/TextDate.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/text/example/TextDate.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Text from 'terra-text';
 import moment from 'moment';
+import VisuallyHiddenText from 'terra-visually-hidden-text';
 
-const dateVar = moment('01-10-2022', 'MM-DD-YYYY').format('MM-DD-YYYY');
-
-const valueToDisplay = (
-  <time dateTime={dateVar}>{dateVar}</time>
-);
+const dateVar = moment('01-06-2022', 'MM-DD-YYYY').format('MM-DD-YYYY');
 
 const TextDate = () => (
-  <div><Text id="dateTest">{valueToDisplay}</Text></div>
+  <>
+    <p>Date format using Semantic Element:</p>
+    <Text id="dateTest"><time dateTime={dateVar}>{dateVar}</time></Text>
+
+    <p>Date format using custom labels:</p>
+    <Text id="text"><VisuallyHiddenText text="June, 01, 2017"/><span aria-hidden="true">2017-06-01</span></Text> {/*to make screen readers to announce value as date*/}
+
+
+    <p>Date format using date as a string:</p>
+    <Text id="text">2017/06/01</Text> {/*to make screen readers to announce slash or any type of separator with date values*/}
+  </>
 );
 
 export default TextDate;
--- packages/terra-core-docs/src/terra-dev-site/doc/text/example/TextTime.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/text/example/TextTime.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Text from 'terra-text';
-import moment from 'moment';
-
-const timeVar = moment('10:22:30', 'hh:mm:ss').format('hh:mm:ss');
-
-const valueToDisplay = (
-  <time dateTime={timeVar}>{timeVar}</time>
-);
+import VisuallyHiddenText from 'terra-visually-hidden-text';
 
 const TextTime = () => (
-  <div><Text id="timeTest">{valueToDisplay}</Text></div>
+  <>
+    <p>Time format using Semantic Element:</p>
+    <Text id="timeTest"><time dateTime="10:22:30">10:22:30</time></Text>
+
+    <p>Time format using custom labels:</p>
+    <Text id="text"><VisuallyHiddenText text="10 Hours 22 Minutes 30 Seconds"/><span aria-hidden="true">10:22:30</span></Text> {/*to make screen readers to announce value as time*/}
+  </>
 );
 
 export default TextTime;
