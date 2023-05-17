e25585e041bd7530426863ffaf5e1cab3f6b1893
Alla Doroshkevych <alla.doroshkevych@oracle.com>
2023-05-17 13:27:14 -0400

    overlay examples

--- packages/terra-core-docs/src/terra-dev-site/doc/overlay/example/LoadingOverlayExample.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/overlay/example/LoadingOverlayExample.jsx
@@ -11,21 +11,24 @@ class OverlayExample extends React.Component {
   constructor() {
     super();
 
-    this.state = { show: false };
+    this.state = {
+      show: false,
+      showCustomMessage: false,
+    };
     this.handleTriggerOverlay = this.handleTriggerOverlay.bind(this);
     this.handleTriggerFullScreenOverlay = this.handleTriggerFullScreenOverlay.bind(this);
   }
 
-  handleTriggerOverlay() {
-    this.setState({ show: true, isRelativeToContainer: true });
+  handleTriggerOverlay(showCustomMessage) {
+    this.setState({ show: true, isRelativeToContainer: true, showCustomMessage });
 
     setTimeout(() => {
       this.setState({ show: false });
     }, 5000);
   }
 
-  handleTriggerFullScreenOverlay() {
-    this.setState({ show: true, isRelativeToContainer: false });
+  handleTriggerFullScreenOverlay(showCustomMessage) {
+    this.setState({ show: true, isRelativeToContainer: false, showCustomMessage });
 
     setTimeout(() => {
       this.setState({ show: false });
@@ -34,15 +37,17 @@ class OverlayExample extends React.Component {
 
   addLoadingOverlay() {
     return (
-      <LoadingOverlay isOpen={this.state.show} isAnimated isRelativeToContainer={this.state.isRelativeToContainer} zIndex="6000" />
+      <LoadingOverlay isOpen={this.state.show} message={this.state.showCustomMessage ? 'The custom loading message' : null} isAnimated isRelativeToContainer={this.state.isRelativeToContainer} zIndex="6000" />
     );
   }
 
   render() {
     return (
       <OverlayContainer className={cx('overlay-container')} overlay={this.addLoadingOverlay()}>
-        <Button text="Trigger Container Overlay" onClick={this.handleTriggerOverlay} />
-        <Button text="Trigger FullScreen Overlay" onClick={this.handleTriggerFullScreenOverlay} />
+        <Button text="Trigger Container Overlay" onClick={() => this.handleTriggerOverlay(false)} />
+        <Button text="Trigger FullScreen Overlay" onClick={() => this.handleTriggerFullScreenOverlay(false)} />
+        <Button text="Trigger Container Overlay with Custom Message" onClick={() => this.handleTriggerOverlay(true)} />
+        <Button text="Trigger FullScreen Overlay with Custom Message" onClick={() => this.handleTriggerFullScreenOverlay(true)} />
       </OverlayContainer>
     );
   }
