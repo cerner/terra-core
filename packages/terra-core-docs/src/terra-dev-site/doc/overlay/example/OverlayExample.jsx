c65b66e39cce19367cddf401dbaab1064f9e955f
Alla Doroshkevych <alla.doroshkevych@oracle.com>
2023-05-19 15:03:17 -0400

    Overlay component examples added (#3801)
    * overlay component examples added

--- packages/terra-core-docs/src/terra-dev-site/doc/overlay/example/OverlayExample.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/overlay/example/OverlayExample.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Overlay from 'terra-overlay';
 import OverlayContainer from 'terra-overlay/lib/OverlayContainer';
 import Button from 'terra-button';
+import Text from 'terra-text';
 import classNames from 'classnames/bind';
 import styles from './OverlayDocCommon.module.scss';
 
@@ -11,28 +12,42 @@ class OverlayExample extends React.Component {
   constructor() {
     super();
 
-    this.state = { show: false };
+    this.state = {
+      show: false,
+      showCustomContent: false,
+    };
     this.handleTriggerOverlay = this.handleTriggerOverlay.bind(this);
     this.handleTriggerFullScreenOverlay = this.handleTriggerFullScreenOverlay.bind(this);
     this.handleOnRequestESC = this.handleOnRequestESC.bind(this);
   }
 
-  handleTriggerOverlay() {
-    this.setState({ show: true, relative: true });
+  handleTriggerOverlay(showCustomContent) {
+    this.setState({ show: true, relative: true, showCustomContent });
   }
 
-  handleTriggerFullScreenOverlay() {
-    this.setState({ show: true, relative: false });
+  handleTriggerFullScreenOverlay(showCustomContent) {
+    this.setState({ show: true, relative: false, showCustomContent });
   }
 
   handleOnRequestESC() {
     this.setState({ show: false });
   }
 
+  getCustomContent() {
+    return (
+      <div className={cx('overlay-context-wrapper')}>
+        <Text fontSize={14}>This overlay has custom content in it.</Text>
+        <Button text="Close" onClick={this.handleOnRequestESC} className={cx('button')} />
+      </div>
+    );
+  }
+
   addOverlay() {
     return (
-      <Overlay isOpen={this.state.show} isRelativeToContainer={this.state.relative} onRequestClose={this.handleOnRequestESC} zIndex="6000">
-        <p>Close by clicking the overlay or pressing the ESC key.</p>
+      <Overlay isOpen={this.state.show} isRelativeToContainer={this.state.relative} onRequestClose={!this.state.showCustomContent ? this.handleOnRequestESC : () => {}} zIndex="6000">
+        {this.state.showCustomContent
+          ? this.getCustomContent()
+          : <p>Close by clicking the overlay or pressing the ESC key.</p>}
       </Overlay>
     );
   }
@@ -40,8 +55,10 @@ class OverlayExample extends React.Component {
   render() {
     return (
       <OverlayContainer className={cx('overlay-container')} overlay={this.addOverlay()}>
-        <Button text="Trigger Container Overlay" onClick={this.handleTriggerOverlay} />
-        <Button text="Trigger FullScreen Overlay" onClick={this.handleTriggerFullScreenOverlay} />
+        <Button text="Trigger Container Overlay" onClick={() => this.handleTriggerOverlay(false)} />
+        <Button text="Trigger FullScreen Overlay" onClick={() => this.handleTriggerFullScreenOverlay(false)} />
+        <Button text="Trigger Container Overlay with Content" onClick={() => this.handleTriggerOverlay(true)} />
+        <Button text="Trigger FullScreen Overlay with Content" onClick={() => this.handleTriggerFullScreenOverlay(true)} />
       </OverlayContainer>
     );
   }
