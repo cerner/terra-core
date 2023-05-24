43f300d38c64046028b9ede388327244c9d0da2d
AH106586Harika <AH106586Harika@cerner.com>
2023-05-24 18:29:59 +0530

    added aria-haspopup

--- packages/terra-dropdown-button/src/DropdownButton.jsx
+++ packages/terra-dropdown-button/src/DropdownButton.jsx
@@ -207,6 +207,7 @@ class DropdownButton extends React.Component {
           tabIndex={isDisabled ? '-1' : undefined}
           aria-disabled={isDisabled}
           aria-expanded={isOpen}
+          aria-haspopup="menu"
           ref={this.setButtonNode}
           aria-label={selectText ? `${selectText}, ${selectedLabel}` : ''}
           onBlur={this.handleBlur}
