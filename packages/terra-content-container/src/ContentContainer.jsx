b6fd91ccd6bcae4531d188ba54c14b6b92aa4652
Akshar <ac086323@cerner.net>
2023-05-24 16:31:21 +0530

    Changing name of prop

--- packages/terra-content-container/src/ContentContainer.jsx
+++ packages/terra-content-container/src/ContentContainer.jsx
@@ -31,7 +31,7 @@ const propTypes = {
    * ![IMPORTANT](https://badgen.net/badge/UX/Accessibility/blue)
    * Sets focus on content when set to `true`. Focus on content helps in scrolling  within container when there is no interactive element to focus within container.
    */
-  setFocusOnContent: PropTypes.bool,
+  setFocusOnContainer: PropTypes.bool,
 };
 
 const defaultProps = {
@@ -40,7 +40,7 @@ const defaultProps = {
   children: undefined,
   fill: false,
   scrollRefCallback: undefined,
-  setFocusOnContent: false,
+  setFocusOnContainer: false,
 };
 
 const ContentContainer = ({
@@ -49,7 +49,7 @@ const ContentContainer = ({
   children,
   fill,
   scrollRefCallback,
-  setFocusOnContent,
+  setFocusOnContainer,
   ...customProps
 }) => {
   const contentLayoutClassNames = cx([
@@ -61,7 +61,7 @@ const ContentContainer = ({
     <div {...customProps} className={contentLayoutClassNames}>
       {header && <div className={cx('header')}>{header}</div>}
       <div className={cx('main')}>
-        <Scroll className={cx('normalizer')} refCallback={scrollRefCallback} tabIndex={setFocusOnContent ? '0' : '-1'}>
+        <Scroll className={cx('normalizer')} refCallback={scrollRefCallback} tabIndex={setFocusOnContainer ? '0' : '-1'}>
           {children}
         </Scroll>
       </div>
