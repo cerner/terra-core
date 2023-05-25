f237bd25aa646d5731c085107caaffa762c6dc9c
Akshar <ac086323@cerner.net>
2023-05-25 14:24:53 +0530

    Adding wdio tests for scrollable content

--- packages/terra-content-container/tests/wdio/content-container-spec.js
+++ packages/terra-content-container/tests/wdio/content-container-spec.js
@@ -10,4 +10,12 @@ Terra.describeViewports('Content Container', ['medium'], () => {
 
     Terra.validates.element('content container fill');
   });
+
+  it('displays Content Container with fill', () => {
+    browser.url('/raw/tests/cerner-terra-core-docs/content-container/scrollable-content-container');
+    Terra.validates.element('before scroll');
+    browser.keys(['Tab', 'ArrowDown','ArrowDown']);
+    Terra.validates.element('after scroll');
+
+  });
 });
