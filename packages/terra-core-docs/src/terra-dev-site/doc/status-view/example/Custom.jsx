c3badd383aca1def7fd5e8d6e09d2a3d3d5f2983
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2023-05-31 16:27:45 +0530

    Terra Status View - Update Doc example

--- packages/terra-core-docs/src/terra-dev-site/doc/status-view/example/Custom.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/status-view/example/Custom.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import IconDiamond from 'terra-icon/lib/icon/IconDiamond';
+import IconDueSoon from 'terra-icon/lib/icon/IconDueSoon';
 import StatusView from 'terra-status-view';
 
 const StatusViewButtons = [
   {
-    text: 'Action 1',
+    'aria-label': 'Submit your Timesheet',
+    id: 'Submit',
     key: 1,
+    text: 'Submit',
+    variant: 'emphasis',
   }, {
-    text: 'Action 2',
+    'aria-label': 'Cancel your Timesheet Submission',
+    id: 'Cancel',
     key: 2,
+    text: 'Cancel',
   },
 ];
 
 const Custom = () => (
-  <StatusView
-    message="Custom Message"
-    customGlyph={<IconDiamond />}
-    title="Custom Title"
-    buttonAttrs={StatusViewButtons}
-  />
+  <div>
+    <StatusView
+      message="Once submitted cannot be revised"
+      customGlyph={<IconDueSoon />}
+      title="Time Sheet Due Soon"
+      buttonAttrs={StatusViewButtons}
+    />
+    <p>
+      Note: We should provide guidance for the action button labels. Those labels should give proper message or title in some way.
+      For Accessibility best practice one can use the ARIA attributes like aria-label to provide the context.
+    </p>
+  </div>
 );
 
 export default Custom;
