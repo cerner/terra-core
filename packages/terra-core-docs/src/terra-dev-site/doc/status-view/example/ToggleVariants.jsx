e6f7921bb71154428a82ab3b9a6def380296b5fe
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2023-06-19 15:47:51 +0530

    Update: Adding apply button on change variant example

--- packages/terra-core-docs/src/terra-dev-site/doc/status-view/example/ToggleVariants.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/status-view/example/ToggleVariants.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
 import StatusView from 'terra-status-view';
 import SingleSelectField from 'terra-form-select/lib/SingleSelectField';
+import Button from 'terra-button';
 
 const ToggleVariants = () => {
   const [variant, setVariant] = useState('error');
+  const [selectValue, setSelectValue] = useState('error');
 
-  const handleOnSelect = (value) => {
-    setVariant(value);
-  };
+  const handleOnSelect = (value) => setSelectValue(value);
+
+  const handleValue = () => setVariant(selectValue);
 
   return (
     <div>
       <StatusView variant={variant} />
-      <SingleSelectField label="Change Variant" onChange={handleOnSelect} value={variant} maxWidth="300px" selectId="change-variant-field" placeholder="Select a Variant">
+      <SingleSelectField label="Change Variant" onChange={handleOnSelect} value={selectValue} maxWidth="300px" selectId="change-variant-field" placeholder="Select a Variant">
         <SingleSelectField.Option value="error" display="Error" />
         <SingleSelectField.Option value="no-data" display="No Results" />
         <SingleSelectField.Option value="no-matching-results" display="No Matching Result" />
         <SingleSelectField.Option value="not-authorized" display="Not Authorized" />
       </SingleSelectField>
+      <Button text="Apply" variant="emphasis" onClick={handleValue} />
       <p>
         Select a status view variant from the dropdown above.
       </p>
