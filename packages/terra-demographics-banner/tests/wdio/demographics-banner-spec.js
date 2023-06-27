d5ebfe38de8ef06434e71c59bd80c3094205847e
Steven Mason <76790350+smason0@users.noreply.github.com>
2023-06-27 11:09:10 -0500

    [terra-demographics-banner] Fix improper HTML, console warnings, and add missing translations (#3828)

--- packages/terra-demographics-banner/tests/wdio/demographics-banner-spec.js
+++ packages/terra-demographics-banner/tests/wdio/demographics-banner-spec.js
@@ -2,36 +2,36 @@ Terra.describeViewports('Demographics Banner', ['tiny', 'large'], () => {
   it('displays blank Demographics Banner', () => {
     browser.url('/raw/tests/cerner-terra-core-docs/demographics-banner/blank-demographics-banner');
 
-    Terra.validates.element('blank banner', { rules: { 'definition-list': { enabled: false }, dlitem: { enabled: false } } });
+    Terra.validates.element('blank banner');
   });
 
   it('displays deceased Demographics Banner', () => {
     browser.url('/raw/tests/cerner-terra-core-docs/demographics-banner/deceased-demographics-banner');
 
-    Terra.validates.element('deceased banner', { rules: { 'definition-list': { enabled: false }, dlitem: { enabled: false } } });
+    Terra.validates.element('deceased banner');
   });
 
   it('displays deceased Demographics Banner with no label', () => {
     browser.url('/raw/tests/cerner-terra-core-docs/demographics-banner/deceased-demographics-banner-no-label');
 
-    Terra.validates.element('deceased - no label', { rules: { 'definition-list': { enabled: false }, dlitem: { enabled: false } } });
+    Terra.validates.element('deceased - no label');
   });
 
   it('displays gestational age Demographics Banner with no label', () => {
     browser.url('/raw/tests/cerner-terra-core-docs/demographics-banner/gestational-age-demographics-banner-no-label');
 
-    Terra.validates.element('gestational age - no label', { rules: { 'definition-list': { enabled: false }, dlitem: { enabled: false } } });
+    Terra.validates.element('gestational age - no label');
   });
 
   it('displays post menstrual Demographics Banner with no label', () => {
     browser.url('/raw/tests/cerner-terra-core-docs/demographics-banner/post-menstrual-age-demographics-banner-no-label');
 
-    Terra.validates.element('post menstrual - no label', { rules: { 'definition-list': { enabled: false }, dlitem: { enabled: false } } });
+    Terra.validates.element('post menstrual - no label');
   });
 
   it('displays populated Demographics Banner', () => {
     browser.url('/raw/tests/cerner-terra-core-docs/demographics-banner/populated-demographics-banner');
 
-    Terra.validates.element('populated banner', { rules: { 'definition-list': { enabled: false }, dlitem: { enabled: false } } });
+    Terra.validates.element('populated banner');
   });
 });
