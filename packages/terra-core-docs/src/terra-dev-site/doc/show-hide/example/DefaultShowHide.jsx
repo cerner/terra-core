1be999dc9aa5a5a84d966d04475bfd5fd3eed9a0
Alla Doroshkevych <alla.doroshkevych@oracle.com>
2023-07-20 13:16:49 -0400

    bottom divided test with JAWS

--- packages/terra-core-docs/src/terra-dev-site/doc/show-hide/example/DefaultShowHide.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/show-hide/example/DefaultShowHide.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ShowHide from 'terra-show-hide';
 
 const previewText = 'Patients are requesting greater affordability and efficiency in healthcare. With procedures performed in an ambulatory surgery center costing up to 60% less compared to a hospital outpatient department (1), the demand for these facilities is increasing. ';
-const text = 'In fact, the U.S. ambulatory surgery center market is expected to see a 6.9 % compound annual growth rate, reaching $33 billion by 2028. (2) Cerner understands the urgency to grow in the ambulatory surgery center market while continuing to deliver excellent care. Healthcare IT products can help improve clinician efficiency and patient outcomes, as well as enhance communication and data exchange between ambulatory surgery center providers and patients.';
+const text = '(2) Cerner understands the urgency to grow in the ambulatory surgery center market while continuing to deliver excellent care. Healthcare IT products can help improve clinician efficiency and patient outcomes, as well as enhance communication and data exchange between ambulatory surgery center providers and patients.';
+const focusText = 'In fact, the U.S. ambulatory surgery center market is expected to see a 6.9 % compound annual growth rate, reaching $33 billion by 2028. ';
 
 class DefaultShowHide extends React.Component {
   constructor(props) {
@@ -25,7 +26,9 @@ class DefaultShowHide extends React.Component {
         <p>
           { previewText }
           <span className="show-hide-text-divider" />
-          <span ref={this.focusRef}>{ text }</span>
+          <span ref={this.focusRef}>{ focusText }</span>
+          <span className="show-hide-text-divider" />
+          <span>{ text }</span>
         </p>
       </ShowHide>
     );
