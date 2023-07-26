1835ec8596a5559988dd6faaa8bbd3466a6706ed
Alla Doroshkevych <alla.doroshkevych@oracle.com>
2023-07-25 20:36:23 -0400

    check if problem is specific to <li> element

--- packages/terra-show-hide/src/ShowHide.jsx
+++ packages/terra-show-hide/src/ShowHide.jsx
@@ -84,7 +84,7 @@ const ShowHide = (props) => {
     if (focusRef?.current) {
       focusRef?.current.focus();
     } else if (contentRef?.current) {
-      setContainerIsActive(false);
+      setContainerIsActive(true);
     }
   }, [isOpen, focusRef, contentRef]);
 
