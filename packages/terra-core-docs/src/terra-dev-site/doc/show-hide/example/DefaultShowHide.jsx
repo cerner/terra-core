85736f723312f6f79c5415f424643d56159d7373
Alla Doroshkevych <alla.doroshkevych@oracle.com>
2023-07-28 11:10:36 -0400

    text edits

--- packages/terra-core-docs/src/terra-dev-site/doc/show-hide/example/DefaultShowHide.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/show-hide/example/DefaultShowHide.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ShowHide, { Focuser } from 'terra-show-hide';
 
 const prefix = 'Patients are requesting greater affordability and efficiency in healthcare. With procedures performed in an ambulatory surgery center costing up to 60% less compared to a hospital outpatient department (1), the demand for these facilities is increasing.';
-const focusableText = 'In fact, the U.S. ambulatory surgery center market is expected to see a 6.9 % compound annual growth rate, reaching $33 billion by 2028. (2) Cerner understands the urgency to grow in the ambulatory surgery center market while continuing to deliver excellent care. Healthcare IT products can help improve clinician efficiency and patient outcomes, as well as enhance communication and data exchange between ambulatory surgery center providers and patients. Read';
+const focusableText = 'In fact, the U.S. ambulatory surgery center market is expected to see a 6.9 % compound annual growth rate, reaching $33 billion by 2028. (2) Cerner understands the urgency to grow in the ambulatory surgery center market while continuing to deliver excellent care. Healthcare IT products can help improve clinician efficiency and patient outcomes, as well as enhance communication and data exchange between ambulatory surgery center providers and patients.';
 
 class DefaultShowHide extends React.Component {
   constructor(props) {
