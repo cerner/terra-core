c2cb7c7e7a827070aa153d28aa62331c20d9e4f5
AH106586Harika <AH106586Harika@users.noreply.github.com>
2023-08-01 04:36:24 +0000

    deploy: 8dc864ffcdbbc4a49123934ea4b8c294ad0f35cf

---
+++ pull/3860/45503-7d8cb240b8e522f772a9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkterra_core=self.webpackChunkterra_core||[]).push([[45503],{45503:function(e,r,t){Object.defineProperty(r,"__esModule",{value:!0}),r.default=void 0;var a,u=(a=t(67294))&&a.__esModule?a:{default:a},o=t(89755);r.default=function(){return u.default.createElement(o.Generic,{variant:"provider",id:"provider",alt:"provider",color:"seven"})}}}]);
