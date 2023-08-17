cdb87a328f8c744883495f88db8acd5eafbf3e8b
AH106586Harika <AH106586Harika@cerner.com>
2023-08-17 11:55:22 +0530

    reduced content

--- packages/terra-core-docs/src/terra-dev-site/doc/scroll/example/ScrollVertical.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/scroll/example/ScrollVertical.jsx
@@ -15,10 +15,7 @@ export default () => (
           individuals seeking medical care. Patient details need to be regularly updated to ensure accuracy and relevancy. This may involve periodic
           verification of personal information, updating medical history, and making necessary changes to insurance or contact details. Hospitals may
           request patients to review and confirm their information during subsequent visits or at designated intervals. Accurate and comprehensive patient
-          details play a vital role in providing effective healthcare. They serve as a foundation for medical assessments, treatment planning, and continuity
-          of care, enabling healthcare providers to deliver personalised and efficient services to patients.Begin filling out the form by entering your personal
-          information. This usually includes your full name, contact details (such as phone number and email address), and residential address. It is crucial to
-          provide accurate contact information as it will be used for communication and verification purposes.
+          details play a vital role in providing effective healthcare.
         </p>
       </Scroll>
     </div>
