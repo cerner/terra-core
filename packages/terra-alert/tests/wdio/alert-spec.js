fcfbb3334033048c261b10bc89941bc6578a132d
Alla Doroshkevych <alla.doroshkevych@oracle.com>
2023-08-21 16:26:42 -0400

    test to check the button is click-able

--- packages/terra-alert/tests/wdio/alert-spec.js
+++ packages/terra-alert/tests/wdio/alert-spec.js
@@ -75,7 +75,17 @@ Terra.describeViewports('Alert', ['tiny', 'large'], () => {
     it('focuses on show more button', () => {
       browser.url('/raw/tests/cerner-terra-core-docs/alert/long-text-alert');
       browser.keys(['Tab']);
+      expect($('button.button').isFocused).toBeTruthy();
       Terra.validates.element('show more button focused');
+    });
+
+    it('open show hide upon button click', () => {
+      $('button.button').click();
+      Terra.validates.element('disclosed show hide');
+    });
+
+    it('close show hide', () => {
+      $('button.button').click();
     });
 
     it('wraps naturally at high magnification', () => {
