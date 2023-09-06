4e48ae62a2a1b38797b5137fac39f399663315a2
SM051274 <sm051274@cerner.net>
2023-09-07 01:37:14 +0530

    typo

--- packages/terra-core-docs/src/terra-dev-site/doc/form-radio/example/radio/HiddenLabelRadioExample.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/form-radio/example/radio/HiddenLabelRadioExample.jsx
@@ -4,9 +4,10 @@ import Radio from 'terra-form-radio';
 const hiddenLabelRadioExample = () => (
   <div>
     <p>
-      <b>Note:</b>
+      <b>Note: </b>
       Radio button should always have label to ensures that people with visual or cognitive disabilities will recognize radio buttons and understand what information to provide to fill in the fields.
-      For example: When we have radio feilds for selection of account types, authors may feel that just providing programmatically determined names of the fields will be sufficient to identify them. However, even if all the fields have programmatically determined names, a text label must also identify the set of fields as a account type.
+      <br />
+      For example: When we have radio fields for selection of account types, authors may feel that just providing programmatically determined names of the fields will be sufficient to identify them. However, even if all the fields have programmatically determined names, a text label must also identify the set of fields as a account type.
     </p>
     <Radio id="hidden-radio" labelText="can you see me?" isLabelHidden />
   </div>
