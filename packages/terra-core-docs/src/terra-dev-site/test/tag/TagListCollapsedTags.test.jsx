bce8b90dcf9aaf6a2ae35eb4689f7079ca525052
swetasingh9714 <swetasingh9714@gmail.com>
2023-09-15 16:26:45 +0530

    Lint resolve

---
+++ packages/terra-core-docs/src/terra-dev-site/test/tag/TagListCollapsedTags.test.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-alert */
+import React from 'react';
+import IconPerson from 'terra-icon/lib/icon/IconPerson';
+import IconBookmark from 'terra-icon/lib/icon/IconTile';
+import Tag from 'terra-tag';
+import TagList from 'terra-tag/src/TagList';
+import ThemeContext from 'terra-theme-context';
+import classNamesBind from 'classnames/bind';
+import Toolbar from 'terra-toolbar';
+import styles from '../../doc/tag/example/FilterTagsCommon.module.scss';
+
+const cx = classNamesBind.bind(styles);
+
+const text = 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores ';
+
+const TagListCollapsed = () => {
+  const theme = React.useContext(ThemeContext);
+
+  return (
+    <>
+      <div className={cx(['terra-filter-tags-doc-example-applied-filters', theme.className])}>
+        <Toolbar className={cx('terra-filter-tags-doc-example-applied-filters-toolbar')}>
+          <div className={cx('toolbar-flex-container')}>
+            <div className={cx('toolbar-flex-item-start')} />
+            <div className={cx('toolbar-flex-item-fill')}>
+              <TagList isCollapsible ariaLabel="Collapsible Tag List Example with collapsed list items">
+                <Tag icon={<IconPerson />} text="Asthma" onClick={() => window.alert('Patient is diagnosed with asthma')} id="asthma" />
+                <Tag icon={<IconBookmark />} text={text} id="longTextIcon" onClick={() => window.alert('Tag has been clicked!')} />
+                <Tag id="bronchitis" text="Bronchitis" onClick={() => window.alert('Patient is diagnosed with bronchitis')} />
+                <Tag id="fibro" text="Fibro" onClick={() => window.alert('Patient is diagnosed with Fibro')} />
+                <Tag icon={<IconPerson />} text="Dust Allergy" onClick={() => window.alert('Patient is diagnosed with dust allergy')} id="allergy" />
+                <Tag icon={<IconPerson />} text="Diabetes" onClick={() => window.alert('Patient is diagnosed with Diabetes')} id="diabetes" />
+                <Tag text="Arthritis" onClick={() => window.alert('Patient is diagnosed with Arthritis')} id="arthritis" />
+                <Tag text="Hypertension" onClick={() => window.alert('Patient is diagnosed with Hypertension (High Blood Pressure)')} id="hypertension" />
+                <Tag icon={<IconPerson />} text="Multiple Sclerosis (MS)" onClick={() => window.alert('Patient is diagnosed with Multiple Sclerosis (MS)')} id="sclerosis" />
+                <Tag text="Psoriasis" onClick={() => window.alert('Patient is diagnosed with Psoriasis')} id="psoriasis" />
+                <Tag icon={<IconBookmark />} text="Epilepsy" onClick={() => window.alert('Patient is diagnosed with Epilepsy')} id="epilepsy" />
+                <Tag icon={<IconPerson />} text="Ulcerative Colitis" onClick={() => window.alert('Patient is diagnosed with Ulcerative Colitis')} id="ulcer" />
+              </TagList>
+            </div>
+          </div>
+        </Toolbar>
+      </div>
+    </>
+  );
+};
+
+export default TagListCollapsed;
--- packages/terra-tag/tests/jest/TagList.test.jsx
+++ packages/terra-tag/tests/jest/TagList.test.jsx
@@ -4,12 +4,13 @@ import { shallowWithIntl } from 'terra-enzyme-intl';
 import TagList from '../../src/TagList';
 
 describe('TagList Component', () => {
-
   it('should render tagList with 2 items"', () => {
-    const wrapper = shallowWithIntl(<TagList ariaLabel="Tag List">
+    const wrapper = shallowWithIntl(
+      <TagList ariaLabel="Tag List">
         <div id="tag1">Tag 1</div>
         <div id="tag2">Tag 2</div>
-    </TagList>);
+      </TagList>,
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
