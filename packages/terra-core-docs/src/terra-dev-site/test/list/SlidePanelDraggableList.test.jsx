235ec3e8b6d6f9225d30702b99f86740aa816307
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2023-09-21 15:46:10 +0530

    Update SlidePanelDraggableList.test.jsx

--- packages/terra-core-docs/src/terra-dev-site/test/list/SlidePanelDraggableList.test.jsx
+++ packages/terra-core-docs/src/terra-dev-site/test/list/SlidePanelDraggableList.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import List, { Item } from 'terra-list/lib/index';
 import SlidePanel from 'terra-slide-panel';
 
-const DraggableListItemTest = () => (
+const MainContent = () => (
   <List role="listbox" aria-label="test-label" dividerStyle="standard" isDraggable>
     <Item
       key="selected"
@@ -33,10 +33,41 @@ const DraggableListItemTest = () => (
   </List>
 );
 
+const PanelContent = () => (
+  <List role="listbox" aria-label="test-label" dividerStyle="standard" isDraggable>
+    <Item
+      key="selected-1"
+      isSelected
+      isSelectable
+      id="selectable-item-1"
+    >
+      <p>List item 1</p>
+    </Item>
+    <Item
+      key="selectable-2"
+      isSelectable
+    >
+      <p>List item 2</p>
+    </Item>
+    <Item
+      key="selectable-3"
+      isSelectable
+    >
+      <p>List item 3</p>
+    </Item>
+    <Item
+      key="selectabl-e4"
+      isSelectable
+    >
+      <p>List item 4</p>
+    </Item>
+  </List>
+);
+
 const SlidePanelDraggableListTest = () => (
   <SlidePanel
-    mainContent={<DraggableListItemTest />}
-    panelContent={<DraggableListItemTest />}
+    mainContent={<MainContent />}
+    panelContent={<PanelContent />}
     panelPosition="start"
     isOpen
   />
