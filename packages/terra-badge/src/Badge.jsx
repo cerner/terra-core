73b761f2e07354db8a4e82170466fe926d29eacc
Akshar <ac086323@cerner.net>
2023-09-22 16:28:55 +0530

    Update the example and removed aria-hidden

--- packages/terra-badge/src/Badge.jsx
+++ packages/terra-badge/src/Badge.jsx
@@ -90,7 +90,7 @@ const Badge = ({
     customProps.className,
   );
 
-  const textContent = text ? <span className={styles.text} aria-hidden="true">{text}</span> : null;
+  const textContent = text ? <span className={styles.text}>{text}</span> : null;
   const visuallyHiddenTextContent = visuallyHiddenText ? <VisuallyHiddenText text={visuallyHiddenText} /> : null;
   const content = isReversed ? [visuallyHiddenTextContent, textContent, icon, customProps.children] : [icon, visuallyHiddenTextContent, textContent, customProps.children];
   return React.createElement('span', { ...customProps, className: badgeClassNames }, ...content);
--- packages/terra-core-docs/src/terra-dev-site/doc/badge/example/BadgeInTable.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/badge/example/BadgeInTable.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Badge from 'terra-badge';
+import IconHelpInverse from 'terra-icon/lib/icon/IconHelpInverse';
 import Table, {
   Header,
   HeaderCell,
@@ -7,6 +8,8 @@ import Table, {
   Cell,
   Row,
 } from 'terra-html-table';
+
+const Icon = <IconHelpInverse/>
 
 const BadgeInTable = () => (
   <div>
@@ -21,13 +24,24 @@ const BadgeInTable = () => (
           <Cell key="NAME">John Smith</Cell>
           <Cell key="ADDRESS">123 Adams Drive</Cell>
           <Cell key="BADGE">
-            <Badge intent="primary" text="Active" />
+            <Badge intent="positive" text="Active" />
           </Cell>
         </Row>
         <Row key="PERSON_1">
           <Cell key="NAME">Jane Smith</Cell>
           <Cell key="ADDRESS">321 Drive Street</Cell>
-          <Cell key="BADGE"><Badge intent="negative" text="Inactive" /></Cell>
+          <Cell key="BADGE">
+            <Badge intent="negative" size="large" text="Inactive" />
+            <Badge intent="info" icon={Icon} text="Verification Pending" visuallyHiddenText="Please check back in 2-4 business days"/>
+          </Cell>
+        </Row>
+        <Row key="PERSON_2">
+          <Cell key="NAME">Jason Tyler</Cell>
+          <Cell key="ADDRESS">213 Scuba Drive</Cell>
+          <Cell key="BADGE">
+            <Badge intent="primary" size="large" text="Active" />
+            <Badge intent="warning" text="Order Pending"/>
+          </Cell>
         </Row>
       </Body>
     </Table>
