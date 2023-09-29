9e33b295a0302fb465969c8d007788a5d2d97db4
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2023-09-29 14:10:20 +0530

    Update: remove expose of rolluptag and tagutils

--- packages/terra-tag/src/index.js
+++ packages/terra-tag/src/index.js
@@ -1,7 +1,5 @@
 import TagList from './TagList';
-import RollupTag from './RollupTag';
-import TagsUtils from './TagsUtils';
 import Tag from './Tag';
 
 export default Tag;
-export { TagList, RollupTag, TagsUtils };
+export { TagList };
