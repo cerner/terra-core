d5b6af361058f12295eccebc08851ec430c29288
SM051274 <sm051274@cerner.net>
2023-10-14 22:40:15 +0530

    revert test file change

--- packages/terra-core-docs/src/terra-dev-site/test/icon/IconDefault.test.jsx
+++ packages/terra-core-docs/src/terra-dev-site/test/icon/IconDefault.test.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { IconAdd, IconBase } from 'terra-icon';
+import { IconAdd } from 'terra-icon';
 
 const IconDefault = () => (
   <div>
     <h3>Default Icon</h3>
     <IconAdd id="icon-default" />
-    <IconBase>
-      <path d="M48 21H27V0h-6v21H0v6h21v21h6V27h21z" />
-    </IconBase>
   </div>
 );
 
