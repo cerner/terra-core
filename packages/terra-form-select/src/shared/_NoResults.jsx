61c674d0deda5d83dd09a18dc0d8497a1c5bfc65
gopi <gt106551@cerner.com>
2023-10-31 10:53:51 +0530

    remove unwanted code

--- packages/terra-form-select/src/shared/_NoResults.jsx
+++ packages/terra-form-select/src/shared/_NoResults.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNamesBind from 'classnames/bind';
-import ThemeContext from 'terra-theme-context';
 import { FormattedMessage } from 'react-intl';
-import styles from './_NoResults.module.scss';
-
-const cx = classNamesBind.bind(styles);
 
 const propTypes = {
   /**
@@ -19,9 +14,8 @@ const propTypes = {
 };
 
 const NoResults = ({ noResultContent, value }) => {
-  const theme = React.useContext(ThemeContext);
   return (
-    <div role="option" className={cx('no-results', theme.className)} aria-selected="false">
+    <div role="option" aria-selected="false">
       {noResultContent || <FormattedMessage id="Terra.form.select.noResults" values={{ text: value }} />}
     </div>
   );
