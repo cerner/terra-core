46d648563c37dae39a6696e03a203295bbbe7145
sugan2416 <sugan2416@users.noreply.github.com>
2023-11-06 05:17:50 +0000

    deploy: 39eb9b28247fd30f48f5c7906a574a17dabaa352

--- pull/3954/51160-4b6b4fbff05b4460d880.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkterra_core=self.webpackChunkterra_core||[]).push([[51160],{51160:function(e,t,u){Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var n=a(u(67294)),r=a(u(7812));function a(e){return e&&e.__esModule?e:{default:e}}t.default=function(){return n.default.createElement(r.default,{text:"Closed Section Header",onClick:function(){}})}}}]);
