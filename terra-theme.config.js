4eb40a69141e4d43669c5e594359c481d32c5ff3
Jeremy Fuksa <hello@orangefla.me>
2023-11-09 15:49:16 -0600

    Base colors and fonts set

--- terra-theme.config.js
+++ terra-theme.config.js
@@ -1,5 +1,5 @@
 const themeConfig = {
-  scoped: ['clinical-lowlight-theme', 'orion-fusion-theme'],
+  scoped: ['clinical-lowlight-theme', 'orion-fusion-theme','redwood-theme'],
 };
 
 module.exports = themeConfig;
