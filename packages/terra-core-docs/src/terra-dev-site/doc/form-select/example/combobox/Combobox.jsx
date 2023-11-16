e97afd638812cebf857163093208202301646683
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2023-11-16 18:11:09 +0530

    update: css style for label example

--- packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/combobox/Combobox.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/combobox/Combobox.jsx
@@ -6,16 +6,18 @@ import styles from '../FormSelectDocCommon.module.scss';
 const cx = classNames.bind(styles);
 
 const ComboboxExample = () => (
-  <>
+  <React.Fragment>
     <label htmlFor="color-field-1"><strong>Colors</strong></label>
-    <Combobox placeholder="Select a color" className={cx('form-select')} inputId="color-field-1">
-      <Combobox.Option value="blue" display="Blue" />
-      <Combobox.Option value="green" display="Green" />
-      <Combobox.Option value="purple" display="Purple" />
-      <Combobox.Option value="red" display="Red" />
-      <Combobox.Option value="violet" display="Violet" />
-    </Combobox>
-  </>
+    <div className={cx('combobox-label-example')}>
+      <Combobox placeholder="Select a color" className={cx('form-select')} inputId="color-field-1">
+        <Combobox.Option value="blue" display="Blue" />
+        <Combobox.Option value="green" display="Green" />
+        <Combobox.Option value="purple" display="Purple" />
+        <Combobox.Option value="red" display="Red" />
+        <Combobox.Option value="violet" display="Violet" />
+      </Combobox>
+    </div>
+  </React.Fragment>
 );
 
 export default ComboboxExample;
