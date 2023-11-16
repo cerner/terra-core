35138446cc476aad1fb5b0a96587e8669826e920
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2023-11-16 18:52:56 +0530

    Update: label with form field

--- packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/combobox/Invalid.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/combobox/Invalid.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Combobox from 'terra-form-select/lib/Combobox';
+import Field from 'terra-form-field';
 import classNames from 'classnames/bind';
 import styles from '../FormSelectDocCommon.module.scss';
 
 const cx = classNames.bind(styles);
 
-const InvalidExample = () => (
-  <Combobox placeholder="Select a color" isInvalid className={cx('form-select')}>
-    <Combobox.Option value="blue" display="Blue" />
-    <Combobox.Option value="green" display="Green" />
-    <Combobox.Option value="purple" display="Purple" />
-    <Combobox.Option value="red" display="Red" />
-    <Combobox.Option value="violet" display="Violet" />
-  </Combobox>
-);
+const InvalidExample = () => {
+  const [isInvalid, setIsInvalid] = useState(true);
+
+  const handleOnChange = (val) => {
+    if (val !== '') {
+      setIsInvalid(false);
+    } else {
+      setIsInvalid(true);
+    }
+  };
+
+  return (
+    <Field
+      label="Colors"
+      error="Please select a color"
+      htmlFor="input_id"
+      isInvalid={isInvalid}
+    >
+      <Combobox placeholder="Select a color" inputId="input_id" allowClear onSelect={handleOnChange} isInvalid={isInvalid} className={cx('form-select')}>
+        <Combobox.Option value="blue" display="Blue" />
+        <Combobox.Option value="green" display="Green" />
+        <Combobox.Option value="purple" display="Purple" />
+        <Combobox.Option value="red" display="Red" />
+        <Combobox.Option value="violet" display="Violet" />
+      </Combobox>
+    </Field>
+  );
+};
 
 export default InvalidExample;
