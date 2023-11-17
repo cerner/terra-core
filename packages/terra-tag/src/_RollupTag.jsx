62494fe04a0e3bf5a440e3c430496ac2a3eaf36b
gopi <gt106551@cerner.com>
2023-11-17 19:14:57 +0530

    Terra-Tag SR issue fixed

--- packages/terra-tag/src/_RollupTag.jsx
+++ packages/terra-tag/src/_RollupTag.jsx
@@ -74,7 +74,7 @@ const RollUpTag = (props) => {
       onBlur={handleOnBlur}
       refCallback={(ref) => { rollUpTagRef.current = ref; }}
       isCompact
-      aria-live={(isCollapsed) ? 'polite' : 'off'}
+      aria-live={(navigator.userAgent.indexOf('Edg') === -1 && isCollapsed) ? 'polite' : 'off'}
       aria-label={rollUpHint}
       aria-describedby={rollUpHint}
       aria-atomic="true"
