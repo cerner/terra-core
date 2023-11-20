59a91a7078e476bb1ef658770295022dc6d21a11
KV106606Viswanath <Kadali.Viswanath@cerner.com>
2023-11-20 17:42:03 +0530

    Added error message for invalid example

--- packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/search/Invalid.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/search/Invalid.jsx
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchSelect from 'terra-form-select/lib/SearchSelect';
 import classNames from 'classnames/bind';
+import VisuallyHiddenText from 'terra-visually-hidden-text';
 import styles from '../FormSelectDocCommon.module.scss';
 
 const cx = classNames.bind(styles);
 
-const InvalidExample = () => (
-  <SearchSelect placeholder="Select a color" isInvalid className={cx('form-select')}>
-    <SearchSelect.Option value="blue" display="Blue" />
-    <SearchSelect.Option value="green" display="Green" />
-    <SearchSelect.Option value="purple" display="Purple" />
-    <SearchSelect.Option value="red" display="Red" />
-    <SearchSelect.Option value="violet" display="Violet" />
-  </SearchSelect>
-);
+const InvalidExample = () => {
+  const [isInvalid, setIsInvalid] = useState(true);
+  const [selectedValue, setSelectedValue] = useState('');
+  const [invalidText, setInvalidText] = useState(null);
+
+  const handleSelectChange = (value) => {
+    setIsInvalid(false);
+    setSelectedValue(value);
+  };
+
+  const handleFocus = () => {
+    setInvalidText('Please select a color');
+  };
+
+  return (
+    <>
+      <SearchSelect
+        placeholder="Select a color"
+        isInvalid={isInvalid}
+        className={cx('form-select')}
+        onSelect={handleSelectChange}
+        onFocus={handleFocus}
+      >
+        <SearchSelect.Option value="Blue" display="Blue" />
+        <SearchSelect.Option value="Green" display="Green" />
+        <SearchSelect.Option value="Purple" display="Purple" />
+        <SearchSelect.Option value="Red" display="Red" />
+        <SearchSelect.Option value="Violet" display="Violet" />
+      </SearchSelect>
+      { isInvalid && (
+        // eslint-disable-next-line react/forbid-dom-props
+        <p className={cx('error-text')}>
+          {selectedValue}
+          {' '}
+          Please select a color
+        </p>
+      )}
+      <VisuallyHiddenText
+        aria-atomic="true"
+        aria-live="assertive"
+        text={invalidText}
+      />
+      <p>Required: please select a color from above list</p>
+    </>
+  );
+};
 
 export default InvalidExample;
--- packages/terra-form-select/src/SearchSelect.jsx
+++ packages/terra-form-select/src/SearchSelect.jsx
@@ -126,6 +126,7 @@ const defaultProps = {
   isTouchAccessible: false,
   value: undefined,
   inputId: undefined,
+  onFocus: undefined,
 };
 
 class SearchSelect extends React.Component {
