dd79c16add2eaa4b42480246c8390ec0e3b3ce8a
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2023-11-21 14:56:41 +0530

    Update: error text example

--- packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/combobox/Invalid.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/combobox/Invalid.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Combobox from 'terra-form-select/lib/Combobox';
 import classNames from 'classnames/bind';
+import VisuallyHiddenText from 'terra-visually-hidden-text';
 import styles from '../FormSelectDocCommon.module.scss';
 
 const cx = classNames.bind(styles);
 
-const InvalidExample = () => (
-  <Combobox placeholder="Select a color" ariaLabel="Select a color" isInvalid className={cx('form-select')}>
-    <Combobox.Option value="blue" display="Blue" />
-    <Combobox.Option value="green" display="Green" />
-    <Combobox.Option value="purple" display="Purple" />
-    <Combobox.Option value="red" display="Red" />
-    <Combobox.Option value="violet" display="Violet" />
-  </Combobox>
-);
+const InvalidExample = () => {
+  const [isInvalid, setIsInvalid] = useState(true);
+  const [selectedValue, setSelectedValue] = useState('');
+  const [invalidText, setInvalidText] = useState(null);
+
+  const handleSelectChange = (value) => {
+    setIsInvalid(false);
+    setSelectedValue(value);
+  };
+
+  const handleFocus = () => {
+    setInvalidText('Please select a color');
+  };
+
+  return (
+    <>
+      <Combobox placeholder="Select a color" ariaLabel="Select a color" isInvalid={isInvalid} onSelect={handleSelectChange} onFocus={handleFocus} className={cx('form-select')}>
+        <Combobox.Option value="blue" display="Blue" />
+        <Combobox.Option value="green" display="Green" />
+        <Combobox.Option value="purple" display="Purple" />
+        <Combobox.Option value="red" display="Red" />
+        <Combobox.Option value="violet" display="Violet" />
+      </Combobox>
+      {isInvalid && (
+        <p className={cx('error-text')}>
+          {selectedValue}
+          {' '}
+          Please select a color
+        </p>
+      )}
+      <VisuallyHiddenText
+        aria-atomic="true"
+        aria-live="assertive"
+        text={invalidText}
+      />
+      <p>Required: Please select a color from above list</p>
+    </>
+  );
+};
 export default InvalidExample;
--- packages/terra-form-select/src/combobox/Frame.jsx
+++ packages/terra-form-select/src/combobox/Frame.jsx
@@ -398,7 +398,7 @@ class Frame extends React.Component {
   getDisplay(ariaDescribedBy, id) {
     const { hasSearchChanged, searchValue } = this.state;
     const {
-      disabled, display, placeholder, required, inputId,
+      disabled, display, placeholder, required, inputId, isInvalid,
     } = this.props;
 
     const inputAttrs = {
@@ -414,6 +414,7 @@ class Frame extends React.Component {
       'aria-disabled': disabled,
       'aria-owns': this.state.isOpen ? id : undefined,
       'aria-expanded': !disabled && this.state.isOpen,
+      'aria-invalid': isInvalid,
       type: 'text',
       className: cx('search-input'),
       required,
