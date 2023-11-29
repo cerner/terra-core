1148e080a56738b1d278c6da4898a5e3ef942a50
Pravinkumar K S <pk106552@cerner.net>
2023-11-29 12:15:51 +0530

    Reverted aria-invalid changes

--- packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/combobox/Invalid.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/form-select/example/combobox/Invalid.jsx
@@ -1,68 +1,17 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import Combobox from 'terra-form-select/lib/Combobox';
 import classNames from 'classnames/bind';
-import VisuallyHiddenText from 'terra-visually-hidden-text';
 import styles from '../FormSelectDocCommon.module.scss';
 
 const cx = classNames.bind(styles);
 
-const InvalidExample = () => {
-  const [isInvalid, setIsInvalid] = useState(true);
-  const [selectedValue, setSelectedValue] = useState('');
-  const [isFocused, setisFocused] = useState(false);
-  const invalidText = useRef(null);
-
-  const handleSelectChange = (value) => {
-    setSelectedValue(value);
-    if (value == null || value === '') {
-      setIsInvalid(true);
-      invalidText.current = 'Please select a color';
-    } else {
-      setIsInvalid(false);
-      invalidText.current = '';
-    }
-  };
-
-  const handleFocus = () => {
-    if (!isFocused) {
-      setisFocused(true);
-      if (selectedValue === '' || selectedValue === null) {
-        invalidText.current = 'Please select a color';
-      } else {
-        invalidText.current = null;
-      }
-    }
-  };
-
-  const handleBlur = () => {
-    if (isFocused) {
-      setisFocused(false);
-      invalidText.current = null;
-    }
-  };
-
-  return (
-    <>
-      <Combobox placeholder="Select a color" isInvalid={isInvalid} onBlur={handleBlur} onFocus={handleFocus} allowClear onSelect={handleSelectChange} className={cx('form-select')}>
-        <Combobox.Option value="blue" display="Blue" />
-        <Combobox.Option value="green" display="Green" />
-        <Combobox.Option value="purple" display="Purple" />
-        <Combobox.Option value="red" display="Red" />
-        <Combobox.Option value="violet" display="Violet" />
-      </Combobox>
-      {isInvalid && (
-        <p className={cx('error-text')}>
-          Please select a color
-        </p>
-      )}
-      <VisuallyHiddenText
-        aria-atomic="true"
-        aria-relevant="additions text"
-        aria-live="polite"
-        text={invalidText.current}
-      />
-      <p>Required: Please select a color from above list</p>
-    </>
-  );
-};
+const InvalidExample = () => (
+  <Combobox placeholder="Select a color" isInvalid className={cx('form-select')}>
+    <Combobox.Option value="blue" display="Blue" />
+    <Combobox.Option value="green" display="Green" />
+    <Combobox.Option value="purple" display="Purple" />
+    <Combobox.Option value="red" display="Red" />
+    <Combobox.Option value="violet" display="Violet" />
+  </Combobox>
+);
 export default InvalidExample;
--- packages/terra-form-select/src/combobox/Frame.jsx
+++ packages/terra-form-select/src/combobox/Frame.jsx
@@ -398,7 +398,7 @@ class Frame extends React.Component {
   getDisplay(ariaDescribedBy, id) {
     const { hasSearchChanged, searchValue } = this.state;
     const {
-      disabled, display, placeholder, required, inputId, isInvalid,
+      disabled, display, placeholder, required, inputId,
     } = this.props;
 
     const inputAttrs = {
@@ -414,7 +414,6 @@ class Frame extends React.Component {
       'aria-disabled': disabled,
       'aria-owns': this.state.isOpen ? id : undefined,
       'aria-expanded': !disabled && this.state.isOpen,
-      'aria-invalid': isInvalid,
       type: 'text',
       className: cx('search-input'),
       required,
