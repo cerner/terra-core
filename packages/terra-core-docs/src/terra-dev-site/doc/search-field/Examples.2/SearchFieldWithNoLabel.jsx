3c5ce7ae976882c77e2867dde03b7aad90852c86
ns065186 <ns065186@cerner.net>
2023-12-05 17:02:03 -0600

    Add example with no label

---
+++ packages/terra-core-docs/src/terra-dev-site/doc/search-field/Examples.2/SearchFieldWithNoLabel.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SearchField from 'terra-search-field';
+
+const SearchFieldWithNoLabel = () => (
+  <SearchField groupName="Find Person" />
+);
+
+export default SearchFieldWithNoLabel;
