f350222a144a675756cee08e7f5b197ea2e44806
gopi <gt106551@cerner.com>
2023-12-19 15:10:39 +0530

    lint update

--- packages/terra-core-docs/src/terra-dev-site/doc/list/example/DraggableListItemWithInteractiveElements.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/list/example/DraggableListItemWithInteractiveElements.jsx
@@ -7,19 +7,18 @@ import styles from './ListDocCommon.module.scss';
 const cx = classNames.bind(styles);
 
 const ListDraggableCheckboxExample = () => {
-
   const [checkedItems, setCheckedItems] = useState([]);
 
   const handleCheckboxChange = (e) => {
-    if(e.target.checked) {
+    if (e.target.checked) {
       setCheckedItems([...checkedItems, e.target.value]);
     } else {
       const filterData = checkedItems.filter(item => item !== e.target.value);
       setCheckedItems([...filterData]);
     }
-  }
+  };
 
-  return(
+  return (
     <>
       <p id="list-help">
         Select a patient from the list to view patient details.
@@ -28,34 +27,34 @@ const ListDraggableCheckboxExample = () => {
         <Item
           key="1"
           isSelectable
-          isSelected={checkedItems.includes("John Smith")}
+          isSelected={checkedItems.includes('John Smith')}
         >
           <Checkbox id="defaultCheckbox_1" className={cx('list-item')} labelText="John Smith" value="John Smith" onChange={handleCheckboxChange} />
         </Item>
         <Item
           key="2"
           isSelectable
-          isSelected={checkedItems.includes("Mary Jones")}
+          isSelected={checkedItems.includes('Mary Jones')}
         >
           <Checkbox id="defaultCheckbox_2" className={cx('list-item')} labelText="Mary Jones" value="Mary Jones" onChange={handleCheckboxChange} />
         </Item>
         <Item
           key="3"
           isSelectable
-          isSelected={checkedItems.includes("Sam Brown")}
+          isSelected={checkedItems.includes('Sam Brown')}
         >
           <Checkbox id="defaultCheckbox_3" className={cx('list-item')} labelText="Sam Brown" value="Sam Brown" onChange={handleCheckboxChange} />
         </Item>
         <Item
           key="4"
           isSelectable
-          isSelected={checkedItems.includes("John David")}
+          isSelected={checkedItems.includes('John David')}
         >
           <Checkbox id="defaultCheckbox_4" className={cx('list-item')} labelText="John David" value="John David" onChange={handleCheckboxChange} />
         </Item>
       </List>
     </>
   );
-}
+};
 
 export default ListDraggableCheckboxExample;
