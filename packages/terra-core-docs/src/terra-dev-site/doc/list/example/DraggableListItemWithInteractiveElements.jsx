b9a42a0301dbd039c6b2adb4d64f21346dc523de
gopi <gt106551@cerner.com>
2023-12-19 14:57:43 +0530

    code update

--- packages/terra-core-docs/src/terra-dev-site/doc/list/example/DraggableListItemWithInteractiveElements.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/list/example/DraggableListItemWithInteractiveElements.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import List, { Item } from 'terra-list/lib/index';
 import classNames from 'classnames/bind';
 import Checkbox from 'terra-form-checkbox';
@@ -6,39 +6,56 @@ import styles from './ListDocCommon.module.scss';
 
 const cx = classNames.bind(styles);
 
-const ListDraggableCheckboxExample = () => (
-  <>
-    <p id="list-help">
-      Select a patient from the list to view patient details.
-    </p>
-    <List role="listbox" isTabFocusDisabled dividerStyle="standard" ariaDescribedBy="list-help" aria-label="list of patient" isDraggable>
-      <Item
-        key="1"
-        isSelectable
-        isSelected
-      >
-        <Checkbox id="defaultCheckbox_1" className={cx('list-item')} labelText="John Smith" />
-      </Item>
-      <Item
-        key="2"
-        isSelectable
-      >
-        <Checkbox id="defaultCheckbox_2" className={cx('list-item')} labelText="Mary Jones" />
-      </Item>
-      <Item
-        key="3"
-        isSelectable
-      >
-        <Checkbox id="defaultCheckbox_3" className={cx('list-item')} labelText="Sam Brown" />
-      </Item>
-      <Item
-        key="4"
-        isSelectable
-      >
-        <Checkbox id="defaultCheckbox_4" className={cx('list-item')} labelText="John David" />
-      </Item>
-    </List>
-  </>
-);
+const ListDraggableCheckboxExample = () => {
+
+  const [checkedItems, setCheckedItems] = useState([]);
+
+  const handleCheckboxChange = (e) => {
+    if(e.target.checked) {
+      setCheckedItems([...checkedItems, e.target.value]);
+    } else {
+      const filterData = checkedItems.filter(item => item !== e.target.value);
+      setCheckedItems([...filterData]);
+    }
+  }
+
+  return(
+    <>
+      <p id="list-help">
+        Select a patient from the list to view patient details.
+      </p>
+      <List role="listbox" dividerStyle="standard" ariaDescribedBy="list-help" aria-label="list of patient" isDraggable>
+        <Item
+          key="1"
+          isSelectable
+          isSelected={checkedItems.includes("John Smith")}
+        >
+          <Checkbox id="defaultCheckbox_1" className={cx('list-item')} labelText="John Smith" value="John Smith" onChange={handleCheckboxChange} />
+        </Item>
+        <Item
+          key="2"
+          isSelectable
+          isSelected={checkedItems.includes("Mary Jones")}
+        >
+          <Checkbox id="defaultCheckbox_2" className={cx('list-item')} labelText="Mary Jones" value="Mary Jones" onChange={handleCheckboxChange} />
+        </Item>
+        <Item
+          key="3"
+          isSelectable
+          isSelected={checkedItems.includes("Sam Brown")}
+        >
+          <Checkbox id="defaultCheckbox_3" className={cx('list-item')} labelText="Sam Brown" value="Sam Brown" onChange={handleCheckboxChange} />
+        </Item>
+        <Item
+          key="4"
+          isSelectable
+          isSelected={checkedItems.includes("John David")}
+        >
+          <Checkbox id="defaultCheckbox_4" className={cx('list-item')} labelText="John David" value="John David" onChange={handleCheckboxChange} />
+        </Item>
+      </List>
+    </>
+  );
+}
 
 export default ListDraggableCheckboxExample;
