69634c217c954f47147396b330fdc26a301454d8
ns065186 <ns065186@cerner.net>
2024-01-08 10:49:24 -0600

    Fix CVEs - Jest and Stylelint

--- packages/terra-search-field/tests/jest/SearchField.test.jsx
+++ packages/terra-search-field/tests/jest/SearchField.test.jsx
@@ -213,12 +213,16 @@ describe('Auto Search', () => {
   });
 
   it('uses standard timeout for search delay when not provided', () => {
+    jest.useFakeTimers();
+    jest.spyOn(global, 'setTimeout');
     const searchField = shallowWithIntl(<SearchField />).dive();
     searchField.find('.input').simulate('change', { target: {} });
     expect(setTimeout).toBeCalledWith(expect.anything(), 2500);
   });
 
   it('uses custom timeout for search delay when provided', () => {
+    jest.useFakeTimers();
+    jest.spyOn(global, 'setTimeout');
     const onSearch = jest.fn();
     const searchField = shallowWithIntl(<SearchField searchDelay={1000} onSearch={onSearch} />).dive();
 
