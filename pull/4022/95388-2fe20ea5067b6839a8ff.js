88631b87fa92a0e3ebc13aa9d81a19eb6b0d685d
KV106606Viswanath <KV106606Viswanath@users.noreply.github.com>
2024-02-06 10:16:18 +0000

    deploy: 8307725f5492f3b7c60c3011bb4bdd6474bd8567

---
+++ pull/4022/95388-2fe20ea5067b6839a8ff.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkterra_core=self.webpackChunkterra_core||[]).push([[95388],{95388:function(e,t,u){Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var n=a(u(67294)),r=a(u(10933));function a(e){return e&&e.__esModule?e:{default:e}}t.default=function(){return n.default.createElement(r.default,{onPageChange:function(e){return console.log(e)}})}}}]);
