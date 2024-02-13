8d882002808388329698648dded31ac66cd45805
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2024-02-13 16:08:14 +0530

    Update: add example

---
+++ packages/terra-core-docs/src/terra-dev-site/doc/progress-bar/example/ProgressBarValueTextProp.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProgressBar from 'terra-progress-bar';
+
+const ProgressBarValueTextProp = () => (
+  <div>
+    <ProgressBar ariaLabel="Supply Remaining" heightSize="medium" valueText="Completed 3 out of 5 Refills" value={3} max={5} />
+  </div>
+);
+
+export default ProgressBarValueTextProp;
