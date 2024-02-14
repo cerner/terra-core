90f0840b2961a617452496bbe974a240e66669dc
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2024-02-14 17:46:23 +0530

    update: file name

--- packages/terra-core-docs/src/terra-dev-site/doc/progress-bar/example/ProgressBarCustomText.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/progress-bar/example/ProgressBarCustomText.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ProgressBar from 'terra-progress-bar';
 
-const ProgressBarValueTextProp = () => (
+const ProgressBarCustomText = () => (
   <div>
     <ProgressBar ariaLabel="Supply progress" heightSize="medium" valueText="Completed 3 out of 5 Refills" value={3} max={5} />
   </div>
 );
 
-export default ProgressBarValueTextProp;
+export default ProgressBarCustomText;
