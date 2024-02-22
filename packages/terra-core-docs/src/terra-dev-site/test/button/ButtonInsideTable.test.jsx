cab4628099e596569c3ef9721a0de916c69cf33f
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2024-02-22 16:09:25 +0530

    Update ButtonInsideTable.test.jsx

--- packages/terra-core-docs/src/terra-dev-site/test/button/ButtonInsideTable.test.jsx
+++ packages/terra-core-docs/src/terra-dev-site/test/button/ButtonInsideTable.test.jsx
@@ -1,49 +1,64 @@
 import React from 'react';
-import Button from 'terra-button';
 import Table from 'terra-table';
+import Button from 'terra-button';
 
-const buttonCell = <Button type="button" aria-label="Alert" text="Alert" />;
+const TableWithButton = () => {
+  const buttonCell = <Button type="button" aria-label="Alert" text="Alert" />;
 
-const tableData = {
-  cols: [
-    { id: 'Column-0', displayName: 'Patient' },
-    { id: 'Column-1', displayName: 'Location' },
-    { id: 'Column-2', displayName: 'Illness Severity' },
-    { id: 'Column-3', displayName: 'Visit' },
-  ],
-  rows: [
-    {
-      id: '1',
-      cells: [
-        { content: <>
-        Fleck, Arthur 
-        {buttonCell}</> },
-        { content: '1007-MTN' },
-        { content: 'Unstable' },
-        { content: 'Inpatient, 2 months' },
-      ],
-    },
-    {
-      id: '2',
-      cells: [
-        { content: <>
-        Wayne, Bruce
-         {buttonCell}</> },
-        { content: '1007-MTN-DR' },
-        { content: 'Stable' },
-        { content: 'Outpatient, 2 days' },
-      ],
-    },
-  ],
+  const tableDataJSON = {
+    cols: [
+      { id: 'Column-0', displayName: 'Patient' },
+      { id: 'Column-1', displayName: 'Location' },
+      { id: 'Column-2', displayName: 'Illness Severity' },
+      { id: 'Column-3', displayName: 'Visit' },
+    ],
+    rows: [
+      {
+        id: '1',
+        cells: [
+          {
+            content: <>
+              Fleck, Arthur
+              {buttonCell}
+              {/* eslint-disable-next-line react/jsx-closing-tag-location */}
+            </>,
+          },
+          { content: '1007-MTN' },
+          { content: 'Unstable' },
+          { content: 'Inpatient, 2 months' },
+        ],
+      },
+      {
+        id: '2',
+        cells: [
+          { content: 'Wayne, Bruce' },
+          {
+            content: (<div>
+              Wayne, Bruce
+              {buttonCell}
+              {/* eslint-disable-next-line react/jsx-closing-tag-location */}
+            </div>),
+          },
+          { content: '1007-MTN-DR' },
+          { content: 'Stable' },
+          { content: 'Outpatient, 2 days' },
+        ],
+      },
+    ],
+  };
+
+  const { cols, rows } = tableDataJSON;
+
+  return (
+    <>
+      <Table
+        id="table-focusable-cell"
+        overflowColumns={cols}
+        rows={rows}
+        ariaLabel="Table with Focusable Elements"
+      />
+    </>
+  );
 };
-
-const TableWithButton = () => (
-  <Table
-    id="my-table-id"
-    overflowColumns={tableData.cols}
-    rows={tableData.rows}
-    ariaLabel="Table"
-  />
-);
 
 export default TableWithButton;
