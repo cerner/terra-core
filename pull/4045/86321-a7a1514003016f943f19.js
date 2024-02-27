076646cd17327b5d31ede4342f9eb243aca24a47
sdadn <sdadn@users.noreply.github.com>
2024-02-27 18:41:50 +0000

    deploy: 77344a639190eafc1fe6d1fb524a99be6adc211d

--- pull/4045/86321-a7a1514003016f943f19.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkterra_core=self.webpackChunkterra_core||[]).push([[86321],{86321:function(e,t,u){Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var n=a(u(96540)),r=a(u(77436));function a(e){return e&&e.__esModule?e:{default:e}}t.default=function(){return n.default.createElement(r.default,{onPageChange:function(e){return console.log(e)}})}}}]);
