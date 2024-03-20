07d8bf47f159125ddbdae8f77474d094af727c68
Saad Adnan <38024451+sdadn@users.noreply.github.com>
2024-03-20 11:20:41 -0500

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { config } = require('@cerner/terra-functional-testing');
 
-config.specs = ['./packages/**/wdio/**/*-spec.js'];
+config.specs = ['./packages/**/tests/wdio/**/*-spec.js'];
 
 exports.config = config;
