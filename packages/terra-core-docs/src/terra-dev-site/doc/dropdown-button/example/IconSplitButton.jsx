5934a9440b01fe3c4362d4833ada0bcf384851ca
sc069282 <sydneyycombs@gmail.com>
2024-04-02 14:19:31 -0500

    Fix lint again

--- packages/terra-core-docs/src/terra-dev-site/doc/dropdown-button/example/IconSplitButton.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/dropdown-button/example/IconSplitButton.jsx
@@ -7,55 +7,53 @@ import styles from './IconSplitButton.module.scss';
 
 const cx = classNames.bind(styles);
 
-const Example = () => {
-  return (
-    <>
-      <SplitButton
-        primaryOptionLabel="Reply"
-        icon={<IconFeaturedOutlineYellow />}
-        onSelect={() => {}}
-        buttonAttrs={{
-          'aria-label': 'icon split',
-        }}
-        className={cx('icon-button')}
-      >
-        <Item label="Reply All" onSelect={() => setMessage('Reply All clicked')} />
-        <Item label="Forward" onSelect={() => {}} />
-        <Item label="Reply in 10 minutes" onSelect={() => {}} />
-        <Item label="Selective Reply" onSelect={() => {}} />
-      </SplitButton>
-      <SplitButton
-        primaryOptionLabel="Reply"
-        icon={<IconFeaturedOutlineYellow />}
-        isReversed
-        onSelect={() => {}}
-        buttonAttrs={{
-          'aria-label': 'reverse icon split',
-        }}
-        className={cx('icon-button')}
-      >
-        <Item label="Reply All" onSelect={() => {}} />
-        <Item label="Forward" onSelect={() => {}} />
-        <Item label="Reply in 10 minutes" onSelect={() => {}} />
-        <Item label="Selective Reply" onSelect={() => {}} />
-      </SplitButton>
-      <SplitButton
-        primaryOptionLabel="Reply"
-        icon={<IconFeaturedOutlineYellow />}
-        isIconOnly
-        onSelect={() => {}}
-        buttonAttrs={{
-          'aria-label': 'icon only split',
-        }}
-        className={cx('icon-button')}
-      >
-        <Item label="Reply All" onSelect={() => {}} />
-        <Item label="Forward" onSelect={() => {}} />
-        <Item label="Reply in 10 minutes" onSelect={() => {}} />
-        <Item label="Selective Reply" onSelect={() => {}} />
-      </SplitButton>
-    </>
-  );
-};
+const Example = () => (
+  <>
+    <SplitButton
+      primaryOptionLabel="Reply"
+      icon={<IconFeaturedOutlineYellow />}
+      onSelect={() => {}}
+      buttonAttrs={{
+        'aria-label': 'icon split',
+      }}
+      className={cx('icon-button')}
+    >
+      <Item label="Reply All" onSelect={() => {}} />
+      <Item label="Forward" onSelect={() => {}} />
+      <Item label="Reply in 10 minutes" onSelect={() => {}} />
+      <Item label="Selective Reply" onSelect={() => {}} />
+    </SplitButton>
+    <SplitButton
+      primaryOptionLabel="Reply"
+      icon={<IconFeaturedOutlineYellow />}
+      isReversed
+      onSelect={() => {}}
+      buttonAttrs={{
+        'aria-label': 'reverse icon split',
+      }}
+      className={cx('icon-button')}
+    >
+      <Item label="Reply All" onSelect={() => {}} />
+      <Item label="Forward" onSelect={() => {}} />
+      <Item label="Reply in 10 minutes" onSelect={() => {}} />
+      <Item label="Selective Reply" onSelect={() => {}} />
+    </SplitButton>
+    <SplitButton
+      primaryOptionLabel="Reply"
+      icon={<IconFeaturedOutlineYellow />}
+      isIconOnly
+      onSelect={() => {}}
+      buttonAttrs={{
+        'aria-label': 'icon only split',
+      }}
+      className={cx('icon-button')}
+    >
+      <Item label="Reply All" onSelect={() => {}} />
+      <Item label="Forward" onSelect={() => {}} />
+      <Item label="Reply in 10 minutes" onSelect={() => {}} />
+      <Item label="Selective Reply" onSelect={() => {}} />
+    </SplitButton>
+  </>
+);
 
 export default Example;
