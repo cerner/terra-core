7bae355645e41f315299b257c7b2f9b3d676cf71
Saad <38024451+sdadn@users.noreply.github.com>
2024-04-04 15:23:52 -0500

    linter fixes

--- packages/terra-core-docs/src/terra-dev-site/doc/section-header/example/AccordionSectionHeaderAccess.jsx
+++ packages/terra-core-docs/src/terra-dev-site/doc/section-header/example/AccordionSectionHeaderAccess.jsx
@@ -10,31 +10,25 @@ const AccordionSectionHeaderAccess = () => {
     setIsOpen(!isOpen);
   };
 
-  const sectionHeaderProps = {
-    isOpen,
-    level: 3,
-    onClick: handleClick,
-  };
-
   return (
     <div>
       <Card>
         <Card.Body>
-            <h2>Documented Allergies</h2>
-            <SectionHeader
-              aria-expanded={isOpen}
-              isOpen={isOpen}
-              level={3}
-              onClick={handleClick}
-              text="Patient is Allergic to:"
-            />
-            <Toggle isOpen={isOpen} isAnimated>
-              <p>Cats</p>
-              <p>Dogs</p>
-              <p>Dust</p>
-              <p>Mold</p>
-              <p>Latex</p>
-            </Toggle>
+          <h2>Documented Allergies</h2>
+          <SectionHeader
+            aria-expanded={isOpen}
+            isOpen={isOpen}
+            level={3}
+            onClick={handleClick}
+            text="Patient is Allergic to:"
+          />
+          <Toggle isOpen={isOpen} isAnimated>
+            <p>Cats</p>
+            <p>Dogs</p>
+            <p>Dust</p>
+            <p>Mold</p>
+            <p>Latex</p>
+          </Toggle>
         </Card.Body>
       </Card>
     </div>
