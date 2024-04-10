5111bfc450e55de547b234569988919874b440f8
adavijit <adavijit@yahoo.com>
2024-04-10 12:55:17 +0530

    UXPLATFORM-10191 Fixed accessibility issue in form multi select

--- packages/terra-form-select/src/shared/_Tag.jsx
+++ packages/terra-form-select/src/shared/_Tag.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import classNamesBind from 'classnames/bind';
 import ThemeContext from 'terra-theme-context';
+import { injectIntl } from 'react-intl';
 import styles from './_Tag.module.scss';
 
 const cx = classNamesBind.bind(styles);
@@ -19,17 +20,58 @@ const propTypes = {
    * The value of the tag.
    */
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  /**
+   * Specifies whether the tag is disabled.
+   */
+  disabled: PropTypes.bool,
+  /**
+   * @private
+   * The intl object containing translations. This is retrieved from the context automatically by injectIntl.
+   */
+  intl: PropTypes.shape({ formatMessage: PropTypes.func }).isRequired,
 };
 
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-const Tag = ({ children, onDeselect, value }) => {
+const Tag = ({
+  children, onDeselect, value, disabled, intl,
+}) => {
   const theme = React.useContext(ThemeContext);
+  const deselectRef = useRef(null);
+
+  const handleEnterKeyPress = (event) => {
+    if (event.key === 'Enter' && !disabled) {
+      onDeselect(value);
+      const parentUl = deselectRef.current.closest('ul');
+      // Filtering out hidden children
+      const visibleChildrenLis = Array.from(parentUl.children).filter(child => child.tagName === 'LI' && !/visually-hidden-component/.test(child.className));
+      if (visibleChildrenLis.length >= 2) {
+        const secondLastLi = visibleChildrenLis[visibleChildrenLis.length - 2];
+        if (secondLastLi) {
+          const spanChildren = secondLastLi.children;
+          if (spanChildren[1]) {
+            spanChildren[1].focus();
+          }
+        }
+      } else {
+        parentUl.click();
+      }
+    }
+  };
   return (
     <li className={cx('tag', theme.className)}>
       <span className={cx('display')}>
         {children}
       </span>
-      <span className={cx('deselect')} onClick={() => { onDeselect(value); }} role="presentation">
+      <span
+        ref={deselectRef}
+        onKeyDown={handleEnterKeyPress}
+        className={cx('deselect')}
+        onClick={() => { if (!disabled) onDeselect(value); }}
+        // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+        tabIndex={!disabled ? 0 : null}
+        role={!disabled ? 'button' : null}
+        aria-label={!disabled ? `${intl.formatMessage({ id: 'Terra.form.select.deselect' })} ${children}` : null}
+      >
         <span className={cx('icon')} />
       </span>
     </li>
@@ -38,4 +80,4 @@ const Tag = ({ children, onDeselect, value }) => {
 
 Tag.propTypes = propTypes;
 
-export default Tag;
+export default injectIntl(Tag);
--- packages/terra-form-select/tests/jest/Tag.test.jsx
+++ packages/terra-form-select/tests/jest/Tag.test.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import ThemeContextProvider from 'terra-theme-context/lib/ThemeContextProvider';
-
+import { IntlProvider } from 'react-intl';
 import Tag from '../../src/shared/_Tag';
 
 describe('Tag', () => {
   it('should render a default Tag', () => {
-    const wrapper = enzyme.shallow(<Tag value="value" onDeselect={() => {}}>Content</Tag>);
+    const wrapper = enzyme.shallow(
+      <IntlProvider locale="en">
+        <Tag value="value" onDeselect={() => {}}>Content</Tag>
+      </IntlProvider>,
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('correctly applies the theme context className', () => {
     const wrapper = enzyme.mount(
-      <ThemeContextProvider theme={{ className: 'orion-fusion-theme' }}>
-        <Tag value="value" onDeselect={() => {}}>
-          Content
-        </Tag>
-      </ThemeContextProvider>,
+      <IntlProvider locale="en">
+        <ThemeContextProvider theme={{ className: 'orion-fusion-theme' }}>
+          <Tag value="value" onDeselect={() => {}}>
+            Content
+          </Tag>
+        </ThemeContextProvider>
+      </IntlProvider>,
     );
     expect(wrapper).toMatchSnapshot();
   });
